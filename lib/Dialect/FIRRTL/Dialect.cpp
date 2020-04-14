d939853cb57a058b020a7d3fa6cdc23cec246a38
Chris Lattner <clattner@nondot.org>
2020-04-13 17:11:37 -0700

    Use a pretty name for constant operations to make it easier to read the IR.

--- lib/Dialect/FIRRTL/Dialect.cpp
+++ lib/Dialect/FIRRTL/Dialect.cpp
@@ -43,9 +43,38 @@ struct FIRRTLOpAsmDialectInterface : public OpAsmDialectInterface {
   /// OpAsmInterface.td#getAsmResultNames for usage details and documentation.
   void getAsmResultNames(Operation *op,
                          OpAsmSetValueNameFn setNameFn) const override {
+    // Many firrtl dialect operations have an optional 'name' attribute.  If
+    // present, use it.
     if (op->getNumResults() > 0)
       if (auto nameAttr = op->getAttrOfType<StringAttr>("name"))
         setNameFn(op->getResult(0), nameAttr.getValue());
+
+    // For constants in particular, propagate the value into the result name to
+    // make it easier to read the IR.
+    if (auto constant = dyn_cast<ConstantOp>(op)) {
+      auto intTy = constant.getType().dyn_cast<IntType>();
+
+      // Otherwise, build a complex name with the value and type.
+      SmallString<32> specialNameBuffer;
+      llvm::raw_svector_ostream specialName(specialNameBuffer);
+      specialName << 'c';
+      if (intTy) {
+        if (!intTy.isSigned() || !constant.value().isNegative())
+          constant.value().print(specialName, /*isSigned:*/ false);
+        else {
+          specialName << 'm';
+          (-constant.value()).print(specialName, /*isSigned:*/ false);
+        }
+
+        specialName << (intTy.isSigned() ? "_si" : "_ui");
+        auto width = intTy.getWidthOrSentinel();
+        if (width != -1)
+          specialName << width;
+      } else {
+        constant.value().print(specialName, /*isSigned:*/ false);
+      }
+      setNameFn(constant.getResult(), specialName.str());
+    }
   }
 
   /// Get a special name to use when printing the entry block arguments of the
