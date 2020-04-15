c890451efce620c343f7515794e7597df6c7c7fd
Chris Lattner <clattner@nondot.org>
2020-04-14 21:01:14 -0700

    Rename FIRToMLIR.h -> FIRParser.h, remove some dead state from FIRParser. NFC.

---
+++ include/spt/FIRParser.h
@@ -0,0 +1,16 @@
+//===- FIRParser.h - .fir to FIRRTL dialect parser --------------*- C++ -*-===//
+//
+// Defines the interface to the .fir file parser.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SPT_DIALECT_FIRPARSER_H
+#define SPT_DIALECT_FIRPARSER_H
+
+namespace spt {
+
+void registerFIRParserTranslation();
+
+} // namespace spt
+
+#endif // SPT_DIALECT_FIRPARSER_H
