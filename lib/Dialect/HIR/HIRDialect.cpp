15fadc5c3d1144e8093535b12c8e1f812a411b9c
kingshukmajumder <kingshuk1000@gmail.com>
2020-08-09 19:37:11 +0530

    [HIR] Adding sync_time op

--- lib/Dialect/HIR/HIRDialect.cpp
+++ lib/Dialect/HIR/HIRDialect.cpp
@@ -6,34 +6,34 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/StringRef.h"
 
-namespace mlir{
-  namespace hir{
-    HIRDialect::HIRDialect(mlir::MLIRContext *context)
-      : Dialect(getDialectNamespace(), context) {
-        addTypes<TimeType>();
-        addOperations<
+namespace mlir {
+namespace hir {
+HIRDialect::HIRDialect(mlir::MLIRContext *context)
+    : Dialect(getDialectNamespace(), context) {
+  addTypes<TimeType>();
+  addOperations<
 #define GET_OP_LIST
 #include "circt/Dialect/HIR/HIR.cpp.inc"
-          >();
-      }
+      >();
+}
 
-    Type HIRDialect::parseType(DialectAsmParser &parser) const {
-      llvm::StringRef typeKeyword;
-      if (parser.parseKeyword(&typeKeyword)){
-        return parser.emitError(parser.getNameLoc(), "unknown hir type"),Type();
-      }
+Type HIRDialect::parseType(DialectAsmParser &parser) const {
+  llvm::StringRef typeKeyword;
+  if (parser.parseKeyword(&typeKeyword)) {
+    return parser.emitError(parser.getNameLoc(), "unknown hir type"), Type();
+  }
 
-      if (typeKeyword == TimeType::getKeyword()){
-        return TimeType::get(getContext());
-      }
+  if (typeKeyword == TimeType::getKeyword()) {
+    return TimeType::get(getContext());
+  }
 
-      return parser.emitError(parser.getNameLoc(), "unknown hir type"),Type();
-    }
+  return parser.emitError(parser.getNameLoc(), "unknown hir type"), Type();
+}
 
-    void HIRDialect::printType(Type type, DialectAsmPrinter &printer) const {
-      if (TimeType time = type.dyn_cast<TimeType>()) {
-        printer << time.getKeyword();
-      }
-    }
+void HIRDialect::printType(Type type, DialectAsmPrinter &printer) const {
+  if (TimeType time = type.dyn_cast<TimeType>()) {
+    printer << time.getKeyword();
   }
 }
+} // namespace hir
+} // namespace mlir
--- test/HIR/ops.mlir
+++ test/HIR/ops.mlir
@@ -3,6 +3,7 @@ module {
   func @foo() {
     %t = hir.def_time_var : !hir.time
     %t2 = hir.duplicate_time_var %t: !hir.time -> !hir.time
+    hir.sync_time(%t, %t2):(!hir.time,!hir.time)
     return
   }
 }
