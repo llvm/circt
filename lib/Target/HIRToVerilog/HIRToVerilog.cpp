58ecd6c91c0d7c710de7606b1b6f7445329c50c1
kingshukmajumder <kingshuk1000@gmail.com>
2020-09-02 17:15:00 +0530

    [HIR] Added print for module arguments with types
    Supported types are memref and integer (i32,i1 etc.)

--- lib/Target/HIRToVerilog/HIRToVerilog.cpp
+++ lib/Target/HIRToVerilog/HIRToVerilog.cpp
@@ -16,6 +16,7 @@
 #include "llvm/Support/FormattedStream.h"
 
 using namespace mlir;
+using namespace hir;
 
 namespace {
 
@@ -24,27 +25,110 @@ public:
   VerilogPrinter(llvm::formatted_raw_ostream &output) : out(output) {}
 
   LogicalResult printModule(ModuleOp op);
-  LogicalResult printOperation(Operation *op, unsigned indentAmount = 0);
 
 private:
+  unsigned newValueNumber() { return nextValueNum++; }
+  LogicalResult printDefOp(DefOp op, unsigned indentAmount = 0);
+  LogicalResult printConstantOp(hir::ConstantOp op, unsigned indentAmount = 0);
+  LogicalResult printForOp(ForOp op, unsigned indentAmount = 0);
+  LogicalResult printAddOp(AddOp op, unsigned indentAmount = 0);
+  LogicalResult printMemReadOp(MemReadOp op, unsigned indentAmount = 0);
+  LogicalResult printMemWriteOp(MemWriteOp op, unsigned indentAmount = 0);
+  LogicalResult printType(Type type);
+
   llvm::formatted_raw_ostream &out;
+  unsigned nextValueNum = 0;
+  DenseMap<Value, unsigned> mapValueToNum;
 };
 
-LogicalResult VerilogPrinter::printModule(ModuleOp module) {
-  WalkResult result = module.walk([this](hir::DefOp defOp) -> WalkResult {
-    // An EntityOp always has a single block
-    Block &entryBlock = defOp.getBody().front();
+static bool getBitWidth(Type ty, unsigned &bitwidth) {
+  if (auto intTy = ty.dyn_cast<IntegerType>()) {
+    bitwidth = intTy.getWidth();
+    return true;
+  } else if (auto memrefTy = ty.dyn_cast<MemrefType>()) {
+    return getBitWidth(memrefTy.getElementType(), bitwidth);
+  } else if (auto constTy = ty.dyn_cast<ConstType>()) {
+    return getBitWidth(constTy.getElementType(), bitwidth);
+  }
+  return false;
+}
 
-    // Print the module signature
-    out << "module " << defOp.getName();
-    out << ";\n";
-    out << "endmodule\n";
+LogicalResult VerilogPrinter::printDefOp(DefOp op, unsigned indentAmount) {
+  // An EntityOp always has a single block
+  Block &entryBlock = op.getBody().front();
+  auto args = entryBlock.getArguments();
+  // Print the module signature
+  FunctionType funcType = op.getType();
+  out << "module " << op.getName() << "(";
+  for (int i = 0; i < args.size() - 1; i++) {
+    unsigned bitwidth = 0;
+    Value arg = args[i];
+    Type argType = arg.getType();
+    unsigned valueNumber = newValueNumber();
+    mapValueToNum.insert(std::make_pair(arg, valueNumber));
+
+    // Print verilog
+    if(i==0)
+      out <<"\n";
+
+    if (i > 0)
+      out << ",\n";
+
+    if (argType.isa<IntegerType>()) {
+      getBitWidth(argType, bitwidth);
+      out << "input wire[" << bitwidth - 1 << ":0]  ";
+      out << "v" << valueNumber;
+    } else if (argType.isa<MemrefType>()) {
+      MemrefType memrefTy = argType.dyn_cast<MemrefType>();
+      ArrayRef<unsigned> shape = memrefTy.getShape();
+      Type elementType = memrefTy.getElementType();
+      ArrayRef<unsigned> packing = memrefTy.getPacking();
+      hir::Details::PortKind port = memrefTy.getPort();
+      unsigned addrWidth = 0;
+      // FIXME: Currently we assume that all dims are power of two.
+      for (auto dim : shape) {
+        addrWidth += ceil(log2(dim));
+      }
+      out << "output wire[" << addrWidth - 1 << ":0] v_addr" << valueNumber;
+      if (port == hir::Details::r || port == hir::Details::rw) {
+        bool bitwidth_valid = getBitWidth(argType, bitwidth);
+        if (!bitwidth_valid)
+          return emitError(arg.getLoc(), "Unsupported argument type!");
+        out << ",\n"
+            << "input wire[" << bitwidth - 1 << ":0]  ";
+        out << "v_data_rd" << valueNumber;
+      }
+      if (port == hir::Details::w || port == hir::Details::rw) {
+        bool bitwidth_valid = getBitWidth(argType, bitwidth);
+        if (!bitwidth_valid)
+          return emitError(arg.getLoc(), "Unsupported argument type!");
+        out << ",\n"
+            << "output wire[" << bitwidth - 1 << ":0]  ";
+        out << "v_data_wr" << valueNumber;
+      }
+    } else
+      return emitError(arg.getLoc(), "Unsupported argument type!");
+    if(i==args.size())
+      out <<"\n";
+  }
+  out << ")";
+
+  out << ";\n";
+  out << "endmodule\n";
+  return success();
+}
+
+LogicalResult VerilogPrinter::printModule(ModuleOp module) {
+  WalkResult result = module.walk([this](DefOp defOp) -> WalkResult {
+    if (!printDefOp(defOp).value)
+      return WalkResult::interrupt();
     return WalkResult::advance();
   });
   // if printing of a single operation failed, fail the whole translation
   return failure(result.wasInterrupted());
 }
 } // namespace
+
 LogicalResult mlir::hir::printVerilog(ModuleOp module, raw_ostream &os) {
   llvm::formatted_raw_ostream out(os);
   VerilogPrinter printer(out);
