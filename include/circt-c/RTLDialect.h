963d8b4f89067630a636638d6805925fdeb9dfe5
George <>
2020-11-02 11:23:31 -0800

    Introduce C API

---
+++ include/circt-c/RTLDialect.h
@@ -0,0 +1,29 @@
+/*===-- circt-c/RTLDialect.h - C API for RTL dialect --------------*- C -*-===*\
+|*                                                                            *|                                                                          *|
+\*===----------------------------------------------------------------------===*/
+
+#ifndef CIRCT_C_RTLDIALECT_H
+#define CIRCT_C_RTLDIALECT_H
+
+#include "mlir-c/IR.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Registers the RTL dialect with the given context. This allows the
+ * dialect to be loaded dynamically if needed when parsing. */
+void mlirContextRegisterRTLDialect(MlirContext context);
+
+/** Loads the RTL dialect into the given context. The dialect does _not_
+ * have to be registered in advance. */
+MlirDialect mlirContextLoadRTLDialect(MlirContext context);
+
+/** Returns the namespace of the RTL dialect, suitable for loading it. */
+MlirStringRef mlirRTLDialectGetNamespace();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CIRCT_C_RTLDIALECT_H
---
+++ lib/CAPI/RTL/RTLDialect.cpp
@@ -0,0 +1,21 @@
+//===- RegisterAllDialects.cpp - Registers all CIRCT dialects from C ------===//
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir-c/IR.h"
+#include "circt-c/RTLDialect.h"
+#include "mlir/CAPI/IR.h"
+#include "mlir/CAPI/Support.h"
+#include "circt/Dialect/RTL/Ops.h"
+
+void mlirContextRegisterRTLDialect(MlirContext context) {
+  unwrap(context)->getDialectRegistry().insert<circt::rtl::RTLDialect>();
+}
+
+MlirDialect mlirContextLoadRTLDialect(MlirContext context) {
+  return wrap(unwrap(context)->getOrLoadDialect<circt::rtl::RTLDialect>());
+}
+
+MlirStringRef mlirRTLDialectGetNamespace() {
+  return wrap(circt::rtl::RTLDialect::getDialectNamespace());
+}
