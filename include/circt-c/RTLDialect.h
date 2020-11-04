2a9eb3506d3a1cd171454fbb5f00a9bd654664db
George Lyon <GeorgeLyon@users.noreply.github.com>
2020-11-04 11:37:51 -0800

    Create a minimal C API (#196)
    * Enable cloning llvm submodule over HTTP
    
    * Introduce C API
    
    * Undo unrelated changes
    
    * clang-format
    
    * More format
    
    * Add Header Comments
    
    * Format
    
    * Add basic test
    
    * Add missing incantation
    
    * Format
    
    Co-authored-by: George <>

---
+++ include/circt-c/RTLDialect.h
@@ -0,0 +1,35 @@
+/*===-- circt-c/RTLDialect.h - C API for RTL dialect --------------*- C -*-===*\
+|*                                                                            *|
+|* This header declares the C interface for registering and accessing the     *|
+|* RTL dialect. A dialect should be registered with a context to make it      *|
+|* available to users of the context. These users must load the dialect       *|
+|* before using any of its attributes, operations or types. Parser and pass   *|
+|* manager can load registered dialects automatically.                        *|
+|*                                                                            *|
+\*===----------------------------------------------------------------------===*/
+
+#ifndef CIRCT_C_RTLDIALECT_H
+#define CIRCT_C_RTLDIALECT_H
+
+#include "mlir-c/IR.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Registers the RTL dialect with the given context. This allows the
+ * dialect to be loaded dynamically if needed when parsing. */
+void mlirContextRegisterRTLDialect(MlirContext context);
+
+/** Loads the RTL dialect into the given context. The dialect does _not_
+ * have to be registered in advance. */
+MlirDialect mlirContextLoadRTLDialect(MlirContext context);
+
+/** Returns the namespace of the RTL dialect, suitable for loading it. */
+MlirStringRef mlirRTLDialectGetNamespace();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CIRCT_C_RTLDIALECT_H
