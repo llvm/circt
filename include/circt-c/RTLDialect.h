6c795416f26f4f47afd26779d3040f9982658b30
George <>
2020-11-04 14:40:31 -0800

    Comment like its 1999

--- include/circt-c/RTLDialect.h
+++ include/circt-c/RTLDialect.h
@@ -1,12 +1,12 @@
-/*===-- circt-c/RTLDialect.h - C API for RTL dialect --------------*- C -*-===*\
-|*                                                                            *|
-|* This header declares the C interface for registering and accessing the     *|
-|* RTL dialect. A dialect should be registered with a context to make it      *|
-|* available to users of the context. These users must load the dialect       *|
-|* before using any of its attributes, operations or types. Parser and pass   *|
-|* manager can load registered dialects automatically.                        *|
-|*                                                                            *|
-\*===----------------------------------------------------------------------===*/
+//===-- circt-c/RTLDialect.h - C API for RTL dialect --------------*- C -*-===//
+//
+// This header declares the C interface for registering and accessing the
+// RTL dialect. A dialect should be registered with a context to make it
+// available to users of the context. These users must load the dialect
+// before using any of its attributes, operations or types. Parser and pass
+// manager can load registered dialects automatically.
+//
+//===----------------------------------------------------------------------===//
 
 #ifndef CIRCT_C_RTLDIALECT_H
 #define CIRCT_C_RTLDIALECT_H
@@ -17,15 +17,15 @@
 extern "C" {
 #endif
 
-/** Registers the RTL dialect with the given context. This allows the
- * dialect to be loaded dynamically if needed when parsing. */
+/// Registers the RTL dialect with the given context. This allows the dialect to
+/// be loaded dynamically if needed when parsing.
 void mlirContextRegisterRTLDialect(MlirContext context);
 
-/** Loads the RTL dialect into the given context. The dialect does _not_
- * have to be registered in advance. */
+/// Loads the RTL dialect into the given context. The dialect does _not_ have to
+/// be registered in advance.
 MlirDialect mlirContextLoadRTLDialect(MlirContext context);
 
-/** Returns the namespace of the RTL dialect, suitable for loading it. */
+/// Returns the namespace of the RTL dialect, suitable for loading it.
 MlirStringRef mlirRTLDialectGetNamespace();
 
 #ifdef __cplusplus
