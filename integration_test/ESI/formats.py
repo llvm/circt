4dbff39c689de7e06c61fee5d25377229c8ae613
John Demme <john.demme@microsoft.com>
2020-11-06 00:18:53 -0800

    Implementing timeouts... it's ugly but it works.

--- integration_test/ESI/formats.py
+++ integration_test/ESI/formats.py
@@ -5,10 +5,11 @@ import os
 import importlib
 from pprint import pprint
 import subprocess
-import sys
 import re
+import signal
+import sys
 import traceback
-import multiprocessing
+import time
 
 formatsDir = os.path.dirname(__file__)
 
@@ -19,7 +20,7 @@ class CosimTest(lit.formats.FileBasedTest):
             return Test.Result(Test.UNSUPPORTED, "ESI-Cosim feature not present")
         if 'verilator' not in test.config.available_features:
             return Test.Result(Test.UNSUPPORTED, "Verilator not present")
-        testRun = CosimTestRunner(test)
+        testRun = CosimTestRunner(test, litConfig)
         parsed = testRun.parse()
         if parsed.code != Test.PASS:
             return parsed
@@ -31,8 +32,9 @@ class CosimTest(lit.formats.FileBasedTest):
 
 
 class CosimTestRunner:
-    def __init__(self, test):
+    def __init__(self, test, litConfig):
         self.test = test
+        self.litConfig = litConfig
         self.file = test.getSourcePath()
         self.execdir = test.getExecPath()
         self.runs = list()
@@ -40,6 +42,10 @@ class CosimTestRunner:
         self.cppSources = list()
         self.imports = list()
         self.top = "main"
+        if litConfig.maxIndividualTestTime > 0:
+            self.deadline = time.time() + litConfig.maxIndividualTestTime
+        else:
+            self.deadline = None
 
     def parse(self):
         fileReader = open(self.file, "r")
@@ -70,8 +76,7 @@ class CosimTestRunner:
         self.sources.insert(0, os.path.join(cosimInclude, "Cosim_DpiPkg.sv"))
         self.sources.insert(1, os.path.join(cosimInclude, "Cosim_Endpoint.sv"))
         sources = " ".join(self.sources + self.cppSources)
-        if not os.path.exists(self.execdir):
-            os.mkdir(self.execdir)
+        os.makedirs(self.execdir, exist_ok=True)
         vrun = subprocess.run(
             f"{cfg.verilator_path} --cc --top-module {self.top} --build --exe {sources} {cfg.esi_cosim_path} -LDFLAGS '-Wl,-rpath={cfg.circt_shlib_dir}'".split(),
             capture_output=True,
@@ -81,68 +86,97 @@ class CosimTestRunner:
         return Test.Result(Test.PASS if vrun.returncode == 0 else Test.FAIL, output)
 
     def run(self):
-        origdir = os.getcwd()
+        pid = os.fork()
+        if pid == 0:
+            # Does not return. Runs test and exits.
+            signal.signal(signal.SIGINT, signal.default_int_handler)
+            self.runTest()
+
+        rc = waitpid_deadline(pid, self.deadline)
+        if rc == False:
+            os.kill(pid, signal.SIGINT)
+            os.kill(pid, signal.SIGINT)
+            if waitpid_deadline(pid, time.time() + 5.0) == False:
+                print(f"Force kill {pid}")
+                os.kill(pid, signal.SIGKILL)
+            (_, msg) = self.readLogs()
+            return Test.Result(Test.TIMEOUT, msg)
+
+        procrc = rc[1]
+        (err, msg) = self.readLogs()
+        passed = not err and procrc == 0
+        return Test.Result(Test.PASS if passed else Test.FAIL, msg)
+
+    def runTest(self):
         os.chdir(self.execdir)
         try:
-            testStdout = open("test_stdout.log", "w")
-            testStderr = open("test_stderr.log", "w")
+            sys.stdout = open("test_stdout.log", "w")
+            sys.stderr = open("test_stderr.log", "w")
 
             simStdout = open("sim_stdout.log", "w")
             simStderr = open("sim_stderr.log", "w")
             simProc = subprocess.Popen([f"./obj_dir/V{self.top}", "--cycles", "-1"],
                                        stdout=simStdout, stderr=simStderr)
-
-            origStdout = sys.stdout
-            origStderr = sys.stderr
-            sys.stdout = testStdout
-            sys.stderr = testStderr
+            result = False
             try:
-                result = self.runPyTest()
+                cfg = self.test.config
+                rpcSchemaPath = os.path.join(
+                    cfg.circt_include_dir, "circt", "Dialect", "ESI", "cosim", "CosimDpi.capnp")
+                for imp in self.imports:
+                    exec(f"import {imp}")
+                for run in self.runs:
+                    sys.stdout.flush()
+                    sys.stderr.flush()
+                    exec(run)
+                result = True
+            except KeyboardInterrupt:
+                pass
             except Exception:
-                result = False
                 traceback.print_exc()
             finally:
-                sys.stdout = origStdout
-                sys.stderr = origStderr
                 simProc.kill()
-                testStderr.close()
-                testStdout.close()
                 simStderr.close()
                 simStdout.close()
-                msg = self.readLogs()
         finally:
-            os.chdir(origdir)
-        return Test.Result(Test.PASS if result else Test.FAIL, msg)
-
-    def runPyTest(self):
-        cfg = self.test.config
-        rpcSchemaPath = os.path.join(
-            cfg.circt_include_dir, "circt", "Dialect", "ESI", "cosim", "CosimDpi.capnp")
-        for imp in self.imports:
-            exec(f"import {imp}")
-        for run in self.runs:
-            exec(run)
-        return True
+            sys.stdout.flush()
+            sys.stderr.flush()
+            os._exit(0 if result else 1)
 
     def readLogs(self):
+        foundErr = False
         ret = "----- Simulation stdout -----\n"
-        with open("sim_stdout.log") as f:
+        with open(os.path.join(self.execdir, "sim_stdout.log")) as f:
             ret += f.read()
 
-        with open("sim_stderr.log") as f:
+        with open(os.path.join(self.execdir, "sim_stderr.log")) as f:
             stderr = f.read()
             if stderr != "":
                 ret += "\n----- Simulation stderr -----\n"
                 ret += stderr
+                foundErr = True
 
         ret += "\n----- Test stdout -----\n"
-        with open("test_stdout.log") as f:
+        with open(os.path.join(self.execdir, "test_stdout.log")) as f:
             ret += f.read()
 
-        with open("test_stderr.log") as f:
+        with open(os.path.join(self.execdir, "test_stderr.log")) as f:
             stderr = f.read()
             if stderr != "":
                 ret += "\n----- Test stderr -----\n"
                 ret += stderr
+                foundErr = True
 
-        return ret
+        return (foundErr, ret)
+
+
+def waitpid_deadline(pid, deadline):
+    if deadline == None:
+        (waitpid, rc) = os.waitpid(pid, 0)
+        return (True, rc)
+
+    while time.time() < deadline:
+        (waitpid, rc) = os.waitpid(pid, os.WNOHANG)
+        if waitpid != 0:
+            return (True, rc)
+        time.sleep(0.01)
+    return False
