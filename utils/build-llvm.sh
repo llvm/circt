a4f1ec2e1148f66e77768e4006fcd9d5bbe28e46
John Demme <john.demme@microsoft.com>
2020-11-19 17:11:04 -0800

    [LLVM] Creates llvm build script to avoid replication
    Closes #265.

---
+++ utils/build-llvm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Build LLVM with the standard options. Intended to be called from the github
+# workflows.
+mkdir llvm/build
+mkdir llvm/install
+cd llvm/build
+cmake ../llvm \
+  -DLLVM_BUILD_EXAMPLES=OFF \
+  -DLLVM_TARGETS_TO_BUILD="host" \
+  -DCMAKE_INSTALL_PREFIX=../install \
+  -DLLVM_ENABLE_PROJECTS='mlir' \
+  -DLLVM_OPTIMIZED_TABLEGEN=ON \
+  -DLLVM_ENABLE_OCAMLDOC=OFF \
+  -DLLVM_ENABLE_BINDINGS=OFF \
+  -DLLVM_INSTALL_UTILS=ON \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DLLVM_ENABLE_LLD=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_ENABLE_ASSERTIONS=ON
+cmake --build . --target install -- -j$(nproc)
