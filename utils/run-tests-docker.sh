12955673b43188a2c44380d6da7945289a91f448
John Demme <john.demme@microsoft.com>
2020-11-20 12:38:56 -0800

    Not fully tested, but pushing for testing.

--- utils/run-tests-docker.sh
+++ utils/run-tests-docker.sh
@@ -1,13 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script should be run in the docker container started in the
 # 'run-docker.sh' script.
 
 set -e
 
+echo "=== Building MLIR"
+mkdir -p llvm/build_20.04
+cd llvm/build_20.04
+cmake ../llvm \
+  -DLLVM_BUILD_EXAMPLES=OFF \
+  -DLLVM_TARGETS_TO_BUILD="host" \
+  -DLLVM_ENABLE_PROJECTS='mlir' \
+  -DLLVM_OPTIMIZED_TABLEGEN=ON \
+  -DLLVM_ENABLE_OCAMLDOC=OFF \
+  -DLLVM_ENABLE_BINDINGS=OFF \
+  -DLLVM_INSTALL_UTILS=ON \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DLLVM_ENABLE_LLD=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_ENABLE_ASSERTIONS=ON
+cmake --build . -j$(nproc)
+
+echo "=== Building CIRCT"
 cmake -Bdocker_build \
-  -DMLIR_DIR=llvm/build/lib/cmake/mlir \
-  -DLLVM_DIR=llvm/build/lib/cmake/llvm \
+  -DMLIR_DIR=llvm/build_20.04/lib/cmake/mlir \
+  -DLLVM_DIR=llvm/build_20.04/lib/cmake/llvm \
   -DLLVM_ENABLE_ASSERTIONS=ON \
   -DCMAKE_BUILD_TYPE=DEBUG
 
