68114369f08285eb418637bc7a5d16faba18de9c
John Demme <john.demme@microsoft.com>
2020-11-21 22:08:48 -0800

    Improves build environment flexibility (#253)
    * Adds script to ease local, clean environment testing
    
    * Env change
    
    Co-authored-by: Schuyler Eldridge <schuyler.eldridge@gmail.com>
    
    * Not fully tested, but pushing for testing.
    
    * Use build_llvm.sh script
    
    * Removing the `cd` after MLIR build -- no longer necessary
    
    * Adding ability to specify paths
    
    * Adding cmake headers
    
    Co-authored-by: Schuyler Eldridge <schuyler.eldridge@gmail.com>

--- utils/build-llvm.sh
+++ utils/build-llvm.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+BUILD_DIR=${1:-"build"}
+INSTALL_DIR=${2:-"install"}
 
 # Build LLVM with the standard options. Intended to be called from the github
 # workflows.
-mkdir llvm/build
-mkdir llvm/install
-cd llvm/build
+mkdir -p llvm/$BUILD_DIR
+mkdir -p llvm/$INSTALL_DIR
+cd llvm/$BUILD_DIR
 cmake ../llvm \
   -DLLVM_BUILD_EXAMPLES=OFF \
   -DLLVM_TARGETS_TO_BUILD="host" \
-  -DCMAKE_INSTALL_PREFIX=../install \
+  -DCMAKE_INSTALL_PREFIX=../$INSTALL_DIR \
   -DLLVM_ENABLE_PROJECTS='mlir' \
   -DLLVM_OPTIMIZED_TABLEGEN=ON \
   -DLLVM_ENABLE_OCAMLDOC=OFF \
@@ -19,4 +22,5 @@ cmake ../llvm \
   -DLLVM_ENABLE_LLD=ON \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_ENABLE_ASSERTIONS=ON
+
 cmake --build . --target install -- -j$(nproc)
