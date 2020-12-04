72ed2d214128196cbf4eb9da1a58cba52674d0f5
John Demme <john.demme@microsoft.com>
2020-12-03 21:21:59 -0800

    [SV] Types and ops to allow InterfaceOps to be instantiated and passed around (#294)
    * SV interface types working
    
    * Improving the extract op syntax
    
    * Adding documentation
    
    * Removing unnecessari include
    
    * Wrong type error
    
    * Renaming extract to getmodport and switching to decl asm
    
    * Adding interface.instance and renaming getmodport
    
    * Cleanup changes
    
    * Cleaning up #includes
    
    * Adding verification to instance op
    
    * Returns modport
    
    * Cleanups based on Mike's feedback

--- lib/Dialect/SV/Dialect.cpp
+++ lib/Dialect/SV/Dialect.cpp
@@ -4,9 +4,12 @@
 
 #include "circt/Dialect/SV/Dialect.h"
 #include "circt/Dialect/SV/Ops.h"
+#include "circt/Dialect/SV/Types.h"
 
-using namespace circt;
-using namespace sv;
+#include "mlir/IR/DialectImplementation.h"
+
+using namespace circt::sv;
+using namespace mlir;
 
 //===----------------------------------------------------------------------===//
 // Dialect specification.
@@ -16,6 +19,12 @@ SVDialect::SVDialect(MLIRContext *context)
     : Dialect(getDialectNamespace(), context,
     ::mlir::TypeID::get<SVDialect>()) {
 
+  // Register types.
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "circt/Dialect/SV/SVTypes.cpp.inc"
+      >();
+
   // Register operations.
   addOperations<
 #define GET_OP_LIST
@@ -24,3 +33,22 @@ SVDialect::SVDialect(MLIRContext *context)
 }
 
 SVDialect::~SVDialect() {}
+
+/// Parses a type registered to this dialect
+Type SVDialect::parseType(DialectAsmParser &parser) const {
+  llvm::StringRef mnemonic;
+  auto loc = parser.getCurrentLocation();
+  if (parser.parseKeyword(&mnemonic))
+    return Type();
+  if (auto type = generatedTypeParser(getContext(), parser, mnemonic))
+    return type;
+  parser.emitError(loc, "Failed to parse type sv.") << mnemonic << "\n";
+  return Type();
+}
+
+/// Print a type registered to this dialect
+void SVDialect::printType(Type type, DialectAsmPrinter &printer) const {
+  if (succeeded(generatedTypePrinter(type, printer)))
+    return;
+  llvm_unreachable("unexpected 'rtl' type kind");
+}
--- lib/Dialect/SV/Ops.cpp
+++ lib/Dialect/SV/Ops.cpp
@@ -113,6 +113,41 @@ static void printModportStructs(OpAsmPrinter &p, Operation *,
   p << ')';
 }
 
+/// Ensure that the symbol being instantiated exists and is an InterfaceOp.
+static LogicalResult verifyInterfaceInstanceOp(InterfaceInstanceOp &op) {
+  auto symtable = SymbolTable::getNearestSymbolTable(op);
+  if (!symtable)
+    return op.emitError("sv.interface.instance must exist within a region "
+                        "which has a symbol table.");
+  auto ifaceTy = op.getType().cast<InterfaceType>();
+  auto referencedOp =
+      SymbolTable::lookupSymbolIn(symtable, ifaceTy.getInterface());
+  if (!referencedOp)
+    return op.emitError("Symbol not found: ") << ifaceTy.getInterface() << ".";
+  if (!isa<InterfaceOp>(referencedOp))
+    return op.emitError("Symbol ")
+           << ifaceTy.getInterface() << " is not an InterfaceOp.";
+  return success();
+}
+
+/// Ensure that the symbol being instantiated exists and is an
+/// InterfaceModportOp.
+static LogicalResult verifyGetModportOp(GetModportOp &op) {
+  auto symtable = SymbolTable::getNearestSymbolTable(op);
+  if (!symtable)
+    return op.emitError("sv.interface.instance must exist within a region "
+                        "which has a symbol table.");
+  auto ifaceTy = op.getType().cast<InterfaceModportType>();
+  auto referencedOp =
+      SymbolTable::lookupSymbolIn(symtable, ifaceTy.getModport());
+  if (!referencedOp)
+    return op.emitError("Symbol not found: ") << ifaceTy.getModport() << ".";
+  if (!isa<InterfaceModportOp>(referencedOp))
+    return op.emitError("Symbol ")
+           << ifaceTy.getModport() << " is not an InterfaceModportOp.";
+  return success();
+}
+
 // TableGen generated logic.
 //===----------------------------------------------------------------------===//
 
