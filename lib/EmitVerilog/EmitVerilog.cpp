08b1578656690097fd474e1b26a0b36e0ed1deaf
Andrew Lenharth <darthscsi@users.noreply.github.com>
2020-12-11 15:50:06 -0600

    FIRRTL-style lowering of invalid (#309)
    FIRRTL-style lowering of invalid

--- lib/EmitVerilog/EmitVerilog.cpp
+++ lib/EmitVerilog/EmitVerilog.cpp
@@ -303,6 +303,7 @@ public:
   // Statements.
   void emitStatementExpression(Operation *op);
   void emitStatement(AttachOp op);
+  void emitStatement(InvalidOp op);
   void emitStatement(ConnectOp op);
   void emitStatement(rtl::ConnectOp op);
   void emitStatement(rtl::OutputOp op);
@@ -1441,6 +1442,18 @@ void ModuleEmitter::emitStatement(ConnectOp op) {
   emitLocationInfoAndNewLine(ops);
 }
 
+void ModuleEmitter::emitStatement(InvalidOp op) {
+  SmallPtrSet<Operation *, 8> ops;
+  ops.insert(op);
+
+  auto dest = op.operand();
+
+  indent() << "assign ";
+  emitExpression(dest, ops);
+  os << " = '0;";
+  emitLocationInfoAndNewLine(ops);
+}
+
 void ModuleEmitter::emitStatement(rtl::ConnectOp op) {
   SmallPtrSet<Operation *, 8> ops;
   ops.insert(op);
@@ -2266,7 +2279,7 @@ void ModuleEmitter::emitOperation(Operation *op) {
     bool visitStmt(AttachOp op) { return emitter.emitStatement(op), true; }
     bool visitStmt(ConnectOp op) { return emitter.emitStatement(op), true; }
     bool visitStmt(DoneOp op) { return true; }
-    bool visitStmt(InvalidOp op) { return true; }
+    bool visitStmt(InvalidOp op) { return emitter.emitStatement(op), true; }
     bool visitStmt(PrintFOp op) { return emitter.emitStatement(op), true; }
     bool visitStmt(SkipOp op) { return true; }
     bool visitStmt(StopOp op) { return emitter.emitStatement(op), true; }
