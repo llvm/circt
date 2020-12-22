7d16e5415c3029b26ebf54d9d04f421c09c304b1
John Demme <john.demme@microsoft.com>
2020-12-21 23:00:25 -0800

    [ESI] Re-organizing passes code
    Just moving around code.

--- lib/Dialect/ESI/ESIPasses.cpp
+++ lib/Dialect/ESI/ESIPasses.cpp
@@ -46,76 +46,7 @@ static size_t getNumBits(Type type) {
 }
 
 //===----------------------------------------------------------------------===//
-// Lower to physical conversions and pass.
-//===----------------------------------------------------------------------===//
-
-namespace {
-/// Lower `ChannelBuffer`s, breaking out the various options. For now, just
-/// replace with the specified number of pipeline stages (since that's the only
-/// option).
-struct ChannelBufferLowering : public OpConversionPattern<ChannelBuffer> {
-public:
-  using OpConversionPattern::OpConversionPattern;
-
-  LogicalResult
-  matchAndRewrite(ChannelBuffer buffer, ArrayRef<Value> operands,
-                  ConversionPatternRewriter &rewriter) const final;
-};
-} // anonymous namespace
-
-LogicalResult ChannelBufferLowering::matchAndRewrite(
-    ChannelBuffer buffer, ArrayRef<Value> operands,
-    ConversionPatternRewriter &rewriter) const {
-  auto loc = buffer.getLoc();
-
-  ChannelBufferOptions opts = buffer.options();
-  auto type = buffer.getType();
-
-  // Expand 'abstract' buffer into 'physical' stages.
-  auto stages = opts.stages();
-  uint64_t numStages = 1;
-  if (stages) {
-    // Guaranteed positive by the parser.
-    numStages = stages.getValue().getLimitedValue();
-  }
-  Value input = buffer.input();
-  for (uint64_t i = 0; i < numStages; ++i) {
-    // Create the stages, connecting them up as we build.
-    auto stage = rewriter.create<PipelineStage>(loc, type, buffer.clk(),
-                                                buffer.rstn(), input);
-    input = stage;
-  }
-
-  // Replace the buffer.
-  rewriter.replaceOp(buffer, input);
-  return success();
-}
-
-namespace {
-/// Run all the physical lowerings.
-struct ESIToPhysicalPass : public LowerESIToPhysicalBase<ESIToPhysicalPass> {
-  void runOnOperation() override;
-};
-} // anonymous namespace
-
-void ESIToPhysicalPass::runOnOperation() {
-  // Set up a conversion and give it a set of laws.
-  ConversionTarget target(getContext());
-  target.addLegalDialect<ESIDialect>();
-  target.addIllegalOp<ChannelBuffer>();
-
-  // Add all the conversion patterns.
-  OwningRewritePatternList patterns;
-  patterns.insert<ChannelBufferLowering>(&getContext());
-
-  // Run the conversion.
-  if (failed(
-          applyPartialConversion(getOperation(), target, std::move(patterns))))
-    signalPassFailure();
-};
-
-//===----------------------------------------------------------------------===//
-// Lower to RTL/SV conversions and pass.
+// ESI custom op builder.
 //===----------------------------------------------------------------------===//
 
 namespace {
@@ -251,72 +182,78 @@ InterfaceOp ESIRTLBuilder::constructInterface(ChannelPort chan) {
   return iface;
 }
 
+//===----------------------------------------------------------------------===//
+// Lower to physical conversions and pass.
+//===----------------------------------------------------------------------===//
+
 namespace {
-/// Lower PipelineStage ops to an RTL implementation. Unwrap and re-wrap
-/// appropriately. Another conversion will take care merging the resulting
-/// adjacent wrap/unwrap ops.
-struct PipelineStageLowering : public OpConversionPattern<PipelineStage> {
+/// Lower `ChannelBuffer`s, breaking out the various options. For now, just
+/// replace with the specified number of pipeline stages (since that's the only
+/// option).
+struct ChannelBufferLowering : public OpConversionPattern<ChannelBuffer> {
 public:
-  PipelineStageLowering(ESIRTLBuilder &builder, MLIRContext *ctxt)
-      : OpConversionPattern(ctxt), builder(builder) {}
   using OpConversionPattern::OpConversionPattern;
 
   LogicalResult
-  matchAndRewrite(PipelineStage stage, ArrayRef<Value> operands,
+  matchAndRewrite(ChannelBuffer buffer, ArrayRef<Value> operands,
                   ConversionPatternRewriter &rewriter) const final;
-
-private:
-  ESIRTLBuilder &builder;
 };
 } // anonymous namespace
 
-LogicalResult PipelineStageLowering::matchAndRewrite(
-    PipelineStage stage, ArrayRef<Value> stageOperands,
+LogicalResult ChannelBufferLowering::matchAndRewrite(
+    ChannelBuffer buffer, ArrayRef<Value> operands,
     ConversionPatternRewriter &rewriter) const {
-  auto loc = stage.getLoc();
-  auto chPort = stage.input().getType().dyn_cast<ChannelPort>();
-  if (!chPort)
-    return failure();
-  auto stageModule = builder.declareStage();
+  auto loc = buffer.getLoc();
 
-  NamedAttrList stageAttrs = stage.getAttrs();
-  size_t width = getNumBits(chPort.getInner());
-  stageAttrs.set(builder.width, rewriter.getUI32IntegerAttr(width));
+  ChannelBufferOptions opts = buffer.options();
+  auto type = buffer.getType();
 
-  // Unwrap the channel. The ready signal is a Value we haven't created yet, so
-  // create a temp value and replace it later. Give this constant an odd-looking
-  // type to make debugging easier.
-  circt::BackedgeBuilder back(rewriter, loc);
-  circt::Backedge wrapReady = back.get(rewriter.getI1Type());
-  auto unwrap =
-      rewriter.create<UnwrapValidReady>(loc, stage.input(), wrapReady);
+  // Expand 'abstract' buffer into 'physical' stages.
+  auto stages = opts.stages();
+  uint64_t numStages = 1;
+  if (stages) {
+    // Guaranteed positive by the parser.
+    numStages = stages.getValue().getLimitedValue();
+  }
+  Value input = buffer.input();
+  for (uint64_t i = 0; i < numStages; ++i) {
+    // Create the stages, connecting them up as we build.
+    auto stage = rewriter.create<PipelineStage>(loc, type, buffer.clk(),
+                                                buffer.rstn(), input);
+    input = stage;
+  }
 
-  // Instantiate the "ESI_PipelineStage" external module.
-  circt::Backedge stageReady = back.get(rewriter.getI1Type());
-  Value operands[] = {stage.clk(), stage.rstn(), unwrap.rawOutput(),
-                      unwrap.valid(), stageReady};
-  Type resultTypes[] = {rewriter.getI1Type(), unwrap.rawOutput().getType(),
-                        rewriter.getI1Type()};
-  auto stageInst = rewriter.create<InstanceOp>(
-      loc, resultTypes, "pipelineStage", stageModule.getName(), operands,
-      stageAttrs.getDictionary(rewriter.getContext()));
-  auto stageInstResults = stageInst.getResults();
-
-  // Set a_ready (from the unwrap) back edge correctly to its output from stage.
-  wrapReady.setValue(stageInstResults[0]);
-
-  Value x = stageInstResults[1];
-  Value xValid = stageInstResults[2];
-
-  // Wrap up the output of the RTL stage module.
-  auto wrap = rewriter.create<WrapValidReady>(loc, chPort, rewriter.getI1Type(),
-                                              x, xValid);
-  // Set the stages x_ready backedge correctly.
-  stageReady.setValue(wrap.ready());
-
-  rewriter.replaceOp(stage, wrap.chanOutput());
+  // Replace the buffer.
+  rewriter.replaceOp(buffer, input);
   return success();
 }
+
+namespace {
+/// Run all the physical lowerings.
+struct ESIToPhysicalPass : public LowerESIToPhysicalBase<ESIToPhysicalPass> {
+  void runOnOperation() override;
+};
+} // anonymous namespace
+
+void ESIToPhysicalPass::runOnOperation() {
+  // Set up a conversion and give it a set of laws.
+  ConversionTarget target(getContext());
+  target.addLegalDialect<ESIDialect>();
+  target.addIllegalOp<ChannelBuffer>();
+
+  // Add all the conversion patterns.
+  OwningRewritePatternList patterns;
+  patterns.insert<ChannelBufferLowering>(&getContext());
+
+  // Run the conversion.
+  if (failed(
+          applyPartialConversion(getOperation(), target, std::move(patterns))))
+    signalPassFailure();
+};
+
+//===----------------------------------------------------------------------===//
+// Lower ESI ports pass.
+//===----------------------------------------------------------------------===//
 
 namespace {
 /// Convert all the ESI ports on modules to some lower construct. SV interfaces
@@ -526,6 +463,76 @@ void ESIPortsPass::updateInstance(RTLExternModuleOp mod, InstanceOp inst) {
   inst.erase();
 }
 
+//===----------------------------------------------------------------------===//
+// Lower to RTL/SV conversions and pass.
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// Lower PipelineStage ops to an RTL implementation. Unwrap and re-wrap
+/// appropriately. Another conversion will take care merging the resulting
+/// adjacent wrap/unwrap ops.
+struct PipelineStageLowering : public OpConversionPattern<PipelineStage> {
+public:
+  PipelineStageLowering(ESIRTLBuilder &builder, MLIRContext *ctxt)
+      : OpConversionPattern(ctxt), builder(builder) {}
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(PipelineStage stage, ArrayRef<Value> operands,
+                  ConversionPatternRewriter &rewriter) const final;
+
+private:
+  ESIRTLBuilder &builder;
+};
+} // anonymous namespace
+
+LogicalResult PipelineStageLowering::matchAndRewrite(
+    PipelineStage stage, ArrayRef<Value> stageOperands,
+    ConversionPatternRewriter &rewriter) const {
+  auto loc = stage.getLoc();
+  auto chPort = stage.input().getType().dyn_cast<ChannelPort>();
+  if (!chPort)
+    return failure();
+  auto stageModule = builder.declareStage();
+
+  NamedAttrList stageAttrs = stage.getAttrs();
+  size_t width = getNumBits(chPort.getInner());
+  stageAttrs.set(builder.width, rewriter.getUI32IntegerAttr(width));
+
+  // Unwrap the channel. The ready signal is a Value we haven't created yet, so
+  // create a temp value and replace it later. Give this constant an odd-looking
+  // type to make debugging easier.
+  circt::BackedgeBuilder back(rewriter, loc);
+  circt::Backedge wrapReady = back.get(rewriter.getI1Type());
+  auto unwrap =
+      rewriter.create<UnwrapValidReady>(loc, stage.input(), wrapReady);
+
+  // Instantiate the "ESI_PipelineStage" external module.
+  circt::Backedge stageReady = back.get(rewriter.getI1Type());
+  Value operands[] = {stage.clk(), stage.rstn(), unwrap.rawOutput(),
+                      unwrap.valid(), stageReady};
+  Type resultTypes[] = {rewriter.getI1Type(), unwrap.rawOutput().getType(),
+                        rewriter.getI1Type()};
+  auto stageInst = rewriter.create<InstanceOp>(
+      loc, resultTypes, "pipelineStage", stageModule.getName(), operands,
+      stageAttrs.getDictionary(rewriter.getContext()));
+  auto stageInstResults = stageInst.getResults();
+
+  // Set a_ready (from the unwrap) back edge correctly to its output from stage.
+  wrapReady.setValue(stageInstResults[0]);
+
+  Value x = stageInstResults[1];
+  Value xValid = stageInstResults[2];
+
+  // Wrap up the output of the RTL stage module.
+  auto wrap = rewriter.create<WrapValidReady>(loc, chPort, rewriter.getI1Type(),
+                                              x, xValid);
+  // Set the stages x_ready backedge correctly.
+  stageReady.setValue(wrap.ready());
+
+  rewriter.replaceOp(stage, wrap.chanOutput());
+  return success();
+}
 namespace {
 struct ESItoRTLPass : public LowerESItoRTLBase<ESItoRTLPass> {
   void runOnOperation() override;
