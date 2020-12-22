5a68135ae5c1ac76d2118d836d6754f2554b1e20
xgupta <shivam98.tkg@gmail.com>
2020-12-23 00:49:02 +0530

    Add LLVM license info to files header

--- utils/get-capnp.sh
+++ utils/get-capnp.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-# Downloads, compiles, and installs CapnProto into $/ext
+##===- utils/get-capnp.sh - Install CapnProto ----------------*- Script -*-===##
+# 
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# 
+##===----------------------------------------------------------------------===##
+#
+# This script downloads, compiles, and installs CapnProto into $/ext
+# Cap'nProto is use by ESI cosim aka Elastic Silicon Interfaces cosimulation as
+# a message format and RPC client/server.
+#
+##===----------------------------------------------------------------------===##
 
 EXT_DIR=$(cd "$(dirname "$BASH_SOURCE[0]")/../ext" && pwd)
 CAPNP_VER=0f1bf4fce79923fb4974aa55a53e26450f83f286
