22869676ad67b9c1249de529a39a2a35cc78387a
xgupta <shivam98.tkg@gmail.com>
2020-12-23 00:54:10 +0530

    Add LLVM license info to files header

--- integration_test/driver.cpp
+++ integration_test/driver.cpp
@@ -1,4 +1,10 @@
-//===- driver.cpp - Verilator software driver -----------------------------===//
+//===- integration_test/driver.cpp - Verilator software driver ------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 //
 // A fairly standard, boilerplate Verilator C++ simulation driver. Assumes the
 // top level exposes just two signals: 'clk' and 'rstn'.
