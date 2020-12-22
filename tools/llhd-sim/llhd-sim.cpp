873262c2feb7fb11ed2ff6bfe9ad9fa50a17bf35
xgupta <shivam98.tkg@gmail.com>
2020-12-22 23:41:53 +0530

    add LLVM license info to files header

--- tools/llhd-sim/llhd-sim.cpp
+++ tools/llhd-sim/llhd-sim.cpp
@@ -1,5 +1,11 @@
 //===- llhd-sim.cpp - LLHD simulator tool -----------------------*- C++ -*-===//
 //
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
 // This file implements a command line tool to run LLHD simulation.
 //
 //===----------------------------------------------------------------------===//
