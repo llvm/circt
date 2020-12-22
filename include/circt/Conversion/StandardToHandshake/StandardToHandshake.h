2f8eff1a3e50f3f4c6058e5d42e6c36b2fa6edf9
xgupta <shivam98.tkg@gmail.com>
2020-12-23 01:58:37 +0530

    Add LLVM license info to file headers

--- include/circt/Conversion/StandardToHandshake/StandardToHandshake.h
+++ include/circt/Conversion/StandardToHandshake/StandardToHandshake.h
@@ -1,11 +1,15 @@
 //===- StandardToHandshake.h ------------------------------------*- C++ -*-===//
 //
-// Copyright 2020 The CIRCT Authors.
-//
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// =============================================================================
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares passes which together will lower the Standard dialect to
+// Handshake dialect.
+//
+//===----------------------------------------------------------------------===//
 
 #ifndef CIRCT_CONVERSION_STANDARDTOHANDSHAKE_H_
 #define CIRCT_CONVERSION_STANDARDTOHANDSHAKE_H_
--- include/circt/Dialect/LLHD/IR/LLHDOps.h
+++ include/circt/Dialect/LLHD/IR/LLHDOps.h
@@ -1,5 +1,11 @@
 //===- LLHDOps.h - Declare LLHD dialect operations --------------*- C++ -*-===//
 //
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
 // This file declares the operation class for the LLHD IR.
 //
 //===----------------------------------------------------------------------===//
