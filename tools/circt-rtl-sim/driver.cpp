09cc61cb1e32c6b63ce929ae4e3472b452280db2
xgupta <shivam98.tkg@gmail.com>
2020-12-23 17:50:50 +0530

    Revert "Add LLVM license info to files header"
    This reverts commit 22869676ad67b9c1249de529a39a2a35cc78387a.

--- tools/circt-rtl-sim/driver.cpp
+++ tools/circt-rtl-sim/driver.cpp
@@ -1,10 +1,4 @@
-//===- integration_test/driver.cpp - Verilator software driver ------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
+//===- driver.cpp - Verilator software driver -----------------------------===//
 //
 // A fairly standard, boilerplate Verilator C++ simulation driver. Assumes the
 // top level exposes just two signals: 'clk' and 'rstn'.
