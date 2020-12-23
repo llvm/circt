5cfce7e61f2bebaad933468cb33080e22cb297f9
xgupta <shivam98.tkg@gmail.com>
2020-12-23 15:48:46 +0530

    add missing file descriptions

--- tools/firtool/firtool.cpp
+++ tools/firtool/firtool.cpp
@@ -1,6 +1,5 @@
 //===- firtool.cpp - The firtool utility for working with .fir files ------===//
 //
-//
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
