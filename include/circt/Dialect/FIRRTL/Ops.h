99ab1e0255886ce1232c8f0ef6665846d5eb44f3
Chris Lattner <clattner@nondot.org>
2020-12-26 14:06:09 -0800

    [FIRRTL] Simplify the type computation logic for un/binops.  NFC.

--- include/circt/Dialect/FIRRTL/Ops.h
+++ include/circt/Dialect/FIRRTL/Ops.h
@@ -28,30 +28,6 @@ namespace firrtl {
 /// Return true if the specified operation is a firrtl expression.
 bool isExpression(Operation *op);
 
-// Binary primitives.
-FIRRTLType getAddSubResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getMulResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getDivResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getRemResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getCompareResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getBitwiseBinaryResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getCatResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getDShlResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getDShlwResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getDShrResult(FIRRTLType lhs, FIRRTLType rhs);
-FIRRTLType getValidIfResult(FIRRTLType lhs, FIRRTLType rhs);
-
-// Unary primitives.
-FIRRTLType getAsAsyncResetResult(FIRRTLType input);
-FIRRTLType getAsClockResult(FIRRTLType input);
-FIRRTLType getAsSIntResult(FIRRTLType input);
-FIRRTLType getAsUIntResult(FIRRTLType input);
-FIRRTLType getCvtResult(FIRRTLType input);
-FIRRTLType getNegResult(FIRRTLType input);
-FIRRTLType getNotResult(FIRRTLType input);
-FIRRTLType getReductionResult(FIRRTLType input);
-FIRRTLType getAsPassiveResult(FIRRTLType input);
-
 /// This holds the name and type that describes the module's ports.
 struct ModulePortInfo {
   StringAttr name;
