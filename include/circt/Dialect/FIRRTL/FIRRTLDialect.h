879078ea214d3af2c47d4fdc04a314cde8a492aa
xgupta <shivam98.tkg@gmail.com>
2020-12-27 15:33:55 +0530

    minor fix

--- include/circt/Dialect/FIRRTL/FIRRTLDialect.h
+++ include/circt/Dialect/FIRRTL/FIRRTLDialect.h
@@ -1,4 +1,4 @@
-//===- FIRRTL/IR/FIRTLDialect.h - FIRRTL dialect declaration ---------*- C++ -*-===//
+//===- FIRRTL/IR/FIRRTLDialect.h - FIRRTL dialect declaration ---------*- C++ -*-===//
 //
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
