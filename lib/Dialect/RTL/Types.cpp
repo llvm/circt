66e55e07b8dec299cf72e3bd0e7f5d0c9f7e4e28
Andrew Lenharth <andrew@lenharth.org>
2020-12-29 11:29:50 -0600

    checkpoint

--- lib/Dialect/RTL/Types.cpp
+++ lib/Dialect/RTL/Types.cpp
@@ -8,7 +8,7 @@
 #include "circt/Dialect/RTL/Dialect.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/DialectImplementation.h"
-#include "mlir/IR/StandardTypes.h"
+#include "mlir/IR/Types.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/TypeSwitch.h"
 
