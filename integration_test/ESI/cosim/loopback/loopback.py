8d2eb72c97aa9b1dc8c1a6f6241c82802cc2b73d
John Demme <john.demme@microsoft.com>
2021-01-17 19:42:52 -0800

    [ESI] [Cosim] Pull everything together into one simple test (#468)
    Adds a simple loopback test and changes/fixes to enable it.
    
    - Uses all the lowering passes to get everything in the RTL dialect.
    - Uses circt-translate --emit-verilog to emit verilog.
    - Uses circt-translate -emit-esi-capnp to get RPC schema.
    - Runs it via esi-cosim-runner.py.

--- integration_test/ESI/cosim/loopback/loopback.py
+++ integration_test/ESI/cosim/loopback/loopback.py
@@ -34,14 +34,7 @@ class LoopbackTester:
         assert openResp.iface is not None
         return openResp.iface
 
-    def write(self, ep):
-        r = random.randrange(0, 2**24)
-        data = r.to_bytes(3, 'big')
-        print(f'Sending: {binascii.hexlify(data)}')
-        ep.send(self.dpi.UntypedData.new_message(data=data)).wait()
-        return data
-
-    def read(self, ep):
+    def readMsg(self, ep, expectedType):
         while True:
             recvResp = ep.recv(False).wait()
             if recvResp.hasData:
@@ -49,31 +42,41 @@ class LoopbackTester:
             else:
                 time.sleep(0.1)
         assert recvResp.resp is not None
-        dataMsg = recvResp.resp.as_struct(self.dpi.UntypedData)
+        return recvResp.resp.as_struct(expectedType)
+
+    def test_i32(self, num_msgs):
+        ep = self.openEP()
+        for _ in range(num_msgs):
+            data = random.randint(0, 2**32)
+            print(f"Sending {data}")
+            ep.send(self.dpi.TYi32.new_message(i=data))
+            result = self.readMsg(ep, self.dpi.TYi32)
+            print(f"Got {result}")
+            assert (result.i == data)
+
+    def write_3bytes(self, ep):
+        r = random.randrange(0, 2**24)
+        data = r.to_bytes(3, 'big')
+        print(f'Sending: {binascii.hexlify(data)}')
+        ep.send(self.dpi.UntypedData.new_message(data=data)).wait()
+        return data
+
+    def read_3bytes(self, ep):
+        dataMsg = self.readMsg(ep, self.dpi.UntypedData)
         data = dataMsg.data
         print(binascii.hexlify(data))
         return data
 
-    def write_read(self):
-        ep = self.openEP()
-        print("Testing writes")
-        dataSent = self.write(ep)
-        print()
-        print("Testing reads")
-        dataRecv = self.read(ep)
-        ep.close().wait()
-        assert dataSent == dataRecv
-
-    def write_read_many(self, num_msgs=50):
+    def test_3bytes(self, num_msgs=50):
         ep = self.openEP()
         print("Testing writes")
         dataSent = list()
         for _ in range(num_msgs):
-            dataSent.append(self.write(ep))
+            dataSent.append(self.write_3bytes(ep))
         print()
         print("Testing reads")
         dataRecv = list()
         for _ in range(num_msgs):
-            dataRecv.append(self.read(ep))
+            dataRecv.append(self.read_3bytes(ep))
         ep.close().wait()
         assert dataSent == dataRecv
