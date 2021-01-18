f66e9fa28057e7ee89b3b4f175263fb2cb358a82
George <989903+GeorgeLyon@users.noreply.github.com>
2021-01-18 12:13:39 -0800

    [ExportVerilog][SV] C API

---
+++ include/circt-c/ExportVerilog.h
@@ -0,0 +1,26 @@
+//===-- circt-c/SVDialect.h - C API for emitting Verilog ----------*- C -*-===//
+//
+// This header declares the C interface for emitting Verilog from a CIRCT MLIR
+// module.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_C_EXPORTVERILOG_H
+#define CIRCT_C_EXPORTVERILOG_H
+
+#include "mlir-c/IR.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// Emits verilog for the specified module using the provided callback and user
+/// data
+MlirLogicalResult mlirExportVerilog(MlirModule, MlirStringCallback,
+                                    void *userData);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CIRCT_C_EXPORTVERILOG_H
