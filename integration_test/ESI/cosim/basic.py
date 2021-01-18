40d0c91858ed553b3800d95ef56a7a569a9d0914
John Demme <john.demme@microsoft.com>
2021-01-17 23:11:10 -0800

    [ESI] [Capnp schemas] Change naming scheme, adding array support
    No tests for arrays since adding arrays to the existing tests breaks RTL
    passes. Tests for arrays will be part of the RTL array support PR.

--- integration_test/ESI/cosim/basic.py
+++ integration_test/ESI/cosim/basic.py
@@ -8,14 +8,14 @@ class BasicSystemTester(cosim.CosimBase):
     """Provides methods to test the 'basic' simulation."""
 
     def testIntAcc(self, num_msgs):
-        ep = self.openEP(sendType=self.schema.TYi32,
-                         recvType=self.schema.TYi32)
+        ep = self.openEP(sendType=self.schema.I32,
+                         recvType=self.schema.I32)
         sum = 0
         for _ in range(num_msgs):
             i = random.randint(0, 77)
             sum += i
             print(f"Sending {i}")
-            ep.send(self.schema.TYi32.new_message(i=i))
-            result = self.readMsg(ep, self.schema.TYi32)
+            ep.send(self.schema.I32.new_message(i=i))
+            result = self.readMsg(ep, self.schema.I32)
             print(f"Got {result}")
             assert (result.i == sum)
