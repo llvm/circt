2652e6177e1cbd086d700cbbddc1accef51ea4f3
George <GeorgeLyon@users.noreply.github.com>
2021-03-02 09:06:21 -0800

    Bind more API to C

--- lib/CAPI/Dialect/RTL.cpp
+++ lib/CAPI/Dialect/RTL.cpp
@@ -16,4 +16,34 @@
 #include "mlir/CAPI/Registration.h"
 #include "mlir/CAPI/Support.h"
 
-MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(RTL, rtl, circt::rtl::RTLDialect)
+using namespace circt::rtl;
+
+//===----------------------------------------------------------------------===//
+// Dialect API.
+//===----------------------------------------------------------------------===//
+
+MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(RTL, rtl, RTLDialect)
+
+//===----------------------------------------------------------------------===//
+// Type API.
+//===----------------------------------------------------------------------===//
+
+int64_t rtlGetBitWidth(MlirType type) { return getBitWidth(unwrap(type)); }
+
+bool rtlTypeIsAValueType(MlirType type) { return isRTLValueType(unwrap(type)); }
+
+bool rtlTypeIsAArrayType(MlirType type) {
+  return unwrap(type).isa<ArrayType>();
+}
+
+MlirType rtlArrayTypeGet(MlirType element, size_t size) {
+  return wrap(ArrayType::get(unwrap(element), size));
+}
+
+MlirType rtlArrayTypeGetElementType(MlirType type) {
+  return wrap(unwrap(type).cast<ArrayType>().getElementType());
+}
+
+intptr_t rtlArrayTypeGetSize(MlirType type) {
+  return unwrap(type).cast<ArrayType>().getSize();
+}
