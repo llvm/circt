ff5926f209a6b47612d510fb09fb12e6294e87d4
John Demme <john.demme@microsoft.com>
2021-03-09 15:25:32 -0800

    [Seq] Creating the new sequential dialect
    Just mechanical boilerplate in anticipation of the RegisterOp.

---
+++ include/circt/Dialect/Seq/SeqOps.h
@@ -0,0 +1,21 @@
+//===- SeqOps.h - Declare Seq dialect operations ----------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the operation classes for the Seq dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_SEQ_SEQOPS_H
+#define CIRCT_DIALECT_SEQ_SEQOPS_H
+
+#include "circt/Dialect/Seq/SeqDialect.h"
+
+#define GET_OP_CLASSES
+#include "circt/Dialect/Seq/Seq.h.inc"
+
+#endif // CIRCT_DIALECT_SEQ_SEQOPS_H
