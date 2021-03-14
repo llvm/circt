2a43f22d390b606047c10a0d1bb1add1099294d4
mikeurbach <mikeurbach@gmail.com>
2021-03-13 21:12:44 -0700

    Simplify RTL Python bindings test.

--- integration_test/Bindings/Python/dialects/rtl.py
+++ integration_test/Bindings/Python/dialects/rtl.py
@@ -5,16 +5,12 @@ import circt
 from circt.dialects import rtl
 
 from mlir.ir import *
-from mlir.dialects import std
 
 with Context() as ctx, Location.unknown():
     circt.register_dialects(ctx)
-    m = Module.create()
-    with InsertionPoint.at_block_terminator(m.body):
-        i32 = IntegerType.get_signless(32)
-        a50 = IntegerAttr.get(i32, 50)
-        std.ConstantOp(i32, a50)
-        rtl.ConstantOp(i32, a50)
+    i32 = IntegerType.get_signless(32)
+    a50 = IntegerAttr.get(i32, 50)
+    op = rtl.ConstantOp(i32, a50)
 
-# CHECK: module
-m.operation.print()
+    # CHECK: %{{.+}} = rtl.constant 50 : i32
+    op.print()
