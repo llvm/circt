b5b60cde5af5efa0724ef900ebffd05de67e389a
kingshukmajumder <kingshuk1000@gmail.com>
2021-03-19 07:12:39 -0400

    [HIR] removing Werror

--- include/circt/Dialect/HIR/HIRDialect.h
+++ include/circt/Dialect/HIR/HIRDialect.h
@@ -1,6 +1,7 @@
 #ifndef HIR_HIRDIALECT_H
 #define HIR_HIRDIALECT_H
 
+#include "circt/Support/LLVM.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/FunctionSupport.h"
