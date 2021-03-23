b5fbd0f37face9815d3c0c2297b08bc87bef401b
John Demme <john.demme@microsoft.com>
2021-03-22 21:25:26 -0700

    First draft working!

--- lib/Dialect/MSFT/TclExport/ExportQuartusTcl.cpp
+++ lib/Dialect/MSFT/TclExport/ExportQuartusTcl.cpp
@@ -14,8 +14,6 @@
 #include "circt/Dialect/MSFT/MSFTAttributes.h"
 #include "circt/Dialect/RTL/RTLOps.h"
 #include "circt/Support/LLVM.h"
-#include "circt/Translation/ExportVerilog.h"
-#include "mlir/Support/FileUtilities.h"
 #include "mlir/Translation.h"
 #include "llvm/ADT/TypeSwitch.h"
 #include "llvm/Support/raw_ostream.h"
@@ -24,9 +22,122 @@ using namespace circt;
 using namespace rtl;
 using namespace msft;
 
+// TODO: Currently assumes Stratix 10 and QuartusPro. Make more general.
+
+namespace {
+struct TclOutputState {
+  TclOutputState(llvm::raw_ostream &os) : os(os) {}
+
+  llvm::raw_ostream &os;
+  llvm::raw_ostream &indent() {
+    os.indent(2);
+    return os;
+  };
+
+  SmallPtrSet<Operation *, 32> modulesEmitted;
+};
+
+struct Entity {
+  Entity(TclOutputState &s)
+      : s(s), parent(nullptr), name("$parent"), insideEmittedModule(false) {}
+  Entity(Entity *parent, StringRef name, bool insideEmittedModule)
+      : s(parent->s), parent(parent), name(name),
+        insideEmittedModule(insideEmittedModule) {}
+
+  Optional<Entity> enter(InstanceOp inst);
+  LogicalResult emit(Operation *, StringRef, PhysLocationAttr);
+
+  void emitPath();
+
+  TclOutputState &s;
+  Entity *parent;
+  StringRef name;
+  bool insideEmittedModule;
+};
+} // anonymous namespace
+
+Optional<Entity> Entity::enter(InstanceOp inst) {
+  auto mod = dyn_cast_or_null<rtl::RTLModuleOp>(inst.getReferencedModule());
+  if (!mod)
+    return {};
+  bool modEmitted = insideEmittedModule ||
+                    s.modulesEmitted.find(mod) != s.modulesEmitted.end();
+  s.modulesEmitted.insert(mod);
+
+  return Entity(this, inst.instanceName(), modEmitted);
+}
+
+LogicalResult Entity::emit(Operation *op, StringRef attrKey,
+                           PhysLocationAttr pla) {
+  if (!attrKey.startswith_lower("loc:")) {
+    op->emitError("Error in '")
+        << attrKey << "' PhysLocation attribute. Expected loc:<entityName>.";
+    return failure();
+  }
+  StringRef childEntity = attrKey.substr(4);
+  s.indent() << "set_location_assignment ";
+
+  char numCharacter;
+  switch (pla.getDevType().getValue()) {
+  case DeviceType::M20K:
+    s.os << "M20K";
+    numCharacter = 'N';
+    break;
+  case DeviceType::DSP:
+    s.os << "MSDSP";
+    numCharacter = 'N';
+    break;
+  }
+
+  s.os << "_X" << pla.getX() << "_Y" << pla.getY() << "_" << numCharacter
+       << pla.getNum();
+  s.os << " -to ";
+  emitPath();
+  s.os << childEntity << '\n';
+
+  return success();
+}
+
+void Entity::emitPath() {
+  if (parent)
+    parent->emitPath();
+  s.os << name << "|";
+}
+
+static LogicalResult exportTcl(Entity entity, Operation *op) {
+  if (auto inst = dyn_cast<rtl::InstanceOp>(op)) {
+    Optional<Entity> inModule = entity.enter(inst);
+    if (inModule && failed(exportTcl(*inModule, inst.getReferencedModule())))
+      return failure();
+  }
+
+  for (NamedAttribute attr : op->getAttrs())
+    if (auto loc = attr.second.dyn_cast<PhysLocationAttr>())
+      if (failed(entity.emit(op, attr.first, loc)))
+        return failure();
+
+  auto result = op->walk([&](Operation *innerOp) {
+    if (innerOp != op && failed(exportTcl(entity, innerOp)))
+      return mlir::WalkResult::interrupt();
+    return mlir::WalkResult::advance();
+  });
+  return failure(result.wasInterrupted());
+}
+
 LogicalResult circt::msft::exportQuartusTCL(ModuleOp module,
                                             llvm::raw_ostream &os) {
-  return failure();
+  TclOutputState state(os);
+
+  for (auto &op : module.getBody()->getOperations()) {
+    auto rtlMod = dyn_cast<RTLModuleOp>(op);
+    if (!rtlMod)
+      continue;
+    os << "proc " << rtlMod.getName() << "_config { parent } {\n";
+    if (failed(exportTcl(Entity(state), rtlMod)))
+      return failure();
+    os << "}\n\n";
+  }
+  return success();
 }
 
 void circt::msft::registerMSFTTclTranslation() {
--- test/Dialect/MSFT/location.mlir
+++ test/Dialect/MSFT/location.mlir
@@ -1,8 +1,10 @@
 // RUN: circt-opt %s -verify-diagnostics | circt-opt -verify-diagnostics | FileCheck %s
+// RUN: circt-translate %s --export-quartus-tcl | FileCheck %s --check-prefix=TCL
 
 rtl.module.extern @Foo()
 
-rtl.module @Top() {
-  // CHECK: rtl.instance "foo1" @Foo() {"loc:memBank1" = #msft.physloc<M20K, 0, 0, 0>}
-  rtl.instance "foo1" @Foo() {"loc:memBank1" = #msft.physloc<M20K, 0, 0, 0> } : () -> ()
+rtl.module @top() {
+  // CHECK: rtl.instance "foo1" @Foo() {"loc:memBank1" = #msft.physloc<M20K, 4, 10, 1>}
+  // TCL:   set_location_assignment M20K_X4_Y10_N1 -to $parent|memBank1
+  rtl.instance "foo1" @Foo() {"loc:memBank1" = #msft.physloc<M20K, 4, 10, 1> } : () -> ()
 }
