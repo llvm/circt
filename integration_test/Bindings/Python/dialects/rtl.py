cc8a0f21ec8e2e338261786a640fa9d6e9cf1551
Mike Urbach <mikeurbach@gmail.com>
2021-03-27 00:33:13 -0600

    Copy fancy Python builder style from builtin FuncOp.

--- integration_test/Bindings/Python/dialects/rtl.py
+++ integration_test/Bindings/Python/dialects/rtl.py
@@ -14,6 +14,8 @@ with Context() as ctx, Location.unknown():
 
     m = builtin.ModuleOp()
     with InsertionPoint.at_block_begin(m.body):
+        # CHECK: rtl.module @MyWidget(%my_input: i32) -> (%my_output: i32)
+        # CHECK:   rtl.output %my_input : i32
         op = rtl.RTLModuleOp(
             name='MyWidget',
             input_ports=[('my_input', i32)],
@@ -22,6 +24,20 @@ with Context() as ctx, Location.unknown():
                 [module.entry_block.arguments[0]])
         )
 
-    # CHECK: rtl.module @MyWidget(%my_input: i32) -> (%my_output: i32)
-    # CHECK:   rtl.output %my_input : i32
+        # CHECK: rtl.module @swap(%a: i32, %b: i32) -> (i32, i32)
+        # CHECK:   rtl.output %b, %a : i32, i32
+        @rtl.RTLModuleOp.from_py_func(i32, i32)
+        def swap(a, b):
+            return b, a
+
+        # CHECK: rtl.module @top(%a: i32, %b: i32) -> (i32, i32)
+        # CHECK:   %[[a0:.+]], %[[b0:.+]] = rtl.instance "" @swap(%a, %b)
+        # CHECK:   %[[a1:.+]], %[[b1:.+]] = rtl.instance "" @swap(%[[a0]], %[[b0]])
+        # CHECK:   rtl.output %[[a1:.+]], %[[b1:.+]] : i32, i32
+        @rtl.RTLModuleOp.from_py_func(i32, i32)
+        def top(a, b):
+            a, b = swap(a, b)
+            a, b = swap(a, b)
+            return a, b
+
     m.print()
