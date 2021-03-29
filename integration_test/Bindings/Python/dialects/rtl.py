f6f2d7c0c4c1d424284627e46706ee7d45b253ef
mikeurbach <mikeurbach@gmail.com>
2021-03-29 11:49:29 -0600

    [Python] Add Python builder for RTLModuleOp. (#824)
    This is done using MLIR's Python extension approach. This is
    documented here: https://mlir.llvm.org/docs/Bindings/Python/#extending-generated-op-classes
    
    This particular extension is basically a copy of the builtin FuncOp
    builder from upstream MLIR, with some RTLModuleOp flavored tweaks. It
    is by no means the final state, but this is a decent starting point to
    be able to build RTLModuleOps in a somewhat decent way from Python.
    
    See the upstream extension for cross-reference: https://github.com/llvm/llvm-project/blob/32f252a765d5f76cd1c84c20d55482ab4c86dfe0/mlir/lib/Bindings/Python/mlir/dialects/_builtin_ops_ext.py#L101

--- integration_test/Bindings/Python/dialects/rtl.py
+++ integration_test/Bindings/Python/dialects/rtl.py
@@ -5,12 +5,39 @@ import circt
 from circt.dialects import rtl
 
 from mlir.ir import *
+from mlir.dialects import builtin
 
 with Context() as ctx, Location.unknown():
     circt.register_dialects(ctx)
-    i32 = IntegerType.get_signless(32)
-    a50 = IntegerAttr.get(i32, 50)
-    op = rtl.ConstantOp(i32, a50)
 
-    # CHECK: %{{.+}} = rtl.constant 50 : i32
-    op.print()
+    i32 = IntegerType.get_signless(32)
+
+    m = builtin.ModuleOp()
+    with InsertionPoint(m.body):
+        # CHECK: rtl.module @MyWidget(%my_input: i32) -> (%my_output: i32)
+        # CHECK:   rtl.output %my_input : i32
+        op = rtl.RTLModuleOp(
+            name='MyWidget',
+            input_ports=[('my_input', i32)],
+            output_ports=[('my_output', i32)],
+            body_builder=lambda module: rtl.OutputOp(
+                [module.entry_block.arguments[0]])
+        )
+
+        # CHECK: rtl.module @swap(%a: i32, %b: i32) -> (i32, i32)
+        # CHECK:   rtl.output %b, %a : i32, i32
+        @rtl.RTLModuleOp.from_py_func(i32, i32)
+        def swap(a, b):
+            return b, a
+
+        # CHECK: rtl.module @top(%a: i32, %b: i32) -> (i32, i32)
+        # CHECK:   %[[a0:.+]], %[[b0:.+]] = rtl.instance "" @swap(%a, %b)
+        # CHECK:   %[[a1:.+]], %[[b1:.+]] = rtl.instance "" @swap(%[[a0]], %[[b0]])
+        # CHECK:   rtl.output %[[a1:.+]], %[[b1:.+]] : i32, i32
+        @rtl.RTLModuleOp.from_py_func(i32, i32)
+        def top(a, b):
+            a, b = swap(a, b)
+            a, b = swap(a, b)
+            return a, b
+
+    m.print()
