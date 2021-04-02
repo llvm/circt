252cc0e7315a5f740cd5a22922bc0498ab1e1c04
Chris Lattner <clattner@nondot.org>
2021-04-01 22:38:23 -0700

    [FIRRTL] Make InstanceOp verification O(n) in # ports
    getPortNamed is O(n) in number of ports, so calling it N times in a
    loop is O(n^2).  Eliminate this by building an index once.

--- lib/Dialect/FIRRTL/FIRRTLOps.cpp
+++ lib/Dialect/FIRRTL/FIRRTLOps.cpp
@@ -639,9 +639,16 @@ static LogicalResult verifyInstanceOp(InstanceOp instance) {
     return failure();
   }
 
+  // Build an efficient lookup that maps port name attribute to result #.
+  llvm::SmallDenseMap<Attribute, size_t, 16> portNumbers;
+  auto namesArray = instance.portNames();
+  for (size_t i = 0, e = namesArray.size(); i != e; ++i)
+    portNumbers[namesArray[i]] = i;
+
   for (size_t i = 0; i != numResults; i++) {
-    auto result = instance.getPortNamed(modulePorts[i].name);
-    if (!result) {
+    auto resultNumberIt = portNumbers.find(modulePorts[i].name);
+    if (resultNumberIt == portNumbers.end() ||
+        resultNumberIt->second >= numResults) {
       auto diag = instance.emitOpError()
                   << "is missing a port named '" << modulePorts[i].name
                   << "' expected by referenced module";
@@ -649,6 +656,7 @@ static LogicalResult verifyInstanceOp(InstanceOp instance) {
           << "original module declared here";
       return failure();
     }
+    auto result = instance.getResult(resultNumberIt->second);
 
     auto resultType = result.getType();
     auto expectedType = FlipType::get(modulePorts[i].type);
