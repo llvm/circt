6456ace1ad8eabaf06e51060331b0724ef40a8c3
John Demme <john.demme@microsoft.com>
2021-04-19 20:20:12 -0700

    [Python] Register passes, export_verilog, and stack traces
    - Registers the SV passes when the 'circt' Python module is imported, and the
      ESI passes when 'circt.esi' is imported.
    - Adds a Python 'export_verilog' call.
    - Enables LLVM 'pretty' stack traces for easier debugging. Makes assertion
      messages appear in Python instead of just crashing with no output.

--- lib/Bindings/Python/CIRCTModule.cpp
+++ lib/Bindings/Python/CIRCTModule.cpp
@@ -12,14 +12,53 @@
 #include "circt-c/Dialect/ESI.h"
 #include "circt-c/Dialect/RTL.h"
 #include "circt-c/Dialect/SV.h"
+#include "circt-c/ExportVerilog.h"
 #include "mlir-c/Bindings/Python/Interop.h"
 #include "mlir-c/Registration.h"
 
+#include "llvm-c/ErrorHandling.h"
+
+#include "PybindUtils.h"
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
+static void registerPasses() { registerSVPasses(); }
+
+/// Taken from PybindUtils.h in MLIR
+/// Accumulates into a python file-like object, either writing text (default)
+/// or binary.
+class PyFileAccumulator {
+public:
+  PyFileAccumulator(pybind11::object fileObject, bool binary)
+      : pyWriteFunction(fileObject.attr("write")), binary(binary) {}
+
+  void *getUserData() { return this; }
+
+  MlirStringCallback getCallback() {
+    return [](MlirStringRef part, void *userData) {
+      pybind11::gil_scoped_acquire();
+      PyFileAccumulator *accum = static_cast<PyFileAccumulator *>(userData);
+      if (accum->binary) {
+        // Note: Still has to copy and not avoidable with this API.
+        pybind11::bytes pyBytes(part.data, part.length);
+        accum->pyWriteFunction(pyBytes);
+      } else {
+        pybind11::str pyStr(part.data,
+                            part.length); // Decodes as UTF-8 by default.
+        accum->pyWriteFunction(pyStr);
+      }
+    };
+  }
+
+private:
+  pybind11::object pyWriteFunction;
+  bool binary;
+};
+
 PYBIND11_MODULE(_circt, m) {
   m.doc() = "CIRCT Python Native Extension";
+  registerPasses();
+  LLVMEnablePrettyStackTrace();
 
   m.def(
       "register_dialects",
@@ -46,6 +85,12 @@ PYBIND11_MODULE(_circt, m) {
         mlirDialectHandleLoadDialect(sv, context);
       },
       "Register CIRCT dialects on a PyMlirContext.");
+
+  m.def("export_verilog", [](MlirModule mod, py::object fileObject) {
+    PyFileAccumulator accum(fileObject, false);
+    py::gil_scoped_release();
+    mlirExportVerilog(mod, accum.getCallback(), accum.getUserData());
+  });
 
   py::module esi = m.def_submodule("esi", "ESI API");
   circt::python::populateDialectESISubmodule(esi);
