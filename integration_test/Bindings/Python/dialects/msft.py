eb7b36760bf57cbf2226ca43bc09f8d047a8559d
John Demme <john.demme@microsoft.com>
2021-04-21 23:52:06 -0700

    Fix test

--- integration_test/Bindings/Python/dialects/msft.py
+++ integration_test/Bindings/Python/dialects/msft.py
@@ -27,7 +27,7 @@ with Context() as ctx, Location.unknown():
   with InsertionPoint.at_block_terminator(top.body.blocks[0]):
     inst = op.create("inst1", {})
     msft.locate(inst.operation, "mem", devtype=msft.M20K, x=50, y=100, num=1)
-    # CHECK: rtl.instance "widget" @MyWidget() {"loc:mem" = #msft.physloc<M20K, 50, 100, 1>, parameters = {}} : () -> ()
+    # CHECK: rtl.instance "inst1" @MyWidget() {"loc:mem" = #msft.physloc<M20K, 50, 100, 1>, parameters = {}} : () -> ()
 
   m.operation.print()
 
@@ -35,5 +35,5 @@ with Context() as ctx, Location.unknown():
   print("=== tcl ===")
 
   # CHECK: proc top_config { parent } {
-  # CHECK:   set_location_assignment M20K_X50_Y100_N1 -to $parent|widget|mem
+  # CHECK:   set_location_assignment M20K_X50_Y100_N1 -to $parent|inst1|mem
   msft.export_tcl(m, sys.stdout)
