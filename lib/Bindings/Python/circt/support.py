bbe57b91a64dea6fadaca7258af1e5988d11376f
Mike Urbach <mikeurbach@gmail.com>
2021-04-24 15:32:06 -0600

    Ensure the temporary, invalidated IR is cleaned up.

--- lib/Bindings/Python/circt/support.py
+++ lib/Bindings/Python/circt/support.py
@@ -24,17 +24,25 @@ class BackedgeBuilder:
 
   def check(self):
     for edge in self.edges:
+      # Build a nice error message about the uninitialized port.
       builder = self.builders[repr(edge)]
-      value = str(edge)
-      instance = str(builder.operation)
-      module = str(builder.module)
+      instance = builder.operation
+      module = builder.module
 
       import re
-      value_ident = re.search("(%\w+) =", value)[1]
-      module_decl = re.search("(rtl.module @.+\))", module)[1]
+      value_ident = re.search("(%\w+) =", str(edge))[1]
+      module_decl = re.search("(rtl.module @.+\))", str(module))[1]
 
       msg = "Uninitialized ports remain in circuit!\n"
       msg += "Port:     " + value_ident + "\n"
-      msg += "Instance: " + instance + "\n"
+      msg += "Instance: " + str(instance) + "\n"
       msg += "Module:   " + module_decl + "\n"
+
+      # Clean up the IR and Python references.
+      self.remove(edge)
+      instance.destroy()
+      del builder
+      del instance
+      del edge
+
       raise RuntimeError(msg)
