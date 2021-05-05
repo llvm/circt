ac4847b61eee5381e01476c7bedd6601a20d5274
mikeurbach <mikeurbach@gmail.com>
2021-05-05 11:06:02 -0600

    [Python][Seq] Add Python bindings to Seq CompReg and tests. (#1008)

--- lib/Bindings/Python/circt/dialects/_ods_common.py
+++ lib/Bindings/Python/circt/dialects/_ods_common.py
@@ -4,4 +4,10 @@
 
 # Generated tablegen dialects expect to be able to find some symbols from
 # the mlir.dialects package.
-from mlir.dialects._ods_common import _cext, segmented_accessor, equally_sized_accessor, extend_opview_class, get_default_loc_context
+from mlir.dialects._ods_common import (
+    _cext,
+    segmented_accessor,
+    equally_sized_accessor,
+    extend_opview_class,
+    get_default_loc_context,
+)
