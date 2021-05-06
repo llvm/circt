510d46e815671ea731b90927c41c499a5092d2ca
John Demme <john.demme@microsoft.com>
2021-05-06 15:56:58 -0700

    [ESI] [Python] Adding a call for export capnp (#1015)

--- integration_test/Bindings/Python/dialects/esi.py
+++ integration_test/Bindings/Python/dialects/esi.py
@@ -9,21 +9,22 @@ from mlir.ir import *
 from mlir.dialects import builtin
 
 from os import path
+import sys
 
 thisDir = path.dirname(__file__)
 
 with Context() as ctxt, Location.unknown():
   circt.register_dialects(ctxt)
-  sys = esi.System()
-  sys.load_mlir(path.join(thisDir, "esi_load1.mlir"))
-  sys.load_mlir(path.join(thisDir, "esi_load2.mlir"))
+  esisys = esi.System()
+  esisys.load_mlir(path.join(thisDir, "esi_load1.mlir"))
+  esisys.load_mlir(path.join(thisDir, "esi_load2.mlir"))
 
   i1 = IntegerType.get_signless(1)
   i32 = IntegerType.get_signless(32)
   i32_chan = esi.ChannelType.get(i32)
-  sys.print()
+  esisys.print()
 
-  with InsertionPoint(sys.body):
+  with InsertionPoint(esisys.body):
     op = rtl.RTLModuleOp(name='MyWidget',
                          input_ports=[('foo', i32), ('foo_valid', i1)],
                          output_ports=[('foo_ready', i1)],
@@ -37,7 +38,7 @@ with Context() as ctxt, Location.unknown():
                                 [module.entry_block.arguments[0]]))
 
   esi.buildWrapper(op.operation, ["foo"])
-  sys.print()
+  esisys.print()
   # CHECK-LABEL:  rtl.module @MyWidget_esi(%foo: !esi.channel<i32>) {
   # CHECK-NEXT:     %rawOutput, %valid = esi.unwrap.vr %foo, %pearl.foo_ready : i32
   # CHECK-NEXT:     %pearl.foo_ready = rtl.instance "pearl" @MyWidget(%rawOutput, %valid) : (i32, i1) -> i1
@@ -47,13 +48,13 @@ with Context() as ctxt, Location.unknown():
   # CHECK-LABEL:  rtl.module @I32Snoop(%foo_in: !esi.channel<i32>) -> (%foo_out: !esi.channel<i32>) {
   # CHECK-NEXT:     rtl.output %foo_in : !esi.channel<i32>
 
-  prod = sys.lookup("IntProducer")
+  prod = esisys.lookup("IntProducer")
   assert (prod is not None)
   prod.print()
   print()  # Newline.
   # CHECK: rtl.module.extern @IntProducer(%clk: i1) -> (%ints: !esi.channel<i32>)
 
-  acc = sys.lookup("IntAccumulator")
+  acc = esisys.lookup("IntAccumulator")
   assert (acc is not None)
   acc.print()
   print()  # Newline.
@@ -67,4 +68,12 @@ with Context() as ctxt, Location.unknown():
   # CHECK: module MyWidget_esi
   # CHECK: module MyWidget
   # CHECK: module I32Snoop
-  sys.print_verilog()
+  esisys.print_verilog()
+
+  print("\n\n=== Capnp ===")
+  # CHECK-LABEL: === Capnp ===
+  # CHECK:       interface CosimDpiServer @0x85e029b5352bcdb5 {
+  # CHECK:         list @0 () -> (ifaces :List(EsiDpiInterfaceDesc));
+  # CHECK:         open @1 [S, T] (iface :EsiDpiInterfaceDesc) -> (iface :EsiDpiEndpoint(S, T));
+
+  esisys.print_cosim_schema(sys.stdout)
