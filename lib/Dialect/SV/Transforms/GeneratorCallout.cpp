0e7d759d7ac32b93e940e2e0ec725eca712b4a34
Prithayan Barua <prithayan@gatech.edu>
2021-05-11 10:56:36 -0400

    1. Randomly generate temporary output file name for the generator program. (Instead of taking a user specified input file name) 2. Use llvm::sys::path::filename and llvm::sys::path::parent_path to extract the executable name and program path. 3. Add emitError for cases when the genertor executable fails.

--- lib/Dialect/SV/Transforms/GeneratorCallout.cpp
+++ lib/Dialect/SV/Transforms/GeneratorCallout.cpp
@@ -14,7 +14,9 @@
 #include "SVPassDetail.h"
 #include "circt/Dialect/SV/SVPasses.h"
 #include "mlir/IR/Builders.h"
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/Path.h"
 #include "llvm/Support/Process.h"
 
 using namespace circt;
@@ -37,40 +39,23 @@ struct RTLGeneratorCalloutPass
 };
 } // end anonymous namespace
 
-// Parse the \p genExec string, to extract the program executable, the path to
-// the executable and all the required arguments from \p genExecArgs.
-llvm::ErrorOr<std::string> static parseProgramArgs(const std::string &genExec) {
-  std::string execName, execPath;
-
-  // TODO: Find a platform independent way to parse the path from the executable
-  // string. Does LLVM have such a method already ? The executable name can
-  // contain one of the following characters.
-  auto pathLoc = genExec.find_last_of("/\\");
-  if (pathLoc != std::string::npos)
-    execPath = genExec.substr(0, pathLoc + 1);
-  else
-    execPath = "";
-  execName = genExec.substr(pathLoc + 1);
-
-  auto generatorExe = llvm::sys::findProgramByName(execName, {execPath});
-  // If the executable was not found in the provided path (when the path is
-  // empty), then search it in the $PATH.
-  if (!generatorExe)
-    generatorExe = llvm::sys::findProgramByName(execName);
-  return generatorExe;
-}
-
 void RTLGeneratorCalloutPass::runOnOperation() {
   ModuleOp root = getOperation();
   SmallVector<StringRef> genOptions;
   StringRef extraGeneratorArgs(genExecArgs);
   extraGeneratorArgs.split(genOptions, ';');
 
-  auto generatorExe = parseProgramArgs(genExecutable);
+  SmallString<32> execName = llvm::sys::path::filename(genExecutable);
+  SmallString<32> execPath = llvm::sys::path::parent_path(genExecutable);
+
+  auto generatorExe = llvm::sys::findProgramByName(execName, {execPath});
+  // If program not found, search it in $PATH.
+  if (!generatorExe)
+    generatorExe = llvm::sys::findProgramByName(execName);
   // If cannot find the executable, then nothing to do, return.
   if (!generatorExe) {
-    root.emitError("\n Error: Cannot find executable " + genExecutable +
-                   " in program path\n");
+    root.emitError("cannot find executable '" + execName +
+                   "' in path '" + execPath + "'");
     return;
   }
   for (auto &op : llvm::make_early_inc_range(root.getBody()->getOperations())) {
@@ -85,11 +70,12 @@ void RTLGeneratorCalloutPass::processGenerator(
   // Get the corresponding schema associated with this generated op.
   auto genSchema =
       dyn_cast<RTLGeneratorSchemaOp>(generatedModuleOp.getGeneratorKindOp());
-  if (!genSchema)
-    return;
 
+  // Ignore the generator op if the schema does not match the user specified
+  // schema name from command line "-schema-name"
   if (genSchema.descriptor().str() != schemaName)
     return;
+
   SmallVector<std::string> generatorArgs;
   // First argument should be the executable name.
   generatorArgs.push_back(generatorExe.str());
@@ -106,35 +92,60 @@ void RTLGeneratorCalloutPass::processGenerator(
   // Assumption: All the options required by the generator program must be
   // present in the schema.
   for (auto attr : genSchema.requiredAttrs()) {
-    // Get the port name from schema.
-    StringRef portName = attr.dyn_cast<StringAttr>().getValue();
-    generatorArgs.push_back("--" + portName.str());
-    // Get the value for the corresponding port name.
-    // Assumption: the value always exists.
-    auto v = generatedModuleOp->getAttr(portName);
-    if (auto intV = v.dyn_cast<IntegerAttr>())
-      generatorArgs.push_back(intV.getValue().toString(10, false));
-    else if (auto strV = v.dyn_cast<StringAttr>())
-      generatorArgs.push_back(strV.getValue().str());
+    if (auto sAttr = attr.dyn_cast<StringAttr>()) {
+      // Get the port name from schema.
+      StringRef portName = sAttr.getValue();
+      generatorArgs.push_back("--" + portName.str());
+      // Get the value for the corresponding port name.
+      auto v = generatedModuleOp->getAttr(portName);
+      if (auto intV = v.dyn_cast<IntegerAttr>())
+        generatorArgs.push_back(intV.getValue().toString(10, false));
+      else if (auto strV = v.dyn_cast<StringAttr>())
+        generatorArgs.push_back(strV.getValue().str());
+      else {
+        generatedModuleOp.emitError(
+            "portname attribute " + portName +
+            " value specified on the rtl.module.generated operation is not "
+            "handled, "
+            "only integer and string types supported.");
+        return;
+      }
+    }
   }
   SmallVector<StringRef> generatorArgStrRef;
   for (const std::string &a : generatorArgs)
     generatorArgStrRef.push_back(a);
 
   std::string errMsg;
+  SmallString<32> genExecOutFileName;
+  auto errCode = llvm::sys::fs::getPotentiallyUniqueTempFileName(
+      "generatorCalloutTemp", StringRef(""), genExecOutFileName);
+  // Default error code is 0.
+  std::error_code ok;
+  if (errCode != ok) {
+    generatedModuleOp.emitError(
+        "cannot generate a unique temporary file name");
+    return;
+  }
   Optional<StringRef> redirects[] = {None, StringRef(genExecOutFileName), None};
   int result = llvm::sys::ExecuteAndWait(
       generatorExe, generatorArgStrRef, /*Env=*/None,
       /*Redirects=*/redirects,
       /*SecondsToWait=*/0, /*MemoryLimit=*/0, &errMsg);
 
-  // TODO: Silently ignore if any error occurs?
-  if (result != 0)
+  if (result != 0) {
+    generatedModuleOp.emitError("execution of '" + generatorExe +
+                                "' failed");
     return;
+  }
 
   auto bufferRead = llvm::MemoryBuffer::getFile(genExecOutFileName);
-  if (!bufferRead || !*bufferRead)
+  if (!bufferRead || !*bufferRead) {
+    generatedModuleOp.emitError("execution of '" + generatorExe +
+                                "' did not produce any output file named '" +
+                                genExecOutFileName + "'");
     return;
+  }
 
   // Only extract the first line from the output.
   auto fileContent = (*bufferRead)->getBuffer().split('\n').first.str();
