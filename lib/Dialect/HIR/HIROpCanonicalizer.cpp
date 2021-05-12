51d598e6f3f0da9039436ebf4deb0dc1beee1028
kingshukmajumder <kingshuk1000@gmail.com>
2021-05-12 21:37:37 +0530

    [HIR] Added op canonicalization.
    Uses the simple-canonicalizer pass to hoist time offsets to separate
    hir.delay ops.

---
+++ lib/Dialect/HIR/HIROpCanonicalizer.cpp
@@ -0,0 +1,70 @@
+//=========- HIROpCanonicalizer.cpp - Canonicalize HIR Ops ----------------===//
+//
+// This file implements op canonicalizers for HIR dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/HIR/HIR.h"
+#include "circt/Dialect/HIR/HIRDialect.h"
+#include "circt/Dialect/HIR/helper.h"
+#include "mlir/IR/PatternMatch.h"
+
+using namespace mlir;
+using namespace hir;
+using namespace llvm;
+
+template <typename OPTYPE>
+static LogicalResult splitOffsetIntoSeparateOp(OPTYPE op,
+                                               PatternRewriter &rewriter) {
+  auto *context = rewriter.getContext();
+  if (!op.offset())
+    return failure();
+
+  Value tstart = rewriter.create<hir::DelayOp>(
+      op.getLoc(), helper::getTimeType(context), op.tstart(), op.offset(),
+      op.tstart(), Value());
+
+  op.tstartMutable().assign(tstart);
+  op.offsetMutable().clear();
+
+  return success();
+}
+
+LogicalResult LoadOp::canonicalize(LoadOp op,
+                                   ::mlir::PatternRewriter &rewriter) {
+  return splitOffsetIntoSeparateOp(op, rewriter);
+}
+
+LogicalResult StoreOp::canonicalize(StoreOp op,
+                                    ::mlir::PatternRewriter &rewriter) {
+  return splitOffsetIntoSeparateOp(op, rewriter);
+}
+
+LogicalResult SendOp::canonicalize(SendOp op,
+                                   ::mlir::PatternRewriter &rewriter) {
+  return splitOffsetIntoSeparateOp(op, rewriter);
+}
+
+LogicalResult RecvOp::canonicalize(RecvOp op,
+                                   ::mlir::PatternRewriter &rewriter) {
+  return splitOffsetIntoSeparateOp(op, rewriter);
+}
+
+LogicalResult ForOp::canonicalize(ForOp op, PatternRewriter &rewriter) {
+  // Currently ForOp requires an offset to generate an efficient circuit.
+  return failure();
+}
+
+LogicalResult UnrollForOp::canonicalize(UnrollForOp op,
+                                        PatternRewriter &rewriter) {
+  return splitOffsetIntoSeparateOp(op, rewriter);
+}
+
+LogicalResult CallOp::canonicalize(CallOp op, PatternRewriter &rewriter) {
+  return splitOffsetIntoSeparateOp(op, rewriter);
+}
+
+LogicalResult YieldOp::canonicalize(YieldOp op,
+                                    ::mlir::PatternRewriter &rewriter) {
+  return splitOffsetIntoSeparateOp(op, rewriter);
+}
--- test/HIR/loop-unroll.mlir
+++ test/HIR/loop-unroll.mlir
@@ -1,7 +1,7 @@
 // RUN: circt-opt %s
 hir.func @convolution at %t(%x : i32){
   %1 = hir.constant(1) : !hir.const
-  %t2 = hir.unroll_for %i = 0 to 3 step 1 iter_time(%ti = %t){
+  %t2 = hir.unroll_for %i = 0 to 3 step 1 iter_time(%ti = %t+%1){
     hir.yield at %ti 
     %64 = hir.constant (64) : !hir.const
     %128 = hir.constant (128) : !hir.const
