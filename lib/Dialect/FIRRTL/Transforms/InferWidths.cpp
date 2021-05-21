llvm/circt

commit 20a1d111cbbf9dc73b4fc641ebd86b8a22021590

[FIRRTL] Add >= 1 width constraint to Mux select
Add a constraint in the InferWidths transform that the Mux select line
is greater than or equal to one.  This fixes a bug exposed by now
allowing unknown widths on the Mux select line introduced in fb94acd3.

Related to #1108.

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

lib/Dialect/FIRRTL/Transforms/InferWidths.cpp
@@ -638,10 +638,8 @@ LogicalResult InferenceMapping::mapOperation(Operation *op) {
       })
 
       .Case<MuxPrimOp>([&](auto op) {
-        // Caveat: The Scala implementation imposes a constraint on the select
-        // signal to be at least 1 bit wide. The FIRRTL MLIR dialect requries
-        // the select signal to be exactly `uint<1>` anyway, so this constraint
-        // is not needed here.
+        auto sel = getExpr(op.sel());
+        solver.addGeqConstraint(dyn_cast<VarExpr>(sel), solver.known(1));
         auto high = getExpr(op.high());
         auto low = getExpr(op.low());
         auto e = solver.max(high, low);

test/Dialect/FIRRTL/infer-widths.mlir
@@ -286,13 +286,15 @@ firrtl.circuit "Foo" {
   }
 
   // CHECK-LABEL: @MuxOp
-  firrtl.module @MuxOp(in %a: !firrtl.uint<1>) {
+  firrtl.module @MuxOp() {
     // CHECK: %0 = firrtl.wire : !firrtl.uint<2>
     // CHECK: %1 = firrtl.wire : !firrtl.uint<3>
-    // CHECK: %2 = firrtl.mux{{.*}} -> !firrtl.uint<3>
+    // CHECK: %2 = firrtl.wire : !firrtl.uint<1>
+    // CHECK: %3 = firrtl.mux{{.*}} -> !firrtl.uint<3>
     %0 = firrtl.wire : !firrtl.uint
     %1 = firrtl.wire : !firrtl.uint
-    %2 = firrtl.mux(%a, %0, %1) : (!firrtl.uint<1>, !firrtl.uint, !firrtl.uint) -> !firrtl.uint
+    %2 = firrtl.wire : !firrtl.uint
+    %3 = firrtl.mux(%2, %0, %1) : (!firrtl.uint, !firrtl.uint, !firrtl.uint) -> !firrtl.uint
     %c1_ui2 = firrtl.constant 1 : !firrtl.uint<2>
     %c2_ui3 = firrtl.constant 2 : !firrtl.uint<3>
     firrtl.connect %0, %c1_ui2 : !firrtl.uint, !firrtl.uint<2>

commit 6be5f63c5dc943f86a2d259fa23e6a63db327f6d

[FIRRTL] Fix Regression on Mux w/ Known Width
Fix a regression introduced in 20a1d111 for width inference involving
the Mux select line.  20a1d111 was yolo-ing a cast causing width
inference to crash if it ran on a Mux with a select line that had a
known width.  tl;dr: I should learn how to use the APIs...

Fix #1108 (again).

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

lib/Dialect/FIRRTL/Transforms/InferWidths.cpp
@@ -639,7 +639,7 @@ LogicalResult InferenceMapping::mapOperation(Operation *op) {
 
       .Case<MuxPrimOp>([&](auto op) {
         auto sel = getExpr(op.sel());
-        solver.addGeqConstraint(dyn_cast<VarExpr>(sel), solver.known(1));
+        constrainTypes(sel, solver.known(1));
         auto high = getExpr(op.high());
         auto low = getExpr(op.low());
         auto e = solver.max(high, low);