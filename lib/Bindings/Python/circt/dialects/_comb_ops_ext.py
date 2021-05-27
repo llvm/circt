cd2f30d5ea254e7dc7449e967bbfa5b58365ba9e
Mike Urbach <mikeurbach@gmail.com>
2021-05-27 12:56:30 -0600

    Add support for the rest of the comb binary ops.

--- lib/Bindings/Python/circt/dialects/_comb_ops_ext.py
+++ lib/Bindings/Python/circt/dialects/_comb_ops_ext.py
@@ -1,7 +1,7 @@
 from circt.support import NamedValueOpView
 
 
-class BinaryOpBuider(NamedValueOpView):
+class BinaryOpBuilder(NamedValueOpView):
 
   def operand_names(self):
     return ["lhs", "rhs"]
@@ -14,4 +14,53 @@ class DivSOp:
 
   @classmethod
   def create(cls, *args, **kwargs):
-    return BinaryOpBuider(cls, *args, **kwargs)
+    return BinaryOpBuilder(cls, *args, **kwargs)
+
+
+class DivUOp:
+
+  @classmethod
+  def create(cls, *args, **kwargs):
+    return BinaryOpBuilder(cls, *args, **kwargs)
+
+
+class ModSOp:
+
+  @classmethod
+  def create(cls, *args, **kwargs):
+    return BinaryOpBuilder(cls, *args, **kwargs)
+
+
+class ModUOp:
+
+  @classmethod
+  def create(cls, *args, **kwargs):
+    return BinaryOpBuilder(cls, *args, **kwargs)
+
+
+class ShlOp:
+
+  @classmethod
+  def create(cls, *args, **kwargs):
+    return BinaryOpBuilder(cls, *args, **kwargs)
+
+
+class ShrSOp:
+
+  @classmethod
+  def create(cls, *args, **kwargs):
+    return BinaryOpBuilder(cls, *args, **kwargs)
+
+
+class ShrUOp:
+
+  @classmethod
+  def create(cls, *args, **kwargs):
+    return BinaryOpBuilder(cls, *args, **kwargs)
+
+
+class SubOp:
+
+  @classmethod
+  def create(cls, *args, **kwargs):
+    return BinaryOpBuilder(cls, *args, **kwargs)
--- lib/Bindings/Python/circt/dialects/comb.py
+++ lib/Bindings/Python/circt/dialects/comb.py
@@ -4,3 +4,100 @@
 
 # Generated tablegen dialects end up in the mlir.dialects package for now.
 from mlir.dialects._comb_ops_gen import *
+
+from circt.support import NamedValueOpView
+
+from mlir.ir import IntegerAttr, IntegerType
+
+
+# Sugar classes for the various possible verions of ICmpOp.
+class ICmpOpBuilder(NamedValueOpView):
+
+  def operand_names(self):
+    return ["lhs", "rhs"]
+
+  def result_names(self):
+    return ["result"]
+
+  def __init__(self, predicate, data_type, input_port_mapping={}, **kwargs):
+    predicate = IntegerAttr.get(IntegerType.get_signless(64), predicate)
+    import circt
+    super().__init__(circt.dialects.comb.ICmpOp, data_type, input_port_mapping,
+                     [predicate], **kwargs)
+
+
+class EqOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(0, *args, **kwargs)
+
+
+class EqOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(0, *args, **kwargs)
+
+
+class NeOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(1, *args, **kwargs)
+
+
+class LtSOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(2, *args, **kwargs)
+
+
+class LeSOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(3, *args, **kwargs)
+
+
+class GtSOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(4, *args, **kwargs)
+
+
+class GeSOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(5, *args, **kwargs)
+
+
+class LtUOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(6, *args, **kwargs)
+
+
+class LeUOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(7, *args, **kwargs)
+
+
+class GtUOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(8, *args, **kwargs)
+
+
+class GeUOp:
+
+  @staticmethod
+  def create(*args, **kwargs):
+    return ICmpOpBuilder(9, *args, **kwargs)
