afbf3f19b9d8499dc101e6f2d8df0eff15f2fc8c
John Demme <john.demme@microsoft.com>
2021-06-07 22:21:17 -0700

    [PyCDE] Pass module parameters as generate function args

--- frontends/PyCDE/src/pycde/module.py
+++ frontends/PyCDE/src/pycde/module.py
@@ -2,7 +2,7 @@
 #  See https://llvm.org/LICENSE.txt for license information.
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from __future__ import annotations
+from .hacks import attribute_to_var
 
 from circt import support
 from circt.dialects import hw
