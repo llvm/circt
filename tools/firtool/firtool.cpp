b6737713adb1fbd82e2afc6a7da1bce49f322e95
Chris Lattner <clattner@nondot.org>
2021-06-14 22:17:36 -0700

    [firtool] Stop disabling multithreading for the parser.  NFC.
    We previously disabled multithreading when parsing the input file
    to save cost on some mutexes etc, since the parser wasn't threaded.
    Now however, the verifier is threaded, and disabling multithreading
    is disabling it as well.
    
    Just stop doing this.  This provides an aggregate speedup for me,
    e.g. shrinking total firtool time from 29.9s to 26.7s on a large
    design (18.6s -> 15.2s in the parser) which is a pretty big speedup.

--- tools/firtool/firtool.cpp
+++ tools/firtool/firtool.cpp
@@ -172,16 +172,6 @@ processBuffer(std::unique_ptr<llvm::MemoryBuffer> ownedBuffer,
     }
   }
 
-  // Nothing in the parser is threaded.  Disable synchronization overhead.
-  auto isMultithreaded = context.isMultithreadingEnabled();
-  context.disableMultithreading();
-
-  // Apply any pass manager command line options.
-  PassManager pm(&context);
-  pm.enableVerifier(verifyPasses);
-  pm.enableTiming(ts);
-  applyPassManagerCLOptions(pm);
-
   OwningModuleRef module;
   if (inputFormat == InputFIRFile) {
     auto parserTimer = ts.nest("FIR Parser");
@@ -196,8 +186,11 @@ processBuffer(std::unique_ptr<llvm::MemoryBuffer> ownedBuffer,
   if (!module)
     return failure();
 
-  // Allow optimizations to run multithreaded.
-  context.enableMultithreading(isMultithreaded);
+  // Apply any pass manager command line options.
+  PassManager pm(&context);
+  pm.enableVerifier(verifyPasses);
+  pm.enableTiming(ts);
+  applyPassManagerCLOptions(pm);
 
   // Width inference creates canonicalization opportunities.
   if (inferWidths)
