2faec05351497048a604ca7696d27bbfa802d8c1
John Demme <john.demme@microsoft.com>
2021-06-16 16:29:26 -0700

    Adding error checking and struct support

--- frontends/PyCDE/src/pycde/support.py
+++ frontends/PyCDE/src/pycde/support.py
@@ -23,5 +23,15 @@ class Value:
     self.value = support.get_value(value)
     self.type = type
 
-  def __getitem__(self, idx):
-    return hw.ArrayGetOp.create(self.value, idx)
+  def __getitem__(self, sub):
+    if isinstance(self.type, hw.ArrayType):
+      idx = int(sub)
+      if idx >= self.type.size:
+        raise ValueError("Subscript out-of-bounds")
+      return hw.ArrayGetOp.create(self.value, idx)
+
+    if isinstance(self.type, hw.StructType):
+      return hw.StructExtractOp.create(self.value, sub)
+
+    raise TypeError(
+        "Subscripting only supported on hw.array and hw.struct types")
