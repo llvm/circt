dd40b638a1704da0208fadfd57274dc200383c87
Fabian Schuiki <fabian@schuiki.ch>
2021-06-16 15:40:50 +0200

    Apply PR feedback, use firrtl::InstanceGraph

--- lib/Dialect/FIRRTL/Transforms/GrandCentralTaps.cpp
+++ lib/Dialect/FIRRTL/Transforms/GrandCentralTaps.cpp
@@ -15,11 +15,13 @@
 #include "circt/Dialect/FIRRTL/FIRRTLOps.h"
 #include "circt/Dialect/FIRRTL/FIRRTLTypes.h"
 #include "circt/Dialect/FIRRTL/FIRRTLVisitors.h"
+#include "circt/Dialect/FIRRTL/InstanceGraph.h"
 #include "circt/Dialect/FIRRTL/Passes.h"
 #include "circt/Dialect/SV/SVOps.h"
 #include "mlir/IR/ImplicitLocOpBuilder.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/TinyPtrVector.h"
 #include "llvm/Support/Allocator.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/FormatVariadic.h"
@@ -30,6 +32,23 @@ using namespace circt;
 using namespace firrtl;
 using mlir::function_like_impl::getArgAttrDict;
 using mlir::function_like_impl::setAllArgAttrDicts;
+
+//===----------------------------------------------------------------------===//
+// PointerLikeTypeTraits
+//===----------------------------------------------------------------------===//
+
+namespace llvm {
+template <>
+struct PointerLikeTypeTraits<InstanceOp> : PointerLikeTypeTraits<Operation *> {
+public:
+  static inline void *getAsVoidPointer(InstanceOp val) {
+    return const_cast<void *>(val.getAsOpaquePointer());
+  }
+  static inline InstanceOp getFromVoidPointer(void *p) {
+    return InstanceOp::getFromOpaquePointer(p);
+  }
+};
+} // end namespace llvm
 
 //===----------------------------------------------------------------------===//
 // Utilities
@@ -70,17 +89,14 @@ struct TappedValue {
   Annotation anno;
 };
 
-/// A data structure that tracks the instances of modules and can provide
-/// absolute paths to these instances.
-struct InstanceGraph {
-  /// The root circuit.
-  CircuitOp circuitOp;
-  /// The main module in the circuit.
-  Operation *mainModule;
-  /// A mapping from a module to all its instances in the design.
-  DenseMap<Operation *, SmallVector<InstanceOp, 1>> moduleInstances;
+/// A data structure that caches and provides absolute paths to module instances
+/// in the IR.
+struct InstancePaths {
+  /// The instance graph of the IR.
+  InstanceGraph &instanceGraph;
 
-  InstanceGraph(CircuitOp circuitOp);
+  explicit InstancePaths(InstanceGraph &instanceGraph)
+      : instanceGraph(instanceGraph) {}
   ArrayRef<InstancePath> getAbsolutePaths(Operation *op);
 
 private:
@@ -103,42 +119,14 @@ struct PortWiring {
 
 } // namespace
 
-InstanceGraph::InstanceGraph(CircuitOp circuitOp) : circuitOp(circuitOp) {
-  mainModule = circuitOp.getMainModule();
-
-  // Gather all instances in the circuit.
-  circuitOp.walk([&](InstanceOp op) {
-    moduleInstances[op.getReferencedModule()].push_back(op);
-  });
-
-#ifndef NDEBUG
-  LLVM_DEBUG(llvm::dbgs() << "Instance graph:\n");
-  for (auto it : moduleInstances) {
-    LLVM_DEBUG(llvm::dbgs()
-               << "- " << it.first->getName() << " "
-               << it.first->getAttrOfType<StringAttr>("sym_name") << "\n");
-    for (auto inst : it.second)
-      LLVM_DEBUG(llvm::dbgs()
-                 << "  - " << inst.nameAttr() << " in \""
-                 << inst->getParentOfType<FModuleOp>().getName() << "\"\n");
-  }
-#endif
-}
-
-ArrayRef<InstancePath> InstanceGraph::getAbsolutePaths(Operation *op) {
-  if (!isa<FModuleOp, FExtModuleOp>(op))
-    return {};
+ArrayRef<InstancePath> InstancePaths::getAbsolutePaths(Operation *op) {
+  assert((isa<FModuleOp, FExtModuleOp>(op))); // extra parens makes parser smile
 
   // If we have reached the circuit root, we're done.
-  if (mainModule == op) {
+  if (op == instanceGraph.getTopLevelNode()->getModule()) {
     static InstancePath empty{};
     return empty; // array with single empty path
   }
-
-  // Otherwise look up the instances of this module.
-  auto it = moduleInstances.find(op);
-  if (it == moduleInstances.end())
-    return {};
 
   // Fast path: hit the cache.
   auto cached = absolutePathsCache.find(op);
@@ -148,11 +136,11 @@ ArrayRef<InstancePath> InstanceGraph::getAbsolutePaths(Operation *op) {
   // For each instance, collect the instance paths to its parent and append the
   // instance itself to each.
   SmallVector<InstancePath, 8> extendedPaths;
-  for (auto inst : it->second) {
-    auto instPaths = getAbsolutePaths(inst->getParentOfType<FModuleOp>());
+  for (auto inst : instanceGraph[op]->uses()) {
+    auto instPaths = getAbsolutePaths(inst->getParent()->getModule());
     extendedPaths.reserve(instPaths.size());
     for (auto path : instPaths) {
-      extendedPaths.push_back(appendInstance(path, inst));
+      extendedPaths.push_back(appendInstance(path, inst->getInstance()));
     }
   }
 
@@ -168,7 +156,7 @@ ArrayRef<InstancePath> InstanceGraph::getAbsolutePaths(Operation *op) {
   return pathList;
 }
 
-InstancePath InstanceGraph::appendInstance(InstancePath path, InstanceOp inst) {
+InstancePath InstancePaths::appendInstance(InstancePath path, InstanceOp inst) {
   size_t n = path.size() + 1;
   auto newPath = allocator.Allocate<InstanceOp>(n);
   std::copy(path.begin(), path.end(), newPath);
@@ -187,30 +175,21 @@ static InstancePath stripCommonPrefix(InstancePath path, InstancePath other) {
 }
 
 //===----------------------------------------------------------------------===//
-// Static information gathered once upfront
+// Static class names
 //===----------------------------------------------------------------------===//
 
-namespace {
-/// Attributes used throughout the annotations.
-struct Strings {
-  MLIRContext *const context;
-  Strings(MLIRContext *context) : context(context) {}
-
-  StringAttr dataTapsClass = StringAttr::get(
-      context, "sifive.enterprise.grandcentral.DataTapsAnnotation");
-  StringAttr memTapClass = StringAttr::get(
-      context, "sifive.enterprise.grandcentral.MemTapAnnotation");
-
-  StringAttr deletedKeyClass = StringAttr::get(
-      context, "sifive.enterprise.grandcentral.DeletedDataTapKey");
-  StringAttr literalKeyClass = StringAttr::get(
-      context, "sifive.enterprise.grandcentral.LiteralDataTapKey");
-  StringAttr referenceKeyClass = StringAttr::get(
-      context, "sifive.enterprise.grandcentral.ReferenceDataTapKey");
-  StringAttr internalKeyClass = StringAttr::get(
-      context, "sifive.enterprise.grandcentral.DataTapModuleSignalKey");
-};
-} // namespace
+static constexpr const char *dataTapsClass =
+    "sifive.enterprise.grandcentral.DataTapsAnnotation";
+static constexpr const char *memTapClass =
+    "sifive.enterprise.grandcentral.MemTapAnnotation";
+static constexpr const char *deletedKeyClass =
+    "sifive.enterprise.grandcentral.DeletedDataTapKey";
+static constexpr const char *literalKeyClass =
+    "sifive.enterprise.grandcentral.LiteralDataTapKey";
+static constexpr const char *referenceKeyClass =
+    "sifive.enterprise.grandcentral.ReferenceDataTapKey";
+static constexpr const char *internalKeyClass =
+    "sifive.enterprise.grandcentral.DataTapModuleSignalKey";
 
 //===----------------------------------------------------------------------===//
 // Pass Infrastructure
@@ -218,8 +197,9 @@ struct Strings {
 
 class GrandCentralTapsPass : public GrandCentralTapsBase<GrandCentralTapsPass> {
   void runOnOperation() override;
+  void gatherAnnotations(Operation *op);
   void processAnnotation(AnnotatedPort &portAnno, AnnotatedExtModule &blackBox,
-                         const Strings &strings, InstanceGraph &instanceGraph);
+                         InstancePaths &instancePaths);
 
 private:
   DenseMap<Attribute, BlockArgument> tappedArgs;
@@ -258,10 +238,6 @@ void GrandCentralTapsPass::runOnOperation() {
 
   memPortIdx.clear();
 
-  // Gather some string attributes in the context to simplify working with the
-  // annotations.
-  Strings strings(&getContext());
-
   // Gather a list of extmodules that have data or mem tap annotations to be
   // expanded.
   SmallVector<AnnotatedExtModule, 4> modules;
@@ -279,9 +255,8 @@ void GrandCentralTapsPass::runOnOperation() {
       // mem tap ones to the list.
       auto annos = AnnotationSet::forPort(extModule, argNum);
       annos.removeAnnotations([&](Annotation anno) {
-        if (anno.isClass(strings.memTapClass, strings.deletedKeyClass,
-                         strings.literalKeyClass, strings.referenceKeyClass,
-                         strings.internalKeyClass)) {
+        if (anno.isClass(memTapClass, deletedKeyClass, literalKeyClass,
+                         referenceKeyClass, internalKeyClass)) {
           result.portAnnos.push_back({argNum, anno});
           return true;
         }
@@ -294,7 +269,7 @@ void GrandCentralTapsPass::runOnOperation() {
     // case, create a filtered array of annotations with them removed.
     AnnotationSet annos(extModule.getOperation());
     annos.removeAnnotations(
-        [&](Annotation anno) { return anno.isClass(strings.dataTapsClass); });
+        [&](Annotation anno) { return anno.isClass(dataTapsClass); });
     result.filteredModuleAnnos = annos.getArrayAttr();
 
     if (!result.portAnnos.empty())
@@ -314,63 +289,14 @@ void GrandCentralTapsPass::runOnOperation() {
     return;
   }
 
-  // Build an instance graph.
-  InstanceGraph instanceGraph(circuitOp);
+  // Build a generator for absolute module and instance paths in the design.
+  InstancePaths instancePaths(getAnalysis<InstanceGraph>());
 
   // Gather the annotated ports and operations throughout the design that we are
   // supposed to tap in one way or another.
   tappedArgs.clear();
   tappedOps.clear();
-  circuitOp.walk([&](Operation *op) {
-    if (auto module = dyn_cast<FModuleOp>(op)) {
-      // Go through the module ports and collect the annotated ones.
-      SmallVector<DictionaryAttr, 8> filteredArgAttrs;
-      filteredArgAttrs.reserve(module.getNumArguments());
-      for (unsigned argNum = 0, e = module.getNumArguments(); argNum < e;
-           ++argNum) {
-        auto annos = AnnotationSet::forPort(module, argNum);
-        if (annos.empty()) {
-          filteredArgAttrs.push_back(getArgAttrDict(module, argNum));
-          continue;
-        }
-
-        // Go through all annotations on this port and extract the interesting
-        // ones.
-        annos.removeAnnotations([&](Annotation anno) {
-          if (anno.isClass(strings.referenceKeyClass)) {
-            assert(
-                tappedArgs.insert({anno.getDict(), module.getArgument(argNum)})
-                    .second &&
-                "ambiguous tap annotation");
-            return true;
-          }
-          return false;
-        });
-        filteredArgAttrs.push_back(
-            annos.applyToPortDictionaryAttr(getArgAttrDict(module, argNum)));
-      }
-      setAllArgAttrDicts(module, filteredArgAttrs);
-    } else {
-      AnnotationSet annos(op);
-      if (annos.empty())
-        return;
-
-      // Go through all annotations on this op and extract the interesting ones.
-      // Note that the way tap annotations are scattered to their targets, we
-      // should never see multiple values or memories annotated with the exact
-      // same annotation (hence the asserts).
-      annos.removeAnnotations([&](Annotation anno) {
-        if (anno.isClass(strings.memTapClass, strings.referenceKeyClass,
-                         strings.internalKeyClass)) {
-          assert(tappedOps.insert({anno.getDict(), op}).second &&
-                 "ambiguous tap annotation");
-          return true;
-        }
-        return false;
-      });
-      annos.applyToOperation(op);
-    }
-  });
+  circuitOp.walk([&](Operation *op) { gatherAnnotations(op); });
 
 #ifndef NDEBUG
   LLVM_DEBUG(llvm::dbgs() << "Tapped values:\n");
@@ -388,7 +314,7 @@ void GrandCentralTapsPass::runOnOperation() {
 
     // As a first step, gather a list of all absolute paths to instances of this
     // black box.
-    auto paths = instanceGraph.getAbsolutePaths(blackBox.extModule);
+    auto paths = instancePaths.getAbsolutePaths(blackBox.extModule);
 #ifndef NDEBUG
     for (auto path : paths)
       LLVM_DEBUG(llvm::dbgs() << "- " << path << "\n");
@@ -399,7 +325,7 @@ void GrandCentralTapsPass::runOnOperation() {
     portWiring.clear();
     portWiring.reserve(blackBox.portAnnos.size());
     for (auto portAnno : blackBox.portAnnos) {
-      processAnnotation(portAnno, blackBox, strings, instanceGraph);
+      processAnnotation(portAnno, blackBox, instancePaths);
     }
 
 #ifndef NDEBUG
@@ -436,9 +362,8 @@ void GrandCentralTapsPass::runOnOperation() {
 
       // Create a new firrtl.module that implements the data tap.
       auto name =
-          StringAttr::get(&getContext(), (Twine(blackBox.extModule.getName()) +
-                                          "_impl_" + llvm::utostr(implIdx++))
-                                             .str());
+          StringAttr::get(&getContext(), Twine(blackBox.extModule.getName()) +
+                                             "_impl_" + Twine(implIdx++));
       LLVM_DEBUG(llvm::dbgs()
                  << "Implementing " << name << " ("
                  << blackBox.extModule.getName() << " for " << path << ")\n");
@@ -497,22 +422,71 @@ void GrandCentralTapsPass::runOnOperation() {
   }
 }
 
+/// Gather the annotations on ports and operations into the `tappedOps` and
+/// `tappedArgs` maps.
+void GrandCentralTapsPass::gatherAnnotations(Operation *op) {
+  if (auto module = dyn_cast<FModuleOp>(op)) {
+    // Go through the module ports and collect the annotated ones.
+    SmallVector<DictionaryAttr, 8> filteredArgAttrs;
+    filteredArgAttrs.reserve(module.getNumArguments());
+    for (unsigned argNum = 0, e = module.getNumArguments(); argNum < e;
+         ++argNum) {
+      auto annos = AnnotationSet::forPort(module, argNum);
+      if (annos.empty()) {
+        filteredArgAttrs.push_back(getArgAttrDict(module, argNum));
+        continue;
+      }
+
+      // Go through all annotations on this port and extract the interesting
+      // ones.
+      annos.removeAnnotations([&](Annotation anno) {
+        if (anno.isClass(referenceKeyClass)) {
+          assert(tappedArgs.insert({anno.getDict(), module.getArgument(argNum)})
+                     .second &&
+                 "ambiguous tap annotation");
+          return true;
+        }
+        return false;
+      });
+      filteredArgAttrs.push_back(
+          annos.applyToPortDictionaryAttr(getArgAttrDict(module, argNum)));
+    }
+    setAllArgAttrDicts(module, filteredArgAttrs);
+  } else {
+    AnnotationSet annos(op);
+    if (annos.empty())
+      return;
+
+    // Go through all annotations on this op and extract the interesting ones.
+    // Note that the way tap annotations are scattered to their targets, we
+    // should never see multiple values or memories annotated with the exact
+    // same annotation (hence the asserts).
+    annos.removeAnnotations([&](Annotation anno) {
+      if (anno.isClass(memTapClass, referenceKeyClass, internalKeyClass)) {
+        assert(tappedOps.insert({anno.getDict(), op}).second &&
+               "ambiguous tap annotation");
+        return true;
+      }
+      return false;
+    });
+    annos.applyToOperation(op);
+  }
+}
+
 void GrandCentralTapsPass::processAnnotation(AnnotatedPort &portAnno,
                                              AnnotatedExtModule &blackBox,
-                                             const Strings &strings,
-                                             InstanceGraph &instanceGraph) {
+                                             InstancePaths &instancePaths) {
   LLVM_DEBUG(llvm::dbgs() << "- Processing port " << portAnno.portNum
                           << " anno " << portAnno.anno.getDict() << "\n");
   auto portName = getModulePortName(blackBox.extModule, portAnno.portNum);
-  auto cls = portAnno.anno.getClassAttr();
   PortWiring wiring = {portAnno.portNum, {}, {}};
 
   // Handle data taps on signals and ports.
-  if (cls == strings.referenceKeyClass) {
+  if (portAnno.anno.isClass(referenceKeyClass)) {
     // Handle block arguments.
     if (auto blockArg = tappedArgs.lookup(portAnno.anno.getDict())) {
       auto parentModule = blockArg.getOwner()->getParentOp();
-      wiring.prefices = instanceGraph.getAbsolutePaths(parentModule);
+      wiring.prefices = instancePaths.getAbsolutePaths(parentModule);
       wiring.suffix =
           getModulePortName(parentModule, blockArg.getArgNumber()).getValue();
       portWiring.push_back(std::move(wiring));
@@ -547,7 +521,7 @@ void GrandCentralTapsPass::processAnnotation(AnnotatedPort &portAnno,
       }
 
       wiring.prefices =
-          instanceGraph.getAbsolutePaths(wire->getParentOfType<FModuleOp>());
+          instancePaths.getAbsolutePaths(wire->getParentOfType<FModuleOp>());
       wiring.suffix = name.getValue();
       portWiring.push_back(std::move(wiring));
       return;
@@ -565,7 +539,7 @@ void GrandCentralTapsPass::processAnnotation(AnnotatedPort &portAnno,
   }
 
   // Handle data taps on black boxes.
-  if (cls == strings.internalKeyClass) {
+  if (portAnno.anno.isClass(internalKeyClass)) {
     auto op = tappedOps.lookup(portAnno.anno.getDict());
     if (!op) {
       blackBox.extModule.emitOpError(
@@ -585,14 +559,14 @@ void GrandCentralTapsPass::processAnnotation(AnnotatedPort &portAnno,
       return;
     }
 
-    wiring.prefices = instanceGraph.getAbsolutePaths(op);
+    wiring.prefices = instancePaths.getAbsolutePaths(op);
     wiring.suffix = internalPath.getValue();
     portWiring.push_back(std::move(wiring));
     return;
   }
 
   // Handle data taps with literals.
-  if (cls == strings.literalKeyClass) {
+  if (portAnno.anno.isClass(literalKeyClass)) {
     blackBox.extModule.emitError(
         "LiteralDataTapKey annotations not yet supported (on port ")
         << portName << ")";
@@ -601,7 +575,7 @@ void GrandCentralTapsPass::processAnnotation(AnnotatedPort &portAnno,
   }
 
   // Handle memory taps.
-  if (cls == strings.memTapClass) {
+  if (portAnno.anno.isClass(memTapClass)) {
     auto op = tappedOps.lookup(portAnno.anno.getDict());
     if (!op) {
       blackBox.extModule.emitOpError(
@@ -634,7 +608,7 @@ void GrandCentralTapsPass::processAnnotation(AnnotatedPort &portAnno,
     // from an external generator are even worse. This needs a special node
     // in the IR that can properly inject the memory array on emission.
     wiring.prefices =
-        instanceGraph.getAbsolutePaths(op->getParentOfType<FModuleOp>());
+        instancePaths.getAbsolutePaths(op->getParentOfType<FModuleOp>());
     (Twine(name.getValue()) + "[" +
      llvm::utostr(memPortIdx[portAnno.anno.getDict()]++) + "]")
         .toVector(wiring.suffix);
