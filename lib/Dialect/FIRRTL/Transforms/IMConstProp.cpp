42ff2ccbdcc94c63657782b3fe2e04a3209c3d22
Chris Lattner <clattner@nondot.org>
2021-06-16 17:47:15 -0700

    [IMConstProp] Switch to InstanceGraph.  NFC.
    IMConstProp was spending most of its time in InstanceOp.getReferencedModule
    on large designs.  InstanceGraph is the right solution to this!
    
    This reduces time spent in IMConstProp from:
    
      ----User Time----  ----Wall Time----  ----Name----
       2.9201 (  5.3%)    2.9201 (  8.8%)    IMConstProp
    
    to:
    
       1.4728 (  2.8%)    1.4728 (  4.9%)    IMConstProp
       0.0658 (  0.1%)    0.0658 (  0.2%)      (A) circt::firrtl::InstanceGraph
    
    On my laptop.

--- lib/Dialect/FIRRTL/Transforms/IMConstProp.cpp
+++ lib/Dialect/FIRRTL/Transforms/IMConstProp.cpp
@@ -9,6 +9,7 @@
 #include "PassDetails.h"
 #include "circt/Dialect/FIRRTL/FIRRTLAnnotations.h"
 #include "circt/Dialect/FIRRTL/FIRRTLAttributes.h"
+#include "circt/Dialect/FIRRTL/InstanceGraph.h"
 #include "circt/Dialect/FIRRTL/Passes.h"
 #include "llvm/ADT/APSInt.h"
 #include "llvm/ADT/TinyPtrVector.h"
@@ -245,6 +246,9 @@ struct IMConstPropPass : public IMConstPropBase<IMConstPropPass> {
   void visitOperation(Operation *op);
 
 private:
+  /// This is the current instance graph for the Circuit.
+  InstanceGraph *instanceGraph = nullptr;
+
   /// This keeps track of the current state of each tracked value.
   DenseMap<Value, LatticeValue> latticeValues;
 
@@ -268,6 +272,8 @@ private:
 // TODO: handle annotations: [[OptimizableExtModuleAnnotation]]
 void IMConstPropPass::runOnOperation() {
   auto circuit = getOperation();
+
+  instanceGraph = &getAnalysis<InstanceGraph>();
 
   // If the top level module is an external module, mark the input ports
   // overdefined.
@@ -302,6 +308,7 @@ void IMConstPropPass::runOnOperation() {
       rewriteModuleBody(module);
 
   // Clean up our state for next time.
+  instanceGraph = nullptr;
   latticeValues.clear();
   executableBlocks.clear();
   resultPortToInstanceResultMapping.clear();
@@ -414,7 +421,8 @@ void IMConstPropPass::markInvalidValueOp(InvalidValueOp invalid) {
 /// enclosing block is marked live.  This sets up the def-use edges for ports.
 void IMConstPropPass::markInstanceOp(InstanceOp instance) {
   // Get the module being reference or a null pointer if this is an extmodule.
-  auto module = dyn_cast<FModuleOp>(instance.getReferencedModule());
+  auto module =
+      dyn_cast<FModuleOp>(instanceGraph->getReferencedModule(instance));
 
   // If this is an extmodule, just remember that any results and inouts are
   // overdefined.
@@ -495,7 +503,8 @@ void IMConstPropPass::visitConnect(ConnectOp connect) {
   // Driving an instance argument port drives the corresponding argument of the
   // referenced module.
   if (auto instance = dest.getDefiningOp<InstanceOp>()) {
-    auto module = dyn_cast<FModuleOp>(instance.getReferencedModule());
+    auto module =
+        dyn_cast<FModuleOp>(instanceGraph->getReferencedModule(instance));
     if (!module)
       return;
 
