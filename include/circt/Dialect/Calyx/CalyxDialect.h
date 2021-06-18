93df8ace03b868deafc9f81ce57abf5da33b0493
cgyurgyik <gyurgyikcp@gmail.com>
2021-06-17 19:10:45 -0700

    address comments pt2.

--- include/circt/Dialect/Calyx/CalyxDialect.h
+++ include/circt/Dialect/Calyx/CalyxDialect.h
@@ -20,7 +20,9 @@
 // Pull in the Dialect definition.
 #include "circt/Dialect/Calyx/CalyxDialect.h.inc"
 
-// Pull in all enum type definitions and utility function declarations.
+// Pull in all enum type definitions, attributes,
+// and utility function declarations.
 #include "circt/Dialect/Calyx/CalyxEnums.h.inc"
+#include "circt/Dialect/Calyx/CalyxAttrs.h.inc"
 
 #endif // CIRCT_DIALECT_CALYX_CALYXDIALECT_H
