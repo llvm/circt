c77c67c231cc314951465766f744773e098097bf
mikeurbach <mikeurbach@gmail.com>
2021-06-29 00:21:40 -0600

    [PyCDE] Clean up module key and use it as name when necessary. (#1339)
    If the user doesn't specify a get_module_name or otherwise set the
    module_name parameter, use the improved module key as the name.
    
    This will skip renaming for: 1) the first time a module with
    parameters is instantiated, and 2) modules that are unparameterized.
    
    To be able to fully skip renaming when all instantiations of a
    parameterized module are the same, we'll either need some global data
    on the side, or to start putting the parameters into the HWModuleOp.

---
+++ frontends/PyCDE/test/module_naming.py
@@ -0,0 +1,55 @@
+# RUN: %PYTHON% %s 2>&1 | FileCheck %s
+
+import pycde
+import circt.dialects.hw
+
+
+@pycde.module
+def Parameterized(param):
+
+  class Module:
+    x = pycde.Input(pycde.types.i1)
+    y = pycde.Output(pycde.types.i1)
+
+    @pycde.generator
+    def construct(mod):
+      return {"y": mod.x}
+
+  return Module
+
+
+@pycde.module
+class UnParameterized:
+  x = pycde.Input(pycde.types.i1)
+  y = pycde.Output(pycde.types.i1)
+
+  @pycde.generator
+  def construct(mod):
+    return {"y": mod.x}
+
+
+class Test(pycde.System):
+  inputs = []
+  outputs = []
+
+  def build(self, top):
+    c1 = circt.dialects.hw.ConstantOp.create(pycde.types.i1, 1)
+    Parameterized(1)(x=c1)
+    Parameterized(1)(x=c1)
+    Parameterized(2)(x=c1)
+    Parameterized(2)(x=c1)
+    UnParameterized(x=c1)
+    UnParameterized(x=c1)
+
+
+# CHECK: hw.module @Module
+# CHECK-NOT: hw.module @Module
+# CHECK: hw.module @Module_1
+# CHECK-NOT: hw.module @Module_1
+# CHECK: hw.module @Module_2
+# CHECK-NOT: hw.module @Module_2
+# CHECK: hw.module @UnParameterized
+# CHECK-NOT: hw.module @UnParameterized
+t = Test()
+t.generate(["construct"])
+t.print()
