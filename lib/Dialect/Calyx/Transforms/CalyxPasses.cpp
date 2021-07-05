c9226d32a52ad2565ab5cc1f47033fe74d204002
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2021-07-05 14:40:25 -0700

    [Calyx] Add scaffolding for passes. (#1370)
    Adds registration and scaffolding for Calyx passes.

---
+++ lib/Dialect/Calyx/Transforms/CalyxPasses.cpp
@@ -0,0 +1,14 @@
+//===- CalyxPasses.cpp - Source file for Calyx passes -----------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Contains the definitions of Calyx passes.
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/Calyx/CalyxPasses.h"
+#include "./PassDetails.h"
