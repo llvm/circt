51fe094541a47ab979f5f55f24ffd7c1d0351f6d
Julian Oppermann <oppermann@esa.tu-darmstadt.de>
2021-07-08 15:48:27 +0200

    [Scheduling] Separate problems and algorithms.

--- include/circt/Scheduling/Algorithms.h
+++ include/circt/Scheduling/Algorithms.h
@@ -1,4 +1,4 @@
-//===- ASAPScheduler.h - As-soon-as-possible list scheduler -----*- C++ -*-===//
+//===- Algorithms.h - Library of scheduling algorithms ----------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,14 +6,14 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file defines an as-soon-as-possible list scheduler for acyclic problems.
+// This file defines a library of scheduling algorithms.
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef CIRCT_SCHEDULING_ASAPSCHEDULER_H
-#define CIRCT_SCHEDULING_ASAPSCHEDULER_H
+#ifndef CIRCT_SCHEDULING_ALGORITHMS_H
+#define CIRCT_SCHEDULING_ALGORITHMS_H
 
-#include "circt/Scheduling/Scheduler.h"
+#include "circt/Scheduling/Problems.h"
 
 namespace circt {
 namespace scheduling {
@@ -21,15 +21,10 @@ namespace scheduling {
 /// This is a simple list scheduler for solving the basic scheduling problem.
 /// Its objective is to assign each operation its earliest possible start time,
 /// or in other words, to schedule each operation as soon as possible (hence the
-/// name).
-///
-/// The dependence graph must not contain cycles.
-struct ASAPScheduler : public virtual Scheduler {
-  using Scheduler::Scheduler;
-  LogicalResult schedule() override;
-};
+/// name). Fails if the dependence graph contains cycles.
+LogicalResult scheduleASAP(Problem &prob);
 
 } // namespace scheduling
 } // namespace circt
 
-#endif // CIRCT_SCHEDULING_ASAPSCHEDULER_H
+#endif // CIRCT_SCHEDULING_ALGORITHMS_H
