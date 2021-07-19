4f61cc47b42a8c931f736c16bcd2f9f17ab70277
John Demme <john.demme@microsoft.com>
2021-07-19 12:22:52 -0700

    Type alias test

--- frontends/PyCDE/test/syntactic_sugar.py
+++ frontends/PyCDE/test/syntactic_sugar.py
@@ -12,6 +12,8 @@ class Top(System):
     obj_to_value([42, 45], dim(types.i8, 2))
     obj_to_value(5, types.i8)
 
+    obj_to_value({"foo": 7}, types.struct({"foo": types.i12}, "bar"))
+
 
 top = Top()
 top.print()
@@ -22,3 +24,6 @@ top.print()
 # CHECK:  %c45_i8 = hw.constant 45 : i8
 # CHECK:  %1 = hw.array_create %c45_i8, %c42_i8 : i8
 # CHECK:  %c5_i8 = hw.constant 5 : i8
+# CHECK:  %c7_i12_0 = hw.constant 7 : i12
+# CHECK:  %2 = hw.struct_create (%c7_i12_0) : !hw.struct<foo: i12>
+# CHECK:  %3 = hw.bitcast %2 : (!hw.struct<foo: i12>) -> !hw.typealias<@pycde::@bar, !hw.struct<foo: i12>>
