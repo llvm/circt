5ae93529c1e7e0614f0684c519996a660cd1c8cc
Hanchen Ye <hanchenye@gmail.com>
2021-07-20 00:21:02 -0500

    add wavedrom visualization, update regress test case

--- integration_test/Dialect/FSM/driver.sv
+++ integration_test/Dialect/FSM/driver.sv
@@ -1,3 +1,6 @@
+// WaveDrom visulization:
+// https://wavedrom.com/editor.html?%7B%20signal%3A%20%5B%0A%20%20%7B%20name%3A%20%22clk%22%2C%20wave%3A%20%27P........%27%20%7D%2C%0A%20%20%7B%7D%2C%0A%20%20%7B%20name%3A%20%22arvalid_i%22%2C%20wave%3A%20%2201x..1x0.%22%20%7D%2C%0A%20%20%7B%20name%3A%20%22arready_o%22%2C%20wave%3A%20%221.0..101.%22%20%7D%2C%0A%20%20%7B%20name%3A%20%22(araddr_i)%22%2C%20wave%3A%20%22x3xxx4xxx%22%20%7D%2C%0A%20%20%7B%20name%3A%20%22arlen_i%22%2C%20wave%3A%20%22x%3Dxxx%3Dxxx%22%2C%20data%3A%20%5B%223%22%2C%20%221%22%5D%20%7D%2C%0A%20%20%7B%7D%2C%0A%20%20%7B%20name%3A%20%22rvalid_o%22%2C%20wave%3A%20%220.1...1.0%22%20%7D%2C%0A%20%20%7B%20name%3A%20%22rready_i%22%2C%20wave%3A%20%22xx1.....x%22%20%7D%2C%0A%20%20%7B%20name%3A%20%22(rdata_o)%22%2C%20wave%3A%20%222.3456782%22%20%7D%2C%0A%20%20%7B%20name%3A%20%22rlast_o%22%2C%20wave%3A%20%220....1010%22%20%7D%0A%5D%7D
+
 module driver();
   logic clk = 0;
   logic rst_n = 0;
