4a3eaa3bc38773ba7ef26a000c15cc4eebe7ef2c
Hanchen Ye <hanchenye@gmail.com>
2021-07-28 11:20:51 -0500

    format

--- include/circt/Conversion/FSMToHW/FSMToHW.h
+++ include/circt/Conversion/FSMToHW/FSMToHW.h
@@ -1,6 +1,8 @@
-//===- FSMToHW.h - FSM to HW conversions ------------------*- C++ -*-===//
+//===- FSMToHW.h - FSM to HW conversions ------------------------*- C++ -*-===//
 //
-// This file declares passes which will convert the FSM dialect to HW and SV.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 
--- include/circt/Dialect/FSM/FSMPasses.h
+++ include/circt/Dialect/FSM/FSMPasses.h
@@ -4,12 +4,6 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===- FSMPasses.h - FSM dialect passess definition file ------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
 //===----------------------------------------------------------------------===//
 
 #ifndef CIRCT_DIALECT_FSM_FSMPASSES_H
