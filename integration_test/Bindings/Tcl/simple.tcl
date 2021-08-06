21bf07eeacf68186159dd850537f38237c1904b6
jenra <a>
2021-08-06 15:33:43 -0400

    Fixed integration test

--- integration_test/Bindings/Tcl/simple.tcl
+++ integration_test/Bindings/Tcl/simple.tcl
@@ -2,5 +2,5 @@
 # RUN: tclsh %s -- %TCL_PATH%
 load [lindex $argv 1]libcirct-tcl[info sharedlibextension]
 
-set circuit [circt load MLIR [lindex $argv 1]../../tcl/simple.mlir]
+set circuit [circt load MLIR [lindex $argv 1]../../../../tcl/simple.mlir]
 puts $circuit
