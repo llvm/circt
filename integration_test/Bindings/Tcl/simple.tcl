c973e62a0019bf0381b10fd2bde4304536ff28e1
jenra <a>
2021-08-09 11:45:53 -0400

    Implemented some of Mike's suggestions

--- integration_test/Bindings/Tcl/simple.tcl
+++ integration_test/Bindings/Tcl/simple.tcl
@@ -1,36 +1,36 @@
 # REQUIRES: bindings_tcl
-# RUN: tclsh %s -- %TCL_PATH%
+# RUN: tclsh %s -- %TCL_PATH% | FileCheck %s
 load [lindex $argv 1]libcirct-tcl[info sharedlibextension]
 
-set circuit [circt load MLIR [lindex $argv 1]../../../../tcl/simple.mlir]
+set circuit [circt load MLIR [lindex $argv 1]../../../../integration_test/Bindings/Tcl/Inputs/simple.mlir]
 puts $circuit
 
-# CHECK: hw.module.extern @ichi(%a: i2, %b: i3) -> (%c: i4, %d: i5)
-# CHECK: 
-# CHECK: hw.module @owo() -> (%owo_result : i32) {
-# CHECK:   %0 = hw.constant 3 : i32
-# CHECK:   hw.output %0 : i32
-# CHECK: }
-# CHECK: hw.module @uwu() -> () { }
-# CHECK: 
-# CHECK: hw.module @nya(%nya_input : i32) -> () {
-# CHECK:   hw.instance "uwu1" @uwu() : () -> ()
-# CHECK: }
-# CHECK: 
-# CHECK: hw.module @test() -> (%test_result : i32) {
-# CHECK:   %myArray1 = sv.wire : !hw.inout<array<42 x i8>>
-# CHECK:   %0 = hw.instance "owo1" @owo() : () -> (i32)
-# CHECK:   hw.instance "nya1" @nya(%0) : (i32) -> ()
-# CHECK:   hw.output %0 : i32
-# CHECK: }
-# CHECK: 
-# CHECK: hw.module @always() -> () {
-# CHECK:   %clock = hw.constant 1 : i1
-# CHECK:   %3 = sv.wire : !hw.inout<i1>
-# CHECK:   %false = hw.constant 0 : i1
-# CHECK:   sv.alwaysff(posedge %clock)  {
-# CHECK:     sv.passign %3, %false : i1
+# CHECK: module  {
+# CHECK:   hw.module.extern @ichi(%a: i2, %b: i3) -> (%c: i4, %d: i5)
+# CHECK:   hw.module @owo() -> (%owo_result: i32) {
+# CHECK:     %c3_i32 = hw.constant 3 : i32
+# CHECK:     hw.output %c3_i32 : i32
 # CHECK:   }
-# CHECK:   hw.output
+# CHECK:   hw.module @uwu() {
+# CHECK:     hw.output
+# CHECK:   }
+# CHECK:   hw.module @nya(%nya_input: i32) {
+# CHECK:     hw.instance "uwu1" @uwu() : () -> ()
+# CHECK:     hw.output
+# CHECK:   }
+# CHECK:   hw.module @test() -> (%test_result: i32) {
+# CHECK:     %myArray1 = sv.wire  : !hw.inout<array<42xi8>>
+# CHECK:     %owo1.owo_result = hw.instance "owo1" @owo() : () -> i32
+# CHECK:     hw.instance "nya1" @nya(%owo1.owo_result) : (i32) -> ()
+# CHECK:     hw.output %owo1.owo_result : i32
+# CHECK:   }
+# CHECK:   hw.module @always() {
+# CHECK:     %true = hw.constant true
+# CHECK:     %0 = sv.wire  : !hw.inout<i1>
+# CHECK:     %false = hw.constant false
+# CHECK:     sv.alwaysff(posedge %true)  {
+# CHECK:       sv.passign %0, %false : i1
+# CHECK:     }
+# CHECK:     hw.output
+# CHECK:   }
 # CHECK: }
-
