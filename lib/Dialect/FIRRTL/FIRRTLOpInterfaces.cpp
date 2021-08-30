c1a0a3449cea82b83f6292fe6358180b6cb65b86
Andrew Lenharth <andrew@lenharth.org>
2021-08-30 09:33:49 -0500

    formating

--- lib/Dialect/FIRRTL/FIRRTLOpInterfaces.cpp
+++ lib/Dialect/FIRRTL/FIRRTLOpInterfaces.cpp
@@ -1,3 +1,15 @@
+//===- FIRRTLOpInterfaces.cpp - Implement the FIRRTL op interfaces --------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implement the FIRRTL operation interfaces.
+//
+//===----------------------------------------------------------------------===//
+
 #include "circt/Dialect/FIRRTL/FIRRTLOps.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "llvm/ADT/StringRef.h"
