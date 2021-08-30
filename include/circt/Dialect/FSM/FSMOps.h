bbecc5fc2b330c82a6b96a85729f559fbfd173de
Hanchen Ye <hanchenye@gmail.com>
2021-08-30 17:28:46 -0500

    [FSM] Add an empty FSM dialect (#1671)

---
+++ include/circt/Dialect/FSM/FSMOps.h
@@ -0,0 +1,17 @@
+//===- FSMOps.h - Definition of FSM dialect operations --------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_FSM_FSMOPS_H
+#define CIRCT_DIALECT_FSM_FSMOPS_H
+
+#include "circt/Dialect/FSM/FSMDialect.h"
+
+#define GET_OP_CLASSES
+#include "circt/Dialect/FSM/FSM.h.inc"
+
+#endif // CIRCT_DIALECT_FSM_FSMOPS_H
