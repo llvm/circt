e6dfaa018b1216a2b7d2588bc4340d5fb5ea6f7c
John Demme <john.demme@microsoft.com>
2021-09-08 21:14:08 -0700

    [PyCDE] Fixing tests for compreg changes

--- frontends/PyCDE/test/syntactic_sugar.py
+++ frontends/PyCDE/test/syntactic_sugar.py
@@ -85,8 +85,8 @@ sys.print()
 # CHECK:  hw.module @pycde.Comple_Ports(%clk: i1, %data_in: !hw.array<3xi32>, %sel: i2, %struct_data_in: !hw.struct<foo: i36>) -> (%a: i32, %b: i32, %c: i32) {
 # CHECK:    %c0_i2 = hw.constant 0 : i2
 # CHECK:    [[REG0:%.+]] = hw.array_get %data_in[%c0_i2] {name = "data_in__0"} : !hw.array<3xi32>
-# CHECK:    [[REGR1:%.+]] = seq.compreg [[REG0]], %clk {name = "data_in__0__reg1"} : i32
-# CHECK:    [[REGR2:%.+]] = seq.compreg [[REGR1]], %clk {name = "data_in__0__reg2"} : i32
+# CHECK:    [[REGR1:%data_in__0__reg1]] = seq.compreg [[REG0]], %clk : i32
+# CHECK:    [[REGR2:%data_in__0__reg2]] = seq.compreg [[REGR1]], %clk : i32
 # CHECK:    [[REG1:%.+]] = hw.array_get %data_in[%sel] : !hw.array<3xi32>
 # CHECK:    [[REG2:%.+]] = hw.struct_extract %struct_data_in["foo"] {name = "struct_data_in__foo"} : !hw.struct<foo: i36>
 # CHECK:    [[REG3:%.+]] = comb.extract [[REG2]] from 0 {name = "struct_data_in__foo_0upto32"} : (i36) -> i32
