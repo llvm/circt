3390221cdd7a977a7feef2ac59674f853b2ea465
Chris Lattner <clattner@nondot.org>
2021-09-19 13:58:00 -0700

    Two files that were supposed to go with the previous patch. :facepalm:

---
+++ include/circt/Dialect/HW/HWAttributes.h
@@ -0,0 +1,18 @@
+//===- HWAttributes.h - Declare HW dialect attributes ------------*- C++-*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_HW_ATTRIBUTES_H
+#define CIRCT_DIALECT_HW_ATTRIBUTES_H
+
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/BuiltinAttributes.h"
+
+#define GET_ATTRDEF_CLASSES
+#include "circt/Dialect/HW/HWAttributes.h.inc"
+
+#endif // CIRCT_DIALECT_HW_ATTRIBUTES_H
---
+++ lib/Dialect/HW/HWAttributes.cpp
@@ -0,0 +1,66 @@
+//===- HWAttributes.cpp - Implement HW attributes -------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/HW/HWAttributes.h"
+#include "circt/Dialect/HW/HWDialect.h"
+#include "circt/Support/LLVM.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+using namespace circt;
+using namespace circt::hw;
+
+//===----------------------------------------------------------------------===//
+// ODS Boilerplate
+//===----------------------------------------------------------------------===//
+
+#define GET_ATTRDEF_CLASSES
+#include "circt/Dialect/HW/HWAttributes.cpp.inc"
+
+void HWDialect::registerAttributes() {
+  addAttributes<
+#define GET_ATTRDEF_LIST
+#include "circt/Dialect/HW/HWAttributes.cpp.inc"
+      >();
+}
+
+Attribute HWDialect::parseAttribute(DialectAsmParser &p, Type type) const {
+  StringRef attrName;
+  Attribute attr;
+  if (p.parseKeyword(&attrName))
+    return Attribute();
+  auto parseResult =
+      generatedAttributeParser(getContext(), p, attrName, type, attr);
+  if (parseResult.hasValue())
+    return attr;
+  p.emitError(p.getNameLoc(), "Unexpected hw attribute '" + attrName + "'");
+  return {};
+}
+
+void HWDialect::printAttribute(Attribute attr, DialectAsmPrinter &p) const {
+  if (succeeded(generatedAttributePrinter(attr, p)))
+    return;
+  llvm_unreachable("Unexpected attribute");
+}
+
+//===----------------------------------------------------------------------===//
+// ParameterRefAttr
+//===----------------------------------------------------------------------===//
+
+Attribute ParameterRefAttr::parse(MLIRContext *ctxt, DialectAsmParser &p,
+                                  Type type) {
+  StringAttr name;
+  if (p.parseLess() || p.parseAttribute(name) || p.parseGreater())
+    return Attribute();
+
+  return ParameterRefAttr::get(ctxt, name, type);
+}
+
+void ParameterRefAttr::print(DialectAsmPrinter &p) const {
+  p << "parameter.ref<" << getName() << ">";
+}
