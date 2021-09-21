728406e122aca25509041a2203f1f1e3985f4b73
Morten Borup Petersen <morten_bp@live.dk>
2021-09-21 18:41:59 +0100

    [SCFToCalyx] Add calyx::ProgramOp conversion [2/13] (#1813)
    * [SCFToCalyx] Add calyx::ProgramOp conversion [2/13]
    
    This commit introduces logic for converting a top-level MLIR 'module' into a calyx::ProgramOp.
    The implementation works around an issue/restriction of the current infrastructure, being that a top-level 'module' cannot be overwritten. see: https://llvm.discourse.group/t/de-privileging-moduleop-in-translation-apis/3733/26
    
    Furthermore, an input program is legalized using a `ConversionTarget`.

--- lib/Conversion/SCFToCalyx/SCFToCalyx.cpp
+++ lib/Conversion/SCFToCalyx/SCFToCalyx.cpp
@@ -13,6 +13,10 @@
 #include "circt/Conversion/SCFToCalyx/SCFToCalyx.h"
 #include "../PassDetail.h"
 #include "circt/Dialect/Calyx/CalyxOps.h"
+#include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
+#include "mlir/Conversion/LLVMCommon/Pattern.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 #include <variant>
 
@@ -21,14 +25,147 @@ using namespace mlir;
 
 namespace circt {
 
+struct ModuleOpConversion : public OpRewritePattern<mlir::ModuleOp> {
+  ModuleOpConversion(MLIRContext *context, StringRef topLevelFunction,
+                     calyx::ProgramOp *programOpOutput)
+      : OpRewritePattern<mlir::ModuleOp>(context),
+        programOpOutput(programOpOutput), topLevelFunction(topLevelFunction) {
+    assert(programOpOutput->getOperation() == nullptr &&
+           "this function will set programOpOutput post module conversion");
+  }
+
+  LogicalResult matchAndRewrite(mlir::ModuleOp moduleOp,
+                                PatternRewriter &rewriter) const override {
+    if (!moduleOp.getOps<calyx::ProgramOp>().empty())
+      return failure();
+
+    rewriter.updateRootInPlace(moduleOp, [&] {
+      // Create ProgramOp
+      rewriter.setInsertionPointAfter(moduleOp);
+      auto programOp = rewriter.create<calyx::ProgramOp>(
+          moduleOp.getLoc(), StringAttr::get(getContext(), topLevelFunction));
+
+      // Inline the module body region
+      rewriter.inlineRegionBefore(moduleOp.getBodyRegion(),
+                                  programOp.getBodyRegion(),
+                                  programOp.getBodyRegion().end());
+
+      // Inlining the body region also removes ^bb0 from the module body
+      // region, so recreate that, before finally inserting the programOp
+      auto moduleBlock = rewriter.createBlock(&moduleOp.getBodyRegion());
+      rewriter.setInsertionPointToStart(moduleBlock);
+      rewriter.insert(programOp);
+      *programOpOutput = programOp;
+    });
+    return success();
+  }
+
+private:
+  calyx::ProgramOp *programOpOutput = nullptr;
+  StringRef topLevelFunction;
+};
+
 //===----------------------------------------------------------------------===//
 // Pass driver
 //===----------------------------------------------------------------------===//
 class SCFToCalyxPass : public SCFToCalyxBase<SCFToCalyxPass> {
 public:
   SCFToCalyxPass() : SCFToCalyxBase<SCFToCalyxPass>() {}
-  void runOnOperation() override{};
+  void runOnOperation() override;
+
+  LogicalResult setTopLevelFunction(mlir::ModuleOp moduleOp,
+                                    std::string &topLevelFunction) {
+    if (!topLevelFunctionOpt.empty()) {
+      if (SymbolTable::lookupSymbolIn(moduleOp, topLevelFunctionOpt) ==
+          nullptr) {
+        moduleOp.emitError() << "Top level function '" << topLevelFunctionOpt
+                             << "' not found in module.";
+        return failure();
+      }
+      topLevelFunction = topLevelFunctionOpt;
+    } else {
+      /// No top level function set; infer top level if the module only contains
+      /// a single function, else, throw error.
+      auto funcOps = moduleOp.getOps<mlir::FuncOp>();
+      if (std::distance(funcOps.begin(), funcOps.end()) == 1)
+        topLevelFunction = (*funcOps.begin()).sym_name().str();
+      else {
+        moduleOp.emitError()
+            << "Module contains multiple functions, but no top level "
+               "function was set. Please see --top-level-function";
+        return failure();
+      }
+    }
+
+    return success();
+  }
+
+  //// Creates a new Calyx program with the contents of the source module
+  /// inlined within.
+  /// Furthermore, this function performs validation on the input function, to
+  /// ensure that we've implemented the capabilities necessary to convert it.
+  LogicalResult createProgram(StringRef topLevelFunction,
+                              calyx::ProgramOp *programOpOut) {
+    // Program legalization - the partial conversion driver will not run unless
+    // some pattern is provided - provide a dummy pattern.
+    struct DummyPattern : public OpRewritePattern<mlir::ModuleOp> {
+      using OpRewritePattern::OpRewritePattern;
+      LogicalResult matchAndRewrite(mlir::ModuleOp,
+                                    PatternRewriter &) const override {
+        return failure();
+      }
+    };
+
+    ConversionTarget target(getContext());
+    target.addLegalDialect<calyx::CalyxDialect>();
+    target.addLegalDialect<scf::SCFDialect>();
+    target.addIllegalDialect<hw::HWDialect>();
+    target.addIllegalDialect<comb::CombDialect>();
+
+    // For loops should have been lowered to while loops
+    target.addIllegalOp<scf::ForOp>();
+
+    // Only accept std operations which we've added lowerings for
+    target.addIllegalDialect<StandardOpsDialect>();
+    target.addLegalOp<AddIOp, SubIOp, CmpIOp, ShiftLeftOp, UnsignedShiftRightOp,
+                      SignedShiftRightOp, AndOp, XOrOp, OrOp, ZeroExtendIOp,
+                      TruncateIOp, CondBranchOp, BranchOp, ReturnOp, ConstantOp,
+                      IndexCastOp>();
+
+    RewritePatternSet legalizePatterns(&getContext());
+    legalizePatterns.add<DummyPattern>(&getContext());
+    DenseSet<Operation *> legalizedOps;
+    if (applyPartialConversion(getOperation(), target,
+                               std::move(legalizePatterns))
+            .failed())
+      return failure();
+
+    // Program conversion
+    RewritePatternSet conversionPatterns(&getContext());
+    conversionPatterns.add<ModuleOpConversion>(&getContext(), topLevelFunction,
+                                               programOpOut);
+    return applyOpPatternsAndFold(getOperation(),
+                                  std::move(conversionPatterns));
+  }
 };
+
+void SCFToCalyxPass::runOnOperation() {
+  std::string topLevelFunction;
+  if (failed(setTopLevelFunction(getOperation(), topLevelFunction))) {
+    signalPassFailure();
+    return;
+  }
+
+  /// Start conversion
+  calyx::ProgramOp programOp;
+  if (failed(createProgram(topLevelFunction, &programOp))) {
+    signalPassFailure();
+    return;
+  }
+  assert(programOp.getOperation() != nullptr &&
+         "programOp should have been set during module "
+         "conversion, if module conversion succeeded.");
+}
 
 //===----------------------------------------------------------------------===//
 // Pass initialization
