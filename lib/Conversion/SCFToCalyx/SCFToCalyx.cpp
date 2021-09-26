0cc39d79ba2e183d506e262fc0d1952117564795
cgyurgyik <gyurgyikcp@gmail.com>
2021-09-26 15:40:08 -0700

    ws

--- lib/Conversion/SCFToCalyx/SCFToCalyx.cpp
+++ lib/Conversion/SCFToCalyx/SCFToCalyx.cpp
@@ -1897,7 +1897,7 @@ public:
   /// results are skipped for Once patterns).
   template <typename TPattern, typename... PatternArgs>
   void addOncePattern(SmallVectorImpl<LoweringPattern> &patterns,
-                      PatternArgs &&... args) {
+                      PatternArgs &&...args) {
     RewritePatternSet ps(&getContext());
     ps.add<TPattern>(&getContext(), partialPatternRes, args...);
     patterns.push_back(
@@ -1906,7 +1906,7 @@ public:
 
   template <typename TPattern, typename... PatternArgs>
   void addGreedyPattern(SmallVectorImpl<LoweringPattern> &patterns,
-                        PatternArgs &&... args) {
+                        PatternArgs &&...args) {
     RewritePatternSet ps(&getContext());
     ps.add<TPattern>(&getContext(), args...);
     patterns.push_back(
