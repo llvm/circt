ef9c910badd2b9455b43a5e97da9ef11010e6198
Mike Urbach <mikeurbach@gmail.com>
2021-09-28 23:13:47 -0600

    Construct and solve CyclicProblem for innermost AffineForOps.
    Only a handful of operator types are defined for now.

--- lib/Conversion/AffineToStaticLogic/AffineToStaticLogic.cpp
+++ lib/Conversion/AffineToStaticLogic/AffineToStaticLogic.cpp
@@ -8,19 +8,151 @@
 
 #include "circt/Conversion/AffineToStaticLogic/AffineToStaticLogic.h"
 #include "../PassDetail.h"
+#include "circt/Analysis/DependenceAnalysis.h"
+#include "circt/Scheduling/Algorithms.h"
+#include "circt/Scheduling/Problems.h"
+#include "mlir/Dialect/Affine/IR/AffineMemoryOpInterfaces.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "llvm/Support/Debug.h"
 
+#define DEBUG_TYPE "affine-to-staticlogic"
+
+using namespace mlir;
 using namespace circt;
+using namespace circt::analysis;
+using namespace circt::scheduling;
 
 namespace {
 
 struct AffineToStaticLogic
     : public AffineToStaticLogicBase<AffineToStaticLogic> {
   void runOnFunction() override;
+
+private:
+  void runOnAffineFor(AffineForOp forOp,
+                      MemoryDependenceAnalysis memoryAnalysis);
 };
 
 } // namespace
 
-void AffineToStaticLogic::runOnFunction() {}
+void AffineToStaticLogic::runOnFunction() {
+  MemoryDependenceAnalysis memoryAnalysis =
+      getAnalysis<MemoryDependenceAnalysis>();
+  getFunction().walk(
+      [&](AffineForOp forOp) { runOnAffineFor(forOp, memoryAnalysis); });
+}
+
+void AffineToStaticLogic::runOnAffineFor(
+    AffineForOp forOp, MemoryDependenceAnalysis memoryAnalysis) {
+  // Only consider innermost AffineForOps.
+  if (isa<AffineForOp>(forOp.getBody()->front()))
+    return;
+
+  // Create a cyclic scheduling problem.
+  CyclicProblem problem(forOp);
+
+  // Load the Calyx operator library into the problem. This is a very minimal
+  // set of combinational and memory operators for now.
+  Problem::OperatorType combOpr = problem.getOrInsertOperatorType("comb");
+  problem.setLatency(combOpr, 0);
+  Problem::OperatorType seqOpr = problem.getOrInsertOperatorType("seq");
+  problem.setLatency(seqOpr, 1);
+
+  forOp.getBody()->walk([&](Operation *op) {
+    problem.insertOperation(op);
+
+    // Affine dialect ops (if, yield, etc.) are treated as combinational.
+    if (isa<AffineDialect>(op->getDialect()))
+      problem.setLinkedOperatorType(op, combOpr);
+
+    // Some known combinational ops.
+    if (isa<AddIOp, AffineReadOpInterface, ConstantOp, memref::AllocaOp>(op))
+      problem.setLinkedOperatorType(op, combOpr);
+
+    // Some known sequential ops.
+    if (isa<AffineWriteOpInterface>(op))
+      problem.setLinkedOperatorType(op, seqOpr);
+  });
+
+  // Insert memory dependences into the problem.
+  forOp.getBody()->walk([&](Operation *op) {
+    ArrayRef<MemoryDependence> dependences = memoryAnalysis.getDependences(op);
+    if (dependences.empty())
+      return;
+
+    for (MemoryDependence memoryDep : dependences) {
+      // Insert a dependence into the problem.
+      Problem::Dependence dep(memoryDep.source, op);
+      assert(succeeded(problem.insertDependence(dep)));
+
+      // Find the greatest distance lower bound from any loop and use that for
+      // this dependence.
+      unsigned distance = 0;
+      for (DependenceComponent comp : memoryDep.dependenceComponents)
+        if (comp.lb.getValue() > distance)
+          distance = comp.lb.getValue();
+
+      problem.setDistance(dep, distance);
+    }
+  });
+
+  // Insert conditional dependences into the problem.
+  forOp.getBody()->walk([&](AffineIfOp op) {
+    // Insert a dependence from the if to all ops in the then region.
+    for (auto &inner : *op.getThenBlock()) {
+      Problem::Dependence dep(op, &inner);
+      assert(succeeded(problem.insertDependence(dep)));
+      problem.setDistance(dep, 1);
+    }
+
+    // Insert a dependence from the if to all ops in the else region.
+    if (op.hasElse()) {
+      for (auto &inner : *op.getElseBlock()) {
+        Problem::Dependence dep(op, &inner);
+        assert(succeeded(problem.insertDependence(dep)));
+        problem.setDistance(dep, 1);
+      }
+    }
+  });
+
+  // Verify and solve the scheduling problem, and optionally debug it.
+#ifndef NDEBUG
+  if (llvm::isCurrentDebugType(DEBUG_TYPE))
+    forOp.getBody()->walk<WalkOrder::PreOrder>([&](Operation *op) {
+      llvm::dbgs() << "Scheduling inputs for " << *op;
+      auto opr = problem.getLinkedOperatorType(op);
+      llvm::dbgs() << "\nopr = " << opr;
+      llvm::dbgs() << "\nlatency = " << problem.getLatency(*opr);
+      for (auto dep : problem.getDependences(op))
+        if (auto distance = problem.getDistance(dep);
+            distance.hasValue() && *distance > 0)
+          llvm::dbgs() << "\ndep = { distance = " << distance
+                       << ", source = " << *dep.getSource() << '}';
+      llvm::dbgs() << "\n\n";
+    });
+#endif
+
+  if (failed(problem.check()))
+    return signalPassFailure();
+
+  auto *anchor = forOp.getBody()->getTerminator();
+  if (failed(scheduleSimplex(problem, anchor)))
+    return signalPassFailure();
+
+#ifndef NDEBUG
+  if (llvm::isCurrentDebugType(DEBUG_TYPE)) {
+    llvm::dbgs() << "Scheduled initiation interval = "
+                 << problem.getInitiationInterval() << "\n\n";
+    forOp.getBody()->walk<WalkOrder::PreOrder>([&](Operation *op) {
+      llvm::dbgs() << "Scheduling outputs for " << *op;
+      llvm::dbgs() << "\nstart = " << problem.getStartTime(op);
+      llvm::dbgs() << "\n\n";
+    });
+  }
+#endif
+}
 
 std::unique_ptr<mlir::Pass> circt::createAffineToStaticLogic() {
   return std::make_unique<AffineToStaticLogic>();
