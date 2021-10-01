4dbf62d44005c2ee4da9324bb09dee1312098aa7
Parker Mitchell <pmitchell@microsoft.com>
2021-10-01 16:41:03 -0700

    Formatting

--- lib/Dialect/MSFT/PlacementDB.cpp
+++ lib/Dialect/MSFT/PlacementDB.cpp
@@ -1,5 +1,4 @@
-//===- PlacementDB.cpp - Implement a device database
-//-------------------------===//
+//===- PlacementDB.cpp - Implement a device database ----------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
