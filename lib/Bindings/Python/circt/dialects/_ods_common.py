c20b50dbee3a92cd1bb3d2578efc428eb29b2751
Daniel Resnick <danielzresnick@gmail.com>
2021-10-11 21:56:14 -0600

    [Python] Fix python formatting

--- lib/Bindings/Python/circt/dialects/_ods_common.py
+++ lib/Bindings/Python/circt/dialects/_ods_common.py
@@ -5,11 +5,5 @@
 # Generated tablegen dialects expect to be able to find some symbols from
 # the mlir.dialects package.
 from mlir.dialects._ods_common import (
-    _cext,
-    segmented_accessor,
-    equally_sized_accessor,
-    extend_opview_class,
-    get_default_loc_context,
-    get_op_result_or_value,
-    get_op_results_or_values
-)
+    _cext, segmented_accessor, equally_sized_accessor, extend_opview_class,
+    get_default_loc_context, get_op_result_or_value, get_op_results_or_values)
