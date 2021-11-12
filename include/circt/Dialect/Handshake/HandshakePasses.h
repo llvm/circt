e81bc63c1f405fc18e06843cc6f0e37866cbdc85
Morten Borup Petersen <morten_bp@live.dk>
2021-11-12 14:31:38 +0000

    [Handshake] Move resolveInstanceGraph out of HandshakeToFIRRTL
    Resolving a top-level module is not exclusively needed by HandshakeToFIRRTL, so make it generally available.

--- include/circt/Dialect/Handshake/HandshakePasses.h
+++ include/circt/Dialect/Handshake/HandshakePasses.h
@@ -14,13 +14,27 @@
 #define CIRCT_DIALECT_HANDSHAKE_HANDSHAKEPASSES_H
 
 #include "circt/Support/LLVM.h"
+#include <map>
 #include <memory>
+#include <set>
 
 namespace circt {
 namespace handshake {
 
-std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>> createHandshakeDotPrintPass();
-std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>> createHandshakeOpCountPass();
+std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>>
+createHandshakeDotPrintPass();
+std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>>
+createHandshakeOpCountPass();
+
+/// Iterates over the handshake::FuncOp's in the program to build an instance
+/// graph. In doing so, we detect whether there are any cycles in this graph, as
+/// well as infer a top function for the design by performing a topological sort
+/// of the instance graph. The result of this sort is placed in sortedFuncs.
+using InstanceGraph = std::map<std::string, std::set<std::string>>;
+LogicalResult resolveInstanceGraph(ModuleOp moduleOp,
+                                   InstanceGraph &instanceGraph,
+                                   std::string &topLevel,
+                                   SmallVectorImpl<std::string> &sortedFuncs);
 
 /// Generate the code for registering passes.
 #define GEN_PASS_REGISTRATION
--- lib/Conversion/HandshakeToFIRRTL/HandshakeToFIRRTL.cpp
+++ lib/Conversion/HandshakeToFIRRTL/HandshakeToFIRRTL.cpp
@@ -15,6 +15,7 @@
 #include "circt/Dialect/FIRRTL/FIRRTLOps.h"
 #include "circt/Dialect/FIRRTL/FIRRTLTypes.h"
 #include "circt/Dialect/Handshake/HandshakeOps.h"
+#include "circt/Dialect/Handshake/HandshakePasses.h"
 #include "circt/Dialect/Handshake/Visitor.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/Utils.h"
@@ -2353,84 +2354,6 @@ private:
   mutable CircuitOp circuitOp;
 };
 
-using InstanceGraph = std::map<std::string, std::set<std::string>>;
-
-/// Iterates over the handshake::FuncOp's in the program to build an instance
-/// graph. In doing so, we detect whether there are any cycles in this graph, as
-/// well as infer a top module for the design by performing a topological sort
-/// of the instance graph. The result of this sort is placed in sortedFuncs.
-static LogicalResult
-resolveInstanceGraph(ModuleOp moduleOp, InstanceGraph &instanceGraph,
-                     std::string &topLevel,
-                     SmallVectorImpl<std::string> &sortedFuncs) {
-  // Create use graph
-  auto walkFuncOps = [&](handshake::FuncOp funcOp) {
-    auto &funcUses = instanceGraph[funcOp.getName().str()];
-    funcOp.walk([&](handshake::InstanceOp instanceOp) {
-      funcUses.insert(instanceOp.getModule().str());
-    });
-  };
-  moduleOp.walk(walkFuncOps);
-
-  // find top-level (and cycles) using a topological sort. Initialize all
-  // instances as candidate top level modules; these will be pruned whenever
-  // they are referenced by another module.
-  std::set<std::string> visited, marked, candidateTopLevel;
-  SmallVector<std::string> cycleTrace;
-  bool cyclic = false;
-  llvm::transform(instanceGraph,
-                  std::inserter(candidateTopLevel, candidateTopLevel.begin()),
-                  [](auto it) { return it.first; });
-  std::function<void(const std::string &, SmallVector<std::string>)> cycleUtil =
-      [&](const std::string &node, SmallVector<std::string> trace) {
-        if (cyclic || visited.count(node))
-          return;
-        trace.push_back(node);
-        if (marked.count(node)) {
-          cyclic = true;
-          cycleTrace = trace;
-          return;
-        }
-        marked.insert(node);
-        for (auto use : instanceGraph[node]) {
-          candidateTopLevel.erase(use);
-          cycleUtil(use, trace);
-        }
-        marked.erase(node);
-        visited.insert(node);
-        sortedFuncs.insert(sortedFuncs.begin(), node);
-      };
-  for (auto it : instanceGraph) {
-    if (visited.count(it.first) == 0)
-      cycleUtil(it.first, {});
-    if (cyclic)
-      break;
-  }
-
-  if (cyclic) {
-    auto err = moduleOp.emitOpError();
-    err << "cannot lower handshake program - cycle "
-           "detected in instance graph (";
-    llvm::interleave(
-        cycleTrace, err, [&](auto node) { err << node; }, "->");
-    err << ").";
-    return err;
-  }
-  assert(!candidateTopLevel.empty() &&
-         "if non-cyclic, there should be at least 1 candidate top level");
-
-  if (candidateTopLevel.size() > 1) {
-    auto err = moduleOp.emitOpError();
-    err << "multiple candidate top-level modules detected (";
-    llvm::interleaveComma(candidateTopLevel, err,
-                          [&](auto topLevel) { err << topLevel; });
-    err << "). Please remove one of these from the source program.";
-    return err;
-  }
-  topLevel = *candidateTopLevel.begin();
-  return success();
-}
-
 namespace {
 class HandshakeToFIRRTLPass
     : public HandshakeToFIRRTLBase<HandshakeToFIRRTLPass> {
@@ -2441,7 +2364,7 @@ public:
 
     // Resolve the instance graph to get a top-level module.
     std::string topLevel;
-    InstanceGraph uses;
+    handshake::InstanceGraph uses;
     SmallVector<std::string> sortedFuncs;
     if (resolveInstanceGraph(op, uses, topLevel, sortedFuncs).failed()) {
       signalPassFailure();
