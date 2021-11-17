29d8f332b6644b7fd3d70eedcfc2aaf9c9427a48
George <GeorgeLyon@users.noreply.github.com>
2021-11-17 11:52:19 -0800

    Fix comment

--- tools/circt-lsp-server/circt-lsp-server.cpp
+++ tools/circt-lsp-server/circt-lsp-server.cpp
@@ -1,4 +1,4 @@
-//===- mlir-lsp-server.cpp - MLIR Language Server -------------------------===//
+//===- circt-lsp-server.cpp - CIRCT Language Server ---------- ------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
