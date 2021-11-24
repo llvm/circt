dc91507b9073683819587f342540d9f3766fe55d
Morten Borup Petersen <morten_bp@live.dk>
2021-11-24 09:52:55 +0000

    Update indexing method

--- lib/Transforms/FlattenMemRefs.cpp
+++ lib/Transforms/FlattenMemRefs.cpp
@@ -38,25 +38,33 @@ static Value flattenIndices(ConversionPatternRewriter &rewriter, Operation *op,
   Value finalIdx = indices.front();
   for (auto memIdx : llvm::enumerate(indices.drop_front())) {
     Value partialIdx = memIdx.value();
+    int64_t indexMulFactor = 1;
+
+    // Calculate the product of the i'th index and the [0:i-1] shape dims.
     for (unsigned i = 0; i <= memIdx.index(); ++i) {
       int64_t dimSize = memrefType.getShape()[i];
-      if (llvm::isPowerOf2_64(dimSize)) {
-        auto constant =
-            rewriter
-                .create<arith::ConstantOp>(
-                    loc, rewriter.getIndexAttr(llvm::Log2_64(dimSize)))
-                .getResult();
-        partialIdx = rewriter.create<arith::ShLIOp>(loc, partialIdx, constant)
-                         .getResult();
-      } else {
-        auto constant =
-            rewriter
-                .create<arith::ConstantOp>(loc, rewriter.getIndexAttr(dimSize))
-                .getResult();
-        partialIdx = rewriter.create<arith::MulIOp>(loc, partialIdx, constant)
-                         .getResult();
-      }
+      indexMulFactor *= dimSize;
     }
+
+    // Multiply product by the current index operand.
+    if (llvm::isPowerOf2_64(indexMulFactor)) {
+      auto constant =
+          rewriter
+              .create<arith::ConstantOp>(
+                  loc, rewriter.getIndexAttr(llvm::Log2_64(indexMulFactor)))
+              .getResult();
+      partialIdx =
+          rewriter.create<arith::ShLIOp>(loc, partialIdx, constant).getResult();
+    } else {
+      auto constant = rewriter
+                          .create<arith::ConstantOp>(
+                              loc, rewriter.getIndexAttr(indexMulFactor))
+                          .getResult();
+      partialIdx =
+          rewriter.create<arith::MulIOp>(loc, partialIdx, constant).getResult();
+    }
+
+    // Sum up with the prior lower dimension accessors.
     auto sumOp = rewriter.create<arith::AddIOp>(loc, finalIdx, partialIdx);
     finalIdx = sumOp.getResult();
   }
@@ -151,6 +159,71 @@ struct CondBranchOpConversion : public OpConversionPattern<mlir::CondBranchOp> {
   }
 };
 
+struct BranchOpConversion : public OpConversionPattern<mlir::BranchOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(mlir::BranchOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    rewriter.replaceOpWithNewOp<mlir::BranchOp>(op, op.dest(),
+                                                adaptor.destOperands());
+    return success();
+  }
+};
+
+struct CallOpConversion : public OpConversionPattern<mlir::CallOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(mlir::CallOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    llvm::SmallVector<Type> convResTypes;
+    if (typeConverter->convertTypes(op.getResultTypes(), convResTypes).failed())
+      return failure();
+    rewriter.replaceOpWithNewOp<mlir::CallOp>(
+        op, adaptor.callee(), convResTypes, adaptor.getOperands());
+    return success();
+  }
+};
+
+template <typename TOp>
+void addGenericLegalityConstraint(ConversionTarget &target) {
+  target.addDynamicallyLegalOp<TOp>([](TOp op) {
+    return !hasMultiDimMemRef(op->getOperands()) &&
+           !hasMultiDimMemRef(op->getResults());
+  });
+}
+
+static void populateFlattenMemRefsLegality(ConversionTarget &target) {
+  target.addLegalDialect<arith::ArithmeticDialect>();
+  target.addDynamicallyLegalOp<memref::AllocOp>(
+      [](memref::AllocOp op) { return isUniDimensional(op.getType()); });
+  target.addDynamicallyLegalOp<memref::StoreOp>(
+      [](memref::StoreOp op) { return op.getIndices().size() == 1; });
+  target.addDynamicallyLegalOp<memref::LoadOp>(
+      [](memref::LoadOp op) { return op.getIndices().size() == 1; });
+
+  addGenericLegalityConstraint<mlir::CondBranchOp>(target);
+  addGenericLegalityConstraint<mlir::BranchOp>(target);
+  addGenericLegalityConstraint<mlir::CallOp>(target);
+  addGenericLegalityConstraint<mlir::ReturnOp>(target);
+
+  target.addDynamicallyLegalOp<mlir::FuncOp>([](mlir::FuncOp op) {
+    auto argsConverted = llvm::none_of(op.getBlocks(), [](auto &block) {
+      return hasMultiDimMemRef(block.getArguments());
+    });
+
+    auto resultsConverted =
+        llvm::all_of(op.getType().getResults(), [](Type type) {
+          if (auto memref = type.dyn_cast<MemRefType>())
+            return isUniDimensional(memref);
+          return true;
+        });
+
+    return argsConverted && resultsConverted;
+  });
+}
+
 struct FlattenMemRefPass : public FlattenMemRefBase<FlattenMemRefPass> {
 public:
   void runOnOperation() override {
@@ -170,37 +243,12 @@ public:
 
     RewritePatternSet patterns(ctx);
     patterns.add<LoadOpConversion, StoreOpConversion, AllocOpConversion,
-                 ReturnOpConversion, CondBranchOpConversion>(ctx);
+                 ReturnOpConversion, CondBranchOpConversion, BranchOpConversion,
+                 CallOpConversion>(typeConverter, ctx);
     populateFuncOpTypeConversionPattern(patterns, typeConverter);
 
     ConversionTarget target(*ctx);
-    target.addLegalDialect<arith::ArithmeticDialect>();
-    target.addDynamicallyLegalOp<memref::AllocOp>(
-        [](memref::AllocOp op) { return isUniDimensional(op.getType()); });
-    target.addDynamicallyLegalOp<memref::StoreOp>(
-        [](memref::StoreOp op) { return op.getIndices().size() == 1; });
-    target.addDynamicallyLegalOp<memref::LoadOp>(
-        [](memref::LoadOp op) { return op.getIndices().size() == 1; });
-    target.addDynamicallyLegalOp<mlir::ReturnOp>(
-        [](mlir::ReturnOp op) { return !hasMultiDimMemRef(op.operands()); });
-    target.addDynamicallyLegalOp<mlir::CondBranchOp>([](mlir::CondBranchOp op) {
-      return !hasMultiDimMemRef(op.trueDestOperands()) &&
-             !hasMultiDimMemRef(op.falseDestOperands());
-    });
-    target.addDynamicallyLegalOp<mlir::FuncOp>([](mlir::FuncOp op) {
-      auto argsConverted = llvm::none_of(op.getBlocks(), [](auto &block) {
-        return hasMultiDimMemRef(block.getArguments());
-      });
-
-      auto resultsConverted =
-          llvm::all_of(op.getType().getResults(), [](Type type) {
-            if (auto memref = type.dyn_cast<MemRefType>())
-              return isUniDimensional(memref);
-            return true;
-          });
-
-      return argsConverted && resultsConverted;
-    });
+    populateFlattenMemRefsLegality(target);
 
     if (applyPartialConversion(getOperation(), target, std::move(patterns))
             .failed()) {
