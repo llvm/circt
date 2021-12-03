81bef298e225d843d411a3020cea8eac61ecdcbe
Morten Borup Petersen <morten_bp@live.dk>
2021-12-03 09:11:35 +0100

    [Handshake] Add buffer removal pass
    A very simple pass which removes any buffers present in the circuit. This pass can come in handy if:
    1. one would like to re-buffer a circuit
    2. one would like to simulate without buffers
    3. one would like to simplify the circuit (for visual/.dot inspection)

--- include/circt/Dialect/Handshake/HandshakePasses.h
+++ include/circt/Dialect/Handshake/HandshakePasses.h
@@ -27,6 +27,7 @@ createHandshakeDotPrintPass();
 std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>>
 createHandshakeOpCountPass();
 std::unique_ptr<mlir::Pass> createHandshakeMaterializeForksSinksPass();
+std::unique_ptr<mlir::Pass> createHandshakeRemoveBuffersPass();
 
 /// Iterates over the handshake::FuncOp's in the program to build an instance
 /// graph. In doing so, we detect whether there are any cycles in this graph, as
