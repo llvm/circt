6c5e0ce771bdbb914bd1f7db67be2e2f3b29289f
Hideto Ueno <uenoku.tokotoko@gmail.com>
2021-12-12 15:52:52 +0900

    [HW] Use StringAttr instead of StringRef in FieldInfo, nfc (#2325)
    * [HW] Use StringAttr instead of StringRef in FieldInfo, nfc
    
    This commit changes to use use StringAttr instead of StringRef
    in FieldInfo to avoid unnecessary copies of field names.
    Changes are mainly about parser/printer and CAPI.
    This commit also changes Struct/UnionTypeImpl to use ArrayRefParameter
    instead of ArrayRefOfSelfAllocationParameter in their tablegen
    definition. And this deletes unnecessary allocators too.

--- lib/Dialect/FIRRTL/FIRRTLOps.cpp
+++ lib/Dialect/FIRRTL/FIRRTLOps.cpp
@@ -2713,14 +2713,14 @@ static LogicalResult verifyHWStructCastOp(HWStructCastOp cast) {
     if (firFields[findex].name.getValue() != hwFields[findex].name)
       return cast.emitError("field names don't match '")
              << firFields[findex].name.getValue() << "', '"
-             << hwFields[findex].name << "'";
+             << hwFields[findex].name.getValue() << "'";
     int64_t firWidth =
         FIRRTLType(firFields[findex].type).getBitWidthOrSentinel();
     int64_t hwWidth = hw::getBitWidth(hwFields[findex].type);
     if (firWidth > 0 && hwWidth > 0 && firWidth != hwWidth)
       return cast.emitError("size of field '")
-             << hwFields[findex].name << "' don't match " << firWidth << ", "
-             << hwWidth;
+             << hwFields[findex].name.getValue() << "' don't match " << firWidth
+             << ", " << hwWidth;
   }
 
   return success();
