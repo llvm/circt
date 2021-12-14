ff698a216fba23a22dd34bb8ae3f8fd93ea95a26
Mike Urbach <mikeurbach@gmail.com>
2021-12-14 13:58:38 -0700

    Add FileCheck.

--- frontends/PyCDE/test/pycde_values.py
+++ frontends/PyCDE/test/pycde_values.py
@@ -1,3 +1,5 @@
+# RUN: %PYTHON% %s | FileCheck %s
+
 import pycde
 from pycde.dialects import comb, hw
 from pycde import dim, module, generator, Input, Output
@@ -13,9 +15,13 @@ def MyModule(SIZE: int):
     @generator
     def construct(mod):
       c1 = hw.ConstantOp(dim(SIZE), 1)
+      # CHECK: %[[EQ:.+]] = comb.icmp eq
       eq = comb.EqOp(c1, mod.inp)
+      # CHECK: %[[A1:.+]] = hw.array_create %[[EQ]], %[[EQ]]
       a1 = hw.ArrayCreateOp([eq, eq])
+      # CHECK: %[[A2:.+]] = hw.array_create %[[EQ]], %[[EQ]]
       a2 = hw.ArrayCreateOp([eq, eq])
+      # CHECK: %[[COMBINED:.+]] = hw.array_concat %[[A1]], %[[A2]]
       combined = hw.ArrayConcatOp(a1, a2)
       mod.out = hw.BitcastOp(dim(SIZE), combined)
 
