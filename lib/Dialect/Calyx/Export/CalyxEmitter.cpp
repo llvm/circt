7e904bdb376a3bbd346b1e9ad5a347789bbe7ffd
Shivam Gupta <shivam98.tkg@gmail.com>
2021-12-24 00:05:28 +0530

    [NFC] Fix a build warning (#2374)
    Fix build warning `suggest parentheses around '&&' within '||'`.

--- lib/Dialect/Calyx/Export/CalyxEmitter.cpp
+++ lib/Dialect/Calyx/Export/CalyxEmitter.cpp
@@ -368,9 +368,8 @@ private:
   /// Recursively emits the Calyx control.
   void emitCalyxControl(Block *body) {
     Operation *parent = body->getParentOp();
-    assert(isa<ControlOp>(parent) ||
-           parent->hasTrait<ControlLike>() &&
-               "This should only be used to emit Calyx Control structures.");
+    assert((isa<ControlOp>(parent) || parent->hasTrait<ControlLike>()) &&
+           "This should only be used to emit Calyx Control structures.");
 
     // Check to see if this is a stand-alone EnableOp, i.e.
     // calyx.control { calyx.enable @G }
