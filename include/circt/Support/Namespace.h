85b24de8cb7f5e8db0a58a7dd50310b1dde4eb7e
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2021-12-22 21:15:23 -0500

    [Support] Move FIRRTL Namespace to Support, NFC
    Move the FIRRTL utility "BaseNamespace" to Support and rename it
    "Namespace".  This class is generally useful for any dialect or
    conversion which needs to generate names on the fly.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

---
+++ include/circt/Support/Namespace.h
@@ -0,0 +1,79 @@
+//===- Namespace.h - Utilities for generating names -------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file provides utilities for generating new names that do not conflict
+// with existing names.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_SUPPORT_NAMESPACE_H
+#define CIRCT_SUPPORT_NAMESPACE_H
+
+#include "circt/Support/LLVM.h"
+#include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/StringSet.h"
+#include "llvm/ADT/Twine.h"
+
+namespace circt {
+
+/// A namespace that is used to store existing names and generate new names in
+/// some scope within the IR. This exists to work around limitations of
+/// SymbolTables. This acts as a base class providing facilities common to all
+/// namespaces implementations.
+class Namespace {
+public:
+  Namespace() {}
+  Namespace(const Namespace &other) = default;
+  Namespace(Namespace &&other) : internal(std::move(other.internal)) {}
+
+  Namespace &operator=(const Namespace &other) = default;
+  Namespace &operator=(Namespace &&other) {
+    internal = std::move(other.internal);
+    return *this;
+  }
+
+  /// Empty the namespace.
+  void clear() { internal.clear(); }
+
+  /// Return a unique name, derived from the input `name`, and add the new name
+  /// to the internal namespace.  There are two possible outcomes for the
+  /// returned name:
+  ///
+  /// 1. The original name is returned.
+  /// 2. The name is given a `_<n>` suffix where `<n>` is a number starting from
+  ///    `_0` and incrementing by one each time.
+  StringRef newName(const Twine &name) {
+    // Special case the situation where there is no name collision to avoid
+    // messing with the SmallString allocation below.
+    llvm::SmallString<64> tryName;
+    auto inserted = internal.insert(name.toStringRef(tryName));
+    if (inserted.second)
+      return inserted.first->getKey();
+
+    // Try different suffixes until we get a collision-free one.
+    size_t i = 0;
+    if (tryName.empty())
+      name.toVector(tryName); // toStringRef may leave tryName unfilled
+    tryName.push_back('_');
+    size_t baseLength = tryName.size();
+    for (;;) {
+      tryName.resize(baseLength);
+      Twine(i++).toVector(tryName); // append integer to tryName
+      auto inserted = internal.insert(tryName);
+      if (inserted.second)
+        return inserted.first->getKey();
+    }
+  }
+
+protected:
+  llvm::StringSet<> internal;
+};
+
+} // namespace circt
+
+#endif // CIRCT_SUPPORT_NAMESPACE_H
