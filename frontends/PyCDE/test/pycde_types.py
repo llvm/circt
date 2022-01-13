a949a76841e8168b07685f22ff447e75ed28af83
John Demme <john.demme@microsoft.com>
2022-01-13 11:18:16 -0800

    [PyCDE] Improving the PyCDE type system (#2452)
    No longer inherit the specific type, which involved some black magic.
    Move to a more sane class hierarchy and base everything around the new
    PyCDE type class hierarchy. Eventually, we'd like to have `Type` be the
    root of the hierarchy, but inheriting from `mlir.ir.Type` makes the
    transition less painful.
    
    Enables using the standard python function overrides to both print more
    reasonable type strings and avoid some of the `isinstance` switch-like
    structures elsewhere. More of the latter to come.

--- frontends/PyCDE/test/pycde_types.py
+++ frontends/PyCDE/test/pycde_types.py
@@ -1,55 +1,59 @@
-# RUN: %PYTHON% %s 2>&1 | FileCheck %s
+# RUN: %PYTHON% %s 2>err.txt | FileCheck %s
+# RUN: cat err.txt | FileCheck --check-prefix=ERR %s
 
 from pycde import dim, types
-import sys
 
 # CHECK: [('foo', Type(i1)), ('bar', Type(i13))]
-# CHECK: i1
+# ERR: i1
 st1 = types.struct({"foo": types.i1, "bar": types.i13})
-fields = st1.get_fields()
-sys.stderr.write(str(fields) + "\n")
-sys.stderr.flush()
-st1.get_field("foo").dump()
+print(st1.fields)
+st1.foo.dump()
 print()
 
 from mlir.ir import Module
 
-# CHECK: i6
+# ERR: i6
 array1 = dim(types.i6)
 array1.dump()
 print()
+# CHECK: i6
+print(array1)
 
-# CHECK: !hw.array<12xarray<10xi6>>
+# ERR: !hw.array<12xarray<10xi6>>
 array2 = dim(6, 10, 12)
 array2.dump()
 print()
+# CHECK: [12][10]i6
+print(array2)
 
-# CHECK: !hw.typealias<@pycde::@myname1, i8>
+# ERR: !hw.typealias<@pycde::@myname1, i8>
+int_alias = types.int(8, "myname1")
+int_alias.dump()
+print()
+# CHECK: myname1
+print(int_alias)
+
+# ERR: !hw.typealias<@pycde::@myname1, i8>
 int_alias = types.int(8, "myname1")
 int_alias.dump()
 print()
 
-# CHECK: !hw.typealias<@pycde::@myname1, i8>
-int_alias = types.int(8, "myname1")
-int_alias.dump()
-print()
-
-# CHECK: !hw.typealias<@pycde::@myname2, i8>
+# ERR: !hw.typealias<@pycde::@myname2, i8>
 int_alias = types.int(8, "myname2")
 int_alias.dump()
 print()
 
-# CHECK: !hw.typealias<@pycde::@myname3, !hw.array<8xi1>>
+# ERR: !hw.typealias<@pycde::@myname3, !hw.array<8xi1>>
 array_alias = types.array(types.i1, 8, "myname3")
 array_alias.dump()
 print()
 
-# CHECK: !hw.typealias<@pycde::@myname4, !hw.struct<a: i1, b: i1>>
+# ERR: !hw.typealias<@pycde::@myname4, !hw.struct<a: i1, b: i1>>
 struct_alias = types.struct({"a": types.i1, "b": types.i1}, "myname4")
 struct_alias.dump()
 print()
 
-# CHECK: !hw.typealias<@pycde::@myname5, !hw.array<8xi1>
+# ERR: !hw.typealias<@pycde::@myname5, !hw.array<8xi1>
 dim_alias = dim(1, 8, name="myname5")
 dim_alias.dump()
 print()
