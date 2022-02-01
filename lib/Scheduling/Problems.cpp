21926afdf3cb905a65222344ce6963046444f687
Julian Oppermann <oppermann@esa.tu-darmstadt.de>
2022-02-01 08:57:09 +0100

    [Scheduling] Fix comment, NFC.

--- lib/Scheduling/Problems.cpp
+++ lib/Scheduling/Problems.cpp
@@ -1,4 +1,4 @@
-//===- Problems.h - Modeling of scheduling problems -----------------------===//
+//===- Problems.cpp - Modeling of scheduling problems ---------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
