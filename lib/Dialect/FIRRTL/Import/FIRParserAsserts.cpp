llvm/circt

commit f29af0c52562d6c52bf8edd1c319e4f49d82c3df

[FIRRTL] Fix printf-encoded lint issues (#2547)
Fix a bug when parsing certain types of printf-encoded
asserts ("assert:", "assume:", "cover:" and "assertNotX:") to not add an
unnecessary format string argument.  Instead, only add argumnets after
the first argument if any exist.  The latter case is extremely rare (to
use this from the Chisel APIs you would have to hand-craft a printf
operation and not use the built-in helper utilities).

Change Chisel asserts ("Assertion failed:") to always parse as immediate
assertions.  These assertions are expected to have format string
arguments and this avoids a catch-22 with VCS lint warnings.

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

lib/Dialect/FIRRTL/Import/FIRParserAsserts.cpp
@@ -413,24 +413,40 @@ ParseResult circt::firrtl::foldWhenEncodedVerifOp(PrintFOp printOp) {
 
     // TODO: Sanitize the label by replacing whitespace with "_" as done in the
     // Scala impl of ExtractTestCode.
+    ValueRange args;
+    if (printOp.operands().size())
+      args = printOp.operands().drop_front();
+    if (args.size())
+      printOp.emitWarning()
+          << "printf-encoded assertion has format string arguments which may "
+             "cause lint warnings";
     if (flavor == VerifFlavor::Assert || flavor == VerifFlavor::AssertNotX)
       builder.create<AssertOp>(printOp.clock(), predicate, printOp.cond(),
-                               message, printOp.operands(), label, true);
+                               message, args, label, true);
     else if (flavor == VerifFlavor::Assume)
       builder.create<AssumeOp>(printOp.clock(), predicate, printOp.cond(),
-                               message, printOp.operands(), label, true);
+                               message, args, label, true);
     else // VerifFlavor::Cover
       builder.create<CoverOp>(printOp.clock(), predicate, printOp.cond(),
-                              message, printOp.operands(), label, true);
+                              message, args, label, true);
     printOp.erase();
     break;
   }
 
     // Handle the case of builtin Chisel assertions.
+    //
+    // These are historically emitted as non-concurrent assertions and are
+    // expected to have format string arguments.  Parsing these as
+    // non-concurrent assertions works around two VCS lint warnings that arise
+    // if these are emitted as concurrent assertions:
+    //   1. VCS warns about non-sampled values used in the pass/fail statements
+    //      of concurrent assertions.
+    //   2. VCS warns if you sample the values ($sample) indicating that the
+    //      assertion has side effects.
   case VerifFlavor::ChiselAssert: {
     builder.create<AssertOp>(
         printOp.clock(), builder.create<NotPrimOp>(whenStmt.condition()),
-        printOp.cond(), fmt, printOp.operands(), "chisel3_builtin", true);
+        printOp.cond(), fmt, printOp.operands(), "chisel3_builtin", false);
     printOp.erase();
     break;
   }

test/Dialect/FIRRTL/parse-basic.fir
@@ -905,35 +905,32 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
     when cond:
       printf(clock, enable, "assert:foo 0", value)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
-    ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "foo 0"(%value) : !firrtl.uint<42>
+    ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "foo 0" {eventControl = 0 : i32, isConcurrent = true}
 
     when cond:
       printf(clock, enable, "assume:foo 1", value)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
-    ; CHECK-NEXT: firrtl.assume %clock, [[TMP]], %enable, "foo 1"(%value) : !firrtl.uint<42>
+    ; CHECK-NEXT: firrtl.assume %clock, [[TMP]], %enable, "foo 1" {eventControl = 0 : i32, isConcurrent = true}
 
     when cond:
       printf(clock, enable, "cover:foo 2", value)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
-    ; CHECK-NEXT: firrtl.cover %clock, [[TMP]], %enable, "foo 2"(%value) : !firrtl.uint<42>
+    ; CHECK-NEXT: firrtl.cover %clock, [[TMP]], %enable, "foo 2" {eventControl = 0 : i32, isConcurrent = true}
 
     when cond:
       printf(clock, enable, "assert:custom label 0:foo 3", value)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
-    ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "foo 3"(%value) : !firrtl.uint<42>
-    ; CHECK-SAME: name = "custom label 0"
+    ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "foo 3" {eventControl = 0 : i32, isConcurrent = true, name = "custom label 0"}
 
     when cond:
       printf(clock, enable, "assume:custom label 1:foo 4", value)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
-    ; CHECK-NEXT: firrtl.assume %clock, [[TMP]], %enable, "foo 4"(%value) : !firrtl.uint<42>
-    ; CHECK-SAME: name = "custom label 1"
+    ; CHECK-NEXT: firrtl.assume %clock, [[TMP]], %enable, "foo 4" {eventControl = 0 : i32, isConcurrent = true, name = "custom label 1"}
 
     when cond:
       printf(clock, enable, "cover:custom label 2:foo 5", value)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
-    ; CHECK-NEXT: firrtl.cover %clock, [[TMP]], %enable, "foo 5"(%value) : !firrtl.uint<42>
-    ; CHECK-SAME: name = "custom label 2"
+    ; CHECK-NEXT: firrtl.cover %clock, [[TMP]], %enable, "foo 5" {eventControl = 0 : i32, isConcurrent = true, name = "custom label 2"}
 
     ; Optional `stop` with same clock and condition should be removed.
     when cond:
@@ -941,6 +938,11 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
       stop(clock, enable, 1)
     ; CHECK: firrtl.assert %clock, {{%.+}}, %enable, "without_stop"
     ; CHECK-NOT: firrtl.stop
+
+    when cond:
+      ; expected-warning @+1 {{printf-encoded assertion has format string arguments which may cause lint warnings}}
+      printf(clock, enable, "assert:foo 6, %d", value, value)
+    ; CHECK: firrtl.assert {{.+}} "foo 6, %d"(%value)
 
     ; AssertNotX -- usually `cond` only checks for not-in-reset, and `enable` is
     ; just set to 1; the actual check `^value !== 'x` is implicit.
@@ -955,17 +957,19 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
 
     ; Chisel built-in assertions
     when cond:
-      printf(clock, enable, "Assertion failed", value)
+      printf(clock, enable, "Assertion failed with value %d", value)
       stop(clock, enable, 1)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
-    ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "Assertion failed"(%value) : !firrtl.uint<42>
+    ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "Assertion failed with value %d"(%value) : !firrtl.uint<42>
+    ; CHECK-SAME: isConcurrent = false
     ; CHECK-SAME: name = "chisel3_builtin"
 
     when cond:
-      printf(clock, enable, "Assertion failed: some message", value)
+      printf(clock, enable, "Assertion failed: some message with value %d", value)
       stop(clock, enable, 1)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
-    ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "Assertion failed: some message"(%value) : !firrtl.uint<42>
+    ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "Assertion failed: some message with value %d"(%value) : !firrtl.uint<42>
+    ; CHECK-SAME: isConcurrent = false
     ; CHECK-SAME: name = "chisel3_builtin"
 
     ; Verification Library Assertions

commit fa7c79c246a2544aae3bc2e7438fee7740d6f288

[FIRRTL] Revert to Chisel assertions as concurrent
Partially revert changes introduced in f29af0c5 that would parse Chisel
assertions as immediate as opposed to concurrent.  Since CIRCT now
properly wraps assert action block values in $sampled, these can once
again be parsed into concurrent assertions.

Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

lib/Dialect/FIRRTL/Import/FIRParserAsserts.cpp
@@ -435,18 +435,12 @@ ParseResult circt::firrtl::foldWhenEncodedVerifOp(PrintFOp printOp) {
 
     // Handle the case of builtin Chisel assertions.
     //
-    // These are historically emitted as non-concurrent assertions and are
-    // expected to have format string arguments.  Parsing these as
-    // non-concurrent assertions works around two VCS lint warnings that arise
-    // if these are emitted as concurrent assertions:
-    //   1. VCS warns about non-sampled values used in the pass/fail statements
-    //      of concurrent assertions.
-    //   2. VCS warns if you sample the values ($sample) indicating that the
-    //      assertion has side effects.
+    // These are historically emitted as non-concurrent assertions but we choose
+    // to treat them as concurrent.
   case VerifFlavor::ChiselAssert: {
     builder.create<AssertOp>(
         printOp.clock(), builder.create<NotPrimOp>(whenStmt.condition()),
-        printOp.cond(), fmt, printOp.operands(), "chisel3_builtin", false);
+        printOp.cond(), fmt, printOp.operands(), "chisel3_builtin", true);
     printOp.erase();
     break;
   }

test/Dialect/FIRRTL/parse-basic.fir
@@ -961,7 +961,7 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
       stop(clock, enable, 1)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
     ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "Assertion failed with value %d"(%value) : !firrtl.uint<42>
-    ; CHECK-SAME: isConcurrent = false
+    ; CHECK-SAME: isConcurrent = true
     ; CHECK-SAME: name = "chisel3_builtin"
 
     when cond:
@@ -969,7 +969,7 @@ circuit MyModule :     ; CHECK: firrtl.circuit "MyModule" {
       stop(clock, enable, 1)
     ; CHECK: [[TMP:%.+]] = firrtl.not %cond
     ; CHECK-NEXT: firrtl.assert %clock, [[TMP]], %enable, "Assertion failed: some message with value %d"(%value) : !firrtl.uint<42>
-    ; CHECK-SAME: isConcurrent = false
+    ; CHECK-SAME: isConcurrent = true
     ; CHECK-SAME: name = "chisel3_builtin"
 
     ; Verification Library Assertions