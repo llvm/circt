8605a8728a4f9fcfbb70e49ee8fa4460f4f05cc3
Andrew Lenharth <andrew@lenharth.org>
2022-02-11 10:11:25 -0600

    tidy, though under protest

--- lib/Dialect/FIRRTL/Transforms/RemoveResets.cpp
+++ lib/Dialect/FIRRTL/Transforms/RemoveResets.cpp
@@ -42,7 +42,8 @@ static bool isInvalid(Value val) {
           continue;
         val = connect.src();
         return;
-      } else if (auto connect = dyn_cast<StrictConnectOp>(user)) {
+      }
+      if (auto connect = dyn_cast<StrictConnectOp>(user)) {
         if (connect.dest() != val)
           continue;
         val = connect.src();
