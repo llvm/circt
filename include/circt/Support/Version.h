d29dd6d0ea11ed894500c9a1d0eace7e9d3cfb30
sinofp <sinofp@tuta.io>
2022-02-12 11:10:19 +0000

    [Support] Add getCirctVersion
    Version.cpp will be regenerated if circt/.git/logs/HEAD changes,
    so getCirctVersion will reflect your real commit hash.
    
    The output of git describe is similar to Schuyler's second version format.
    
    The tag prefix is proposed by Mike.

---
+++ include/circt/Support/Version.h
@@ -0,0 +1,10 @@
+#ifndef CIRCT_SUPPORT_VERSION_H
+#define CIRCT_SUPPORT_VERSION_H
+
+#include <string>
+
+namespace circt {
+std::string getCirctVersion();
+} // namespace circt
+
+#endif // CIRCT_SUPPORT_VERSION_H
