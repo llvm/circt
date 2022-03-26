2ee6e11009b984fe54cde74a098b71baf86f71ad
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2022-03-25 17:27:26 -0700

    [CHALK] Run clang-format, remove disabled code.

--- include/circt/Dialect/CHALK/CHALKDialect.h
+++ include/circt/Dialect/CHALK/CHALKDialect.h
@@ -1,4 +1,5 @@
-//===- CHALKDialect.h - Definition of CHALK dialect ---------------------------===//
+//===- CHALKDialect.h - Definition of CHALK dialect
+//---------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- include/circt/Dialect/CHALK/CHALKOps.h
+++ include/circt/Dialect/CHALK/CHALKOps.h
@@ -1,4 +1,5 @@
-//===- CHALKOps.h - Definition of CHALK dialect operations --------------------===//
+//===- CHALKOps.h - Definition of CHALK dialect operations
+//--------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
