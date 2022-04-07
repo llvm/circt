03f0d9fd3d3649672f5d236f5280aea2a34fcd71
Nandor Licker <n@ndor.email>
2022-04-07 10:54:54 +0300

    fixed headers

--- include/circt/Dialect/FIRRTL/FIRRTLInstanceGraph.h
+++ include/circt/Dialect/FIRRTL/FIRRTLInstanceGraph.h
@@ -1,4 +1,4 @@
-//===- InstanceGraph.h - Instance graph -------------------------*- C++ -*-===//
+//===- FIRRTLInstanceGraph.h - Instance graph -------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
