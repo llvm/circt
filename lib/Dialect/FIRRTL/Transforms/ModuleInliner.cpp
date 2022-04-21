4cf7bc9a300f9e05ddfc264e99526b4b88d5f93f
Andrew Young <youngar17@gmail.com>
2022-04-21 10:28:55 -0700

    [FIRRTL][ModuleInliner] Don't prefix memory names (#2932)
    A memory operation has a name attribute which is used for both the name
    of the generated module and all instances of that module.  The
    ModuleInliner pass is supposed to prefix the name of instance but not
    the name of the module.  This change stops prefixing the memory, which
    corrects the name of the module, but gives the incorrect instance name.
    The assumption here is that its more important to get the name of the
    memory module correct than the instance.

--- lib/Dialect/FIRRTL/Transforms/ModuleInliner.cpp
+++ lib/Dialect/FIRRTL/Transforms/ModuleInliner.cpp
@@ -517,10 +517,14 @@ bool Inliner::doesNLAMatchCurrentPath(NonLocalAnchor nla) {
 /// these are unique in the namespace.
 void Inliner::rename(StringRef prefix, Operation *op,
                      ModuleNamespace &moduleNamespace) {
-  // Add a prefix to _anything_ that has a "name" attribute.
-  if (auto nameAttr = op->getAttrOfType<StringAttr>("name"))
-    op->setAttr("name", StringAttr::get(op->getContext(),
-                                        (prefix + nameAttr.getValue())));
+  // Add a prefix to things that has a "name" attribute.  We don't prefix
+  // memories since it will affect the name of the generated module.
+  // TODO: We should find a way to prefix the instance of a memory module.
+  if (!isa<MemOp, SeqMemOp, CombMemOp, MemoryPortOp>(op)) {
+    if (auto nameAttr = op->getAttrOfType<StringAttr>("name"))
+      op->setAttr("name", StringAttr::get(op->getContext(),
+                                          (prefix + nameAttr.getValue())));
+  }
 
   // If the operation has an inner symbol, ensure that it is unique.  Record
   // renames for any NLAs that this participates in if the symbol was renamed.
