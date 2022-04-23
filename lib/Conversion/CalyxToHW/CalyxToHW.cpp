1ba341845f6871da66a0bbd308df9655e531f488
Mike Urbach <mikeurbach@gmail.com>
2022-04-23 11:08:31 -0600

    Add basic Component->Module conversion.

--- lib/Conversion/CalyxToHW/CalyxToHW.cpp
+++ lib/Conversion/CalyxToHW/CalyxToHW.cpp
@@ -13,6 +13,7 @@
 #include "circt/Conversion/CalyxToHW.h"
 #include "../PassDetail.h"
 #include "circt/Dialect/Calyx/CalyxOps.h"
+#include "circt/Dialect/HW/HWDialect.h"
 #include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/SV/SVOps.h"
 #include "mlir/Transforms/DialectConversion.h"
@@ -34,6 +35,40 @@ struct ConvertProgramOp : public OpConversionPattern<ProgramOp> {
     rewriter.inlineRegionBefore(program.body(), &mod.getBodyRegion().front());
     rewriter.eraseBlock(&mod.getBodyRegion().back());
     return success();
+  }
+};
+
+struct ConvertComponentOp : public OpConversionPattern<ComponentOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(ComponentOp component, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    SmallVector<hw::PortInfo> hwPortInfo;
+    auto calyxPortInfo = component.getPortInfo();
+    for (auto portInfo : calyxPortInfo)
+      hwPortInfo.push_back(
+          {portInfo.name, hwDirection(portInfo.direction), portInfo.type});
+
+    auto hwMod = rewriter.create<HWModuleOp>(
+        component.getLoc(), component.getNameAttr(), hwPortInfo);
+
+    rewriter.inlineRegionBefore(component.body(), hwMod.getBodyRegion(),
+                                hwMod.getBodyRegion().begin());
+    rewriter.eraseOp(component);
+    rewriter.eraseBlock(&hwMod.getBodyRegion().back());
+
+    return success();
+  }
+
+private:
+  hw::PortDirection hwDirection(calyx::Direction dir) const {
+    switch (dir) {
+    case calyx::Direction::Input:
+      return hw::PortDirection::INPUT;
+    case calyx::Direction::Output:
+      return hw::PortDirection::OUTPUT;
+    }
   }
 };
 
@@ -62,9 +97,12 @@ LogicalResult CalyxToHWPass::runOnProgram(ProgramOp program) {
   ConversionTarget target(context);
   // target.addIllegalDialect<CalyxDialect>();
   target.addIllegalOp<ProgramOp>();
+  target.addIllegalOp<ComponentOp>();
+  target.addLegalDialect<HWDialect>();
 
   RewritePatternSet patterns(&context);
   patterns.add<ConvertProgramOp>(&context);
+  patterns.add<ConvertComponentOp>(&context);
 
   return applyPartialConversion(program, target, std::move(patterns));
 }
