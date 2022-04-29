bb2157269dcc9ce99387e91aed341a413513bda3
Will Dietz <will.dietz@sifive.com>
2022-04-29 13:47:05 -0500

    FIRRTLUtils: enable creating strictconnect's in emitConnect

--- lib/Dialect/FIRRTL/FIRRTLUtils.cpp
+++ lib/Dialect/FIRRTL/FIRRTLUtils.cpp
@@ -33,8 +33,7 @@ void circt::firrtl::emitConnect(ImplicitLocOpBuilder &builder, Value dst,
   // If the types are the exact same we can just connect them.
   if (dstType == srcType) {
     // Strict connect does not allow uninferred widths.
-    // TODO: enable strict connect.
-    if (true || dstType.hasUninferredWidth())
+    if (dstType.hasUninferredWidth())
       builder.create<ConnectOp>(dst, src);
     else
       builder.create<StrictConnectOp>(dst, src);
