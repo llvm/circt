8feae132b7ed4e25dcbd5f189c790bb206f3d717
Morten Borup Petersen <morten_bp@live.dk>
2022-05-05 20:01:36 +0200

    [Calyx] Implement ClkInsertion and ResetInsertion passes (#3047)

--- include/circt/Dialect/Calyx/CalyxPasses.h
+++ include/circt/Dialect/Calyx/CalyxPasses.h
@@ -22,10 +22,10 @@ namespace circt {
 namespace calyx {
 
 std::unique_ptr<mlir::Pass> createCompileControlPass();
-
 std::unique_ptr<mlir::Pass> createGoInsertionPass();
-
 std::unique_ptr<mlir::Pass> createRemoveGroupsPass();
+std::unique_ptr<mlir::Pass> createClkInsertionPass();
+std::unique_ptr<mlir::Pass> createResetInsertionPass();
 
 /// Generate the code for registering passes.
 #define GEN_PASS_REGISTRATION
