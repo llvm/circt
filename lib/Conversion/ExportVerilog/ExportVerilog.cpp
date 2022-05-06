c1e46ef88fc82f7c4ed0bc579c2f869b27683789
Hideto Ueno <uenoku.tokotoko@gmail.com>
2022-05-06 20:04:46 +0900

    Limit expressions

--- lib/Conversion/ExportVerilog/ExportVerilog.cpp
+++ lib/Conversion/ExportVerilog/ExportVerilog.cpp
@@ -538,6 +538,73 @@ static bool isOkToBitSelectFrom(Value v) {
   return false;
 }
 
+/// Return true if we are unable to ever inline the specified operation.  This
+/// happens because not all Verilog expressions are composable, notably you
+/// can only use bit selects like x[4:6] on simple expressions, you cannot use
+/// expressions in the sensitivity list of always blocks, etc.
+static bool isExpressionUnableToInline(Operation *op) {
+  if (auto cast = dyn_cast<BitcastOp>(op))
+    if (!haveMatchingDims(cast.input().getType(), cast.result().getType(),
+                          op->getLoc()))
+      // Bitcasts rely on the type being assigned to, so we cannot inline.
+      return true;
+
+  // StructCreateOp needs to be assigning to a named temporary so that types
+  // are inferred properly by verilog
+  if (isa<StructCreateOp>(op))
+    return true;
+
+  // Verbatim with a long string should be emitted as an out-of-line declration.
+  if (auto verbatim = dyn_cast<VerbatimExprOp>(op))
+    if (verbatim.string().size() > 32)
+      return true;
+
+  // Scan the users of the operation to see if any of them need this to be
+  // emitted out-of-line.
+  for (auto user : op->getUsers()) {
+    // Verilog bit selection is required by the standard to be:
+    // "a vector, packed array, packed structure, parameter or concatenation".
+    //
+    // It cannot be an arbitrary expression, e.g. this is invalid:
+    //     assign bar = {{a}, {b}, {c}, {d}}[idx];
+    //
+    // To handle these, we push the subexpression into a temporary.
+    if (isa<ExtractOp, ArraySliceOp, ArrayGetOp, StructExtractOp>(user))
+      if (op->getResult(0) == user->getOperand(0) && // ignore index operands.
+          !isOkToBitSelectFrom(op->getResult(0)))
+        return true;
+
+    // Always blocks must have a name in their sensitivity list, not an expr.
+    if (isa<AlwaysOp>(user) || isa<AlwaysFFOp>(user)) {
+      // Anything other than a read of a wire must be out of line.
+      if (auto read = dyn_cast<ReadInOutOp>(op))
+        if (read.input().getDefiningOp<WireOp>() ||
+            read.input().getDefiningOp<RegOp>())
+          continue;
+      return true;
+    }
+  }
+  return false;
+}
+
+/// Return true if this expression should be emitted inline into any statement
+/// that uses it.
+static bool isExpressionEmittedInline(Operation *op) {
+  // Never create a temporary which is only going to be assigned to an output
+  // port.
+  if (op->hasOneUse() && isa<hw::OutputOp>(*op->getUsers().begin()))
+    return true;
+
+  // If this operation has multiple uses, we can't generally inline it unless
+  // the op is duplicatable.
+  if (!op->getResult(0).hasOneUse() && !isDuplicatableExpression(op))
+    return false;
+
+  // If it isn't structurally possible to inline this expression, emit it out
+  // of line.
+  return !isExpressionUnableToInline(op);
+}
+
 /// Find a nested IfOp in an else block that can be printed as `else if`
 /// instead of nesting it into a new `begin` - `end` block.  The block must
 /// contain a single IfOp and optionally expressions which can be hoisted out.
@@ -2204,72 +2271,7 @@ SubExprInfo ExprEmitter::visitUnhandledExpr(Operation *op) {
 // NameCollector
 //===----------------------------------------------------------------------===//
 
-/// Return true if we are unable to ever inline the specified operation.  This
-/// happens because not all Verilog expressions are composable, notably you
-/// can only use bit selects like x[4:6] on simple expressions, you cannot use
-/// expressions in the sensitivity list of always blocks, etc.
-static bool isExpressionUnableToInline(Operation *op) {
-  if (auto cast = dyn_cast<BitcastOp>(op))
-    if (!haveMatchingDims(cast.input().getType(), cast.result().getType(),
-                          op->getLoc()))
-      // Bitcasts rely on the type being assigned to, so we cannot inline.
-      return true;
 
-  // StructCreateOp needs to be assigning to a named temporary so that types
-  // are inferred properly by verilog
-  if (isa<StructCreateOp>(op))
-    return true;
-
-  // Verbatim with a long string should be emitted as an out-of-line declration.
-  if (auto verbatim = dyn_cast<VerbatimExprOp>(op))
-    if (verbatim.string().size() > 32)
-      return true;
-
-  // Scan the users of the operation to see if any of them need this to be
-  // emitted out-of-line.
-  for (auto user : op->getUsers()) {
-    // Verilog bit selection is required by the standard to be:
-    // "a vector, packed array, packed structure, parameter or concatenation".
-    //
-    // It cannot be an arbitrary expression, e.g. this is invalid:
-    //     assign bar = {{a}, {b}, {c}, {d}}[idx];
-    //
-    // To handle these, we push the subexpression into a temporary.
-    if (isa<ExtractOp, ArraySliceOp, ArrayGetOp, StructExtractOp>(user))
-      if (op->getResult(0) == user->getOperand(0) && // ignore index operands.
-          !isOkToBitSelectFrom(op->getResult(0)))
-        return true;
-
-    // Always blocks must have a name in their sensitivity list, not an expr.
-    if (isa<AlwaysOp>(user) || isa<AlwaysFFOp>(user)) {
-      // Anything other than a read of a wire must be out of line.
-      if (auto read = dyn_cast<ReadInOutOp>(op))
-        if (read.input().getDefiningOp<WireOp>() ||
-            read.input().getDefiningOp<RegOp>())
-          continue;
-      return true;
-    }
-  }
-  return false;
-}
-
-/// Return true if this expression should be emitted inline into any statement
-/// that uses it.
-static bool isExpressionEmittedInline(Operation *op) {
-  // Never create a temporary which is only going to be assigned to an output
-  // port.
-  if (op->hasOneUse() && isa<hw::OutputOp>(*op->getUsers().begin()))
-    return true;
-
-  // If this operation has multiple uses, we can't generally inline it unless
-  // the op is duplicatable.
-  if (!op->getResult(0).hasOneUse() && !isDuplicatableExpression(op))
-    return false;
-
-  // If it isn't structurally possible to inline this expression, emit it out
-  // of line.
-  return !isExpressionUnableToInline(op);
-}
 
 namespace {
 class NameCollector {
