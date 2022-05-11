llvm/circt

commit 1bcd3f49c2a7d3421e84f7fac3686e46afb9282a

[PrepareForEmission] Reuse wires for output ports (#3088)
This commit changes to reuse existing wires as placeholder of output ports
when output ports are uniquely assigned to a wire. This change reduces
the number of temporary wires in the large design 15%.

lib/Conversion/ExportVerilog/PrepareForEmission.cpp
@@ -94,13 +94,6 @@ static void lowerBoundInstance(InstanceOp op) {
   }
 }
 
-static bool onlyUseIsAssign(Value v) {
-  if (!v.hasOneUse())
-    return false;
-  if (!dyn_cast_or_null<AssignOp>(v.getDefiningOp()))
-    return false;
-  return true;
-}
 
 // Ensure that each output of an instance are used only by a wire
 static void lowerInstanceResults(InstanceOp op) {
@@ -115,22 +108,28 @@ static void lowerInstanceResults(InstanceOp op) {
     auto result = op.getResult(nextResultNo);
     ++nextResultNo;
 
-    if (onlyUseIsAssign(result))
-      continue;
-
+    Value newWire;
     if (result.hasOneUse()) {
       OpOperand &use = *result.getUses().begin();
       if (dyn_cast_or_null<OutputOp>(use.getOwner()))
         continue;
+      if (auto assign = dyn_cast_or_null<AssignOp>(use.getOwner())) {
+        newWire = assign.dest();
+        // NOTE: Erase the assign op to simplify the implementation. We will
+        // re-create the same assignment at the end.
+        assign.erase();
+      }
     }
 
-    nameTmp.resize(namePrefixSize);
-    if (port.name)
-      nameTmp += port.name.getValue().str();
-    else
-      nameTmp += std::to_string(nextResultNo - 1);
+    if (!newWire) {
+      nameTmp.resize(namePrefixSize);
+      if (port.name)
+        nameTmp += port.name.getValue().str();
+      else
+        nameTmp += std::to_string(nextResultNo - 1);
+      newWire = builder.create<WireOp>(result.getType(), nameTmp);
+    }
 
-    auto newWire = builder.create<WireOp>(result.getType(), nameTmp);
     while (!result.use_empty()) {
       auto newWireRead = builder.create<ReadInOutOp>(newWire);
       OpOperand &use = *result.getUses().begin();

test/Conversion/ExportVerilog/hw-dialect.mlir
@@ -687,6 +687,24 @@ hw.module @out_of_order_multi_result() -> (b: i1, c: i2) {
   hw.output %b, %c : i1, i2
 }
 
+hw.module.extern @single_result() -> (res: i3)
+// CHECK-LABEL: module instance_result_reuse_wires(
+hw.module @instance_result_reuse_wires() -> (b: i3) {
+  // CHECK:       wire {{.*}} some_wire;
+  // CHECK-EMPTY:
+  // CHECK-NEXT:  single_result b1 (
+  // CHECK-NEXT:  .res (some_wire)
+  // CHECK-NEXT:  );
+  // CHECK-NEXT:  assign b = some_wire;
+  %some_wire = sv.wire : !hw.inout<i3>
+  %read = sv.read_inout %some_wire : !hw.inout<i3>
+
+  %out1 = hw.instance "b1" @single_result() -> (res: i3)
+  sv.assign %some_wire, %out1 : i3
+
+  hw.output %read : i3
+}
+
 
 hw.module.extern @ExternDestMod(%a: i1, %b: i2) -> (c: i3, d: i4)
 hw.module @InternalDestMod(%a: i1, %b: i3) {}

test/Dialect/FIRRTL/SFCTests/GrandCentralInterfaces/Wire.fir
@@ -185,7 +185,7 @@ circuit Top :
     ; NOEXTRACT:      module MyView_companion();
     ; NOEXTRACT:        Tap tap (
     ; NOEXTRACT-NEXT:     .b     (r),
-    ; NOEXTRACT-NEXT:     .clock (_tap_clock),
+    ; NOEXTRACT-NEXT:     .clock (clock),
     ; NOEXTRACT-NEXT:     .a     (_tap_a)
     ; NOEXTRACT-NEXT:   );
     ; NOEXTRACT-NEXT:   MyView_mapping MyView_mapping{{ *}}();
@@ -221,7 +221,7 @@ circuit Top :
     ; EXTRACT:        module MyView_companion();
     ; EXTRACT:          Tap tap (
     ; EXTRACT-NEXT:       .b     (r),
-    ; EXTRACT-NEXT:       .clock (_tap_clock),
+    ; EXTRACT-NEXT:       .clock (clock),
     ; EXTRACT-NEXT:       .a     (_tap_a)
     ; EXTRACT-NEXT:     );
     ; EXTRACT-NEXT:     MyView_mapping MyView_mapping{{ *}}();

commit fa6fda2217bfe1611d75b85b4e53d21a325ab09f

[PrepareForEmission] Fix use-after-free, nfc
This fixes a bug introduced by 1bcd3f49c2a. It was illegal to
use `getUses` after erasing the use. This commit fixes the issue
by just skipping re-creation of assignments.

lib/Conversion/ExportVerilog/PrepareForEmission.cpp
@@ -108,27 +108,23 @@ static void lowerInstanceResults(InstanceOp op) {
     auto result = op.getResult(nextResultNo);
     ++nextResultNo;
 
-    Value newWire;
     if (result.hasOneUse()) {
       OpOperand &use = *result.getUses().begin();
       if (dyn_cast_or_null<OutputOp>(use.getOwner()))
         continue;
       if (auto assign = dyn_cast_or_null<AssignOp>(use.getOwner())) {
-        newWire = assign.dest();
-        // NOTE: Erase the assign op to simplify the implementation. We will
-        // re-create the same assignment at the end.
-        assign.erase();
+        // Move assign op after instance to resolve cyclic dependencies.
+        assign->moveAfter(op);
+        continue;
       }
     }
 
-    if (!newWire) {
-      nameTmp.resize(namePrefixSize);
-      if (port.name)
-        nameTmp += port.name.getValue().str();
-      else
-        nameTmp += std::to_string(nextResultNo - 1);
-      newWire = builder.create<WireOp>(result.getType(), nameTmp);
-    }
+    nameTmp.resize(namePrefixSize);
+    if (port.name)
+      nameTmp += port.name.getValue().str();
+    else
+      nameTmp += std::to_string(nextResultNo - 1);
+    Value newWire = builder.create<WireOp>(result.getType(), nameTmp);
 
     while (!result.use_empty()) {
       auto newWireRead = builder.create<ReadInOutOp>(newWire);