2f598e9327a30af9054b5b14713a049b8c7825f8
Mike Urbach <mikeurbach@gmail.com>
2022-05-16 14:10:36 -0700

    [Calyx] Initial Calyx to HW conversion. (#3012)
    This adds initial support for converting Calyx to the "core" CIRCT
    dialects: HW, Comb, Seq, and SV. This is done in a fairly direct way
    using the dialect conversion framework. This is direct in the sense
    that a Calyx reg is mapped to a Seq reg, a Calyx assign is mapped to a
    SV assign, a Calyx std_add is mapped to a Comb add, etc.
    
    I tried to port big chunks of the Calyx primitives over at once, when
    the conversion could be templated and shared. There are still some ops
    missing conversions.
    
    I also deviate slightly in the behavior of pipelined ops, like
    multiplication. This may lead to slight cycle-by-cycle differences
    with the Calyx standard library implementations, but should still
    produce functionally identical behavior.
    
    The implementation makes use of lots of SV wires and assigns, in order
    to stay simple and in-line with the form of the Calyx IR. These are
    massively cleaned up by canonicalization, which can be run after the
    conversion.

--- lib/Conversion/CalyxToHW/CalyxToHW.cpp
+++ lib/Conversion/CalyxToHW/CalyxToHW.cpp
@@ -13,45 +13,402 @@
 #include "circt/Conversion/CalyxToHW.h"
 #include "../PassDetail.h"
 #include "circt/Dialect/Calyx/CalyxOps.h"
+#include "circt/Dialect/Comb/CombDialect.h"
+#include "circt/Dialect/Comb/CombOps.h"
+#include "circt/Dialect/HW/HWDialect.h"
 #include "circt/Dialect/HW/HWOps.h"
+#include "circt/Dialect/HW/HWTypes.h"
+#include "circt/Dialect/SV/SVDialect.h"
 #include "circt/Dialect/SV/SVOps.h"
+#include "circt/Dialect/Seq/SeqOps.h"
+#include "mlir/IR/ImplicitLocOpBuilder.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "llvm/ADT/TypeSwitch.h"
 
 using namespace mlir;
 using namespace circt;
 using namespace circt::calyx;
+using namespace circt::comb;
 using namespace circt::hw;
+using namespace circt::seq;
 using namespace circt::sv;
 
-hw::HWModuleExternOp circt::calyx::getExternHWModule(OpBuilder &builder,
-                                                     ComponentOp op) {
-  SmallVector<hw::PortInfo, 8> hwPortInfos;
-  auto addHWPortInfo = [&](auto portInfos, hw::PortDirection direction) {
-    for (auto portInfo : enumerate(portInfos)) {
-      hw::PortInfo hwPortInfo;
-      hwPortInfo.direction = direction;
-      hwPortInfo.type = portInfo.value().type;
-      hwPortInfo.argNum = portInfo.index();
-      hwPortInfo.name = portInfo.value().name;
-      hwPortInfos.push_back(hwPortInfo);
+/// ConversionPatterns.
+
+struct ConvertProgramOp : public OpConversionPattern<ProgramOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(ProgramOp program, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    ModuleOp mod = program->getParentOfType<ModuleOp>();
+    rewriter.inlineRegionBefore(program.body(), &mod.getBodyRegion().front());
+    rewriter.eraseBlock(&mod.getBodyRegion().back());
+    return success();
+  }
+};
+
+struct ConvertComponentOp : public OpConversionPattern<ComponentOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(ComponentOp component, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    SmallVector<hw::PortInfo> hwInputInfo;
+    for (auto [name, type, direction, _] : component.getPortInfo())
+      hwInputInfo.push_back({name, hwDirection(direction), type});
+
+    auto hwMod = rewriter.create<HWModuleOp>(
+        component.getLoc(), component.getNameAttr(), hwInputInfo);
+
+    rewriter.eraseOp(hwMod.getBodyBlock()->getTerminator());
+    ConversionPatternRewriter::InsertionGuard g(rewriter);
+    rewriter.setInsertionPointToEnd(hwMod.getBodyBlock());
+
+    SmallVector<Value> argValues;
+    SmallVector<Value> outputWires;
+    size_t portIdx = 0;
+    for (auto [name, type, direction, _] : component.getPortInfo()) {
+      switch (direction) {
+      case calyx::Direction::Input:
+        assert(hwMod.getArgument(portIdx).getType() == type);
+        argValues.push_back(hwMod.getArgument(portIdx));
+        break;
+      case calyx::Direction::Output:
+        auto wire = rewriter.create<sv::WireOp>(component.getLoc(), type, name);
+        auto wireRead =
+            rewriter.create<sv::ReadInOutOp>(component.getLoc(), wire);
+        argValues.push_back(wireRead);
+        outputWires.push_back(wireRead);
+        break;
+      }
+      ++portIdx;
     }
-  };
 
-  addHWPortInfo(op.getInputPortInfo(), hw::PortDirection::INPUT);
-  addHWPortInfo(op.getOutputPortInfo(), hw::PortDirection::OUTPUT);
+    rewriter.mergeBlocks(component.getBody(), hwMod.getBodyBlock(), argValues);
+    rewriter.create<OutputOp>(component.getLoc(), outputWires);
+    rewriter.eraseOp(component);
 
-  return builder.create<hw::HWModuleExternOp>(
-      op->getLoc(), builder.getStringAttr(op.getName()), hwPortInfos);
-}
+    return success();
+  }
+
+private:
+  hw::PortDirection hwDirection(calyx::Direction dir) const {
+    switch (dir) {
+    case calyx::Direction::Input:
+      return hw::PortDirection::INPUT;
+    case calyx::Direction::Output:
+      return hw::PortDirection::OUTPUT;
+    }
+  }
+};
+
+struct ConvertWiresOp : public OpConversionPattern<WiresOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(WiresOp wires, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    HWModuleOp hwMod = wires->getParentOfType<HWModuleOp>();
+    rewriter.inlineRegionBefore(wires.body(), hwMod.getBodyRegion(),
+                                hwMod.getBodyRegion().end());
+    rewriter.eraseOp(wires);
+    rewriter.mergeBlockBefore(&hwMod.getBodyRegion().getBlocks().back(),
+                              &hwMod.getBodyBlock()->back());
+    return success();
+  }
+};
+
+struct ConvertControlOp : public OpConversionPattern<ControlOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(ControlOp control, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    if (!control.getBody()->empty())
+      return control.emitOpError("calyx control must be structural");
+    rewriter.eraseOp(control);
+    return success();
+  }
+};
+
+struct ConvertAssignOp : public OpConversionPattern<calyx::AssignOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(calyx::AssignOp assign, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    Value dest = adaptor.dest();
+
+    // To make life easy in ConvertComponentOp, we read from the output wires so
+    // the dialect conversion block argument mapping would work without a type
+    // converter. This means assigns to ComponentOp outputs will try to assign
+    // to a read from a wire, so we need to map to the wire.
+    if (auto readInOut = dyn_cast<ReadInOutOp>(adaptor.dest().getDefiningOp()))
+      dest = readInOut.input();
+
+    Value src = adaptor.src();
+    if (auto guard = adaptor.guard()) {
+      auto zero =
+          rewriter.create<hw::ConstantOp>(assign.getLoc(), src.getType(), 0);
+      src = rewriter.create<MuxOp>(assign.getLoc(), guard, src, zero);
+    }
+
+    rewriter.replaceOpWithNewOp<sv::AssignOp>(assign, dest, src);
+
+    return success();
+  }
+};
+
+struct ConvertCellOp : public OpInterfaceConversionPattern<CellInterface> {
+  using OpInterfaceConversionPattern::OpInterfaceConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(CellInterface cell, ArrayRef<Value> operands,
+                  ConversionPatternRewriter &rewriter) const override {
+    assert(operands.empty() && "calyx cells do not have operands");
+
+    SmallVector<Value> wires;
+    ImplicitLocOpBuilder builder(cell.getLoc(), rewriter);
+    convertPrimitiveOp(cell, wires, builder);
+    if (wires.size() != cell.getPortInfo().size()) {
+      auto diag = cell.emitOpError("couldn't convert to core primitive");
+      for (Value wire : wires)
+        diag.attachNote() << "with wire: " << wire;
+      return diag;
+    }
+
+    rewriter.replaceOp(cell, wires);
+
+    return success();
+  }
+
+private:
+  void convertPrimitiveOp(Operation *op, SmallVectorImpl<Value> &wires,
+                          ImplicitLocOpBuilder &b) const {
+    TypeSwitch<Operation *>(op)
+        // Comparison operations.
+        .Case([&](EqLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::eq, wires, b);
+        })
+        .Case([&](NeqLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::ne, wires, b);
+        })
+        .Case([&](LtLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::ult, wires, b);
+        })
+        .Case([&](LeLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::ule, wires, b);
+        })
+        .Case([&](GtLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::ugt, wires, b);
+        })
+        .Case([&](GeLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::uge, wires, b);
+        })
+        .Case([&](SltLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::slt, wires, b);
+        })
+        .Case([&](SleLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::sle, wires, b);
+        })
+        .Case([&](SgtLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::sgt, wires, b);
+        })
+        .Case([&](SgeLibOp op) {
+          convertCompareBinaryOp(op, ICmpPredicate::sge, wires, b);
+        })
+        // Combinational arithmetic and logical operations.
+        .Case([&](AddLibOp op) {
+          convertArithBinaryOp<AddLibOp, AddOp>(op, wires, b);
+        })
+        .Case([&](SubLibOp op) {
+          convertArithBinaryOp<SubLibOp, SubOp>(op, wires, b);
+        })
+        .Case([&](RshLibOp op) {
+          convertArithBinaryOp<RshLibOp, ShrUOp>(op, wires, b);
+        })
+        .Case([&](LshLibOp op) {
+          convertArithBinaryOp<LshLibOp, ShlOp>(op, wires, b);
+        })
+        .Case([&](AndLibOp op) {
+          convertArithBinaryOp<AndLibOp, AndOp>(op, wires, b);
+        })
+        .Case([&](OrLibOp op) {
+          convertArithBinaryOp<OrLibOp, OrOp>(op, wires, b);
+        })
+        .Case([&](XorLibOp op) {
+          convertArithBinaryOp<XorLibOp, XorOp>(op, wires, b);
+        })
+        // Pipelined arithmetic operations.
+        .Case([&](MultPipeLibOp op) {
+          auto clk =
+              wireIn(op.clk(), op.instanceName(), op.portName(op.clk()), b);
+          auto reset =
+              wireIn(op.reset(), op.instanceName(), op.portName(op.reset()), b);
+          auto go = wireIn(op.go(), op.instanceName(), op.portName(op.go()), b);
+          auto left =
+              wireIn(op.left(), op.instanceName(), op.portName(op.left()), b);
+          auto right =
+              wireIn(op.right(), op.instanceName(), op.portName(op.right()), b);
+
+          auto mul = b.create<MulOp>(left, right);
+          auto mulReg = reg(mul, clk, reset, op.instanceName(), b);
+          auto doneReg =
+              reg(go, clk, reset,
+                  op.instanceName() + "_" + op.portName(op.done()), b);
+
+          auto out =
+              wireOut(mulReg, op.instanceName(), op.portName(op.out()), b);
+          auto done =
+              wireOut(doneReg, op.instanceName(), op.portName(op.done()), b);
+          wires.append({clk.input(), reset.input(), go.input(), left.input(),
+                        right.input(), out, done});
+        })
+        // Sequential operations.
+        .Case([&](RegisterOp op) {
+          auto in = wireIn(op.in(), op.instanceName(), op.portName(op.in()), b);
+          auto writeEn = wireIn(op.write_en(), op.instanceName(),
+                                op.portName(op.write_en()), b);
+          auto clk =
+              wireIn(op.clk(), op.instanceName(), op.portName(op.clk()), b);
+          auto reset =
+              wireIn(op.reset(), op.instanceName(), op.portName(op.reset()), b);
+
+          auto outReg = reg(in, clk, reset, op.instanceName() + "_reg", b);
+          auto doneReg =
+              reg(writeEn, clk, reset, op.instanceName() + "_done_reg", b);
+
+          auto out = wireOut(outReg, op.instanceName(), "", b);
+          auto done =
+              wireOut(doneReg, op.instanceName(), op.portName(op.done()), b);
+          wires.append({in.input(), writeEn.input(), clk.input(), reset.input(),
+                        out, done});
+        })
+        // Unary operqations.
+        .Case([&](SliceLibOp op) {
+          auto in = wireIn(op.in(), op.instanceName(), op.portName(op.in()), b);
+          auto outWidth = op.out().getType().getIntOrFloatBitWidth();
+
+          auto extract = b.create<ExtractOp>(in, 0, outWidth);
+
+          auto out =
+              wireOut(extract, op.instanceName(), op.portName(op.out()), b);
+          wires.append({in.input(), out});
+        })
+        .Case([&](NotLibOp op) {
+          auto in = wireIn(op.in(), op.instanceName(), op.portName(op.in()), b);
+          auto one = b.create<hw::ConstantOp>(op.in().getType(), 0);
+
+          auto xorOp = b.create<XorOp>(in, one);
+
+          auto out =
+              wireOut(xorOp, op.instanceName(), op.portName(op.out()), b);
+          wires.append({in.input(), out});
+        })
+        .Case([&](WireLibOp op) {
+          auto wire = wireIn(op.in(), op.instanceName(), "", b);
+          wires.append({wire.input(), wire});
+        })
+        .Default([](Operation *) { return SmallVector<Value>(); });
+  }
+
+  template <typename OpTy, typename ResultTy>
+  void convertArithBinaryOp(OpTy op, SmallVectorImpl<Value> &wires,
+                            ImplicitLocOpBuilder &b) const {
+    auto left = wireIn(op.left(), op.instanceName(), op.portName(op.left()), b);
+    auto right =
+        wireIn(op.right(), op.instanceName(), op.portName(op.right()), b);
+
+    auto add = b.create<ResultTy>(left, right);
+
+    auto out = wireOut(add, op.instanceName(), op.portName(op.out()), b);
+    wires.append({left.input(), right.input(), out});
+  }
+
+  template <typename OpTy>
+  void convertCompareBinaryOp(OpTy op, ICmpPredicate pred,
+                              SmallVectorImpl<Value> &wires,
+                              ImplicitLocOpBuilder &b) const {
+    auto left = wireIn(op.left(), op.instanceName(), op.portName(op.left()), b);
+    auto right =
+        wireIn(op.right(), op.instanceName(), op.portName(op.right()), b);
+
+    auto add = b.create<ICmpOp>(pred, left, right);
+
+    auto out = wireOut(add, op.instanceName(), op.portName(op.out()), b);
+    wires.append({left.input(), right.input(), out});
+  }
+
+  ReadInOutOp wireIn(Value source, StringRef instanceName, StringRef portName,
+                     ImplicitLocOpBuilder &b) const {
+    auto wire =
+        b.create<WireOp>(source.getType(), createName(instanceName, portName));
+    return b.create<ReadInOutOp>(wire);
+  }
+
+  ReadInOutOp wireOut(Value source, StringRef instanceName, StringRef portName,
+                      ImplicitLocOpBuilder &b) const {
+    auto wire =
+        b.create<WireOp>(source.getType(), createName(instanceName, portName));
+    b.create<sv::AssignOp>(wire, source);
+    return b.create<ReadInOutOp>(wire);
+  }
+
+  CompRegOp reg(Value source, Value clock, Value reset, Twine name,
+                ImplicitLocOpBuilder &b) const {
+    auto resetValue = b.create<hw::ConstantOp>(source.getType(), 0);
+    auto regName = b.getStringAttr(name);
+    return b.create<CompRegOp>(source.getType(), source, clock, regName, reset,
+                               resetValue, regName);
+  }
+
+  std::string createName(StringRef instanceName, StringRef portName) const {
+    std::string name = instanceName.str();
+    if (!portName.empty())
+      name += ("_" + portName).str();
+    return name;
+  }
+};
+
+/// Pass entrypoint.
 
 namespace {
 class CalyxToHWPass : public CalyxToHWBase<CalyxToHWPass> {
 public:
   void runOnOperation() override;
+
+private:
+  LogicalResult runOnProgram(ProgramOp program);
 };
 } // end anonymous namespace
 
 void CalyxToHWPass::runOnOperation() {
-  // auto op = getOperation();
+  ModuleOp mod = getOperation();
+  for (auto program : llvm::make_early_inc_range(mod.getOps<ProgramOp>()))
+    if (failed(runOnProgram(program)))
+      return signalPassFailure();
+}
+
+LogicalResult CalyxToHWPass::runOnProgram(ProgramOp program) {
+  MLIRContext &context = getContext();
+
+  ConversionTarget target(context);
+  target.addIllegalDialect<CalyxDialect>();
+  target.addLegalDialect<HWDialect>();
+  target.addLegalDialect<CombDialect>();
+  target.addLegalDialect<SeqDialect>();
+  target.addLegalDialect<SVDialect>();
+
+  RewritePatternSet patterns(&context);
+  patterns.add<ConvertProgramOp>(&context);
+  patterns.add<ConvertComponentOp>(&context);
+  patterns.add<ConvertWiresOp>(&context);
+  patterns.add<ConvertControlOp>(&context);
+  patterns.add<ConvertCellOp>(&context);
+  patterns.add<ConvertAssignOp>(&context);
+
+  return applyPartialConversion(program, target, std::move(patterns));
 }
 
 std::unique_ptr<mlir::Pass> circt::createCalyxToHWPass() {
