94f56fba6b5c4b3076882f541e5c0d075f6b84b9
Morten Borup Petersen <morten_bp@live.dk>
2022-06-15 16:27:26 +0200

    format

--- frontends/PyCDE/test/errors.py
+++ frontends/PyCDE/test/errors.py
@@ -2,12 +2,15 @@
 
 from pycde.module import externmodule
 
+
 # CHECK: TypeError: Module parameter definitions cannot have *args
 @externmodule
 def foo(*args):
   pass
 
+
 # -----
+
 
 # CHECK: TypeError: Module parameter definitions cannot have **kwargs
 @externmodule
