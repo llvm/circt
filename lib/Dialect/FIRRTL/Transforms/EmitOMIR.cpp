e2f234168c8b5421ce537f37bfe3e3006cb40a08
Will Dietz <will.dietz@sifive.com>
2022-06-20 13:30:24 -0500

    [FIRRTL] EmitOMIR: Drop temporary HierPath's that may be invalid
    Restore behavior for deleting the HierPath's added in this pass.
    
    They're known to be dead and sometimes are invalid, causing
    breakage during circuit verification.
    
    While we cannot delete incoming HierPath's attached to operations in
    input IR without a more careful analysis of all possible users, any NLA
    explicitly created in this pass can be reasoned about and removed at the
    end for cleaner (and valid) IR.
    
    Fixes assertion failure on a real design.
    
    The invalid NLA encountered is when the target is in the root module,
    and so we end up making a one-element (InnerRef) NLA.

--- lib/Dialect/FIRRTL/Transforms/EmitOMIR.cpp
+++ lib/Dialect/FIRRTL/Transforms/EmitOMIR.cpp
@@ -126,7 +126,8 @@ private:
   /// collected.
   SmallVector<Attribute> symbols;
   SmallDenseMap<Attribute, unsigned> symbolIndices;
-  /// Temporary `firrtl.nla` operations to be deleted at the end of the pass.
+  /// Temporary `firrtl.hierpath` operations to be deleted at the end of the
+  /// pass. Vector elements are unique.
   SmallVector<HierPathOp> removeTempNLAs;
   DenseMap<Operation *, ModuleNamespace> moduleNamespaces;
   /// Lookup table of instances by name and parent module.
@@ -293,7 +294,6 @@ void EmitOMIRPass::runOnOperation() {
           return true;
         }
         tracker.nla = cast<HierPathOp>(tmp);
-        removeTempNLAs.push_back(tracker.nla);
       }
       if (sramIDs.erase(tracker.id))
         makeTrackerAbsolute(tracker);
@@ -327,7 +327,14 @@ void EmitOMIRPass::runOnOperation() {
   if (anyFailures)
     return signalPassFailure();
 
+  // Drop temporary (and sometimes invalid) NLA's created during the pass:
+  for (auto nla : removeTempNLAs) {
+    LLVM_DEBUG(llvm::dbgs() << "Removing '" << nla << "'\n");
+    nlaTable->erase(nla);
+    nla.erase();
+  }
   removeTempNLAs.clear();
+
   // Remove the temp symbol from instances.
   for (auto *op : tempSymInstances)
     cast<InstanceOp>(op)->removeAttr("inner_sym");
