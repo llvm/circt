81ea86fccce492177db50a5f81b58ac367a86889
Will Dietz <will.dietz@sifive.com>
2022-06-29 16:34:15 -0500

    [FIRRTL][CreateSiFiveMetadata] Reduce temporary string objs. NFC.

--- lib/Dialect/FIRRTL/Transforms/CreateSiFiveMetadata.cpp
+++ lib/Dialect/FIRRTL/Transforms/CreateSiFiveMetadata.cpp
@@ -150,7 +150,7 @@ LogicalResult CreateSiFiveMetadataPass::emitMemoryMetadata() {
             auto parentModule = inst->getParentOfType<FModuleOp>();
             if (dutMod == parentModule)
               hierName = parentModule.getName().str();
-            hierName = hierName + "." + inst.name().str();
+            hierName += (Twine(".") + inst.name()).str();
           }
           hierNames.push_back(hierName);
           jsonStream.value(hierName);
