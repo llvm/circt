01f614b4b6bdc739b39c349cb54fce8dc9bce5cd
Hideto Ueno <uenoku.tokotoko@gmail.com>
2022-07-05 22:25:47 +0900

    minor

--- include/circt/Support/SVAttributes.h
+++ include/circt/Support/SVAttributes.h
@@ -6,7 +6,6 @@ namespace circt {
 bool hasSVAttributes(mlir::Operation *op);
 
 mlir::Attribute getSVAttributes(mlir::Operation *op);
-
 void setSVAttributes(mlir::Operation *op, mlir::Attribute);
 } // namespace circt
 
