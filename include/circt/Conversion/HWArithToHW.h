13351d4c952c7e314fa2e7f378124f2b5394bb57
7FM <41307817+7FM@users.noreply.github.com>
2022-07-17 21:43:22 +0200

    [HWArithToHW] Add lowering boilerplate

---
+++ include/circt/Conversion/HWArithToHW.h
@@ -0,0 +1,27 @@
+//===- HWArithToHW.h - HWArith to HW conversions ----------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This header file defines prototypes that expose the HWArithToHW pass
+// constructor.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_CONVERSION_HWARITHTOHW_HWARITHTOHW_H
+#define CIRCT_CONVERSION_HWARITHTOHW_HWARITHTOHW_H
+
+#include <memory>
+
+namespace mlir {
+class Pass;
+} // namespace mlir
+
+namespace circt {
+std::unique_ptr<mlir::Pass> createHWArithToHWPass();
+} // namespace circt
+
+#endif // CIRCT_CONVERSION_HWARITHTOHW_HWARITHTOHW_H
