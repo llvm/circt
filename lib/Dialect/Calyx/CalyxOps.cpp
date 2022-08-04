llvm/circt

commit 2be831fdb1263c8980d9a46787da0953504f6285

Update llvm::{Optional,FailureOr} uses to new methods (NFCI). (#3659)
Primarily:

getValue -> value
hasValue -> has_value

Value accesses not dominated by a presence check use 'value'.
(only use 'operator *' when clearly dominated by check)

"operator bool" and "operator *" used only when meaning was clear
(if variable looks like an integer, like getBitWidth(),
don't use operator bool unless the code already does so;
if returning result of a presence check prefer has_value, etc.)

Few places are slightly simplified, such as: using value_or,
avoiding calling same method for presence check and for the value,
and X.getValue().y -> X->y.

Fixes #3552.
(LLVM will be deprecating the old method names)

lib/Dialect/Calyx/CalyxOps.cpp
@@ -298,12 +298,11 @@ static void eraseControlWithGroupAndConditional(OpTy op,
 
   // Save information about the operation, and erase it.
   Value cond = op.cond();
-  Optional<StringRef> groupName = op.groupName();
   auto component = op->template getParentOfType<ComponentOp>();
   rewriter.eraseOp(op);
 
   // Clean up the attached conditional and combinational group (if it exists).
-  if (groupName.hasValue()) {
+  if (auto groupName = op.groupName()) {
     auto group = component.getWiresOp().template lookupSymbol<GroupInterface>(
         *groupName);
     if (SymbolTable::symbolKnownUseEmpty(group, component.getRegion()))
@@ -1481,11 +1480,11 @@ LogicalResult IfOp::verify() {
     return emitError() << "empty 'else' region.";
 
   Optional<StringRef> optGroupName = groupName();
-  if (!optGroupName.hasValue()) {
+  if (!optGroupName) {
     // No combinational group was provided.
     return success();
   }
-  StringRef groupName = optGroupName.getValue();
+  StringRef groupName = *optGroupName;
   auto groupOp = wiresOp.lookupSymbol<GroupInterface>(groupName);
   if (!groupOp)
     return emitOpError() << "with group '" << groupName
@@ -1570,7 +1569,7 @@ struct CommonTailPatternWithSeq : mlir::OpRewritePattern<IfOp> {
     Optional<EnableOp> lastThenEnableOp = getLastEnableOp(thenControl),
                        lastElseEnableOp = getLastEnableOp(elseControl);
 
-    if (!lastThenEnableOp.hasValue() || !lastElseEnableOp.hasValue())
+    if (!lastThenEnableOp || !lastElseEnableOp)
       return failure();
     if (lastThenEnableOp->groupName() != lastElseEnableOp->groupName())
       return failure();
@@ -1683,11 +1682,11 @@ LogicalResult WhileOp::verify() {
   auto wiresOp = component.getWiresOp();
 
   Optional<StringRef> optGroupName = groupName();
-  if (!optGroupName.hasValue()) {
+  if (!optGroupName) {
     /// No combinational group was provided
     return success();
   }
-  StringRef groupName = optGroupName.getValue();
+  StringRef groupName = *optGroupName;
   auto groupOp = wiresOp.lookupSymbol<GroupInterface>(groupName);
   if (!groupOp)
     return emitOpError() << "with group '" << groupName

include/circt/Dialect/FIRRTL/FIRRTLExpressions.td
@@ -161,7 +161,7 @@ def SubfieldOp : FIRRTLExprOp<"subfield"> {
     OpBuilder<(ins "Value":$input, "StringRef":$fieldName), [{
       auto bundleType = input.getType().cast<BundleType>();
       auto fieldIndex = bundleType.getElementIndex(fieldName);
-      assert(fieldIndex.hasValue() && "subfield operation to unknown field");
+      assert(fieldIndex.has_value() && "subfield operation to unknown field");
       return build($_builder, $_state, input, *fieldIndex);
     }]>
   ];

include/circt/Dialect/FIRRTL/FIRRTLTypes.h
@@ -201,13 +201,10 @@ public:
   }
 
   /// Return the width of this type, or -1 if it has none specified.
-  int32_t getWidthOrSentinel() {
-    auto width = getWidth();
-    return width.hasValue() ? width.getValue() : -1;
-  }
+  int32_t getWidthOrSentinel() { return getWidth().value_or(-1); }
 
   /// Return true if this type has a known width.
-  bool hasWidth() { return getWidth().hasValue(); }
+  bool hasWidth() { return getWidth().has_value(); }
 
   /// Return a new type with the width changed to a different value.
   ConcreteType changeWidth(int32_t width) {
@@ -231,16 +228,13 @@ public:
   bool isUnsigned() { return isa<UIntType>(); }
 
   /// Return true if this integer type has a known width.
-  bool hasWidth() { return getWidth().hasValue(); }
+  bool hasWidth() { return getWidth().has_value(); }
 
   /// Return the bitwidth of this type or None if unknown.
   Optional<int32_t> getWidth();
 
   /// Return the width of this type, or -1 if it has none specified.
-  int32_t getWidthOrSentinel() {
-    auto width = getWidth();
-    return width.hasValue() ? width.getValue() : -1;
-  }
+  int32_t getWidthOrSentinel() { return getWidth().value_or(-1); }
 
   static bool classof(Type type) {
     return type.isa<SIntType>() || type.isa<UIntType>();

include/circt/Dialect/LLHD/IR/SignalOps.td
@@ -280,7 +280,7 @@ def LLHD_RegOp : LLHD_Op<"reg", [
     static RegMode getRegModeByName(StringRef name) {
       llvm::Optional<RegMode> optional =  symbolizeRegMode(name);
       assert(optional && "Invalid RegMode string.");
-      return optional.getValue();
+      return optional.value();
     }
 
     bool hasGate(unsigned index) {

lib/Analysis/SchedulingAnalysis.cpp
@@ -72,7 +72,7 @@ void circt::analysis::CyclicSchedulingAnalysis::analyzeForOp(
       // assumes outer loops execute sequentially, i.e. one iteration of the
       // inner loop completes before the next iteration is initiated. With
       // proper analysis and lowerings, this can be relaxed.
-      unsigned distance = memoryDep.dependenceComponents.back().lb.getValue();
+      unsigned distance = memoryDep.dependenceComponents.back().lb.value();
       if (distance > 0)
         problem.setDistance(dep, distance);
     }

lib/Analysis/TestPasses.cpp
@@ -60,10 +60,10 @@ void TestDependenceAnalysisPass::runOnOperation() {
       SmallVector<Attribute> comps;
       for (auto comp : dep.dependenceComponents) {
         SmallVector<Attribute> vector;
-        vector.push_back(IntegerAttr::get(IntegerType::get(context, 64),
-                                          comp.lb.getValue()));
-        vector.push_back(IntegerAttr::get(IntegerType::get(context, 64),
-                                          comp.ub.getValue()));
+        vector.push_back(
+            IntegerAttr::get(IntegerType::get(context, 64), comp.lb.value()));
+        vector.push_back(
+            IntegerAttr::get(IntegerType::get(context, 64), comp.ub.value()));
         comps.push_back(ArrayAttr::get(context, vector));
       }
 
@@ -197,7 +197,7 @@ void InferTopModulePass::runOnOperation() {
   }
 
   llvm::SmallVector<Attribute, 4> attrs;
-  for (auto *node : res.getValue())
+  for (auto *node : *res)
     attrs.push_back(node->getModule().moduleNameAttr());
 
   analysis.getParent()->setAttr("test.top",

lib/Conversion/AffineToStaticLogic/AffineToStaticLogic.cpp
@@ -363,8 +363,7 @@ LogicalResult AffineToStaticLogic::createStaticLogicPipeline(
   // iter arg is created for the induction variable.
   TypeRange resultTypes = innerLoop.getResultTypes();
 
-  auto ii =
-      builder.getI64IntegerAttr(problem.getInitiationInterval().getValue());
+  auto ii = builder.getI64IntegerAttr(problem.getInitiationInterval().value());
 
   SmallVector<Value> iterArgs;
   iterArgs.push_back(lowerBound);

lib/Conversion/ExportVerilog/ExportVerilog.cpp
@@ -1509,13 +1509,13 @@ ModuleEmitter::printParamValue(Attribute value, raw_ostream &os,
   // TODO: This could try harder to omit redundant casts like the mainline
   // expression emitter.
   auto emitOperand = [&](Attribute operand) -> bool {
-    if (operandSign.hasValue())
-      os << (operandSign.getValue() == IsSigned ? "$signed(" : "$unsigned(");
+    if (operandSign.has_value())
+      os << (operandSign.value() == IsSigned ? "$signed(" : "$unsigned(");
     auto signedness =
         printParamValue(operand, os, subprecedence, emitError).signedness;
-    if (operandSign.hasValue()) {
+    if (operandSign.has_value()) {
       os << ')';
-      signedness = operandSign.getValue();
+      signedness = operandSign.value();
     }
     return signedness == IsSigned;
   };

commit 9fc849c7359490be75441fd3808e454eb1f9fa85

[Calyx] Fix use of erased op introduced in Optional migration.
This was broken in 2be831fdb1263c8980d9a46787da0953504f6285 .

lib/Dialect/Calyx/CalyxOps.cpp
@@ -300,11 +300,12 @@ static void eraseControlWithGroupAndConditional(OpTy op,
 
   // Save information about the operation, and erase it.
   Value cond = op.cond();
+  Optional<StringRef> groupName = op.groupName();
   auto component = op->template getParentOfType<ComponentOp>();
   rewriter.eraseOp(op);
 
   // Clean up the attached conditional and combinational group (if it exists).
-  if (auto groupName = op.groupName()) {
+  if (groupName) {
     auto group = component.getWiresOp().template lookupSymbol<GroupInterface>(
         *groupName);
     if (SymbolTable::symbolKnownUseEmpty(group, component.getRegion()))