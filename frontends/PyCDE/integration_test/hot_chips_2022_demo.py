74b453f6ca165222614a2ce59bd228fb118d2828
Mike Urbach <mikeurbach@gmail.com>
2022-08-11 19:24:28 -0600

    More WIP towards wrapping.

--- frontends/PyCDE/integration_test/hot_chips_2022_demo.py
+++ frontends/PyCDE/integration_test/hot_chips_2022_demo.py
@@ -4,7 +4,7 @@ from mlir.ir import Module, NoneType
 
 from circt.dialects import hw
 
-from pycde import InputChannel, OutputChannel, module, generator, types
+from pycde import Input, InputChannel, Output, OutputChannel, module, generator, types
 from pycde.system import System
 from pycde.module import import_hw_module
 
@@ -24,34 +24,34 @@ class HandshakeToESIWrapper:
   # Control Ports
 
   ## Generic ports always present
-  clock = types.i1
-  reset = types.i1
+  clock = Input(types.i1)
+  reset = Input(types.i1)
 
   ## Go signal
-  inCtrl = InputChannel(NoneType.get())
+  in_ctrl = InputChannel(NoneType.get())
 
   ## Done signal
-  outCtrl = OutputChannel(NoneType.get())
+  out_ctrl = OutputChannel(NoneType.get())
 
   # Input 0 Ports
 
   ## Channels from Memory
-  in0_ldData0 = InputChannel(types.i32)
+  in0_ld_data0 = InputChannel(types.i32)
 
-  in0_ldDone0 = InputChannel(NoneType.get())
+  in0_ld_done0 = InputChannel(NoneType.get())
 
   ## Channels to Memory
-  in0_ldAddr0 = OutputChannel(types.i64)
+  in0_ld_addr0 = OutputChannel(types.i64)
 
   # Input 1 Ports
 
   ## Channels from Memory
-  in1_ldData0 = InputChannel(types.i32)
+  in1_ld_data0 = InputChannel(types.i32)
 
-  in1_ldDone0 = InputChannel(NoneType.get())
+  in1_ld_done0 = InputChannel(NoneType.get())
 
   ## Channels to Memory
-  in1_ldAddr0 = OutputChannel(types.i64)
+  in1_ld_addr0 = OutputChannel(types.i64)
 
   # Output 0 Ports
 
@@ -59,8 +59,12 @@ class HandshakeToESIWrapper:
   out0 = OutputChannel(types.i32)
 
   @generator
-  def generate(mod):
-    top()
+  def generate(ports):
+    wrapped_top = top(clock=ports.clock, reset=ports.reset)
+
+    _, in_ctrl_valid = ports.in_ctrl.unwrap(wrapped_top.inCtrl_ready)
+    wrapped_top.inCtrl_valid.connect(in_ctrl_valid)
+
 
 system = System([HandshakeToESIWrapper])
 system.import_modules(imported_modules)
