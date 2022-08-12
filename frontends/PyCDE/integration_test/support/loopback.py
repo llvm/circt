b550c27152d6125e9023d334c112709e6851ab47
John Demme <john.demme@microsoft.com>
2022-08-11 20:49:32 -0700

    Update and add new integration test

--- frontends/PyCDE/integration_test/support/loopback.py
+++ frontends/PyCDE/integration_test/support/loopback.py
@@ -8,7 +8,7 @@ class LoopbackTester(esi_cosim.CosimBase):
   """Provides methods to test the loopback simulations."""
 
   def test_two_chan_loopback(self, num_msgs):
-    to_hw = self.openEP(1002, sendType=self.schema.I1, recvType=self.schema.I32)
+    to_hw = self.openEP(1003, sendType=self.schema.I1, recvType=self.schema.I32)
     from_hw = self.openEP(1001,
                           sendType=self.schema.I32,
                           recvType=self.schema.I1)
@@ -19,3 +19,13 @@ class LoopbackTester(esi_cosim.CosimBase):
       result = self.readMsg(from_hw, self.schema.I32)
       print(f"Got {result}")
       assert (result.i == data)
+
+  def test_one_chan_loopback(self, num_msgs):
+    hw = self.openEP(1002, sendType=self.schema.I16, recvType=self.schema.I32)
+    for _ in range(num_msgs):
+      data = random.randint(0, 2**32 - 1)
+      print(f"Sending {data}")
+      hw.send(self.schema.I32.new_message(i=data))
+      result = self.readMsg(hw, self.schema.I16)
+      print(f"Got {result}")
+      assert (result.i == data & 0xFFFF)
