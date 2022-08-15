1315acf0a7847e26e9a305c8afd9fca276b16d01
Christian Ulmann <christianulmann@gmail.com>
2022-08-15 20:29:04 +0200

    [Handshake] Extend cocotb helper and add new tests (#3728)

--- integration_test/Dialect/Handshake/helper.py
+++ integration_test/Dialect/Handshake/helper.py
@@ -6,11 +6,10 @@ class HandshakePort:
   Helper class that encapsulates a handshake port from the DUT.
   """
 
-  def __init__(self, dut, rdy, val, data=None):
+  def __init__(self, dut, rdy, val):
     self.dut = dut
     self.ready = rdy
     self.valid = val
-    self.data = data
 
   def isReady(self):
     return self.ready.value.is_resolvable and self.ready.value == 1
@@ -49,10 +48,28 @@ class HandshakePort:
       await RisingEdge(self.dut.clock)
 
   async def send(self, val=None):
-    if (val is not None):
-      self.data.value = val
     self.setValid(1)
     await self.awaitHandshake()
+
+  async def awaitNOutputs(self, n):
+    assert (self.isReady())
+    for _ in range(n):
+      await self.waitUntilValid()
+      await RisingEdge(self.dut.clock)
+
+
+class HandshakeDataPort(HandshakePort):
+  """
+  A handshaked port with a data field.
+  """
+
+  def __init__(self, dut, rdy, val, data):
+    super().__init__(dut, rdy, val)
+    self.data = data
+
+  async def send(self, val):
+    self.data.value = val
+    await super().send()
 
   async def checkOutputs(self, results):
     assert (self.isReady())
@@ -62,34 +79,71 @@ class HandshakePort:
       await RisingEdge(self.dut.clock)
 
 
+class HandshakeTuplePort(HandshakePort):
+  """
+  A handshaked port that sends a tuple.
+  """
+
+  def __init__(self, dut, rdy, val, fields):
+    super().__init__(dut, rdy, val)
+    self.fields = fields
+
+  async def send(self, val):
+    assert (len(list(val)) == len(self.fields))
+    for (f, v) in zip(self.fields, list(val)):
+      f.value = v
+
+    await super().send()
+
+  async def checkOutputs(self, results):
+    assert (self.isReady())
+    for res in results:
+      await self.waitUntilValid()
+
+      assert (len(list(res)) == len(self.fields))
+      for (f, r) in zip(self.fields, list(res)):
+        assert (f.value == r)
+      await RisingEdge(self.dut.clock)
+
+
+def _findPort(dut, name):
+  """
+  Checks if dut has a port of the provided name. Either throws an exception or
+  returns a HandshakePort that encapsulates the dut's interface.
+  """
+  readyName = f"{name}_ready"
+  validName = f"{name}_valid"
+  dataName = f"{name}_data"
+  if (not hasattr(dut, readyName) or not hasattr(dut, validName)):
+    raise Exception(f"dut does not have a port named {n}")
+
+  ready = getattr(dut, readyName)
+  valid = getattr(dut, validName)
+  data = getattr(dut, dataName, None)
+
+  # Needed, as it otherwise would try to resolve the value
+  hasData = not isinstance(data, type(None))
+  if hasData:
+    return HandshakeDataPort(dut, ready, valid, data)
+
+  isCtrl = not hasattr(dut, f"{name}_data_field0")
+
+  if (isCtrl):
+    return HandshakePort(dut, ready, valid)
+
+  fields = []
+  i = 0
+  while hasattr(dut, f"{name}_data_field{i}"):
+    fields.append(getattr(dut, f"{name}_data_field{i}"))
+    i += 1
+
+  return HandshakeTuplePort(dut, ready, valid, fields)
+
+
 def getPorts(dut, inNames, outNames):
   """
   Helper function to produce in and out ports for the provided dut.
   """
-  ins = []
-  outs = []
-
-  for inName in inNames:
-    readyName = f"{inName}_ready"
-    validName = f"{inName}_valid"
-    dataName = f"{inName}_data"
-    if (not hasattr(dut, readyName) or not hasattr(dut, validName)):
-      raise Exception(f"dut does not have the input {i}")
-
-    ready = getattr(dut, readyName)
-    valid = getattr(dut, validName)
-
-    ins.append(HandshakePort(dut, ready, valid, getattr(dut, dataName, None)))
-
-  for outName in outNames:
-    readyName = f"{outName}_ready"
-    validName = f"{outName}_valid"
-    dataName = f"{outName}_data"
-    if (not hasattr(dut, readyName) or not hasattr(dut, validName)):
-      raise Exception(f"dut does not have the output {i}")
-
-    ready = getattr(dut, readyName)
-    valid = getattr(dut, validName)
-
-    outs.append(HandshakePort(dut, ready, valid, getattr(dut, dataName, None)))
+  ins = [_findPort(dut, name) for name in inNames]
+  outs = [_findPort(dut, name) for name in outNames]
   return ins, outs
---
+++ integration_test/Dialect/Handshake/tuple_packing/tuple_packing.mlir
@@ -0,0 +1,40 @@
+// REQUIRES: iverilog,cocotb
+
+// This test is executed with all different buffering strategies
+
+// RUN: circt-opt %s \
+// RUN:   --canonicalize='top-down=true region-simplify=true' \
+// RUN:   --handshake-materialize-forks-sinks --canonicalize \
+// RUN:   --handshake-insert-buffers=strategy=all --lower-handshake-to-firrtl | \
+// RUN: firtool --format=mlir --verilog --lowering-options=disallowLocalVariables > %t.sv && \
+// RUN: %PYTHON% %S/../cocotb_driver.py --objdir=%T --topLevel=top --pythonModule=tuple_packing --pythonFolder=%S %t.sv | FileCheck %s
+
+// RUN: circt-opt %s \
+// RUN:   --canonicalize='top-down=true region-simplify=true' \
+// RUN:   --handshake-materialize-forks-sinks --canonicalize \
+// RUN:   --handshake-insert-buffers=strategy=allFIFO --lower-handshake-to-firrtl | \
+// RUN: firtool --format=mlir --verilog --lowering-options=disallowLocalVariables > %t.sv && \
+// RUN: %PYTHON% %S/../cocotb_driver.py --objdir=%T --topLevel=top --pythonModule=tuple_packing --pythonFolder=%S %t.sv | FileCheck %s
+
+// RUN: circt-opt %s \
+// RUN:   --canonicalize='top-down=true region-simplify=true' \
+// RUN:   --handshake-materialize-forks-sinks --canonicalize \
+// RUN:   --handshake-insert-buffers=strategy=cycles --lower-handshake-to-firrtl | \
+// RUN: firtool --format=mlir --verilog --lowering-options=disallowLocalVariables > %t.sv && \
+// RUN: %PYTHON% %S/../cocotb_driver.py --objdir=%T --topLevel=top --pythonModule=tuple_packing --pythonFolder=%S %t.sv | FileCheck %s
+
+// CHECK:      ** TEST
+// CHECK-NEXT: ********************************
+// CHECK-NEXT: ** tuple_packing.oneInput
+// CHECK-NEXT: ********************************
+// CHECK-NEXT: ** TESTS=1 PASS=1 FAIL=0 SKIP=0
+// CHECK-NEXT: ********************************
+
+module {
+  handshake.func @top(%arg0: tuple<i32, i32>, %arg1: none, ...) -> (i32, none) attributes {argNames = ["in0", "inCtrl"], resNames = ["out0", "outCtrl"]} {
+    %res:2 = handshake.unpack %arg0 : tuple<i32, i32>
+    %sum = arith.addi %res#0, %res#1 : i32
+    return %sum, %arg1 : i32, none
+  }
+}
+
