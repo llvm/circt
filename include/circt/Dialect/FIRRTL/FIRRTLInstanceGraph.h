fbb62f39267a7677c92b7713194fbacd27b0a1ff
Prithayan Barua <prithayan@gmail.com>
2022-08-24 11:11:27 -0400

    [FIRRTL] Add helper to keep InstancePathCache updated

--- include/circt/Dialect/FIRRTL/FIRRTLInstanceGraph.h
+++ include/circt/Dialect/FIRRTL/FIRRTLInstanceGraph.h
@@ -75,6 +75,12 @@ struct InstancePathCache {
       : instanceGraph(instanceGraph) {}
   ArrayRef<InstancePath> getAbsolutePaths(Operation *op);
 
+  /// Clear the cache.
+  void invalidate() { absolutePathsCache.clear(); }
+
+  /// Replace an InstanceOp. This is required to keep the cache updated.
+  void replaceInstance(InstanceOp oldOp, InstanceOp newOp);
+
 private:
   /// An allocator for individual instance paths and entire path lists.
   llvm::BumpPtrAllocator allocator;
