b7d098acde8e00b74a908bb0b82ca9d1eaddbe7d
Prithayan Barua <prithayan@gmail.com>
2022-08-29 12:42:13 -0400

    Move LowerXMR pass into OutputIRFir path

--- tools/firtool/firtool.cpp
+++ tools/firtool/firtool.cpp
@@ -700,13 +700,14 @@ processBuffer(MLIRContext &context, TimingScope &ts, llvm::SourceMgr &sourceMgr,
     pm.nest<firrtl::CircuitOp>().nest<firrtl::FModuleOp>().addPass(
         firrtl::createMergeConnectionsPass(mergeConnectionsAgggresively));
 
-  // Lower the ref.resolve and ref.send ops and remove the RefType ports.
-  // LowerToHW cannot handle RefType so, this pass must be run to remove all
-  // RefType ports and ops.
-  pm.nest<firrtl::CircuitOp>().addPass(firrtl::createLowerXMRPass());
-
   // Lower if we are going to verilog or if lowering was specifically requested.
   if (outputFormat != OutputIRFir) {
+
+    // Lower the ref.resolve and ref.send ops and remove the RefType ports.
+    // LowerToHW cannot handle RefType so, this pass must be run to remove all
+    // RefType ports and ops.
+    pm.nest<firrtl::CircuitOp>().addPass(firrtl::createLowerXMRPass());
+
     pm.addPass(createLowerFIRRTLToHWPass(enableAnnotationWarning.getValue(),
                                          emitChiselAssertsAsSVA.getValue()));
 
