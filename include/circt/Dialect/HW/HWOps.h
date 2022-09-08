449fe01ad75ef661b86f54bb080a7517f6eb9fdb
Morten Borup Petersen <morten_bp@live.dk>
2022-09-08 09:18:14 +0200

    revert more stuff

--- include/circt/Dialect/HW/HWOps.h
+++ include/circt/Dialect/HW/HWOps.h
@@ -16,7 +16,6 @@
 #include "circt/Dialect/HW/HWDialect.h"
 #include "circt/Dialect/HW/HWOpInterfaces.h"
 #include "circt/Dialect/HW/HWTypes.h"
-#include "circt/Support/BackedgeBuilder.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/FunctionInterfaces.h"
 #include "mlir/IR/OpImplementation.h"
