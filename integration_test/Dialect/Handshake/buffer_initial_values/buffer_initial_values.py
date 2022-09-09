748499c8c6f187d048bb34cd76845d7c22249367
Christian Ulmann <christianulmann@gmail.com>
2022-09-09 11:13:19 +0200

    yapf

--- integration_test/Dialect/Handshake/buffer_initial_values/buffer_initial_values.py
+++ integration_test/Dialect/Handshake/buffer_initial_values/buffer_initial_values.py
@@ -1,9 +1,11 @@
 import cocotb
 from helper import initDut
 
+
 @cocotb.test()
 async def oneInput(dut):
-  [inCtrl], [out0, outCtrl] = await initDut(dut, ["inCtrl"], ["out0", "outCtrl"])
+  [inCtrl], [out0, outCtrl] = await initDut(dut, ["inCtrl"],
+                                            ["out0", "outCtrl"])
 
   out0Check = cocotb.start_soon(out0.checkOutputs([10]))
   inCtrlSend = cocotb.start_soon(inCtrl.send())
