e7405f7be924c1669bd3e8c2ab71a8ff0ee1907c
Morten Borup Petersen <morten_bp@live.dk>
2022-09-14 17:41:29 +0200

    [Pipeline] Add `pipeline.pipeline` HW lowering (#3874)
    This is a fairly straight-forward transformation since the brunt of the
    work of detecting which values will be registered in a given pipeline
    stage has already been performed by a prior pass.
    This pass simply elaborates the `pipeline.stage.register` operations
    into `seq.compreg` operations and stitches up the circuit.

---
+++ lib/Conversion/PipelineToHW/PipelineToHW.cpp
@@ -0,0 +1,99 @@
+//===- PipelineToHW.cpp - Translate Pipeline into HW ----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+//===----------------------------------------------------------------------===//
+//
+// This is the main Pipeline to HW Conversion Pass Implementation.
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Conversion/PipelineToHW.h"
+#include "../PassDetail.h"
+#include "circt/Dialect/Comb/CombOps.h"
+#include "circt/Dialect/HW/HWOps.h"
+#include "circt/Dialect/Pipeline/Pipeline.h"
+#include "circt/Dialect/Seq/SeqOps.h"
+#include "mlir/IR/Builders.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+using namespace mlir;
+using namespace circt;
+using namespace pipeline;
+
+static LogicalResult lowerPipeline(PipelineOp pipeline, OpBuilder &builder) {
+  if (pipeline.isLatencyInsensitive())
+    return pipeline.emitOpError() << "Only latency-sensitive pipelines are "
+                                     "supported at the moment";
+
+  // Simply move the ops from the pipeline to the enclosing hw.module scope,
+  // converting any stage ops to seq registers.
+  Value clk = pipeline.clock();
+  Value rst = pipeline.reset();
+  llvm::SmallVector<Value, 4> retVals;
+  builder.setInsertionPoint(pipeline);
+
+  for (auto [arg, barg] :
+       llvm::zip(pipeline.getOperands(), pipeline.getBody()->getArguments()))
+    barg.replaceAllUsesWith(arg);
+
+  for (auto &op : llvm::make_early_inc_range(*pipeline.getBody())) {
+    auto loc = op.getLoc();
+    llvm::TypeSwitch<Operation *, void>(&op)
+        .Case<PipelineStageRegisterOp>([&](auto stage) {
+          unsigned stageIdx = stage.index();
+          auto validRegName =
+              builder.getStringAttr("s" + std::to_string(stageIdx) + "_valid");
+          auto validReg = builder.create<seq::CompRegOp>(
+              loc, builder.getI1Type(), stage.when(), clk, validRegName, rst,
+              Value(), StringAttr());
+          stage.valid().replaceAllUsesWith(validReg);
+
+          for (auto &it : llvm::enumerate(stage.regIns())) {
+            auto regIdx = it.index();
+            auto regIn = it.value();
+            auto regName =
+                builder.getStringAttr("s" + std::to_string(stageIdx) + "_reg" +
+                                      std::to_string(regIdx));
+            auto reg = builder.create<seq::CompRegOp>(loc, regIn.getType(),
+                                                      regIn, clk, regName, rst,
+                                                      Value(), StringAttr());
+            stage.regOuts()[regIdx].replaceAllUsesWith(reg);
+          }
+        })
+        .Case<pipeline::ReturnOp>([&](auto ret) { retVals = ret.operands(); })
+        .Default([&](auto op) { op->moveBefore(pipeline); });
+  }
+
+  pipeline->replaceAllUsesWith(retVals);
+  pipeline.erase();
+  return success();
+}
+
+//===----------------------------------------------------------------------===//
+// Pipeline to HW Conversion Pass
+//===----------------------------------------------------------------------===//
+
+namespace {
+struct PipelineToHWPass : public PipelineToHWBase<PipelineToHWPass> {
+  void runOnOperation() override;
+};
+
+void PipelineToHWPass::runOnOperation() {
+  OpBuilder builder(&getContext());
+  // Iterate over each pipeline op in the module and convert.
+  // Note: This pass matches on `hw::ModuleOp`s and not directly on the
+  // `PipelineOp` due to the `PipelineOp` being erased during this pass.
+  for (auto pipeline :
+       llvm::make_early_inc_range(getOperation().getOps<PipelineOp>()))
+    if (failed(lowerPipeline(pipeline, builder)))
+      signalPassFailure();
+}
+
+} // namespace
+
+std::unique_ptr<mlir::Pass> circt::createPipelineToHWPass() {
+  return std::make_unique<PipelineToHWPass>();
+}
