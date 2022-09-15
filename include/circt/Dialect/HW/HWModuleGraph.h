9e1add86aa2ea8ee4fee1c1053f29c4a0bdd0b8f
Morten Borup Petersen <morten_bp@live.dk>
2022-09-15 16:45:57 +0200

    [RTL] Add RTLGraph, RTL graph printer
    Motivation: I have a fairly small threshold for the amount of RTL IR i can parse while reasoning about the overall structure of the IR.
    This commit adds support for printing the body of a `hw::HWModuleOp` as a `.dot` graph. This should be useful for developers and users alike :).
    
    This represents a basic implementation of the idea, and can easily be improved. E.g., the graph does not yet show  module inputs. Furthermore, the graph can be structured "better" by having separate groups for input, inner, and output nodes, and ensuring ordering between these.
    ... future work!
    
    remove funk
    
    english
    
    link
    
    work
    
    add edge verbosity option

---
+++ include/circt/Dialect/HW/HWModuleGraph.h
@@ -0,0 +1,193 @@
+//===- HWModuleGraph.h - HWModule graph -------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the HWModuleGraph.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_RTL_HWMODULEGRAPH_H
+#define CIRCT_DIALECT_RTL_HWMODULEGRAPH_H
+
+#include "circt/Dialect/Comb/CombOps.h"
+#include "circt/Dialect/HW/HWOps.h"
+#include "circt/Dialect/HW/InstanceGraphBase.h"
+#include "circt/Dialect/Seq/SeqOps.h"
+#include "circt/Support/LLVM.h"
+#include "llvm/ADT/GraphTraits.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "llvm/ADT/iterator.h"
+#include "llvm/Support/DOTGraphTraits.h"
+#include "llvm/Support/GraphWriter.h"
+
+namespace circt {
+namespace hw {
+namespace detail {
+
+// Using declaration to avoid polluting global namespace with CIRCT-specific
+// graph traits for mlir::Operation.
+using HWOperation = mlir::Operation;
+
+} // namespace detail
+} // namespace hw
+} // namespace circt
+
+template <>
+struct llvm::GraphTraits<circt::hw::detail::HWOperation *> {
+  using NodeType = circt::hw::detail::HWOperation;
+  using NodeRef = NodeType *;
+
+  using ChildIteratorType = mlir::Operation::user_iterator;
+  static NodeRef getEntryNode(NodeRef op) { return op; }
+  static ChildIteratorType child_begin(NodeRef op) { return op->user_begin(); }
+  static ChildIteratorType child_end(NodeRef op) { return op->user_end(); }
+};
+
+template <>
+struct llvm::GraphTraits<circt::hw::HWModuleOp>
+    : public llvm::GraphTraits<circt::hw::detail::HWOperation *> {
+  using GraphType = circt::hw::HWModuleOp;
+
+  static NodeRef getEntryNode(GraphType mod) {
+    return &mod.getBodyBlock()->front();
+  }
+
+  using nodes_iterator = pointer_iterator<mlir::Block::iterator>;
+  static nodes_iterator nodes_begin(GraphType mod) {
+    return nodes_iterator{mod.getBodyBlock()->begin()};
+  }
+  static nodes_iterator nodes_end(GraphType mod) {
+    return nodes_iterator{mod.getBodyBlock()->end()};
+  }
+};
+
+template <>
+struct llvm::DOTGraphTraits<circt::hw::HWModuleOp>
+    : public llvm::DefaultDOTGraphTraits {
+  using DefaultDOTGraphTraits::DefaultDOTGraphTraits;
+
+  static std::string getNodeLabel(circt::hw::detail::HWOperation *node,
+                                  circt::hw::HWModuleOp) {
+    return llvm::TypeSwitch<mlir::Operation *, std::string>(node)
+        .Case<circt::comb::AddOp>([&](auto) { return "+"; })
+        .Case<circt::comb::SubOp>([&](auto) { return "-"; })
+        .Case<circt::comb::AndOp>([&](auto) { return "&"; })
+        .Case<circt::comb::OrOp>([&](auto) { return "|"; })
+        .Case<circt::comb::XorOp>([&](auto) { return "^"; })
+        .Case<circt::comb::MulOp>([&](auto) { return "*"; })
+        .Case<circt::comb::MuxOp>([&](auto) { return "mux"; })
+        .Case<circt::comb::ShrSOp, circt::comb::ShrUOp>(
+            [&](auto) { return ">>"; })
+        .Case<circt::comb::ShlOp>([&](auto) { return "<<"; })
+        .Case<circt::comb::ICmpOp>([&](auto op) {
+          switch (op.getPredicate()) {
+          case circt::comb::ICmpPredicate::eq:
+          case circt::comb::ICmpPredicate::ceq:
+          case circt::comb::ICmpPredicate::weq:
+            return "==";
+          case circt::comb::ICmpPredicate::wne:
+          case circt::comb::ICmpPredicate::cne:
+          case circt::comb::ICmpPredicate::ne:
+            return "!=";
+          case circt::comb::ICmpPredicate::uge:
+          case circt::comb::ICmpPredicate::sge:
+            return ">=";
+          case circt::comb::ICmpPredicate::ugt:
+          case circt::comb::ICmpPredicate::sgt:
+            return ">";
+          case circt::comb::ICmpPredicate::ule:
+          case circt::comb::ICmpPredicate::sle:
+            return "<=";
+          case circt::comb::ICmpPredicate::ult:
+          case circt::comb::ICmpPredicate::slt:
+            return "<";
+          }
+          assert(false && "unhandled ICmp predicate");
+        })
+        .Case<circt::seq::CompRegOp>([&](auto) { return "reg"; })
+        .Case<circt::hw::ConstantOp>([&](auto op) {
+          llvm::SmallString<64> valueString;
+          op.getValue().toString(valueString, 10, false);
+          return valueString.str().str();
+        })
+        .Default([&](auto op) { return op->getName().getStringRef().str(); });
+  }
+
+  std::string getNodeAttributes(circt::hw::detail::HWOperation *node,
+                                circt::hw::HWModuleOp) {
+    std::string attrs;
+
+    llvm::TypeSwitch<mlir::Operation *>(node)
+        .Case<circt::hw::ConstantOp>(
+            [&](auto) { attrs = "fillcolor=darkgoldenrod1,style=filled"; })
+        .Case<circt::comb::MuxOp>([&](auto) {
+          attrs = "shape=invtrapezium,fillcolor=bisque,style=filled";
+        })
+        .Case<circt::hw::OutputOp>(
+            [&](auto) { attrs = "fillcolor=lightblue,style=filled"; })
+        .Default([&](auto op) {
+          llvm::TypeSwitch<mlir::Dialect *>(op->getDialect())
+              .Case<circt::comb::CombDialect>([&](auto) {
+                attrs = "shape=oval,fillcolor=bisque,style=filled";
+              })
+              .template Case<circt::seq::SeqDialect>([&](auto) {
+                attrs = "shape=folder,fillcolor=gainsboro,style=filled";
+              })
+              .Default([&](auto) {});
+        });
+    return attrs;
+  }
+
+  static void
+  addCustomGraphFeatures(circt::hw::HWModuleOp mod,
+                         llvm::GraphWriter<circt::hw::HWModuleOp> &g) {
+
+    // Add module input args.
+    auto &os = g.getOStream();
+    os << "subgraph cluster_entry_args {\n";
+    os << "label=\"Input arguments\";\n";
+    for (auto arg : mod.getPorts().inputs) {
+      g.emitSimpleNode(reinterpret_cast<void *>(arg.getId()), "",
+                       arg.getName().str());
+    }
+    os << "}\n";
+    for (auto [info, arg] :
+         llvm::zip(mod.getPorts().inputs, mod.getArguments())) {
+      for (auto user : arg.getUsers()) {
+        g.emitEdge(reinterpret_cast<void *>(info.getId()), 0, user, -1, "");
+      }
+    }
+  }
+
+  template <typename Iterator>
+  static std::string getEdgeAttributes(circt::hw::detail::HWOperation *node,
+                                       Iterator it,
+                                       circt::hw::HWModuleOp module) {
+
+    bool verboseEdges =
+        module.getAttrOfType<mlir::BoolAttr>("dot_verboseEdges");
+
+    if (!verboseEdges.getValue())
+      return "";
+
+    mlir::OpOperand &operand = *it.getCurrent();
+    mlir::Value v = operand.get();
+    std::string str;
+    llvm::raw_string_ostream os(str);
+    os << "label=\"" << operand.getOperandNumber() << " (" << v.getType()
+       << ")\"";
+
+    int64_t width = circt::hw::getBitWidth(v.getType());
+    if (width > 1)
+      os << ",style=bold";
+
+    return os.str();
+  };
+};
+
+#endif // CIRCT_DIALECT_RTL_HWMODULEGRAPH_H
