ba183e10e73356abd7ac3e730e4f2a3e57243f07
Prithayan Barua <prithayan@gmail.com>
2022-09-22 07:05:07 -0700

    [FIRRTL][CheckCombCycles] Ignore self loop of registers(#3965)
    CheckCombCycles should handle register self initialization connects properly. It should not error out on self initialization of individual fields of an aggregate kind of register.

--- lib/Dialect/FIRRTL/Transforms/CheckCombCycles.cpp
+++ lib/Dialect/FIRRTL/Transforms/CheckCombCycles.cpp
@@ -358,7 +358,21 @@ public:
           return InstanceNodeIterator(instance, node, end);
         })
         .Case<SubfieldOp>([&](SubfieldOp subfield) {
-          return SubfieldNodeIterator(subfield, node, end);
+          if (isa_and_nonnull<MemOp>(subfield.getInput().getDefiningOp()))
+            return static_cast<variant_iterator>(
+                SubfieldNodeIterator(subfield, node, end));
+          // This is required to explicitly ignore self loops of register.
+          if (isa_and_nonnull<RegOp, RegResetOp>(
+                  getFieldRefFromValue(subfield).getDefiningOp()))
+            return static_cast<variant_iterator>(NodeIterator(node, true));
+          return static_cast<variant_iterator>(NodeIterator(node, end));
+        })
+        .Case<SubindexOp>([&](SubindexOp sub) {
+          // This is required to explicitly ignore self loops of register.
+          if (isa_and_nonnull<RegOp, RegResetOp>(
+                  getFieldRefFromValue(sub).getDefiningOp()))
+            return NodeIterator(node, true);
+          return NodeIterator(node, end);
         })
         // The children of reg or regreset op are not iterated.
         .Case<RegOp, RegResetOp>([&](auto) { return NodeIterator(node, true); })
