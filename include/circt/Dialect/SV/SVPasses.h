3cd4d3fabb0f56455309a38aabd90c88e99afb18
Morten Borup Petersen <morten_bp@live.dk>
2022-09-28 15:50:33 +0200

    [SV] Add iverilog trace instrumentation pass
    Adds a macro to a HWModule(s) which enables tracing for iverilog (see https://docs.cocotb.org/en/stable/simulator_support.html#waveforms).

--- include/circt/Dialect/SV/SVPasses.h
+++ include/circt/Dialect/SV/SVPasses.h
@@ -23,6 +23,7 @@ std::unique_ptr<mlir::Pass> createPrettifyVerilogPass();
 std::unique_ptr<mlir::Pass> createHWCleanupPass();
 std::unique_ptr<mlir::Pass> createHWStubExternalModulesPass();
 std::unique_ptr<mlir::Pass> createHWLegalizeModulesPass();
+std::unique_ptr<mlir::Pass> createSVTraceIVerilogPass();
 std::unique_ptr<mlir::Pass> createHWGeneratorCalloutPass();
 std::unique_ptr<mlir::Pass>
 createHWMemSimImplPass(bool replSeqMem = false,
