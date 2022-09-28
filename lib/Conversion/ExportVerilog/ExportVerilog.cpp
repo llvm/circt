c750bafac9f8e82e81b3f905c482693d26e67c04
Andrew Lenharth <andrew@lenharth.org>
2022-09-28 12:17:50 -0500

    [NFC] Make prepare an embedded pass run by the pass manager

--- lib/Conversion/ExportVerilog/ExportVerilog.cpp
+++ lib/Conversion/ExportVerilog/ExportVerilog.cpp
@@ -34,6 +34,7 @@
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/ImplicitLocOpBuilder.h"
 #include "mlir/IR/Threading.h"
+#include "mlir/Pass/PassManager.h"
 #include "mlir/Support/FileUtilities.h"
 #include "llvm/ADT/MapVector.h"
 #include "llvm/ADT/STLExtras.h"
@@ -4990,26 +4991,14 @@ void SharedEmitterState::emitOps(EmissionList &thingsToEmit, raw_ostream &os,
   }
 }
 
-/// Prepare the given MLIR module for emission.
-static void prepareForEmission(ModuleOp module,
-                               const LoweringOptions &options) {
-  SmallVector<HWModuleOp> modulesToPrepare;
-  module.walk([&](HWModuleOp op) { modulesToPrepare.push_back(op); });
-  parallelForEach(module->getContext(), modulesToPrepare,
-                  [&](auto op) { prepareHWModule(op, options); });
-}
-
 //===----------------------------------------------------------------------===//
 // Unified Emitter
 //===----------------------------------------------------------------------===//
 
 LogicalResult circt::exportVerilog(ModuleOp module, llvm::raw_ostream &os) {
-  // Prepare the ops in the module for emission and legalize the names that will
-  // end up in the output.
-  LoweringOptions options(module);
-  prepareForEmission(module, options);
   GlobalNameTable globalNames = legalizeGlobalNames(module);
 
+  LoweringOptions options(module);
   SharedEmitterState emitter(module, options, std::move(globalNames));
   emitter.gatherFiles(false);
 
@@ -5054,6 +5043,13 @@ struct ExportVerilogPass : public ExportVerilogBase<ExportVerilogPass> {
     // on the command line.
     // TODO: This should be moved up to circt-opt and circt-translate.
     applyLoweringCLOptions(getOperation());
+
+    // Prepare the ops in the module for emission.
+    LoweringOptions options(getOperation());
+    mlir::OpPassManager preparePM("builtin.module");
+    auto &modulePM = preparePM.nest<hw::HWModuleOp>();
+    modulePM.addPass(createPrepareForEmissionPass(options));
+    (void)runPipeline(preparePM, getOperation());
 
     if (failed(exportVerilog(getOperation(), os)))
       signalPassFailure();
@@ -5127,7 +5123,6 @@ LogicalResult circt::exportSplitVerilog(ModuleOp module, StringRef dirname) {
   // Prepare the ops in the module for emission and legalize the names that will
   // end up in the output.
   LoweringOptions options(module);
-  prepareForEmission(module, options);
   GlobalNameTable globalNames = legalizeGlobalNames(module);
 
   SharedEmitterState emitter(module, options, std::move(globalNames));
@@ -5199,6 +5194,14 @@ struct ExportSplitVerilogPass
     // on the command line.
     // TODO: This should be moved up to circt-opt and circt-translate.
     applyLoweringCLOptions(getOperation());
+
+    // Prepare the ops in the module for emission.
+    LoweringOptions options(getOperation());
+    mlir::OpPassManager preparePM("builtin.module");
+    auto &modulePM = preparePM.nest<hw::HWModuleOp>();
+    modulePM.addPass(createPrepareForEmissionPass(options));
+    (void)runPipeline(preparePM, getOperation());
+
     if (failed(exportSplitVerilog(getOperation(), directoryName)))
       signalPassFailure();
   }
