41a886feb8aa188bec4d82e260b9b6b3d7039781
Will Dietz <will.dietz@sifive.com>
2022-10-11 14:15:34 -0500

    PrettyPrinterTest: add unittest for PrettyPrinter.

---
+++ unittests/Support/PrettyPrinterTest.cpp
@@ -0,0 +1,333 @@
+//===- PrettyPrinterTest.cpp - Pretty printer unit tests ------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Support/PrettyPrinter.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "gtest/gtest.h"
+
+using namespace llvm;
+using namespace circt;
+
+namespace {
+
+class FuncTest : public testing::Test {
+protected:
+  // Test inputs.
+  SmallVector<pretty::Token *> funcTokens;
+  SmallVector<pretty::Token *> nestedTokens;
+  SmallVector<pretty::Token *> indentNestedTokens;
+
+  /// Scratch buffer used by print.
+  SmallString<256> out;
+
+  void addArgs(SmallVectorImpl<pretty::Token *> &t) {
+    // Build argument list with comma + break between tokens.
+    using namespace pretty;
+    auto args = {"int a",
+                 "int b",
+                 "int a1",
+                 "int b1",
+                 "int a2",
+                 "int b2",
+                 "int a3",
+                 "int b3",
+                 "int a4",
+                 "int b4",
+                 "int a5",
+                 "int b5",
+                 "int a6",
+                 "int b6",
+                 "int a7",
+                 "int b7",
+                 "float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"};
+
+    llvm::interleave(
+        args, [&](auto &arg) { t.push_back(new StringToken(arg)); },
+        [&]() {
+          t.push_back(new StringToken(","));
+          t.push_back(new BreakToken());
+        });
+  }
+
+  void SetUp() override {
+    auto constexpr ppInfinity = 0xffffU;
+
+    using namespace pretty;
+
+    {
+      // foooooo(ARGS)
+      // With ARGS in an ibox.
+      funcTokens.append({new StringToken("foooooo"), new StringToken("("),
+                         new BeginToken(0, Breaks::Inconsistent),
+                         new BreakToken(0)});
+      addArgs(funcTokens);
+      funcTokens.append({new BreakToken(0), new EndToken(),
+                         new StringToken(");"), new BreakToken(ppInfinity)});
+    }
+    {
+      // baroo(AR..  barooga(ARGS) .. GS)
+      // Nested function call, nested method wrapped in cbox(0) w/breaks.
+      nestedTokens.append({new StringToken("baroo"), new StringToken("("),
+                           new BeginToken(0, Breaks::Inconsistent),
+                           new BreakToken(0)});
+      SmallVector<Token *> argTokens;
+      addArgs(argTokens);
+      SmallVectorImpl<Token *>::iterator argMiddle =
+          argTokens.begin() + argTokens.size() / 2;
+      nestedTokens.append(argTokens.begin(), argMiddle);
+
+      nestedTokens.append({
+          new BeginToken(0, Breaks::Consistent),
+          new StringToken("barooga"),
+          new StringToken("("),
+          new BeginToken(0, Breaks::Inconsistent),
+          new BreakToken(0),
+      });
+      addArgs(nestedTokens);
+      nestedTokens.append({new BreakToken(0), new EndToken(),
+                           new StringToken("),"), new BreakToken(),
+                           new EndToken(),
+                           /* new BreakToken(0), */});
+      nestedTokens.append(argMiddle, argTokens.end());
+      nestedTokens.append({new BreakToken(0), new EndToken(),
+                           new StringToken(");"), new BreakToken(ppInfinity)});
+    }
+    {
+      // wahoo(ARGS)
+      // If wrap args, indent on next line
+      indentNestedTokens.append({
+          new BeginToken(2, Breaks::Consistent),
+          new StringToken("wahoo"),
+          new StringToken("("),
+          new BreakToken(0),
+          new BeginToken(0, Breaks::Inconsistent),
+      });
+
+      SmallVector<Token *> argTokens;
+      addArgs(argTokens);
+      SmallVectorImpl<Token *>::iterator argMiddle =
+          argTokens.begin() + argTokens.size() / 2;
+      indentNestedTokens.append(argTokens.begin(), argMiddle);
+
+      indentNestedTokens.append({
+          new BeginToken(0, Breaks::Consistent),
+          new StringToken("yahooooooo"),
+          new StringToken("("),
+          new BeginToken(0, Breaks::Inconsistent),
+          new BreakToken(0),
+      });
+      addArgs(indentNestedTokens);
+      indentNestedTokens.append({
+          new BreakToken(0), new EndToken(), new StringToken("),"),
+          new BreakToken(), new EndToken(), /* new BreakToken(0), */
+      });
+      indentNestedTokens.append(argMiddle, argTokens.end());
+      indentNestedTokens.append({new EndToken(), new BreakToken(0, -2),
+                                 new StringToken(");"), new EndToken(),
+                                 new BreakToken(ppInfinity)});
+    }
+  }
+
+  template <typename R>
+  void clear(R &&tokens) {
+    for (const auto *t : tokens)
+      delete t;
+  }
+  void TearDown() override {
+    clear(funcTokens);
+    clear(nestedTokens);
+    clear(indentNestedTokens);
+  }
+
+  void print(SmallVectorImpl<pretty::Token *> &tokens, size_t margin) {
+    out = "\n";
+    raw_svector_ostream os(out);
+    pretty::PrettyPrinter pp(os, margin);
+    for (auto *t : tokens)
+      pp.add(*t);
+    pp.eof();
+  }
+};
+
+TEST_F(FuncTest, Margin20) {
+  auto constexpr margin = 20;
+  {
+    print(funcTokens, margin);
+    EXPECT_EQ(out.str(), StringRef(R"""(
+foooooo(int a,
+        int b,
+        int a1,
+        int b1,
+        int a2,
+        int b2,
+        int a3,
+        int b3,
+        int a4,
+        int b4,
+        int a5,
+        int b5,
+        int a6,
+        int b6,
+        int a7,
+        int b7,
+        float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        );
+)"""));
+  }
+  {
+    print(nestedTokens, margin);
+    EXPECT_EQ(out.str(), StringRef(R"""(
+baroo(int a, int b,
+      int a1,
+      int b1,
+      int a2,
+      int b2,
+      int a3,
+      int b3,
+      barooga(int a,
+              int b,
+              int a1,
+              int b1,
+              int a2,
+              int b2,
+              int a3,
+              int b3,
+              int a4,
+              int b4,
+              int a5,
+              int b5,
+              int a6,
+              int b6,
+              int a7,
+              int b7,
+              float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+              ),
+      int a4,
+      int b4,
+      int a5,
+      int b5,
+      int a6,
+      int b6,
+      int a7,
+      int b7,
+      float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+      );
+)"""));
+  }
+  {
+    print(indentNestedTokens, margin);
+    EXPECT_EQ(out.str(), StringRef(R"""(
+wahoo(
+  int a, int b,
+  int a1, int b1,
+  int a2, int b2,
+  int a3, int b3,
+  yahooooooo(int a,
+             int b,
+             int a1,
+             int b1,
+             int a2,
+             int b2,
+             int a3,
+             int b3,
+             int a4,
+             int b4,
+             int a5,
+             int b5,
+             int a6,
+             int b6,
+             int a7,
+             int b7,
+             float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+             ),
+  int a4, int b4,
+  int a5, int b5,
+  int a6, int b6,
+  int a7, int b7,
+  float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+);
+)"""));
+  }
+}
+
+TEST_F(FuncTest, Margin50) {
+  auto constexpr margin = 50;
+  {
+    print(funcTokens, margin);
+    EXPECT_EQ(out.str(), StringRef(R"""(
+foooooo(int a, int b, int a1, int b1, int a2,
+        int b2, int a3, int b3, int a4, int b4,
+        int a5, int b5, int a6, int b6, int a7,
+        int b7,
+        float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        );
+)"""));
+  }
+  {
+    print(nestedTokens, margin);
+    EXPECT_EQ(out.str(), StringRef(R"""(
+baroo(int a, int b, int a1, int b1, int a2,
+      int b2, int a3, int b3,
+      barooga(int a, int b, int a1, int b1,
+              int a2, int b2, int a3, int b3,
+              int a4, int b4, int a5, int b5,
+              int a6, int b6, int a7, int b7,
+              float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+              ),
+      int a4, int b4, int a5, int b5, int a6,
+      int b6, int a7, int b7,
+      float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+      );
+)"""));
+  }
+  {
+    print(indentNestedTokens, margin);
+    EXPECT_EQ(out.str(), StringRef(R"""(
+wahoo(
+  int a, int b, int a1, int b1, int a2, int b2,
+  int a3, int b3,
+  yahooooooo(int a, int b, int a1, int b1, int a2,
+             int b2, int a3, int b3, int a4,
+             int b4, int a5, int b5, int a6,
+             int b6, int a7, int b7,
+             float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+             ),
+  int a4, int b4, int a5, int b5, int a6, int b6,
+  int a7, int b7,
+  float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+);
+)"""));
+  }
+}
+
+TEST_F(FuncTest, Margin2048) {
+  auto constexpr margin = 2048;
+  {
+    print(funcTokens, margin);
+    EXPECT_EQ(out.str(), StringRef(R"""(
+foooooo(int a, int b, int a1, int b1, int a2, int b2, int a3, int b3, int a4, int b4, int a5, int b5, int a6, int b6, int a7, int b7, float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+)"""));
+  }
+  {
+    print(nestedTokens, margin);
+    EXPECT_EQ(out.str(), StringRef(R"""(
+baroo(int a, int b, int a1, int b1, int a2, int b2, int a3, int b3, barooga(int a, int b, int a1, int b1, int a2, int b2, int a3, int b3, int a4, int b4, int a5, int b5, int a6, int b6, int a7, int b7, float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx), int a4, int b4, int a5, int b5, int a6, int b6, int a7, int b7, float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+)"""));
+  }
+  {
+    print(indentNestedTokens, margin);
+    EXPECT_EQ(out.str(), StringRef(R"""(
+wahoo(int a, int b, int a1, int b1, int a2, int b2, int a3, int b3, yahooooooo(int a, int b, int a1, int b1, int a2, int b2, int a3, int b3, int a4, int b4, int a5, int b5, int a6, int b6, int a7, int b7, float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx), int a4, int b4, int a5, int b5, int a6, int b6, int a7, int b7, float xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+)"""));
+  }
+}
+
+} // end anonymous namespace
