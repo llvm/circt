5c72a20a647694f0ff229bf5114ac6a957059066
Will Dietz <will.dietz@sifive.com>
2022-10-11 14:02:34 -0500

    PrettyPrinter: init.

---
+++ include/circt/Support/PrettyPrinter.h
@@ -0,0 +1,244 @@
+//===- PrettyPrinter.h - Pretty printing ----------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This implements a pretty-printer.
+// "PrettyPrinting", Derek C. Oppen, 1980.
+// https://dx.doi.org/10.1145/357114.357115
+//
+// This was selected as it is linear in number of tokens O(n) and requires
+// memory O(linewidth).
+//
+// See PrettyPrinter.cpp for more information.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_SUPPORT_PRETTYPRINTER_H
+#define CIRCT_SUPPORT_PRETTYPRINTER_H
+
+#include "circt/Support/LLVM.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/SaveAndRestore.h"
+
+#include <cstdint>
+#include <deque>
+#include <vector>
+
+namespace circt {
+namespace pretty {
+
+//===----------------------------------------------------------------------===//
+// Tokens
+//===----------------------------------------------------------------------===//
+
+enum class Breaks { Consistent, Inconsistent };
+
+class Token {
+public:
+  enum class Kind { String, Break, Begin, End };
+  struct StringInfo {
+    const char *str;
+    uint32_t len;
+  };
+  struct BreakInfo {
+    uint32_t spaces; // how many spaces when not broken
+    int32_t offset;  // amount to adjust indentation level by if breaks here
+  };
+  struct BeginInfo {
+    int32_t offset;
+    Breaks breaks;
+  };
+  struct EndInfo {
+    // Nothing
+  };
+
+private:
+  union {
+    StringInfo stringInfo;
+    BreakInfo breakInfo;
+    BeginInfo beginInfo;
+    EndInfo endInfo;
+  } data;
+  const Kind kind;
+
+protected:
+  template <Kind k>
+  auto &getInfoImpl() {
+    if constexpr (k == Kind::String)
+      return data.stringInfo;
+    if constexpr (k == Kind::Break)
+      return data.breakInfo;
+    if constexpr (k == Kind::Begin)
+      return data.beginInfo;
+    if constexpr (k == Kind::End)
+      return data.endInfo;
+    llvm_unreachable("unhandled token kind");
+  }
+
+  Token(Kind k) : kind(k) {}
+
+public:
+  auto getKind() const { return kind; }
+};
+
+/// Helper class to CRTP-derive common functions.
+template <class DerivedT, Token::Kind DerivedKind>
+struct TokenBase : public Token {
+  static bool classof(const Token *t) { return t->getKind() == DerivedKind; }
+
+protected:
+  TokenBase() : Token(DerivedKind) {}
+  auto &getInfoMut() { return getInfoImpl<DerivedKind>(); }
+
+public:
+  const auto &getInfo() { return getInfoMut(); }
+};
+
+struct StringToken : public TokenBase<StringToken, Token::Kind::String> {
+  StringToken(llvm::StringRef text) {
+    assert(text.size() == (uint32_t)text.size());
+    getInfoMut() = {text.data(), (uint32_t)text.size()};
+  }
+  StringRef text() { return StringRef(getInfo().str, getInfo().len); }
+};
+
+struct BreakToken : public TokenBase<BreakToken, Token::Kind::Break> {
+  BreakToken(uint32_t spaces = 1, int32_t offset = 0) {
+    getInfoMut() = {spaces, offset};
+  }
+  auto spaces() { return getInfo().spaces; }
+  auto offset() { return getInfo().offset; }
+};
+
+struct BeginToken : public TokenBase<BeginToken, Token::Kind::Begin> {
+  BeginToken(int32_t offset = 2, Breaks breaks = Breaks::Inconsistent) {
+    getInfoMut() = {offset, breaks};
+  }
+  auto offset() { return getInfo().offset; }
+  auto breaks() { return getInfo().breaks; }
+};
+
+struct EndToken : public TokenBase<EndToken, Token::Kind::End> {};
+
+//===----------------------------------------------------------------------===//
+// PrettyPrinter
+//===----------------------------------------------------------------------===//
+
+class PrettyPrinter {
+public:
+  /// Listener to Token storage events.
+  struct Listener {
+    virtual ~Listener();
+    /// No tokens referencing external memory are present.
+    virtual void clear(){};
+  };
+
+  // TODO: allow setting starting indentation level!
+  PrettyPrinter(llvm::raw_ostream &os, uint32_t margin,
+                Listener *listener = nullptr)
+      : space(margin), margin(margin), os(os), listener(listener) {
+    assert(margin < kInfinity / 2);
+  }
+
+  /// Add token for printing.  In Oppen, this is "scan".
+  void add(Token t);
+
+  /// Add a range of tokens.
+  template <typename R>
+  void addTokens(R &&tokens) {
+    // Don't invoke listener until range processed, we own it now.
+    {
+      llvm::SaveAndRestore<Listener *> save(listener, nullptr);
+      for (Token &t : tokens)
+        add(t);
+    }
+    // Invoke it now if appropriate.
+    if (scanStack.empty())
+      clear();
+  }
+
+  void eof();
+
+  void setListener(Listener *newListener) { listener = newListener; };
+  auto *getListener() const { return listener; }
+
+  static constexpr uint32_t kInfinity = 0xFFFFFU;
+
+private:
+  /// Format token with tracked size.
+  struct FormattedToken {
+    Token token;  /// underlying token
+    int32_t size; /// calculate size when positive.
+  };
+
+  enum class PrintBreaks { Consistent, Inconsistent, Fits };
+
+  /// Printing information for active scope, stored in printStack.
+  struct PrintEntry {
+    uint32_t offset;
+    PrintBreaks breaks;
+  };
+
+  /// Print out tokens we know sizes for, and drop from token buffer.
+  void advanceLeft();
+
+  /// Break encountered, set sizes of begin/breaks in scanStack we now know.
+  void checkStack();
+
+  /// Check if there's enough tokens to hit width, if so print.
+  /// If scan size is wider than line, it's infinity.
+  void checkStream();
+
+  /// Print a token, maintaining printStack for context.
+  void print(FormattedToken f);
+
+  /// Clear token buffer, scanStack must be empty.
+  void clear();
+
+  /// Characters left on this line.
+  int32_t space;
+
+  /// Sizes: printed, enqueued
+  int32_t leftTotal;
+  int32_t rightTotal;
+
+  /// Unprinted tokens, combination of 'token' and 'size' in Oppen.
+  std::deque<FormattedToken> tokens;
+  /// index of first token, for resolving scanStack entries.
+  uint32_t tokenOffset = 0;
+
+  /// Stack of begin/break tokens, adjust by tokenOffset to index into tokens.
+  std::deque<uint32_t> scanStack;
+
+  /// Stack of printing contexts (indentation + breaking behavior).
+  std::vector<PrintEntry> printStack;
+
+  /// Current indentation level
+  // TODO: implement this!
+  // uint32_t indent;
+
+  /// Whitespace to print before next, tracked to avoid trailing whitespace.
+  // TODO: implement this!
+  // uint32_t pendingIndentation;
+
+  // sizeInfinity
+  // printStack
+
+  /// Target line width.
+  uint32_t margin;
+
+  /// Output stream.
+  llvm::raw_ostream &os;
+
+  /// Hook for Token storage events.
+  Listener *listener = nullptr;
+};
+
+} // end namespace pretty
+} // end namespace circt
+
+#endif // CIRCT_SUPPORT_PRETTYPRINTER_H
---
+++ lib/Support/PrettyPrinter.cpp
@@ -0,0 +1,235 @@
+//===- PrettyPrinter.h - Pretty printing ----------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This implements a pretty-printer.
+// "PrettyPrinting", Derek C. Oppen, 1980.
+// https://dx.doi.org/10.1145/357114.357115
+//
+// This was selected as it is linear in number of tokens O(n) and requires
+// memory O(linewidth).
+//
+// This has been adjusted from the paper:
+// * Deque for tokens instead of ringbuffer + left/right cursors.
+//   This is simpler to reason about and allows us to easily grow the buffer
+//   to accommodate longer widths when needed (and not reserve 3*linewidth).
+//   Since scanStack references buffered tokens by index, we track an offset
+//   that we increase when dropping off the front.
+//   When the scan stack is cleared the buffer is reset, including this offset.
+// * Optionally, minimum amount of space is granted regardless of indentation.
+//   To avoid forcing expressions against the line limit, never try to print
+//   an expression in, say, 2 columns, as this is unlikely to produce good
+//   output.
+//   (TODO)
+// * Indentation tracked from left not relative to margin (linewidth).
+//   (TODO)
+// * Indentation emitted lazily, avoid trailing whitespace.
+//   (TODO)
+//
+// There are many pretty-printing implementations based on this paper,
+// and research literature is rich with functional formulations based originally
+// on this algorithm.
+//
+// Implementations of note that have interesting modifications for their
+// languages and modernization of the paper's algorithm:
+// * prettyplease / rustc_ast_pretty
+//   Pretty-printers for rust, the first being useful for rustfmt-like output.
+//   These have largely the same code and were based on one another.
+//     https://github.com/dtolnay/prettyplease
+//     https://github.com/rust-lang/rust/tree/master/compiler/rustc_ast_pretty
+//   This is closest to the paper's algorithm with modernizations,
+//   and most of the initial tweaks have also been implemented here (thanks!).
+// * swift-format: https://github.com/apple/swift-format/
+//
+// If we want fancier output or need to handle more complicated constructs,
+// both are good references for lessons and ideas.
+//
+// FWIW, at time of writing these have compatible licensing (Apache 2.0).
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Support/PrettyPrinter.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace circt {
+namespace pretty {
+
+// TODO: parameter or not at all
+// auto constexpr MIN_SPACE = 20;
+
+auto constexpr debug = false;
+
+/// Destructor, anchor.
+PrettyPrinter::Listener::~Listener() = default;
+
+/// Add token for printing.  In Oppen, this is "scan".
+void PrettyPrinter::add(Token t) {
+  llvm::TypeSwitch<Token *, void>(&t)
+      .Case([&](StringToken *s) {
+        // If nothing on stack, directly print
+        FormattedToken f{t, (int32_t)s->text().size()};
+        if (scanStack.empty())
+          return print(f);
+        tokens.push_back(f);
+        rightTotal += f.size;
+        checkStream();
+      })
+      .Case([&](BreakToken *b) {
+        if (scanStack.empty())
+          clear();
+        else
+          checkStack();
+        tokens.push_back({t, -rightTotal});
+        scanStack.push_back(tokenOffset + tokens.size() - 1);
+        rightTotal += b->spaces();
+      })
+      .Case([&](BeginToken *b) {
+        if (scanStack.empty())
+          clear();
+        tokens.push_back({t, -rightTotal});
+        scanStack.push_back(tokenOffset + tokens.size() - 1);
+      })
+      .Case([&](EndToken *end) {
+        if (scanStack.empty())
+          return print({t, 0});
+        tokens.push_back({t, -1});
+        scanStack.push_back(tokenOffset + tokens.size() - 1);
+      });
+}
+
+void PrettyPrinter::eof() {
+  if (!scanStack.empty()) {
+    checkStack();
+    advanceLeft();
+  }
+  assert(scanStack.empty() && "unclosed groups at EOF");
+  if (scanStack.empty())
+    clear();
+}
+
+void PrettyPrinter::clear() {
+  assert(scanStack.empty() && "clearing tokens while still on scan stack");
+  leftTotal = rightTotal = 1;
+  assert(tokens.empty());
+  tokens.clear();
+  tokenOffset = 0;
+  if (listener)
+    listener->clear();
+}
+
+/// Break encountered, set sizes of begin/breaks in scanStack that we now know.
+void PrettyPrinter::checkStack() {
+  unsigned depth = 0;
+  while (!scanStack.empty()) {
+    auto x = scanStack.back();
+    auto &t = tokens[x - tokenOffset];
+    if (auto *b = llvm::dyn_cast<BeginToken>(&t.token)) {
+      if (depth == 0)
+        break;
+      scanStack.pop_back();
+      t.size += rightTotal;
+      --depth;
+    } else if (auto *e = llvm::dyn_cast<EndToken>(&t.token)) {
+      scanStack.pop_back();
+      t.size = 1;
+      ++depth;
+    } else {
+      scanStack.pop_back();
+      t.size += rightTotal;
+      if (depth == 0)
+        break;
+    }
+  }
+}
+
+/// Check if there's enough tokens to hit width, if so print.
+/// If scan size is wider than line, it's infinity.
+void PrettyPrinter::checkStream() {
+  // While buffer needs more than 1 line to print, print and consume.
+  assert(!tokens.empty());
+  while (rightTotal - leftTotal > space && !tokens.empty()) {
+
+    // Ran out of space, set size to infinity and take off scan stack.
+    // No need to keep track as we know enough to know this won't fit.
+    if (!scanStack.empty() && tokenOffset == scanStack.front()) {
+      tokens.front().size = kInfinity;
+      scanStack.pop_front();
+    }
+    advanceLeft();
+  }
+}
+
+/// Print out tokens we know sizes for, and drop from token buffer.
+void PrettyPrinter::advanceLeft() {
+  assert(!tokens.empty());
+
+  while (!tokens.empty() && tokens.front().size >= 0) {
+    auto f = tokens.front();
+    tokens.pop_front();
+    ++tokenOffset;
+
+    print(f);
+    leftTotal += llvm::TypeSwitch<Token *, int32_t>(&f.token)
+                     .Case([&](BreakToken *b) { return b->spaces(); })
+                     .Case([&](StringToken *s) { return s->text().size(); })
+                     .Default([](auto *) { return 0; });
+  }
+}
+
+/// Print a token, maintaining printStack for context.
+void PrettyPrinter::print(FormattedToken f) {
+  llvm::TypeSwitch<Token *, void>(&f.token)
+      .Case([&](StringToken *s) {
+        space -= f.size;
+        os << s->text();
+      })
+      .Case([&](BreakToken *b) {
+        // If nothing on print stack (no begin context),
+        // wrap w/no offset and emit greedily.
+        PrintEntry outer{margin, PrintBreaks::Inconsistent};
+        auto &frame = printStack.empty() ? outer : printStack.back();
+        bool fits =
+            frame.breaks == PrintBreaks::Fits ||
+            (frame.breaks == PrintBreaks::Inconsistent && f.size <= space);
+        if (fits) {
+          space -= b->spaces();
+          os.indent(b->spaces());
+        } else {
+          if (debug) {
+            if (space)
+              os << "┆";
+            if (space > 2)
+              os.indent(space - 2);
+            if (space > 1)
+              os << "┇";
+          }
+          os << "\n";
+          space = frame.offset - b->offset();
+          os.indent(std::max<ssize_t>(ssize_t(margin) - space, 0));
+        }
+      })
+      .Case([&](BeginToken *b) {
+        if (f.size > space) {
+          auto breaks = b->breaks() == Breaks::Consistent
+                            ? PrintBreaks::Consistent
+                            : PrintBreaks::Inconsistent;
+          printStack.push_back({uint32_t(space - b->offset()), breaks});
+        } else {
+          printStack.push_back({0, PrintBreaks::Fits});
+        }
+      })
+      .Case([&](EndToken *) {
+        assert(!printStack.empty() && "more ends than begins?");
+        // Try to tolerate this when assertions are disabled.
+        if (!printStack.empty())
+          printStack.pop_back(); // breaks
+      });
+}
+} // end namespace pretty
+} // end namespace circt
