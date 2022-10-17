98308368331332c807689a5c2f4c327a9795d49e
Martin Erhart <maerhart@outlook.com>
2022-10-17 14:17:13 +0200

    [HW/Seq] Fix file headers

--- lib/Dialect/Seq/Transforms/PassDetails.h
+++ lib/Dialect/Seq/Transforms/PassDetails.h
@@ -1,4 +1,4 @@
-//===- PassDetails.h - Seq pass class details -----------===-----*- C++ -*-===//
+//===- PassDetails.h - Seq pass class details -------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
