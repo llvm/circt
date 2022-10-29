0d163b0efd5f35db80aa45079d70fccacea94f4a
John Demme <john.demme@microsoft.com>
2022-10-28 20:41:11 -0700

    docs

--- frontends/PyCDE/integration_test/pytorch/dot_prod_system.py
+++ frontends/PyCDE/integration_test/pytorch/dot_prod_system.py
@@ -19,6 +19,10 @@ import sys
 import time
 from typing import List
 
+################################################################################
+# Harware design
+################################################################################
+
 
 class DotModule(torch.nn.Module):
 
@@ -32,25 +36,6 @@ torch_module = torch_mlir.compile(DotModule(), [shape, shape],
 
 
 @module
-class Gasket:
-  clk = Clock()
-  rst = Input(types.i1)
-
-  @generator
-  def generate(ports):
-    go = TorchControl.go("go")
-    ForwardEsi(clock=ports.clk, reset=ports.rst, in3=go)
-    dot_a.write(TorchControl.a_write("a"))
-    dot_b.write(TorchControl.b_write("b"))
-    read_address = Wire(dot_x.read.to_server_type)
-    read_data = dot_x.read(read_address)
-    read_address.assign(TorchControl.x_read(read_data, "x"))
-    dot_a.instantiate_builtin("sv_mem", inputs=[ports.clk, ports.rst])
-    dot_b.instantiate_builtin("sv_mem", inputs=[ports.clk, ports.rst])
-    dot_x.instantiate_builtin("sv_mem", inputs=[ports.clk, ports.rst])
-
-
-@module
 class top:
   clk = Clock()
   rst = Input(types.i1)
@@ -58,18 +43,57 @@ class top:
   @generator
   def generate(ports):
     Gasket(clk=ports.clk, rst=ports.rst)
+
+    # Use cosim to communicate with the accelerator running in an RTL simulator.
     Cosim(TorchControl, ports.clk, ports.rst)
+
+
+@module
+class Gasket:
+  """Wrap the accelerator IP module. Instantiate the requiste memories. Wire the
+  memories to the host and the host to the module control signals."""
+
+  clk = Clock()
+  rst = Input(types.i1)
+
+  @generator
+  def generate(ports):
+    # Get a 'go' signal from the host.
+    go = TorchControl.go("go")
+
+    # Instantiate the accelerator IP, passing it the 'go' signal from the host.
+    # All other communication is done through ESI services.
+    DotAccelIP(clock=ports.clk, reset=ports.rst, in3=go)
+
+    # Implement the three memories which the IP needs with simple SystemVerilog
+    # unpacked arrays.
+    dot_a.instantiate_builtin("sv_mem", inputs=[ports.clk, ports.rst])
+    dot_b.instantiate_builtin("sv_mem", inputs=[ports.clk, ports.rst])
+    dot_x.instantiate_builtin("sv_mem", inputs=[ports.clk, ports.rst])
+
+    # Give the host access to the memories. Write access to the 'a' and 'b'
+    # memories and read access to the 'x' memory.
+    dot_a.write(TorchControl.a_write("a"))
+    dot_b.write(TorchControl.b_write("b"))
+    read_address = Wire(dot_x.read.to_server_type)
+    read_data = dot_x.read(read_address)
+    read_address.assign(TorchControl.x_read(read_data, "x"))
 
 
 if __name__ == "__main__":
   system = System([top], name="PyTorchDotProd", output_directory=sys.argv[1])
+
+  # Import the torch_mlir module.
   syms = system.import_mlir(torch_module)
 
-  ForwardEsi = syms["forward_esi_wrapper"]
+  # Grab references to the imported IP and requested memories which must
+  # implemented in a gasket/wrapper.
+  DotAccelIP = syms["forward_esi_wrapper"]
   dot_a: ServiceDecl = syms["in0"]
   dot_b: ServiceDecl = syms["in1"]
   dot_x: ServiceDecl = syms["in2"]
 
+  # Define an interface (API) for software.
   @ServiceDecl
   class TorchControl:
     go = FromServer(types.i0)
@@ -77,16 +101,16 @@ if __name__ == "__main__":
     b_write = FromServer(dot_b.write.to_server_type)
     x_read = ToFromServer(dot_x.read.to_client_type, dot_x.read.to_server_type)
 
-  with open("dot_sys.pregen.mlir", "w") as f:
-    f.write(str(system.mod))
+  # Generate the hardware design.
   system.generate()
-  with open("dot_sys.postgen.mlir", "w") as f:
-    f.write(str(system.mod))
-  system.run_passes()
-  with open("dot_sys.postpasses.mlir", "w") as f:
-    f.write(str(system.mod))
+  # Emit SystemVerilog and other collateral.
   system.emit_outputs()
+  # Build a Python API into the accelerator.
   system.build_api("python")
+
+################################################################################
+# Software runtime
+################################################################################
 
 
 def write_vector(vec: List[int], port):
@@ -95,10 +119,18 @@ def write_vector(vec: List[int], port):
 
 
 def hw_dotprod(hw, a: List[int], b: List[int]):
+  # Write the two vectors to device memories.
   write_vector(a, hw.torch_control.a_write[0])
   write_vector(b, hw.torch_control.b_write[0])
+
+  # Tell the dot module to go!
   hw.torch_control.go[0].write()
+
+  # Wait for unit to compute.
+  #       (Hack around missing functionality in XRT bridge.)
   time.sleep(0.01)
+
+  # Read the result.
   x = hw.torch_control.x_read[0]()
   print(f"{a} x {b} = {x}")
   return x
