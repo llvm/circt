bcdf94bd56ca11eff8841d1f5902a7f2601da1e4
Will Dietz <will.dietz@sifive.com>
2022-11-01 18:57:53 -0500

    PrettyPrinter: init, w/convenience classes. (#4192)
    Add PrettyPrinter and supporting classes.  See PR #4192 for more details.

---
+++ lib/Support/PrettyPrinterHelpers.cpp
@@ -0,0 +1,54 @@
+//===- PrettyPrinterHelpers.cpp - Pretty printing helpers -----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Helper classes for using PrettyPrinter.
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Support/PrettyPrinterHelpers.h"
+
+#include "llvm/ADT/SmallString.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace circt {
+namespace pretty {
+
+//===----------------------------------------------------------------------===//
+// Convenience builders.
+//===----------------------------------------------------------------------===//
+
+void TokenStringSaver::clear() { alloc.Reset(); }
+
+/// Add multiple non-breaking spaces as a single token.
+void detail::emitNBSP(unsigned n, llvm::function_ref<void(Token)> add) {
+  static const char spaces[] = {
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+  };
+  auto size = std::size(spaces);
+  if (n < size) {
+    if (n != 0)
+      add(StringToken({spaces, n}));
+    return;
+  }
+  while (n) {
+    auto chunk = std::min<uint32_t>(n, size - 1);
+    add(StringToken({spaces, chunk}));
+    n -= chunk;
+  }
+}
+
+} // end namespace pretty
+} // end namespace circt
