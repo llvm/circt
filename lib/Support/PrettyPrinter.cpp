3ad2040b1d6ca60d19e0f8720c76c43a1cb94933
Will Dietz <will.dietz@sifive.com>
2022-11-01 17:43:55 -0500

    PrettyPrinter: fix header, thanks @youngar!

--- lib/Support/PrettyPrinter.cpp
+++ lib/Support/PrettyPrinter.cpp
@@ -1,4 +1,4 @@
-//===- PrettyPrinter.h - Pretty printing ----------------------------------===//
+//===- PrettyPrinter.cpp - Pretty printing --------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
