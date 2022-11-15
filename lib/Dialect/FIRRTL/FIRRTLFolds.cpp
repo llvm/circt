e0835377b78eea6fde4b156cc81b62bfdca14561
Andrew Lenharth <andrew@lenharth.org>
2022-11-15 06:33:46 -0800

    more comments

--- lib/Dialect/FIRRTL/FIRRTLFolds.cpp
+++ lib/Dialect/FIRRTL/FIRRTLFolds.cpp
@@ -39,7 +39,9 @@ static Value dropWrite(PatternRewriter &rewriter, OpResult old,
 }
 
 // Move a name hint from a soon to be deleted operation to a new operation.
-// Pass through the new operation to make patterns easier to write.
+// Pass through the new operation to make patterns easier to write.  This cannot
+// move a name to a port (block argument), doing so would require rewriting all
+// instance sites as well as the module.
 static Value moveNameHint(OpResult old, Value passthrough) {
   Operation *op = passthrough.getDefiningOp();
   // This should handle ports, but it isn't clear we can change those in
