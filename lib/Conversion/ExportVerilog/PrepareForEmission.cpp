9e8cafd1dd6fa7b0a06ee26cad11e042ccaae4f7
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2022-11-22 00:48:41 -0500

    [SV][ExportVerilog] Add XMRRefOp to ExportVerilog
    Add XMRRefOp emission to ExportVerilog.  Test that this works when
    inlined into instances and bound instances.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- lib/Conversion/ExportVerilog/PrepareForEmission.cpp
+++ lib/Conversion/ExportVerilog/PrepareForEmission.cpp
@@ -49,7 +49,7 @@ bool ExportVerilog::isSimpleReadOrPort(Value v) {
   auto readSrc = read.getInput().getDefiningOp();
   if (!readSrc)
     return false;
-  return isa<WireOp, RegOp, LogicOp, XMROp>(readSrc);
+  return isa<WireOp, RegOp, LogicOp, XMROp, XMRRefOp>(readSrc);
 }
 
 // Check if the value is deemed worth spilling into a wire.
