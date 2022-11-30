468446770e74eee9853856a545629148e3f67eb5
Prithayan Barua <prithayan@gmail.com>
2022-11-30 14:22:17 -0800

    [FIRRTL][LowerXMR] Ignore 0-width RefType values (#4380)
    This commit ensures that zero-width RefType values are ignored and symbols are
     not added for 0-width ports. LowerXMR was already replacing zero-width
     ref.resolve with a constant, but it was still adding an unused symbol on
     zero-width ports.
    This commit adds checks for zero width RefType value, and marks them for
    removal, without recording them in the data structures.

--- lib/Dialect/FIRRTL/Transforms/LowerXMR.cpp
+++ lib/Dialect/FIRRTL/Transforms/LowerXMR.cpp
@@ -59,6 +59,10 @@ class LowerXMRPass : public LowerXMRBase<LowerXMRPass> {
             // Get a reference to the actual signal to which the XMR will be
             // generated.
             Value xmrDef = send.getBase();
+            if (isZeroWidth(send.getType().getType())) {
+              markForRemoval(send);
+              return success();
+            }
             // Get an InnerRefAttr to the xmrDef op. If the operation does not
             // take any InnerSym (like firrtl.add, firrtl.or etc) then create a
             // NodeOp to add the InnerSym.
@@ -130,6 +134,9 @@ class LowerXMRPass : public LowerXMRBase<LowerXMRPass> {
             if (!connect.getSrc().getType().isa<RefType>())
               return success();
             markForRemoval(connect);
+            if (isZeroWidth(
+                    connect.getSrc().getType().cast<RefType>().getType()))
+              return success();
             // Merge the dataflow classes of destination into the source of the
             // Connect. This handles two cases:
             // 1. If the dataflow at the source is known, then the
@@ -147,6 +154,8 @@ class LowerXMRPass : public LowerXMRBase<LowerXMRPass> {
           })
           .Case<RefSubOp>([&](RefSubOp op) {
             markForRemoval(op);
+            if (isZeroWidth(op.getType().getType()))
+              return success();
             auto defMem = dyn_cast<MemOp>(op.getInput().getDefiningOp());
             if (!defMem) {
               defMem.emitOpError("can only lower RefSubOp of Memory");
@@ -169,9 +178,10 @@ class LowerXMRPass : public LowerXMRBase<LowerXMRPass> {
             // XMRs. That is, RefResolveOp can be visited before the
             // corresponding RefSendOp is recorded.
 
-            dataFlowClasses.unionSets(resolve.getRef(), resolve.getResult());
-            resolveOps.push_back(resolve);
             markForRemoval(resolve);
+            if (!isZeroWidth(resolve.getType()))
+              dataFlowClasses.unionSets(resolve.getRef(), resolve.getResult());
+            resolveOps.push_back(resolve);
             return success();
           })
           .Default([&](auto) { return success(); });
@@ -343,7 +353,8 @@ class LowerXMRPass : public LowerXMRBase<LowerXMRPass> {
       // Reference ports must be removed.
       setPortToRemove(inst, portNum, numPorts);
       // Drop the dead-instance-ports.
-      if (instanceResult.use_empty())
+      if (instanceResult.use_empty() ||
+          isZeroWidth(instanceResult.getType().cast<RefType>().getType()))
         continue;
       auto refModuleArg = refMod.getArgument(portNum);
       if (inst.getPortDirection(portNum) == Direction::Out) {
@@ -498,6 +509,8 @@ class LowerXMRPass : public LowerXMRBase<LowerXMRPass> {
     dataflowAt.clear();
     refSendPathList.clear();
   }
+
+  bool isZeroWidth(FIRRTLBaseType t) { return t.getBitWidthOrSentinel() == 0; }
 
   /// Cached module namespaces.
   DenseMap<Operation *, ModuleNamespace> moduleNamespaces;
