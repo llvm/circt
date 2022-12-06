e6cc4d21ca2872fffeeeb0c184a52634a64ea1d5
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2022-12-06 01:30:34 -0500

    [HW] Add FieldIDTypeInterface
    Add a rudimentary type interface to the HW Dialect for representing
    types which contain aggregates which can be indexed/referred to by a
    "field ID".  A field ID is an in-order number of an aggregate type that
    includes the top-level aggregate type (field ID zero), all intermediate
    aggregate types, and all leave types.  This is an interface that was
    created from infrastructure inside the FIRRTL Dialect for its aggregate
    types.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

---
+++ lib/Dialect/HW/HWTypeInterfaces.cpp
@@ -0,0 +1,15 @@
+//===- HWTypeInterfaces.cpp - Implement the HW op interfaces --------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements HW type interfaces
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/HW/HWTypeInterfaces.h"
+
+#include "circt/Dialect/HW/HWTypeInterfaces.cpp.inc"
