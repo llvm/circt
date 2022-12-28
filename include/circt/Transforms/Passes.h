908702333b9bfd01db95b10249ab814938bec48e
Lucas Ramirez <lucas.rami@proton.me>
2022-12-28 15:23:48 +0100

    SSA Maximization pass and utilities
    This commit introduces a pass to convert every function within a module
    into maximal SSA form. The pass's functionality is split up between a
    number of utilities to enable their usage outside the pass.

--- include/circt/Transforms/Passes.h
+++ include/circt/Transforms/Passes.h
@@ -13,6 +13,7 @@
 #ifndef CIRCT_TRANSFORMS_PASSES_H
 #define CIRCT_TRANSFORMS_PASSES_H
 
+#include "circt/Support/LLVM.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Pass/Pass.h"
 #include <limits>
@@ -27,6 +28,7 @@ std::unique_ptr<mlir::Pass> createFlattenMemRefPass();
 std::unique_ptr<mlir::Pass> createFlattenMemRefCallsPass();
 std::unique_ptr<mlir::Pass> createStripDebugInfoWithPredPass(
     const std::function<bool(mlir::Location)> &pred);
+std::unique_ptr<mlir::Pass> createMaximizeSSAPass();
 
 //===----------------------------------------------------------------------===//
 // Utility functions.
@@ -35,6 +37,42 @@ std::unique_ptr<mlir::Pass> createStripDebugInfoWithPredPass(
 // Returns true if the provided memref is considered unidimensional (having a
 // shape of size 1).
 bool isUniDimensional(mlir::MemRefType memref);
+
+// Returns true if the region is into maximal SSA form i.e., if all the values
+// within the region are in maximal SSA form.
+bool isRegionSSAMaximized(Region &region);
+
+/// Converts a single value within a function into maximal SSA form. This
+/// removes any implicit dataflow of this specific value within the enclosing
+/// function. The function adds new block arguments wherever necessary to carry
+/// the value explicitly between blocks.
+/// Succeeds when it was possible to convert the value into maximal SSA form.
+LogicalResult maximizeSSA(Value value, PatternRewriter &rewriter);
+
+/// Converts an operation's results within a function into maximal SSA form.
+/// This removes any implicit dataflow of the operation's results within the
+/// enclosing function. The function adds new block arguments wherever necessary
+/// to carry the results explicitly between blocks.
+/// Succeeds when it was possible to convert the operation's results into
+/// maximal SSA form.
+LogicalResult maximizeSSA(Operation *op, PatternRewriter &rewriter);
+
+/// Converts all values defined by a block (i.e., block arguments and block's
+/// operations' results) within a function into maximal SSA form. This removes
+/// any implicit dataflow of those values within the enclosing function. The
+/// function adds new block arguments wherever necessary to carry the values
+/// explicitly between blocks.
+/// Succeeds when it was possible to convert the values defined by the block
+/// into maximal SSA form.
+LogicalResult maximizeSSA(Block *block, PatternRewriter &rewriter);
+
+/// Converts a region within a function into maximal SSA form. This removes any
+/// implicit dataflow of the region's values. The function adds new block
+/// arguments wherever necessary to carry the region's values explicitly between
+/// blocks.
+//' Succeeds when it was possible to convert the all of the region's values into
+// maximal SSA form.
+LogicalResult maximizeSSA(Region &region, PatternRewriter &rewriter);
 
 //===----------------------------------------------------------------------===//
 // Registration
---
+++ lib/Transforms/MaximizeSSA.cpp
@@ -0,0 +1,224 @@
+//===- MaximizeSSA.cpp - SSA Maximization Pass ------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Contains the definitions of the SSA maximization pass as well as utilities
+// for converting a function with standard control flow into maximal SSA form.
+//
+//===----------------------------------------------------------------------===//
+
+#include "PassDetail.h"
+#include "circt/Transforms/Passes.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <stack>
+
+using namespace mlir;
+using namespace circt;
+
+static Block *getDefiningBlock(Value value) {
+  // Value is either a block argument...
+  if (auto blockArg = dyn_cast<BlockArgument>(value); blockArg)
+    return blockArg.getParentBlock();
+
+  // ... or an operation's result
+  auto *defOp = value.getDefiningOp();
+  assert(defOp);
+  return defOp->getBlock();
+}
+
+static LogicalResult addArgToTerminator(Block *block, Block *predBlock,
+                                        Value value) {
+
+  // Identify terminator branching instruction in predecessor block
+  auto branchOp = dyn_cast<BranchOpInterface>(predBlock->getTerminator());
+  if (!branchOp)
+    return predBlock->getTerminator()->emitError(
+        "Expected terminator operation of block to be a "
+        "branch-like operation");
+
+  // In the predecessor block's terminator, find all successors that equal
+  // the block and add the value to the list of operands it's passed
+  auto successors = branchOp->getSuccessors();
+  for (uint idx = 0, numSucc = successors.size(); idx < numSucc; ++idx) {
+    auto *succBlock = successors[idx];
+    if (succBlock == block) {
+      branchOp.getSuccessorOperands(idx).append(value);
+    }
+  }
+
+  return success();
+}
+
+namespace circt {
+
+bool isRegionSSAMaximized(Region &region) {
+
+  // Check whether all operands used within each block are also defined within
+  // the same block
+  for (auto &block : region.getBlocks())
+    for (auto &op : block.getOperations())
+      for (auto operand : op.getOperands())
+        if (getDefiningBlock(operand) != &block)
+          return false;
+
+  return true;
+}
+
+LogicalResult maximizeSSA(Value value, PatternRewriter &rewriter) {
+
+  // Identify the basic block in which the value is defined
+  Block *defBlock = getDefiningBlock(value);
+
+  // Identify all basic blocks in which the value is used (excluding the
+  // value-defining block)
+  DenseSet<Block *> blocksUsing;
+  for (auto &use : value.getUses()) {
+    auto *block = use.getOwner()->getBlock();
+    if (block != defBlock)
+      blocksUsing.insert(block);
+  }
+
+  // Prepare a stack to store the list of basic blocks that must be modified for
+  // the value to be in maximum SSA form. At all points, blocksUsing is a
+  // non-strict superset of the elements contained in blocksToVisit
+  std::stack<Block *> blocksToVisit;
+  for (auto *block : blocksUsing) {
+    blocksToVisit.push(block);
+  }
+
+  // Backtrack from all blocks using the value to the value-defining basic
+  // block, adding a new block argument for the value along the way. Keep
+  // track of which blocks have already been modified to avoid visiting a block
+  // more than once while backtracking (possible due to branching control flow)
+  DenseMap<Block *, BlockArgument> blockToArg;
+  while (!blocksToVisit.empty()) {
+    // Retrieve the basic block at the top of the stack, remove it from the
+    // stack, and add it to the list of visited blocks
+    auto *block = blocksToVisit.top();
+    blocksToVisit.pop();
+
+    // Add an argument to the block to hold the value
+    blockToArg[block] =
+        block->addArgument(value.getType(), rewriter.getUnknownLoc());
+
+    // In all block predecessors, modify the terminator branching instruction to
+    // also pass the value to the block
+    for (auto *predBlock : block->getPredecessors()) {
+
+      // Modify the terminator instruction
+      if (failed(addArgToTerminator(block, predBlock, value)))
+        return failure();
+
+      // Now the predecessor block is using the value, so we must also make sure
+      // to visit it
+      if (predBlock != defBlock) {
+        auto [_, blockNewlyUsing] = blocksUsing.insert(predBlock);
+        if (blockNewlyUsing) {
+          blocksToVisit.push(predBlock);
+        }
+      }
+    }
+  }
+
+  // Replace all uses of the value with the newly added block arguments
+  SmallVector<Operation *> users;
+  for (auto &use : value.getUses()) {
+    auto *owner = use.getOwner();
+    if (owner->getBlock() != defBlock)
+      users.push_back(owner);
+  }
+  for (auto *user : users)
+    user->replaceUsesOfWith(value, blockToArg[user->getBlock()]);
+
+  return success();
+}
+
+LogicalResult maximizeSSA(Operation *op, PatternRewriter &rewriter) {
+  // Apply SSA maximization on each of the operation's result
+  for (auto res : op->getResults())
+    if (failed(maximizeSSA(res, rewriter)))
+      return failure();
+
+  return success();
+}
+
+LogicalResult maximizeSSA(Block *block, PatternRewriter &rewriter) {
+  // Apply SSA maximization on each of the block's arguments
+  for (auto arg : block->getArguments())
+    if (failed(maximizeSSA(arg, rewriter)))
+      return failure();
+
+  // Apply SSA maximization on each of the block's operations
+  for (auto &op : block->getOperations())
+    if (failed(maximizeSSA(&op, rewriter)))
+      return failure();
+}
+
+LogicalResult maximizeSSA(Region &region, PatternRewriter &rewriter) {
+  // Apply SSA maximization on each of the region's block
+  for (auto &block : region.getBlocks())
+    if (failed(maximizeSSA(&block, rewriter)))
+      return failure();
+
+  assert(isRegionSSAMaximized(region) && "Region isn't SSA maximized");
+
+  return success();
+}
+} // namespace circt
+
+namespace {
+
+struct FuncOpMaxSSAConversion : public OpConversionPattern<func::FuncOp> {
+
+  FuncOpMaxSSAConversion(MLIRContext *ctx) : OpConversionPattern(ctx) {}
+
+  LogicalResult
+  matchAndRewrite(func::FuncOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    LogicalResult conversionStatus = success();
+    rewriter.updateRootInPlace(op, [&] {
+      PatternRewriter &baseRewriter = rewriter;
+      if (failed(maximizeSSA(op.getRegion(), baseRewriter)))
+        conversionStatus = failure();
+    });
+    return success();
+  }
+};
+
+struct MaximizeSSAPass : public MaximizeSSABase<MaximizeSSAPass> {
+public:
+  void runOnOperation() override {
+    ModuleOp m = getOperation();
+    auto *ctx = &getContext();
+
+    RewritePatternSet patterns{ctx};
+    patterns.add<FuncOpMaxSSAConversion>(ctx);
+    ConversionTarget target{*ctx};
+
+    // Each function in the module is turned into maximal SSA form
+    // independently of the others. Function signatures are never modified
+    // by SSA maximization
+    if (failed(applyPartialConversion(m, target, std::move(patterns)))) {
+      signalPassFailure();
+      return;
+    }
+  }
+};
+
+} // namespace
+
+namespace circt {
+std::unique_ptr<mlir::Pass> createMaximizeSSAPass() {
+  return std::make_unique<MaximizeSSAPass>();
+}
+} // namespace circt
