8deab6fbbaa4d6ea389b9c1d29510c58fa45397b
John Demme <john.demme@microsoft.com>
2023-01-21 04:21:52 +0000

    checkpoint

--- frontends/PyCDE/src/module.py
+++ frontends/PyCDE/src/module.py
@@ -145,6 +145,14 @@ class PortProxyBase:
       signal = _obj_to_value(signal, ptype)
     self._output_values[idx] = signal
 
+  def _set_outputs(self, signal_dict: Dict[str, Signal]):
+    """Set outputs from a dictionary of port names to signals."""
+    for name, signal in signal_dict.items():
+      if name not in self._output_port_lookup:
+        raise PortError(f"Could not find output port '{name}'")
+      idx = self._output_port_lookup[name]
+      self._set_output(idx, signal)
+
   def _check_unconnected_outputs(self):
     unconnected_ports = []
     for idx, value in enumerate(self._output_values):
@@ -241,6 +249,7 @@ class ModuleLikeBuilderBase(_PyProxy):
     for idx, (name, port_type) in enumerate(self.inputs):
       proxy_attrs[name] = property(lambda self, idx=idx: self._get_input(idx))
 
+    output_port_lookup: Dict[str, int] = {}
     for idx, (name, port_type) in enumerate(self.outputs):
 
       def fget(self, idx=idx):
@@ -250,6 +259,8 @@ class ModuleLikeBuilderBase(_PyProxy):
         self._set_output(idx, val)
 
       proxy_attrs[name] = property(fget=fget, fset=fset)
+      output_port_lookup[name] = idx
+    proxy_attrs["_output_port_lookup"] = output_port_lookup
 
     return type(self.modcls.__name__ + "Ports", (PortProxyBase,), proxy_attrs)
 
@@ -264,12 +275,18 @@ class ModuleLikeBuilderBase(_PyProxy):
 
       setattr(self.modcls, name, property(fget=fget))
 
+    named_outputs = {}
     for idx, (name, port_type) in enumerate(self.outputs):
 
       def fget(self, idx=idx):
         return Value(self.inst.results[idx])
 
+      named_outputs[name] = fget
       setattr(self.modcls, name, property(fget=fget))
+    setattr(self.modcls,
+            "_outputs",
+            lambda self, outputs=named_outputs:
+            {n: g(self) for n, g in outputs.items()})
 
   @property
   def name(self):
