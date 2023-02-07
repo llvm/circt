d306f39fb54a3d02fe03e47a84abb504ce01e2af
Andrew Lenharth <andrew@lenharth.org>
2023-02-07 10:22:17 -0600

    checkpoint

--- lib/Dialect/FIRRTL/FIRRTLFieldSource.cpp
+++ lib/Dialect/FIRRTL/FIRRTLFieldSource.cpp
@@ -1,3 +1,14 @@
+//===- FIRRTLFieldSource.cpp - Field Source Analysis ------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines a basic points-to like analysis.
+//
+//===----------------------------------------------------------------------===//
 
 
 #include "circt/Dialect/FIRRTL/FIRRTLFieldSource.h"
