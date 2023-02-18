cf2ea58e19d19181aab53977d1de390135aeff48
Andrew Lenharth <darthscsi@users.noreply.github.com>
2023-02-17 21:12:39 -0700

    Handle all data-flow ops in FieldSource (#4673)

--- include/circt/Dialect/FIRRTL/FIRRTLFieldSource.h
+++ include/circt/Dialect/FIRRTL/FIRRTLFieldSource.h
@@ -30,11 +30,31 @@ public:
   explicit FieldSource(Operation *operation);
 
   struct PathNode {
-    PathNode(Value src, ArrayRef<int64_t> ar) : src(src), path(ar) {}
+    PathNode(Value src, ArrayRef<int64_t> ar, Flow flow)
+        : src(src), flow(flow), path(ar) {}
     Value src;
+    Flow flow;
     SmallVector<int64_t, 4> path;
 
+    /// Roots are operations which define the storage or aggregate value.
     bool isRoot() const { return path.empty(); }
+
+    /// Writable sources can appear as a LHS of a connect given this node's
+    /// path.
+    bool isSrcWritable() const { return flow != Flow::Source; }
+
+    /// Transparent sources reflect a value written to them in the same cycle it
+    /// is written.  These are sources which provide dataflow backwards in SSA
+    /// during one logical execution of a module body.
+    /// A port may be transparent (no storage), but not be writable from the
+    /// side of the instance we are on.
+    bool isSrcTransparent() const {
+      // ports are wires
+      if (!src.getDefiningOp())
+        return true;
+      // ports are wires, on this side of the instance too.
+      return isa<WireOp, InstanceOp>(src.getDefiningOp());
+    }
   };
 
   const PathNode *nodeForValue(Value v) const;
@@ -47,7 +67,8 @@ private:
   void visitMem(MemOp mem);
   void visitInst(InstanceOp inst);
 
-  void makeNodeForValue(Value dst, Value src, ArrayRef<int64_t> path);
+  void makeNodeForValue(Value dst, Value src, ArrayRef<int64_t> path,
+                        Flow flow);
 
   DenseMap<Value, PathNode> paths;
 };
