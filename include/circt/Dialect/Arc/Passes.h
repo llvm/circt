0def598d24a7ec21757fd6ba643e4876ace39401
Fabian Schuiki <fabian@schuiki.ch>
2023-02-21 21:25:25 -0800

    [Arc] Add deduplication pass
    Add the `Dedup` pass which deduplicates arcs with identical definitions.
    If arcs differ only by constant values, the pass outlines the constants
    such that the arcs can be deduplicated.
    
    Identical arcs are surprisingly common since splitting modules along the
    fundamental state elements yields a finer-grained subdivision than the
    modules themselves, which has the potential to uncover additional
    redundancies.
    
    Co-authored-by: Martin Erhart <maerhart@outlook.com>
    Co-authored-by: Zachary Yedidia <zyedidia@gmail.com>

---
+++ include/circt/Dialect/Arc/Passes.h
@@ -0,0 +1,30 @@
+//===- Passes.h - Arc dialect passes --------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_ARC_PASSES_H
+#define CIRCT_DIALECT_ARC_PASSES_H
+
+#include "mlir/Pass/Pass.h"
+#include <memory>
+
+namespace mlir {
+class Pass;
+} // namespace mlir
+
+namespace circt {
+namespace arc {
+
+std::unique_ptr<mlir::Pass> createDedupPass();
+
+#define GEN_PASS_REGISTRATION
+#include "circt/Dialect/Arc/Passes.h.inc"
+
+} // namespace arc
+} // namespace circt
+
+#endif // CIRCT_DIALECT_ARC_PASSES_H
---
+++ lib/Dialect/Arc/Transforms/PassDetails.h
@@ -0,0 +1,33 @@
+//===- PassDetails.h ------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// clang-tidy seems to expect the absolute path in the header guard on some
+// systems, so just disable it.
+// NOLINTNEXTLINE(llvm-header-guard)
+#ifndef DIALECT_ARC_TRANSFORMS_PASSDETAILS_H
+#define DIALECT_ARC_TRANSFORMS_PASSDETAILS_H
+
+#include "circt/Dialect/Arc/Dialect.h"
+#include "circt/Dialect/Arc/Ops.h"
+#include "circt/Dialect/Arc/Passes.h"
+#include "circt/Dialect/HW/HWOps.h"
+#include "circt/Dialect/Seq/SeqOps.h"
+#include "mlir/Pass/Pass.h"
+
+namespace circt {
+namespace arc {
+
+using namespace circt;
+
+#define GEN_PASS_CLASSES
+#include "circt/Dialect/Arc/Passes.h.inc"
+
+} // namespace arc
+} // namespace circt
+
+#endif // DIALECT_ARC_TRANSFORMS_PASSDETAILS_H
