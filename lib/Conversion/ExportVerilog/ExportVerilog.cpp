1e73cc87f0b7a872268a7772aaa58c20cf2cc023
Hideto Ueno <uenoku.tokotoko@gmail.com>
2023-03-08 00:29:20 -0800

    tidy

--- lib/Conversion/ExportVerilog/ExportVerilog.cpp
+++ lib/Conversion/ExportVerilog/ExportVerilog.cpp
@@ -161,7 +161,7 @@ static bool isDuplicatableExpression(Operation *op) {
     if (!indexOp || isa<ConstantOp>(indexOp))
       return true;
     if (auto read = dyn_cast<ReadInOutOp>(indexOp)) {
-      auto readSrc = read.getInput().getDefiningOp();
+      auto* readSrc = read.getInput().getDefiningOp();
       // A port or wire is ok to duplicate reads.
       return !readSrc || isa<WireOp, LogicOp>(readSrc);
     }
