4d30dd93a5714b4088e0455dba2ce9e7fc1100ba
Mike Urbach <mikeurbach@gmail.com>
2023-03-14 19:30:27 -0600

    [OM] Initial implementation of the OM dialect.
    This defines the dialect boilerplate, as well as a rationale document
    that explains the intended use case and initial core constructs that
    will be added in future patches.

---
+++ lib/Dialect/OM/OMDialect.cpp
@@ -0,0 +1,17 @@
+//===- OMDialect.cpp - Object Model dialect definition --------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the Object Model dialect definition.
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/OM/OMDialect.h"
+
+#include "circt/Dialect/OM/OMDialect.cpp.inc"
+
+void circt::om::OMDialect::initialize() {}
