7dc6426de85980d67fb1d950a27a85ba930d5eb0
John Demme <john.demme@microsoft.com>
2023-03-28 01:04:51 +0000

    switch to types

--- lib/Dialect/ESI/ESITypes.cpp
+++ lib/Dialect/ESI/ESITypes.cpp
@@ -16,6 +16,7 @@
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/DialectImplementation.h"
 #include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/TypeSwitch.h"
 
 using namespace circt;
@@ -25,6 +26,46 @@ using namespace circt::esi;
 #include "circt/Dialect/ESI/ESITypes.cpp.inc"
 
 AnyType AnyType::get(MLIRContext *context) { return Base::get(context); }
+
+LogicalResult
+WindowType::verify(llvm::function_ref<InFlightDiagnostic()> emitError,
+                   StringAttr name, Type into,
+                   ArrayRef<WindowFrameType> frames) {
+  auto structInto = into.dyn_cast<hw::StructType>();
+  if (!structInto)
+    return emitError() << "only windows into structs are currently supported";
+
+  auto fields = structInto.getElements();
+  for (auto frame : frames) {
+    DenseMap<StringAttr, WindowFieldType> frameFields;
+    for (auto field : frame.getMembers())
+      frameFields[field.getFieldName()] = field;
+
+    while (!fields.empty() && !frameFields.empty()) {
+      hw::StructType::FieldInfo field = fields.front();
+      auto f = frameFields.find(field.name);
+      if (f != frameFields.end()) {
+        uint64_t numItems = f->getSecond().getNumItems();
+        if (numItems > 0) {
+          auto arrField = field.type.dyn_cast<hw::ArrayType>();
+          if (!arrField)
+            return emitError() << "cannot specify num items on non-array field "
+                               << field.name;
+          if (numItems > arrField.getSize())
+            return emitError()
+                   << "num items is larger than array size in field "
+                   << field.name;
+        }
+        frameFields.erase(f);
+      }
+      fields = fields.drop_front();
+    }
+    if (!frameFields.empty())
+      return emitError() << "invalid field name: "
+                         << frameFields.begin()->getSecond().getFieldName();
+  }
+  return success();
+}
 
 void ESIDialect::registerTypes() {
   addTypes<
--- test/Dialect/ESI/windows.mlir
+++ test/Dialect/ESI/windows.mlir
@@ -1,28 +1,19 @@
 // RUN: circt-opt %s -verify-diagnostics | circt-opt -verify-diagnostics | FileCheck %s
 
-!typeA = !hw.struct<header1: i6, header2: i1, header3: i16>
-
-// CHECK-LABEL:   esi.window @TypeAwin1 into !hw.struct<header1: i6, header2: i1, header3: i16> {
-// CHECK-NEXT:      esi.window.frame {
-// CHECK-NEXT:        esi.window.field "header1" : i6
-// CHECK-NEXT:        esi.window.field "header2" : i1
-// CHECK-NEXT:      }
-// CHECK-NEXT:      esi.window.frame {
-// CHECK-NEXT:        esi.window.field "header3" : i16
-// CHECK-NEXT:      }
-// CHECK-NEXT:    }
-esi.window @TypeAwin1 into !typeA {
-  esi.window.frame {
-    esi.window.field "header1" : i6
-    esi.window.field "header2" : i1
-  }
-  esi.window.frame {
-    esi.window.field "header3" : i16
-  }
-}
+!TypeA = !hw.struct<header1: i6, header2: i1, header3: !hw.array<13xi16>>
+!TypeAwin1 = !esi.window<
+  "TypeAwin1", !TypeA, [
+    <"FrameA", [
+      <"header1">,
+      <"header2">
+    ]>,
+    <"FrameB", [
+      <"header3", 3>
+    ]>
+  ]>
 
 
-// CHECK-LABEL:   hw.module.extern @TypeAModuleDst(%windowed: !esi.window<@typeAwin1>)
-hw.module.extern @TypeAModuleDst(%windowed: !esi.window<@typeAwin1>) 
-// CHECK-LABEL:   hw.module.extern @TypeAModuleSrc() -> (windowed: !esi.window<@typeAwin1>)
-hw.module.extern @TypeAModuleSrc() -> (windowed: !esi.window<@typeAwin1>) 
+// CHECK-LABEL:   hw.module.extern @TypeAModuleDst(%windowed: !esi.window<"TypeAwin1", !hw.struct<header1: i6, header2: i1, header3: !hw.array<13xi16>>, [<"FrameA", [<"header1">, <"header2">]>, <"FrameB", [<"header3", 3>]>]>)
+hw.module.extern @TypeAModuleDst(%windowed: !TypeAwin1)
+// CHECK-LABEL:   hw.module.extern @TypeAModuleSrc() -> (windowed: !esi.window<"TypeAwin1", !hw.struct<header1: i6, header2: i1, header3: !hw.array<13xi16>>, [<"FrameA", [<"header1">, <"header2">]>, <"FrameB", [<"header3", 3>]>]>)
+hw.module.extern @TypeAModuleSrc() -> (windowed: !TypeAwin1)
