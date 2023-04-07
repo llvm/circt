6ba57c33c63d5e4e62c33377ac905e8459a85c4c
Will Dietz <will.dietz@sifive.com>
2023-04-07 10:08:20 -0500

    [FIRRTL] NodeOp: don't require type, get from input. (#4956)
    Easier to build, let result type inference handle.
    Works better when inference computes type of other results.

--- lib/Dialect/FIRRTL/Transforms/LowerXMR.cpp
+++ lib/Dialect/FIRRTL/Transforms/LowerXMR.cpp
@@ -108,9 +108,7 @@ class LowerXMRPass : public LowerXMRBase<LowerXMRPass> {
                   opName = name.getValue();
                   nameKind = NameKindEnum::InterestingName;
                 }
-                xmrDef =
-                    b.create<NodeOp>(xmrDef.getType(), xmrDef, opName, nameKind)
-                        .getResult();
+                xmrDef = b.create<NodeOp>(xmrDef, opName, nameKind).getResult();
               }
             }
             // Create a new entry for this RefSendOp. The path is currently
