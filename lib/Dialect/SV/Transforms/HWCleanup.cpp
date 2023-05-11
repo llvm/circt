0dc1e82317fa9107e5fd6b3f1663b23334a92625
Hideto Ueno <uenoku.tokotoko@gmail.com>
2023-05-11 01:17:58 -0700

    [HWCleanUp] Remove always_comb

--- lib/Dialect/SV/Transforms/HWCleanup.cpp
+++ lib/Dialect/SV/Transforms/HWCleanup.cpp
@@ -155,7 +155,6 @@ void HWCleanupPass::runOnGraphRegion(Region &region) {
   DenseSet<Operation *, AlwaysLikeOpInfo> alwaysFFOpsSeen;
   llvm::SmallDenseMap<Attribute, Operation *, 4> ifdefOps;
   sv::InitialOp initialOpSeen;
-  sv::AlwaysCombOp alwaysCombOpSeen;
 
   for (Operation &op : llvm::make_early_inc_range(body)) {
     // Merge alwaysff and always operations by hashing them to check to see if
@@ -187,14 +186,6 @@ void HWCleanupPass::runOnGraphRegion(Region &region) {
       if (initialOpSeen)
         mergeOperationsIntoFrom(initialOp, initialOpSeen);
       initialOpSeen = initialOp;
-      continue;
-    }
-
-    // Merge always_comb ops anywhere in the module.
-    if (auto alwaysComb = dyn_cast<sv::AlwaysCombOp>(op)) {
-      if (alwaysCombOpSeen)
-        mergeOperationsIntoFrom(alwaysComb, alwaysCombOpSeen);
-      alwaysCombOpSeen = alwaysComb;
       continue;
     }
   }
