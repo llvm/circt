58f1ee546648462e1d4539e47abe29d73913697a
Mike Urbach <mikeurbach@gmail.com>
2023-05-11 21:16:00 -0600

    [firtool] Erase extra verbatim files before printing final MLIR.
    There may be several files generated during any given run of firtool,
    represented as sv.verbatim ops with output_file attributes. This
    information isn't needed in the final MLIR that is emitted, so erase
    those from the MLIR right before emitting it.

--- tools/firtool/firtool.cpp
+++ tools/firtool/firtool.cpp
@@ -22,6 +22,7 @@
 #include "circt/Dialect/HW/HWDialect.h"
 #include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/SV/SVDialect.h"
+#include "circt/Dialect/SV/SVOps.h"
 #include "circt/Dialect/SV/SVPasses.h"
 #include "circt/Dialect/Seq/SeqDialect.h"
 #include "circt/Dialect/Seq/SeqPasses.h"
@@ -438,6 +439,37 @@ static LogicalResult printOp(Operation *op, raw_ostream &os) {
   return success();
 }
 
+// If requested, finalize and print the MLIR into mlirOutFile. The final MLIR
+// will erase any sv.verbatim ops that represented contents for sideband files,
+// so this should always be called after ExportVerilog.
+static LogicalResult finalizeAndPrintMlir(ModuleOp moduleOp) {
+  if (mlirOutFile.empty())
+    return success();
+
+  std::string mlirOutError;
+  auto mlirFile = openOutputFile(mlirOutFile, &mlirOutError);
+  if (!mlirFile) {
+    llvm::errs() << mlirOutError;
+    return failure();
+  }
+
+  // Finalize the MLIR by erasing any sv.verbatim ops for sideband files.
+  for (auto verbatim : llvm::make_early_inc_range(
+           moduleOp.getBodyRegion().getOps<sv::VerbatimOp>()))
+    if (auto outputFile = verbatim->getAttrOfType<hw::OutputFileAttr>(
+            hw::OutputFileAttr::getMnemonic()))
+      if (!outputFile.isDirectory() &&
+          outputFile.getExcludeFromFilelist().getValue())
+        verbatim.erase();
+
+  // Print the final MLIR.
+  if (failed(printOp(moduleOp, mlirFile->os())))
+    return failure();
+  mlirFile->keep();
+
+  return success();
+}
+
 /// Process a single buffer of the input.
 static LogicalResult processBuffer(
     MLIRContext &context, TimingScope &ts, llvm::SourceMgr &sourceMgr,
@@ -809,19 +841,9 @@ static LogicalResult processBuffer(
       return failure();
   }
 
-  // If requested, print the final MLIR into mlirOutFile.
-  if (!mlirOutFile.empty()) {
-    std::string mlirOutError;
-    auto mlirFile = openOutputFile(mlirOutFile, &mlirOutError);
-    if (!mlirFile) {
-      llvm::errs() << mlirOutError;
-      return failure();
-    }
-
-    if (failed(printOp(*module, mlirFile->os())))
-      return failure();
-    mlirFile->keep();
-  }
+  // If requested, finalize and print the MLIR into mlirOutFile.
+  if (failed(finalizeAndPrintMlir(*module)))
+    return failure();
 
   // We intentionally "leak" the Module into the MLIRContext instead of
   // deallocating it.  There is no need to deallocate it right before process
