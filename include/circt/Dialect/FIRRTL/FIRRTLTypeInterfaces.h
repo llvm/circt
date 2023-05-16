2d053b32c9c0e5dfd22250321fa0b29717757d70
Robert Young <rwy0717@gmail.com>
2023-05-16 15:03:26 -0400

    [FIRRTl] Add a type interface for querying phase compatibility (#5206)
    Phases separate the values in a firrtl program into two worlds, the property
    world and the hardware world. Eventually, a firrtl program will be compiled
    into two artifacts:
    
    1. The hardware design (verilog)
    2. The metadata (om dialect mlir bytecode)
    
    The property phased values will be erased from the hardware design and only
    exist in the metadata artifact, while the hardware phased values will be erased
    from the metadata and only exist in the hardware design.

---
+++ include/circt/Dialect/FIRRTL/FIRRTLTypeInterfaces.h
@@ -0,0 +1,23 @@
+//===- FIRRTLTypeInterfaces.h - FIRRTL Type Interfaces ----------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the type interfaces of the FIRRTL dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_FIRRTL_FIRRTLTYPEINTERFACES_H
+#define CIRCT_DIALECT_FIRRTL_FIRRTLTYPEINTERFACES_H
+
+#include "circt/Dialect/FIRRTL/FIRRTLDialect.h"
+#include "circt/Support/LLVM.h"
+
+#include "mlir/IR/Types.h"
+
+#include "circt/Dialect/FIRRTL/FIRRTLTypeInterfaces.h.inc"
+
+#endif // CIRCT_DIALECT_FIRRTL_FIRRTLTYPEINTERFACES_H
