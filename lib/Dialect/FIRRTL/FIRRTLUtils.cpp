d6faa2607a34a43f9e025eb9432752c5ccde6e98
Will Dietz <will.dietz@sifive.com>
2023-06-06 16:30:50 -0500

    [FIRRTL] Add util support for refsub/open aggs (#5315)
    * Support RefSub in getFieldRefFromValue
    * Support OpenAggs/Refsub in getValueByFieldID.
    
    Also, error out hard if encounter unsupported type instead of cast failure/miscompilation.

--- lib/Dialect/FIRRTL/FIRRTLUtils.cpp
+++ lib/Dialect/FIRRTL/FIRRTLUtils.cpp
@@ -455,25 +455,34 @@ FieldRef circt::firrtl::getFieldRefFromValue(Value value) {
     if (!op)
       break;
 
-    auto handled = TypeSwitch<Operation *, bool>(op)
-                       .Case<SubfieldOp, OpenSubfieldOp>([&](auto subfieldOp) {
-                         value = subfieldOp.getInput();
-                         auto bundleType = subfieldOp.getInput().getType();
-                         // Rebase the current index on the parent field's
-                         // index.
-                         id +=
-                             bundleType.getFieldID(subfieldOp.getFieldIndex());
-                         return true;
-                       })
-                       .Case<SubindexOp, OpenSubindexOp>([&](auto subindexOp) {
-                         value = subindexOp.getInput();
-                         auto vecType = subindexOp.getInput().getType();
-                         // Rebase the current index on the parent field's
-                         // index.
-                         id += vecType.getFieldID(subindexOp.getIndex());
-                         return true;
-                       })
-                       .Default(false);
+    auto handled =
+        TypeSwitch<Operation *, bool>(op)
+            .Case<SubfieldOp, OpenSubfieldOp>([&](auto subfieldOp) {
+              value = subfieldOp.getInput();
+              auto bundleType = subfieldOp.getInput().getType();
+              // Rebase the current index on the parent field's
+              // index.
+              id += bundleType.getFieldID(subfieldOp.getFieldIndex());
+              return true;
+            })
+            .Case<SubindexOp, OpenSubindexOp>([&](auto subindexOp) {
+              value = subindexOp.getInput();
+              auto vecType = subindexOp.getInput().getType();
+              // Rebase the current index on the parent field's
+              // index.
+              id += vecType.getFieldID(subindexOp.getIndex());
+              return true;
+            })
+            .Case<RefSubOp>([&](RefSubOp refSubOp) {
+              value = refSubOp.getInput();
+              auto refInputType = refSubOp.getInput().getType();
+              id += TypeSwitch<FIRRTLBaseType, size_t>(refInputType.getType())
+                        .Case<FVectorType, BundleType>([&](auto type) {
+                          return type.getFieldID(refSubOp.getIndex());
+                        });
+              return true;
+            })
+            .Default(false);
     if (!handled)
       break;
   }
@@ -578,17 +587,34 @@ Value circt::firrtl::getValueByFieldID(ImplicitLocOpBuilder builder,
                                        Value value, unsigned fieldID) {
   // When the fieldID hits 0, we've found the target value.
   while (fieldID != 0) {
-    auto type = value.getType();
-    if (auto bundle = type.dyn_cast<BundleType>()) {
-      auto index = bundle.getIndexForFieldID(fieldID);
-      value = builder.create<SubfieldOp>(value, index);
-      fieldID -= bundle.getFieldID(index);
-    } else {
-      auto vector = type.cast<FVectorType>();
-      auto index = vector.getIndexForFieldID(fieldID);
-      value = builder.create<SubindexOp>(value, index);
-      fieldID -= vector.getFieldID(index);
-    }
+    TypeSwitch<Type, void>(value.getType())
+        .Case<BundleType, OpenBundleType>([&](auto bundle) {
+          auto index = bundle.getIndexForFieldID(fieldID);
+          value = builder.create<SubfieldOp>(value, index);
+          fieldID -= bundle.getFieldID(index);
+        })
+        .Case<FVectorType, OpenVectorType>([&](auto vector) {
+          auto index = vector.getIndexForFieldID(fieldID);
+          value = builder.create<SubindexOp>(value, index);
+          fieldID -= vector.getFieldID(index);
+        })
+        .Case<RefType>([&](auto reftype) {
+          TypeSwitch<FIRRTLBaseType, void>(reftype.getType())
+              .template Case<BundleType, FVectorType>([&](auto type) {
+                auto index = type.getIndexForFieldID(fieldID);
+                value = builder.create<RefSubOp>(value, index);
+                fieldID -= type.getFieldID(index);
+              })
+              .Default([&](auto _) {
+                llvm::report_fatal_error(
+                    "unrecognized type for indexing through with fieldID");
+              });
+        })
+        // TODO: Plumb error case out and handle in callers.
+        .Default([&](auto _) {
+          llvm::report_fatal_error(
+              "unrecognized type for indexing through with fieldID");
+        });
   }
   return value;
 }
