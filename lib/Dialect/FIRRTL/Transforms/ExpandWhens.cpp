442d1ffd9347c9c906dedf5c51eea0886e5b8132
Andrew Young <youngar17@gmail.com>
2023-06-12 17:16:48 -0700

    [FIRRTL][ExpandWhens] Support properties (#5373)
    This change adds initialization checking of Property typed ports to the
    ExpandWhens pass.  This ensures that output properties of modules and
    input properties of instances are driven by a PropAssignOp.

--- lib/Dialect/FIRRTL/Transforms/ExpandWhens.cpp
+++ lib/Dialect/FIRRTL/Transforms/ExpandWhens.cpp
@@ -333,6 +333,10 @@ public:
     recordConnect(getFieldRefFromValue(op.getDest()), op);
   }
 
+  void visitStmt(PropAssignOp op) {
+    recordConnect(getFieldRefFromValue(op.getDest()), op);
+  }
+
   void processWhenOp(WhenOp whenOp, Value outerCondition);
 
   /// Combine the connect statements from each side of the block. There are 5
