3153652111a06afe370426450ac92d506fd02318
John Demme <john.demme@microsoft.com>
2023-06-20 16:06:38 -0700

    [LowerComb] Truth table to mux tree lowering (#5405)
    Add a `truth_table` to mux tree lowering to the LowerComb pass.

--- lib/Dialect/Comb/Transforms/LowerComb.cpp
+++ lib/Dialect/Comb/Transforms/LowerComb.cpp
@@ -23,6 +23,49 @@ namespace comb {
 } // namespace circt
 
 namespace {
+/// Lower truth tables to mux trees.
+struct TruthTableToMuxTree : public OpConversionPattern<TruthTableOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+private:
+  /// Get a mux tree for `inputs` corresponding to the given truth table. Do
+  /// this recursively by dividing the table in half for each input.
+  // NOLINTNEXTLINE(misc-no-recursion)
+  Value getMux(Location loc, OpBuilder &b, Value t, Value f,
+               ArrayRef<bool> table, Operation::operand_range inputs) const {
+    assert(table.size() == (1ull << inputs.size()));
+    if (table.size() == 1)
+      return table.front() ? t : f;
+
+    size_t half = table.size() / 2;
+    Value if1 =
+        getMux(loc, b, t, f, table.drop_front(half), inputs.drop_front());
+    Value if0 =
+        getMux(loc, b, t, f, table.drop_back(half), inputs.drop_front());
+    return b.create<MuxOp>(loc, inputs.front(), if1, if0, false);
+  }
+
+public:
+  LogicalResult matchAndRewrite(TruthTableOp op, OpAdaptor adaptor,
+                                ConversionPatternRewriter &b) const override {
+    Location loc = op.getLoc();
+    SmallVector<bool> table(
+        llvm::map_range(op.getLookupTableAttr().getAsValueRange<IntegerAttr>(),
+                        [](const APInt &a) { return !a.isZero(); }));
+    Value t = b.create<hw::ConstantOp>(loc, b.getIntegerAttr(b.getI1Type(), 1));
+    Value f = b.create<hw::ConstantOp>(loc, b.getIntegerAttr(b.getI1Type(), 0));
+
+    Value tree = getMux(loc, b, t, f, table, op.getInputs());
+    b.updateRootInPlace(tree.getDefiningOp(), [&]() {
+      tree.getDefiningOp()->setDialectAttrs(op->getDialectAttrs());
+    });
+    b.replaceOp(op, tree);
+    return success();
+  }
+};
+} // namespace
+
+namespace {
 class LowerCombPass : public impl::LowerCombBase<LowerCombPass> {
 public:
   using LowerCombBase::LowerCombBase;
@@ -31,4 +74,16 @@ public:
 };
 } // namespace
 
-void LowerCombPass::runOnOperation() {}
+void LowerCombPass::runOnOperation() {
+  ModuleOp module = getOperation();
+
+  ConversionTarget target(getContext());
+  RewritePatternSet patterns(&getContext());
+  target.markUnknownOpDynamicallyLegal([](Operation *) { return true; });
+  target.addIllegalOp<TruthTableOp>();
+
+  patterns.add<TruthTableToMuxTree>(patterns.getContext());
+
+  if (failed(applyPartialConversion(module, target, std::move(patterns))))
+    return signalPassFailure();
+}
