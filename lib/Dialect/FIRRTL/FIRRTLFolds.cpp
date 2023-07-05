8077de82a6fafed6248edc2a15ba39a23103c893
Andrew Lenharth <darthscsi@users.noreply.github.com>
2023-07-05 11:29:02 -0500

    [FIRRTL] Canonicalize away CVT and adjust all patterns which matched cvt (#5527)
    closes #5524

--- lib/Dialect/FIRRTL/FIRRTLFolds.cpp
+++ lib/Dialect/FIRRTL/FIRRTLFolds.cpp
@@ -581,8 +581,8 @@ void AndPrimOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                             MLIRContext *context) {
   results.insert<patterns::extendAnd, patterns::moveConstAnd,
                  patterns::AndOfZero, patterns::AndOfAllOne,
-                 patterns::AndOfSelf, patterns::AndOfPad, patterns::AndCvtU>(
-      context);
+                 patterns::AndOfSelf, patterns::AndOfPadU, patterns::AndOfPadS,
+                 patterns::AndOfAsSIntL, patterns::AndOfAsSIntR>(context);
 }
 
 OpFoldResult OrPrimOp::fold(FoldAdaptor adaptor) {
@@ -1041,6 +1041,11 @@ OpFoldResult CvtPrimOp::fold(FoldAdaptor adaptor) {
   return {};
 }
 
+void CvtPrimOp::getCanonicalizationPatterns(RewritePatternSet &results,
+                                            MLIRContext *context) {
+  results.insert<patterns::CVTSigned, patterns::CVTUnSigned>(context);
+}
+
 OpFoldResult NegPrimOp::fold(FoldAdaptor adaptor) {
   if (!hasKnownWidthIntTypes(*this))
     return {};
@@ -1091,8 +1096,10 @@ OpFoldResult AndRPrimOp::fold(FoldAdaptor adaptor) {
 
 void AndRPrimOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                              MLIRContext *context) {
-  results.insert<patterns::AndRasSInt, patterns::AndRasUInt, patterns::AndRCvtU,
-                 patterns::AndRCvtS>(context);
+  results
+      .insert<patterns::AndRasSInt, patterns::AndRasUInt, patterns::AndRPadU,
+              patterns::AndRPadS, patterns::AndRCatOneL, patterns::AndRCatOneR,
+              patterns::AndRCatZeroL, patterns::AndRCatZeroR>(context);
 }
 
 OpFoldResult OrRPrimOp::fold(FoldAdaptor adaptor) {
@@ -1116,7 +1123,7 @@ OpFoldResult OrRPrimOp::fold(FoldAdaptor adaptor) {
 
 void OrRPrimOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                             MLIRContext *context) {
-  results.insert<patterns::OrRasSInt, patterns::OrRasUInt, patterns::OrRCvt,
+  results.insert<patterns::OrRasSInt, patterns::OrRasUInt, patterns::OrRPadU,
                  patterns::OrRCatZeroH, patterns::OrRCatZeroL>(context);
 }
 
@@ -1140,7 +1147,7 @@ OpFoldResult XorRPrimOp::fold(FoldAdaptor adaptor) {
 
 void XorRPrimOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                              MLIRContext *context) {
-  results.insert<patterns::XorRasSInt, patterns::XorRasUInt, patterns::XorRCvt,
+  results.insert<patterns::XorRasSInt, patterns::XorRasUInt, patterns::XorRPadU,
                  patterns::XorRCatZeroH, patterns::XorRCatZeroL>(context);
 }
 
@@ -1244,8 +1251,9 @@ OpFoldResult BitsPrimOp::fold(FoldAdaptor adaptor) {
 
 void BitsPrimOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                              MLIRContext *context) {
-  results.insert<patterns::BitsOfBits, patterns::BitsOfMux,
-                 patterns::BitsOfAsUInt>(context);
+  results
+      .insert<patterns::BitsOfBits, patterns::BitsOfMux, patterns::BitsOfAsUInt,
+              patterns::BitsOfAnd, patterns::BitsOfPad>(context);
 }
 
 /// Replace the specified operation with a 'bits' op from the specified hi/lo
