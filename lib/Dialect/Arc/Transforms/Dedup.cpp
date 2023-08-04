cf67458a99b2ab1ad0f0bd776d32c913d01f9ca7
Andrew Lenharth <andrew@lenharth.org>
2023-08-04 09:57:25 -0500

    bump to head

--- lib/Dialect/Arc/Transforms/Dedup.cpp
+++ lib/Dialect/Arc/Transforms/Dedup.cpp
@@ -321,7 +321,7 @@ private:
 } // namespace
 
 static void addCallSiteOperands(
-    MutableArrayRef<CallOpMutableInterface> callSites,
+    MutableArrayRef<mlir::CallOpInterface> callSites,
     ArrayRef<std::variant<Operation *, unsigned>> operandMappings) {
   SmallDenseMap<Operation *, Operation *> clonedOps;
   SmallVector<Value> newOperands;
@@ -358,7 +358,7 @@ struct DedupPass : public DedupBase<DedupPass> {
   /// A mapping from arc names to arc definitions.
   DenseMap<StringAttr, DefineOp> arcByName;
   /// A mapping from arc definitions to call sites.
-  DenseMap<DefineOp, SmallVector<CallOpMutableInterface, 1>> callSites;
+  DenseMap<DefineOp, SmallVector<mlir::CallOpInterface, 1>> callSites;
 };
 
 struct ArcHash {
@@ -384,7 +384,7 @@ void DedupPass::runOnOperation() {
   }
 
   // Collect the arc call sites.
-  getOperation().walk([&](CallOpMutableInterface callOp) {
+  getOperation().walk([&](mlir::CallOpInterface callOp) {
     if (auto defOp =
             dyn_cast_or_null<DefineOp>(callOp.resolveCallable(&symbolTable)))
       callSites[arcByName.lookup(callOp.getCallableForCallee()
