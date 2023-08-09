58ee6b02e495c6e061bcb87c4ae4908fe659e0b9
Dragon-Git <1762578117@qq.com>
2023-08-09 21:58:33 +0800

    teqdruid's feedback

--- frontends/PyCDE/src/bit_pat.py
+++ frontends/PyCDE/src/bit_pat.py
@@ -1,7 +1,35 @@
-"""
-Based on magma BitPat
+# ===============================================================================
+# Magma Release License
+# ===============================================================================
+# MIT License
+
+# Copyright (C) 2016 Stanford University.
+# All rights reserved.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+""" This code includes modifications based on code licensed under the MIT license. 
+Based on magma bit_pattern
 https://github.com/phanrahan/magma/blob/master/magma/types/bit_pattern.py
 """
+
+#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+#  See https://llvm.org/LICENSE.txt for license information.
+#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 import string
 
 from .types import Bits
@@ -40,7 +68,7 @@ class BitPat:
     self.bits = Bits(count)(bits)
     self.mask = Bits(count)(mask)
     self.width = count
-    self.const = self.mask == Bits(count)((1 << self.width) - 1)
+    self.const = mask == ((1 << count) - 1)
     self.__hash = hash(pattern)
 
   def __eq__(self, other):
@@ -59,7 +87,7 @@ class BitPat:
 
   def as_bits(self):
     if not self.const:
-      raise TypeError("Can only convert BitPat with no don't cares to int")
+      raise TypeError("Can only convert BitPat with no don't cares to Bits")
     return self.bits
 
   def __hash__(self):
--- frontends/PyCDE/test/test_bit_pat.py
+++ frontends/PyCDE/test/test_bit_pat.py
@@ -6,60 +6,49 @@ from pycde.testing import unittestmodule
 
 from pycde.bit_pat import BitPat, dict_lookup
 
-
-# CHECK-LABEL:  msft.module @TestBitPat {} (%inp1: ui36, %inp2: si25, %inp3: i32) -> (out1: i32, out2: i32, match: i1, no_match: i1)
+# CHECK:  msft.module @TestBitPat {} (%inp1: ui36, %inp2: si25, %inp3: i32) -> (out1: i32, out2: i32, match: i1, no_match: i1)
 # CHECK-NEXT:     %c3_i32 = hw.constant 3 : i32
 # CHECK-NEXT:     %c28799_i32 = hw.constant 28799 : i32
-# CHECK-NEXT:     %c-1_i32 = hw.constant -1 : i32
-# CHECK-NEXT:     %0 = comb.icmp bin eq %c28799_i32, %c-1_i32 : i32
 # CHECK-NEXT:     %c4099_i32 = hw.constant 4099 : i32
 # CHECK-NEXT:     %c28799_i32_0 = hw.constant 28799 : i32
-# CHECK-NEXT:     %c-1_i32_1 = hw.constant -1 : i32
-# CHECK-NEXT:     %1 = comb.icmp bin eq %c28799_i32_0, %c-1_i32_1 : i32
 # CHECK-NEXT:     %c8195_i32 = hw.constant 8195 : i32
-# CHECK-NEXT:     %c28799_i32_2 = hw.constant 28799 : i32
-# CHECK-NEXT:     %c-1_i32_3 = hw.constant -1 : i32
-# CHECK-NEXT:     %2 = comb.icmp bin eq %c28799_i32_2, %c-1_i32_3 : i32
+# CHECK-NEXT:     %c28799_i32_1 = hw.constant 28799 : i32
 # CHECK-NEXT:     %c16387_i32 = hw.constant 16387 : i32
-# CHECK-NEXT:     %c28799_i32_4 = hw.constant 28799 : i32
-# CHECK-NEXT:     %c-1_i32_5 = hw.constant -1 : i32
-# CHECK-NEXT:     %3 = comb.icmp bin eq %c28799_i32_4, %c-1_i32_5 : i32
+# CHECK-NEXT:     %c28799_i32_2 = hw.constant 28799 : i32
 # CHECK-NEXT:     %c20483_i32 = hw.constant 20483 : i32
-# CHECK-NEXT:     %c28799_i32_6 = hw.constant 28799 : i32
-# CHECK-NEXT:     %c-1_i32_7 = hw.constant -1 : i32
-# CHECK-NEXT:     %4 = comb.icmp bin eq %c28799_i32_6, %c-1_i32_7 : i32
+# CHECK-NEXT:     %c28799_i32_3 = hw.constant 28799 : i32
 # CHECK-NEXT:     %c2989_i32 = hw.constant 2989 : i32
 # CHECK-NEXT:     %c64206_i32 = hw.constant 64206 : i32
 # CHECK-NEXT:     %c3054_i32 = hw.constant 3054 : i32
 # CHECK-NEXT:     %c48879_i32 = hw.constant 48879 : i32
 # CHECK-NEXT:     %c3245_i32 = hw.constant 3245 : i32
-# CHECK-NEXT:     %5 = hwarith.cast %inp1 : (ui36) -> i32
-# CHECK-NEXT:     %6 = comb.and bin %5, %c28799_i32 : i32
-# CHECK-NEXT:     %7 = comb.icmp bin eq %c28799_i32, %6 : i32
-# CHECK-NEXT:     %8 = hwarith.cast %inp2 : (si25) -> i32
-# CHECK-NEXT:     %9 = comb.and bin %8, %c28799_i32 : i32
-# CHECK-NEXT:     %10 = comb.icmp bin ne %c3_i32, %9 : i32
+# CHECK-NEXT:     %0 = hwarith.cast %inp1 : (ui36) -> i32
+# CHECK-NEXT:     %1 = comb.and bin %0, %c28799_i32 : i32
+# CHECK-NEXT:     %2 = comb.icmp bin eq %c28799_i32, %1 : i32
+# CHECK-NEXT:     %3 = hwarith.cast %inp2 : (si25) -> i32
+# CHECK-NEXT:     %4 = comb.and bin %3, %c28799_i32 : i32
+# CHECK-NEXT:     %5 = comb.icmp bin ne %c3_i32, %4 : i32
 # CHECK-NEXT:     %c19_i32 = hw.constant 19 : i32
-# CHECK-NEXT:     %c-1_i32_8 = hw.constant -1 : i32
-# CHECK-NEXT:     %c-1_i32_9 = hw.constant -1 : i32
-# CHECK-NEXT:     %11 = comb.icmp bin eq %c-1_i32_8, %c-1_i32_9 : i32
+# CHECK-NEXT:     %c-1_i32 = hw.constant -1 : i32
 # CHECK-NEXT:     %c0_i32 = hw.constant 0 : i32
-# CHECK-NEXT:     %12 = comb.and bin %inp3, %c28799_i32 : i32
-# CHECK-NEXT:     %13 = comb.icmp bin eq %c28799_i32, %12 : i32
-# CHECK-NEXT:     %14 = comb.mux bin %13, %c2989_i32, %c0_i32 {sv.namehint = "mux_None_in0_in1"} : i32
-# CHECK-NEXT:     %15 = comb.and bin %inp3, %c28799_i32_0 : i32
-# CHECK-NEXT:     %16 = comb.icmp bin eq %c28799_i32_0, %15 : i32
-# CHECK-NEXT:     %17 = comb.mux bin %16, %c64206_i32, %14 {sv.namehint = "mux_None_mux_None_in0_in1_in1"} : i32
-# CHECK-NEXT:     %18 = comb.and bin %inp3, %c28799_i32_2 : i32
-# CHECK-NEXT:     %19 = comb.icmp bin eq %c28799_i32_2, %18 : i32
-# CHECK-NEXT:     %20 = comb.mux bin %19, %c3054_i32, %17 {sv.namehint = "mux_None_mux_None_mux_None_in0_in1_in1_in1"} : i32
-# CHECK-NEXT:     %21 = comb.and bin %inp3, %c28799_i32_4 : i32
-# CHECK-NEXT:     %22 = comb.icmp bin eq %c28799_i32_4, %21 : i32
-# CHECK-NEXT:     %23 = comb.mux bin %22, %c48879_i32, %20 {sv.namehint = "mux_None_mux_None_mux_None_mux_None_in0_in1_in1_in1_in1"} : i32
-# CHECK-NEXT:     %24 = comb.and bin %inp3, %c28799_i32_6 : i32
-# CHECK-NEXT:     %25 = comb.icmp bin eq %c28799_i32_6, %24 : i32
-# CHECK-NEXT:     %26 = comb.mux bin %25, %c3245_i32, %23 {sv.namehint = "dict_lookup"} : i32
-# CHECK-NEXT:     msft.output %c19_i32, %26, %7, %10 : i32, i32, i1, i1
+# CHECK-NEXT:     %6 = comb.and bin %inp3, %c28799_i32 : i32
+# CHECK-NEXT:     %7 = comb.icmp bin eq %c28799_i32, %6 : i32
+# CHECK-NEXT:     %8 = comb.mux bin %7, %c2989_i32, %c0_i32 {sv.namehint = "mux_None_in0_in1"} : i32
+# CHECK-NEXT:     %9 = comb.and bin %inp3, %c28799_i32_0 : i32
+# CHECK-NEXT:     %10 = comb.icmp bin eq %c28799_i32_0, %9 : i32
+# CHECK-NEXT:     %11 = comb.mux bin %10, %c64206_i32, %8 {sv.namehint = "mux_None_mux_None_in0_in1_in1"} : i32
+# CHECK-NEXT:     %12 = comb.and bin %inp3, %c28799_i32_1 : i32
+# CHECK-NEXT:     %13 = comb.icmp bin eq %c28799_i32_1, %12 : i32
+# CHECK-NEXT:     %14 = comb.mux bin %13, %c3054_i32, %11 {sv.namehint = "mux_None_mux_None_mux_None_in0_in1_in1_in1"} : i32
+# CHECK-NEXT:     %15 = comb.and bin %inp3, %c28799_i32_2 : i32
+# CHECK-NEXT:     %16 = comb.icmp bin eq %c28799_i32_2, %15 : i32
+# CHECK-NEXT:     %17 = comb.mux bin %16, %c48879_i32, %14 {sv.namehint = "mux_None_mux_None_mux_None_mux_None_in0_in1_in1_in1_in1"} : i32
+# CHECK-NEXT:     %18 = comb.and bin %inp3, %c28799_i32_3 : i32
+# CHECK-NEXT:     %19 = comb.icmp bin eq %c28799_i32_3, %18 : i32
+# CHECK-NEXT:     %20 = comb.mux bin %19, %c3245_i32, %17 {sv.namehint = "dict_lookup"} : i32
+# CHECK-NEXT:     msft.output %c19_i32, %20, %2, %5 : i32, i32, i1, i1
+
+
 @unittestmodule()
 class TestBitPat(Module):
   inp1 = Input(UInt(36))
@@ -88,3 +77,13 @@ class TestBitPat(Module):
     self.no_match = LB != self.inp2
     self.out1 = BitPat("b00000000000000000000000000010011").as_bits()
     self.out2 = dict_lookup(inst_map, self.inp3, Bits(32)(0x0))
+
+
+@unittestmodule()
+class BitPatError(Module):
+  out1 = Output(Bits(32))
+
+  @generator
+  def build(self):
+    # CHECK: TypeError: Can only convert BitPat with no don't cares to Bits
+    self.out1 = BitPat("b000000000????0000000000000010011").as_bits()
