d35c2de896cdd0371e3ae87b919a8cd614baaebd
Rachit Nigam <rachit.nigam12@gmail.com>
2023-08-24 09:16:54 +0530

    Calyx native callout pass (#5879)
    * start working on adding a calyx native callout pass
    
    * missing entry in CMakeLists for circt-opt
    
    * emit calyx in pass
    
    * progress
    
    * specify primitives lib
    
    * it works!!
    
    * fewer imports
    
    * fix CMakeList
    
    * cleaup and comments
    
    * remove useless stringref
    
    * better error message when `calyx` binary is missing
    
    * header style
    
    * remove primitives folder option and define pass-pipeline
    
    * remove braces
    
    * add native pass to calyx namespace
    
    * remove empty namespace
    
    * add dependency to CAPI
    
    * clang-tidy

---
+++ include/circt/Conversion/CalyxNative.h
@@ -0,0 +1,31 @@
+//===- CalyxNative.h - Calyx Native pass ------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares passes which together will lower the Calyx dialect to the
+// HW dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_CONVERSION_CALYXNATIVE_H
+#define CIRCT_CONVERSION_CALYXNATIVE_H
+
+#include "circt/Dialect/Calyx/CalyxOps.h"
+#include "circt/Support/LLVM.h"
+#include <memory>
+
+namespace mlir {
+class Pass;
+} // namespace mlir
+
+namespace circt {
+
+std::unique_ptr<mlir::Pass> createCalyxNativePass();
+
+} // namespace circt
+
+#endif // CIRCT_CONVERSION_CALYXNATIVE_H
