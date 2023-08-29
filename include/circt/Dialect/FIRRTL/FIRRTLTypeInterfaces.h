464bc1024d8ca09d485d2dbe40ff12dc52b67f42
Andrew Lenharth <andrew@lenharth.org>
2023-08-29 16:24:10 -0500

    forgot files

---
+++ include/circt/Dialect/FIRRTL/FIRRTLTypeInterfaces.h
@@ -0,0 +1,21 @@
+//===- FIRRTLTypeInterfaces.h - Declare FIRRTL type interfaces --*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares type interfaces for the FIRRTL Dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_FIRRTL_FIRRTLTYPEINTERFACES_H
+#define CIRCT_DIALECT_FIRRTL_FIRRTLTYPEINTERFACES_H
+
+#include "circt/Support/LLVM.h"
+#include "mlir/IR/Types.h"
+
+#include "circt/Dialect/FIRRTL/FIRRTLTypeInterfaces.h.inc"
+
+#endif // CIRCT_DIALECT_FIRRTL_FIRRTLTYPEINTERFACES_H
