68b79fc35fcc2d68abc8a0b830c0f5a303af3f04
Nandor Licker <n@ndor.email>
2023-09-05 03:12:34 -0700

    [NFC][Seq] Add folders to clock conversions

--- lib/Dialect/Seq/SeqOps.cpp
+++ lib/Dialect/Seq/SeqOps.cpp
@@ -896,6 +896,12 @@ LogicalResult ToClockOp::canonicalize(ToClockOp op, PatternRewriter &rewriter) {
   return failure();
 }
 
+OpFoldResult ToClockOp::fold(FoldAdaptor adaptor) {
+  if (auto fromClock = getInput().getDefiningOp<FromClockOp>())
+    return fromClock.getInput();
+  return {};
+}
+
 LogicalResult FromClockOp::canonicalize(FromClockOp op,
                                         PatternRewriter &rewriter) {
   if (auto toClock = op.getInput().getDefiningOp<ToClockOp>()) {
@@ -903,6 +909,12 @@ LogicalResult FromClockOp::canonicalize(FromClockOp op,
     return success();
   }
   return failure();
+}
+
+OpFoldResult FromClockOp::fold(FoldAdaptor adaptor) {
+  if (auto toClock = getInput().getDefiningOp<ToClockOp>())
+    return toClock.getInput();
+  return {};
 }
 
 //===----------------------------------------------------------------------===//
