e0d1804e37f39952be20592f02458b11c1ddd270
Will Dietz <will.dietz@sifive.com>
2023-09-08 17:35:54 -0500

    [FIRRTL] Improve IMCP for non-hw; support mat'izing refs of constants. (#6091)
    Fixes #6076.
    
    Fixes leaving dead property wires around if they're constant-prop'd through.
    
    Tweak export-ref test to demonstrate somewhat that this now enables
    a probe of a constant in a module to be hoisted to a probe of the constant
    at the boundary, allow for more cleanup -- in particular prevents modules
    with probed constants being preserved just for that reason.
    (The test adds a dontTouch to avoid this to check the exported macro)

--- lib/Dialect/FIRRTL/Transforms/IMConstProp.cpp
+++ lib/Dialect/FIRRTL/Transforms/IMConstProp.cpp
@@ -913,7 +913,8 @@ void IMConstPropPass::rewriteModuleBody(FModuleOp module) {
   // Unique constants per <Const,Type> pair, inserted at entry
   DenseMap<std::pair<Attribute, Type>, Operation *> constPool;
 
-  auto getConst = [&](Attribute constantValue, Type type, Location loc) {
+  std::function<Value(Attribute, Type, Location)> getConst =
+      [&](Attribute constantValue, Type type, Location loc) -> Value {
     auto constIt = constPool.find({constantValue, type});
     if (constIt != constPool.end()) {
       auto *cst = constIt->second;
@@ -923,8 +924,19 @@ void IMConstPropPass::rewriteModuleBody(FModuleOp module) {
     }
     OpBuilder::InsertionGuard x(builder);
     builder.setInsertionPoint(cursor);
-    auto *cst = module->getDialect()->materializeConstant(
-        builder, constantValue, type, loc);
+
+    // Materialize reftype "constants" by materializing the constant
+    // and probing it.
+    Operation *cst;
+    if (auto refType = type_dyn_cast<RefType>(type)) {
+      assert(!type_cast<RefType>(type).getForceable() &&
+             "Attempting to materialize rwprobe of constant, shouldn't happen");
+      auto inner = getConst(constantValue, refType.getType(), loc);
+      assert(inner);
+      cst = builder.create<RefSendOp>(loc, inner);
+    } else
+      cst = module->getDialect()->materializeConstant(builder, constantValue,
+                                                      type, loc);
     assert(cst && "all FIRRTL constants can be materialized");
     constPool.insert({{constantValue, type}, cst});
     return cst->getResult(0);
@@ -952,7 +964,7 @@ void IMConstPropPass::rewriteModuleBody(FModuleOp module) {
     // Cannot materialize constants for certain types.
     // TODO: Let materializeConstant tell us what it supports instead of this.
     // Presently it asserts on unsupported combinations, so check this here.
-    if (!type_isa<FIRRTLBaseType, FIntegerType, StringType, BoolType>(
+    if (!type_isa<FIRRTLBaseType, RefType, FIntegerType, StringType, BoolType>(
             value.getType()))
       return false;
 
@@ -1011,8 +1023,8 @@ void IMConstPropPass::rewriteModuleBody(FModuleOp module) {
         auto type = type_dyn_cast<FIRRTLType>(connect.getDest().getType());
         if (!type)
           continue;
-        auto baseType = getBaseType(type);
-        if (!baseType || !baseType.isGround())
+        auto baseType = type_dyn_cast<FIRRTLBaseType>(type);
+        if (baseType && !baseType.isGround())
           continue;
         if (isDeletableWireOrRegOrNode(destOp) && !isOverdefined(fieldRef)) {
           connect.erase();
