6d3c8055852fed132bb0f168e7b1f88d1cb37f9e
Adam Izraelevitz <adam.izraelevitz@sifive.com>
2023-09-12 15:22:54 -0700

    Ran clang-formatter on .h files

--- include/circt/Dialect/Comb/CombOps.h
+++ include/circt/Dialect/Comb/CombOps.h
@@ -18,8 +18,8 @@
 #include "circt/Support/LLVM.h"
 #include "mlir/Bytecode/BytecodeOpInterface.h"
 #include "mlir/IR/BuiltinOps.h"
-#include "mlir/Interfaces/FunctionInterfaces.h"
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/Interfaces/FunctionInterfaces.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
