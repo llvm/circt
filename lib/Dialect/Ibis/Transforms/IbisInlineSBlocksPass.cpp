8b08dbedbb9a1dd9c31a9082d6d71b128710faa7
Morten Borup Petersen <morten_bp@live.dk>
2023-09-18 12:36:34 +0000

    better

--- lib/Dialect/Ibis/Transforms/IbisInlineSBlocksPass.cpp
+++ lib/Dialect/Ibis/Transforms/IbisInlineSBlocksPass.cpp
@@ -27,6 +27,10 @@ using namespace ibis;
 
 namespace {
 
+static size_t getNumOps(mlir::Block *block) {
+  return std::distance(block->begin(), block->end());
+}
+
 struct InlineSBlocksPass : public IbisInlineSBlocksBase<InlineSBlocksPass> {
   void runOnOperation() override;
 
@@ -37,93 +41,104 @@ struct InlineSBlocksPass : public IbisInlineSBlocksBase<InlineSBlocksPass> {
   void inlineSBlock(OpBuilder &b, ibis::StaticBlockOp op,
                     DenseMap<Block *, DictionaryAttr> &blockAttrMap);
 };
+
+class InlineSBlocksPattern : public OpConversionPattern<ibis::StaticBlockOp> {
+public:
+  InlineSBlocksPattern(MLIRContext *context,
+                       DenseMap<Block *, DictionaryAttr> &blockAttrMap)
+      : OpConversionPattern<ibis::StaticBlockOp>(context),
+        blockAttrMap(blockAttrMap) {}
+  using OpAdaptor =
+      typename OpConversionPattern<ibis::StaticBlockOp>::OpAdaptor;
+
+  LogicalResult
+  matchAndRewrite(ibis::StaticBlockOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    Location loc = op.getLoc();
+    Block *predBlock = op->getBlock();
+    Block *succBlock =
+        predBlock->splitBlock(std::next(op.getOperation()->getIterator()));
+
+    // Move the body of the ibis.sblock before the successor block.
+    Block *sblockBody = op.getBodyBlock();
+    sblockBody->moveBefore(succBlock);
+    BlockReturnOp ret = cast<BlockReturnOp>(sblockBody->getTerminator());
+
+    // Replace the ibis.sblock return values with the values that were defined
+    // (returned from) within the sblock body, and erase the return op.
+    for (auto [res, val] : llvm::zip(op.getResults(), ret.getRetValues()))
+      res.replaceAllUsesWith(val);
+    ret.erase();
+
+    // Create control flow from the predecessor block to the sblock body.
+    if (getNumOps(predBlock) == 1) {
+      // Predecessor block only contained the sblock, so we can merge it
+      // with the sblock body.
+      predBlock->getOperations().splice(predBlock->end(),
+                                        sblockBody->getOperations());
+      sblockBody->erase();
+      sblockBody = predBlock;
+
+      // Outgoing control flow should start from the end of the merged block.
+      rewriter.setInsertionPointToEnd(predBlock);
+    } else {
+      rewriter.setInsertionPointToEnd(predBlock);
+      rewriter.create<cf::BranchOp>(loc, sblockBody);
+      // Outgoing control flow should start from the end of the sblock body.
+      rewriter.setInsertionPointToEnd(sblockBody);
+    }
+
+    // Create control flow from the sblock body to the successor block.
+    if (getNumOps(succBlock) == 1) {
+      // Successor block only contains a branch, so we can merge it with the
+      // sblock body.
+      sblockBody->getOperations().splice(sblockBody->end(),
+                                         succBlock->getOperations());
+      rewriter.eraseBlock(succBlock);
+    } else {
+      // Successor block contains more than just a branch, so we need to create
+      // a branch to it.
+      rewriter.create<cf::BranchOp>(loc, succBlock);
+    }
+
+    // Add an attribute for the source block location.
+    llvm::SmallVector<NamedAttribute> blockAttrs;
+    blockAttrs.push_back(
+        rewriter.getNamedAttr("loc", static_cast<LocationAttr>(loc)));
+    if (auto maxThreads = op.getMaxThreadsAttr())
+      blockAttrs.push_back(rewriter.getNamedAttr("maxThreads", maxThreads));
+
+    blockAttrMap[sblockBody] = rewriter.getDictionaryAttr(blockAttrs);
+    rewriter.eraseOp(op);
+    return success();
+  }
+
+  DenseMap<Block *, DictionaryAttr> &blockAttrMap;
+};
+
 } // anonymous namespace
 
 void InlineSBlocksPass::runOnOperation() {
   MethodOp parent = getOperation();
-  // @mortbopet: Would use a PatternRewriter here, if not for the fact that
-  // lowering ibis.sblocks modifies the parent operations' attributes, which
-  // would be illegal.
-  // In the future, if we want speedup here, we could consider introducing
-  // either auxilary information-carrying ops within each block, or look into
-  // adding upstream support for block attributes.
+  ConversionTarget target(getContext());
+  target.addLegalDialect<cf::ControlFlowDialect>();
+  target.addIllegalOp<ibis::StaticBlockOp>();
+
   DenseMap<Block *, DictionaryAttr> blockAttrMap;
-  OpBuilder b(parent);
-  for (auto StaticBlockOp :
-       llvm::make_early_inc_range(parent.getOps<ibis::StaticBlockOp>()))
-    inlineSBlock(b, StaticBlockOp, blockAttrMap);
+  RewritePatternSet patterns(&getContext());
+  patterns.add<InlineSBlocksPattern>(&getContext(), blockAttrMap);
+
+  if (failed(applyPartialConversion(parent, target, std::move(patterns))))
+    return signalPassFailure();
 
   // Add block annotations to the parent method op.
   llvm::SmallVector<NamedAttribute> blockInfo;
   for (auto [block, attrs] : blockAttrMap)
-    blockInfo.push_back(b.getNamedAttr(std::to_string(blockID(block)), attrs));
+    blockInfo.push_back(NamedAttribute(
+        StringAttr::get(&getContext(), std::to_string(blockID(block))), attrs));
 
-  parent->setAttr("ibis.blockinfo", b.getDictionaryAttr(blockInfo));
-}
-
-static size_t getNumOps(mlir::Block *block) {
-  return std::distance(block->begin(), block->end());
-}
-
-void InlineSBlocksPass::inlineSBlock(
-    OpBuilder &b, ibis::StaticBlockOp op,
-    DenseMap<Block *, DictionaryAttr> &blockAttrMap) {
-  b.setInsertionPoint(op);
-  Location loc = op.getLoc();
-  Block *predBlock = op->getBlock();
-  Block *succBlock =
-      predBlock->splitBlock(std::next(op.getOperation()->getIterator()));
-
-  // Move the body of the ibis.sblock before the successor block.
-  Block *sblockBody = op.getBodyBlock();
-  sblockBody->moveBefore(succBlock);
-  BlockReturnOp ret = cast<BlockReturnOp>(sblockBody->getTerminator());
-
-  // Replace the ibis.sblock return values with the values that were defined
-  // (returned from) within the sblock body, and erase the return op.
-  for (auto [res, val] : llvm::zip(op.getResults(), ret.getRetValues()))
-    res.replaceAllUsesWith(val);
-  ret.erase();
-
-  // Create control flow from the predecessor block to the sblock body.
-  if (getNumOps(predBlock) == 1) {
-    // Predecessor block only contained the sblock, so we can merge it
-    // with the sblock body.
-    predBlock->getOperations().splice(predBlock->end(),
-                                      sblockBody->getOperations());
-    sblockBody->erase();
-    sblockBody = predBlock;
-
-    // Outgoing control flow should start from the end of the merged block.
-    b.setInsertionPointToEnd(predBlock);
-  } else {
-    b.setInsertionPointToEnd(predBlock);
-    b.create<cf::BranchOp>(loc, sblockBody);
-    // Outgoing control flow should start from the end of the sblock body.
-    b.setInsertionPointToEnd(sblockBody);
-  }
-
-  // Create control flow from the sblock body to the successor block.
-  if (getNumOps(succBlock) == 1) {
-    // Successor block only contains a branch, so we can merge it with the
-    // sblock body.
-    sblockBody->getOperations().splice(sblockBody->end(),
-                                       succBlock->getOperations());
-    succBlock->erase();
-  } else {
-    // Successor block contains more than just a branch, so we need to create
-    // a branch to it.
-    b.create<cf::BranchOp>(loc, succBlock);
-  }
-
-  // Add an attribute for the source block location.
-  llvm::SmallVector<NamedAttribute> blockAttrs;
-  blockAttrs.push_back(b.getNamedAttr("loc", static_cast<LocationAttr>(loc)));
-  if (auto maxThreads = op.getMaxThreadsAttr())
-    blockAttrs.push_back(b.getNamedAttr("maxThreads", maxThreads));
-
-  blockAttrMap[sblockBody] = b.getDictionaryAttr(blockAttrs);
-  op.erase();
+  parent->setAttr("ibis.blockinfo",
+                  DictionaryAttr::get(&getContext(), blockInfo));
 }
 
 std::unique_ptr<Pass> circt::ibis::createInlineSBlocksPass() {
