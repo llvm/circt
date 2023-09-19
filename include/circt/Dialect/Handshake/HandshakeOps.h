d43cdc3ac356fa33af056cb38e5ff14af52c669f
Morten Borup Petersen <morten_bp@live.dk>
2023-09-19 11:34:17 +0000

    [NFC][Handshake] Adjust handshake interface defs and file locs

--- include/circt/Dialect/Handshake/HandshakeOps.h
+++ include/circt/Dialect/Handshake/HandshakeOps.h
@@ -14,6 +14,7 @@
 #define CIRCT_HANDSHAKEOPS_OPS_H_
 
 #include "circt/Dialect/Handshake/HandshakeDialect.h"
+#include "circt/Dialect/Handshake/HandshakeInterfaces.h"
 #include "circt/Support/LLVM.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
@@ -32,36 +33,6 @@
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 #include "mlir/Pass/Pass.h"
 #include "llvm/ADT/Any.h"
-
-namespace circt {
-namespace handshake {
-
-struct MemLoadInterface {
-  unsigned index;
-  mlir::Value addressIn;
-  mlir::Value dataOut;
-  mlir::Value doneOut;
-};
-
-struct MemStoreInterface {
-  unsigned index;
-  mlir::Value addressIn;
-  mlir::Value dataIn;
-  mlir::Value doneOut;
-};
-
-/// Default implementation for checking whether an operation is a control
-/// operation. This function cannot be defined within ControlInterface
-/// because its implementation attempts to cast the operation to an
-/// SOSTInterface, which may not be declared at the point where the default
-/// trait's method is defined. Therefore, the default implementation of
-/// ControlInterface's isControl method simply calls this function.
-bool isControlOpImpl(Operation *op);
-
-#include "circt/Dialect/Handshake/HandshakeInterfaces.h.inc"
-
-} // end namespace handshake
-} // end namespace circt
 
 namespace mlir {
 namespace OpTrait {
