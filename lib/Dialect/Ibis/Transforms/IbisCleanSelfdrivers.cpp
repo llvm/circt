6735dfa983f6510df7e7653f24f32a13d57b6195
Morten Borup Petersen <morten_bp@live.dk>
2023-09-26 13:17:41 +0000

    [Ibis] Also remove reads of writes in clean-selfdrivers

--- lib/Dialect/Ibis/Transforms/IbisCleanSelfdrivers.cpp
+++ lib/Dialect/Ibis/Transforms/IbisCleanSelfdrivers.cpp
@@ -14,9 +14,12 @@
 #include "circt/Dialect/Ibis/IbisPasses.h"
 #include "circt/Dialect/Ibis/IbisTypes.h"
 #include "circt/Support/InstanceGraph.h"
+#include "llvm/Support/Debug.h"
 
 #include "mlir/Transforms/DialectConversion.h"
 #include "llvm/ADT/TypeSwitch.h"
+
+#define DEBUG_TYPE "ibis-clean-selfdrivers"
 
 using namespace circt;
 using namespace ibis;
@@ -32,6 +35,75 @@ static bool isSelfDriven(InputPortOp op) {
 }
 
 namespace {
+
+// Rewrites cases where an input port is being read in the instantiating module.
+// Replaces the input port read by the assignment value of the input port.
+static LogicalResult replaceReadsOfWrites(ContainerOp containerOp) {
+  // Partition out all of the get_port's wrt. their target port symbol.
+  struct PortAccesses {
+    GetPortOp getAsInput;
+    GetPortOp getAsOutput;
+    llvm::SmallVector<PortReadOp> reads;
+    PortWriteOp writer;
+  };
+  llvm::DenseMap</*instance*/ Value,
+                 /*portName*/ llvm::DenseMap<StringAttr, PortAccesses>>
+      instancePortAccessMap;
+
+  for (auto getPortOp : containerOp.getOps<GetPortOp>()) {
+    PortAccesses &portAccesses =
+        instancePortAccessMap[getPortOp.getInstance()]
+                             [getPortOp.getPortSymbolAttr().getAttr()];
+    if (getPortOp.getDirection() == Direction::Input) {
+      if (portAccesses.getAsInput)
+        return containerOp.emitError(
+            "multiple input get_ports - please CSE the input IR");
+      portAccesses.getAsInput = getPortOp;
+      for (auto *user : getPortOp->getUsers()) {
+        if (auto writer = dyn_cast<PortWriteOp>(user)) {
+          if (portAccesses.writer)
+            return getPortOp.emitError(
+                "multiple writers of the same input port");
+          portAccesses.writer = writer;
+        }
+      }
+    } else {
+      if (portAccesses.getAsOutput)
+        return containerOp.emitError(
+            "multiple get_port as output - please CSE the input IR");
+      portAccesses.getAsOutput = getPortOp;
+
+      for (auto *user : getPortOp->getUsers()) {
+        if (auto reader = dyn_cast<PortReadOp>(user))
+          portAccesses.reads.push_back(reader);
+      }
+    }
+  }
+
+  for (auto &[instance, portAccessMap] : instancePortAccessMap) {
+    for (auto &[portName, portAccesses] : portAccessMap) {
+      // If the port is not written to, nothing to do.
+      if (!portAccesses.writer)
+        continue;
+
+      // if the port is not read, nothing to do.
+      if (!portAccesses.getAsOutput)
+        continue;
+
+      // If the input port is self-driven, we need to replace all reads of the
+      // input port with the value that is being written to it.
+      LLVM_DEBUG(llvm::dbgs() << "Writer is: " << portAccesses.writer << "\n";);
+      for (auto reader : portAccesses.reads) {
+        LLVM_DEBUG(llvm::dbgs() << "Replacing: " << reader << "\n";);
+        reader.replaceAllUsesWith(portAccesses.writer.getValue());
+        reader.erase();
+      }
+      portAccesses.getAsOutput.erase();
+    }
+  }
+
+  return success();
+}
 
 struct InputPortOpConversionPattern : public OpConversionPattern<InputPortOp> {
   InputPortOpConversionPattern(MLIRContext *context, InstanceGraph &ig)
@@ -111,10 +183,21 @@ protected:
 struct CleanSelfdriversPass
     : public IbisCleanSelfdriversBase<CleanSelfdriversPass> {
   void runOnOperation() override;
+
+  LogicalResult cleanInstanceSide();
+  LogicalResult cleanContainerSide();
 };
 } // anonymous namespace
 
-void CleanSelfdriversPass::runOnOperation() {
+LogicalResult CleanSelfdriversPass::cleanInstanceSide() {
+  for (ContainerOp containerOp : getOperation().getOps<ContainerOp>())
+    if (failed(replaceReadsOfWrites(containerOp)))
+      return failure();
+
+  return success();
+}
+
+LogicalResult CleanSelfdriversPass::cleanContainerSide() {
   auto *ctx = &getContext();
   ConversionTarget target(*ctx);
   target.addLegalDialect<IbisDialect>();
@@ -128,7 +211,14 @@ void CleanSelfdriversPass::runOnOperation() {
 
   if (failed(
           applyPartialConversion(getOperation(), target, std::move(patterns))))
-    signalPassFailure();
+    return failure();
+
+  return success();
+}
+
+void CleanSelfdriversPass::runOnOperation() {
+  if (failed(cleanInstanceSide()) || failed(cleanContainerSide()))
+    return signalPassFailure();
 }
 
 std::unique_ptr<Pass> circt::ibis::createCleanSelfdriversPass() {
