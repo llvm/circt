60f24472b5d0e58016f007a81433d0bcc0193a83
Will Dietz <will.dietz@sifive.com>
2023-09-28 07:22:02 -0500

    [FIRRTL][LOA][NFC] Fix stray newline in header comment.

--- lib/Dialect/FIRRTL/Transforms/LowerOpenAggs.cpp
+++ lib/Dialect/FIRRTL/Transforms/LowerOpenAggs.cpp
@@ -1,5 +1,4 @@
 //===- LowerOpenAggs.cpp - Lower Open Aggregate Types -----------*- C++ -*-===//
-
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
