9b61ac6cc524cf65522fcfd6dd4093ce895e3373
John Demme <john.demme@microsoft.com>
2023-11-01 20:41:05 -0700

    [ESI] Cosim integration test without runtime (#6370)
    Add test (and a minor work-around to get it working) testing the CIRCT compiler side of cosim. Also removes some old testing cruft.

--- integration_test/Dialect/ESI/cosim/loopback.py
+++ integration_test/Dialect/ESI/cosim/loopback.py
@@ -12,35 +12,17 @@ class LoopbackTester(esi_cosim.CosimBase):
     ifaces = self.cosim.list().wait().ifaces
     assert len(ifaces) > 0
 
-  def test_open_close(self):
-    ifaces = self.cosim.list().wait().ifaces
-    openResp = self.cosim.open(ifaces[0]).wait()
-    assert openResp.iface is not None
-    ep = openResp.iface
-    ep.close().wait()
-
   def test_two_chan_loopback(self, num_msgs):
-    to_hw = self.openEP("top.TwoChanLoopback_loopback_tohw")
-    from_hw = self.openEP("top.TwoChanLoopback_loopback_fromhw")
+    to_hw = self.openEP("top.loopback_inst5B05D_loopback_tohw_recv")
+    from_hw = self.openEP("top.loopback_inst5B05D_loopback_fromhw_send")
     for _ in range(num_msgs):
-      data = random.randint(0, 2**8 - 1)
-      print(f"Sending {data}")
-      to_hw.send(self.schema.I8.new_message(i=data))
-      result = self.readMsg(from_hw, self.schema.I8)
-      print(f"Got {result}")
-      assert (result.i == data)
-
-  def test_i32(self, num_msgs):
-    ep = self.openEP("top.intLoopbackInst.IntTestEP.loopback",
-                     sendType=self.schema.I32,
-                     recvType=self.schema.I32)
-    for _ in range(num_msgs):
-      data = random.randint(0, 2**32 - 1)
-      print(f"Sending {data}")
-      ep.send(self.schema.I32.new_message(i=data))
-      result = self.readMsg(ep, self.schema.I32)
-      print(f"Got {result}")
-      assert (result.i == data)
+      i = random.randint(0, 2**8 - 1)
+      print(f"Sending {i}")
+      to_hw.sendFromHost(msg=i.to_bytes(1, 'little')).wait()
+      result = self.readMsg(from_hw)
+      result_int = int.from_bytes(result, 'little')
+      print(f"Got {result_int}")
+      assert (result_int == i)
 
   def write_3bytes(self, ep):
     r = random.randrange(0, 2**24 - 1)
@@ -71,32 +53,9 @@ class LoopbackTester(esi_cosim.CosimBase):
     recv_ep.close().wait()
     assert dataSent == dataRecv
 
-  def test_keytext(self, num_msgs=50):
-    cStructType = self.schema.Struct17798359158705484171
-    ep = self.openEP("top.twoListLoopbackInst.KeyTextEP",
-                     sendType=cStructType,
-                     recvType=cStructType)
-    kts = []
-    for i in range(num_msgs):
-      kt = cStructType.new_message(
-          key=[random.randrange(0, 255) for x in range(4)],
-          text=[random.randrange(0, 16000) for x in range(6)])
-      kts.append(kt)
-      ep.send(kt).wait()
-
-    for i in range(num_msgs):
-      kt = self.readMsg(ep, cStructType)
-      print(f"expected: {kts[i]}")
-      print(f"got:      {kt}")
-      assert list(kt.key) == list(kts[i].key)
-      assert list(kt.text) == list(kts[i].text)
-
 
 if __name__ == "__main__":
-  import os
   import sys
   rpc = LoopbackTester(sys.argv[2], sys.argv[1])
   print(rpc.list())
   rpc.test_two_chan_loopback(25)
-  rpc.test_i32(25)
-  rpc.test_keytext(25)
