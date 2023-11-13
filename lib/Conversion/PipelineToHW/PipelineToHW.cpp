349ab0e77295f5a6685b0c25dc2180b141fc7177
Morten Borup Petersen <morten_bp@live.dk>
2023-11-13 14:39:10 +0000

    revert unrelated change

--- lib/Conversion/PipelineToHW/PipelineToHW.cpp
+++ lib/Conversion/PipelineToHW/PipelineToHW.cpp
@@ -186,8 +186,8 @@ public:
           dataReg = builder.create<seq::CompRegClockEnabledOp>(
               stageOp->getLoc(), regIn, args.clock, stageValid, regName);
         } else {
-          dataReg = builder.create<seq::CompRegOp>(stageOp->getLoc(), regIn,
-                                                   args.clock, regName);
+          dataReg = builder.create<seq::CompRegOp>(
+              stageOp->getLoc(), regIn, args.clock, regName.strref());
         }
       }
       rets.regs.push_back(dataReg);
