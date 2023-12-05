22e1f2d13eef4e0a87c289017a0caba298c48545
Will Dietz <will.dietz@sifive.com>
2023-12-05 10:40:20 -0600

    clang-format after rebase.

--- lib/Dialect/HW/HWOps.cpp
+++ lib/Dialect/HW/HWOps.cpp
@@ -2778,7 +2778,8 @@ LogicalResult UnionExtractOp::inferReturnTypes(
     mlir::RegionRange regions, SmallVectorImpl<Type> &results) {
   UnionExtractOpAdaptor op(operands, attrs,
                            properties.as<UnionExtractOp::Properties *>());
-  auto unionElements = hw::type_cast<UnionType>(op.getInput().getType()).getElements();
+  auto unionElements =
+      hw::type_cast<UnionType>(op.getInput().getType()).getElements();
   auto fieldIndex = op.getFieldIndex();
   if (fieldIndex >= unionElements.size()) {
     if (loc)
