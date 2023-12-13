96ddd2fed1a344979b8ffc3a1ca9343f2b5f7cb6
Andrew Lenharth <darthscsi@users.noreply.github.com>
2023-12-13 17:40:21 -0600

    [NFC] Make CHIRRTL more normal in preparation for moving some stuff in (#6521)

--- include/circt/Dialect/FIRRTL/FIRRTLOps.h
+++ include/circt/Dialect/FIRRTL/FIRRTLOps.h
@@ -13,6 +13,7 @@
 #ifndef CIRCT_DIALECT_FIRRTL_OPS_H
 #define CIRCT_DIALECT_FIRRTL_OPS_H
 
+#include "circt/Dialect/FIRRTL/CHIRRTLDialect.h"
 #include "circt/Dialect/FIRRTL/FIRRTLDialect.h"
 #include "circt/Dialect/FIRRTL/FIRRTLOpInterfaces.h"
 #include "circt/Dialect/HW/HWOpInterfaces.h"
