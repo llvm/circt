57372957e8365b34ca469299b8c864d830e836a1
Nandor Licker <n@ndor.email>
2023-12-15 08:33:24 +0200

    [FIRRTL] Add an emitter for options and instance choices (#6520)

--- lib/Dialect/FIRRTL/Export/FIREmitter.cpp
+++ lib/Dialect/FIRRTL/Export/FIREmitter.cpp
@@ -59,6 +59,7 @@ struct Emitter {
                        Block::BlockArgListType arguments = {});
   void emitModuleParameters(Operation *op, ArrayAttr parameters);
   void emitDeclaration(LayerOp op);
+  void emitDeclaration(OptionOp op);
 
   // Statement emission
   void emitStatementsInBlock(Block &block);
@@ -74,6 +75,7 @@ struct Emitter {
   void emitStatement(StrictConnectOp op);
   void emitStatement(PropAssignOp op);
   void emitStatement(InstanceOp op);
+  void emitStatement(InstanceChoiceOp op);
   void emitStatement(AttachOp op);
   void emitStatement(MemOp op);
   void emitStatement(InvalidValueOp op);
@@ -384,6 +386,7 @@ void Emitter::emitCircuit(CircuitOp op) {
             ps << PP::newline;
           })
           .Case<LayerOp>([&](auto op) { emitDeclaration(op); })
+          .Case<OptionOp>([&](auto op) { emitDeclaration(op); })
           .Default([&](auto op) {
             emitOpError(op, "not supported for emission inside circuit");
           });
@@ -531,6 +534,21 @@ void Emitter::emitDeclaration(LayerOp op) {
   });
 }
 
+/// Emit an option declaration.
+void Emitter::emitDeclaration(OptionOp op) {
+  startStatement();
+  ps << "option " << PPExtString(legalize(op.getSymNameAttr())) << " :";
+  emitLocation(op);
+  ps.scopedBox(PP::bbox2, [&] {
+    for (auto caseOp : op.getBody().getOps<OptionCaseOp>()) {
+      ps << PP::newline;
+      ps << PPExtString(legalize(caseOp.getSymNameAttr()));
+      emitLocation(caseOp);
+    }
+  });
+  ps << PP::newline << PP::newline;
+}
+
 /// Check if an operation is inlined into the emission of their users. For
 /// example, subfields are always inlined.
 static bool isEmittedInline(Operation *op) {
@@ -546,11 +564,11 @@ void Emitter::emitStatementsInBlock(Block &block) {
     TypeSwitch<Operation *>(&bodyOp)
         .Case<WhenOp, WireOp, RegOp, RegResetOp, NodeOp, StopOp, SkipOp,
               PrintFOp, AssertOp, AssumeOp, CoverOp, ConnectOp, StrictConnectOp,
-              PropAssignOp, InstanceOp, AttachOp, MemOp, InvalidValueOp,
-              SeqMemOp, CombMemOp, MemoryPortOp, MemoryDebugPortOp,
-              MemoryPortAccessOp, RefDefineOp, RefForceOp, RefForceInitialOp,
-              RefReleaseOp, RefReleaseInitialOp, LayerBlockOp>(
-            [&](auto op) { emitStatement(op); })
+              PropAssignOp, InstanceOp, InstanceChoiceOp, AttachOp, MemOp,
+              InvalidValueOp, SeqMemOp, CombMemOp, MemoryPortOp,
+              MemoryDebugPortOp, MemoryPortAccessOp, RefDefineOp, RefForceOp,
+              RefForceInitialOp, RefReleaseOp, RefReleaseInitialOp,
+              LayerBlockOp>([&](auto op) { emitStatement(op); })
         .Default([&](auto op) {
           startStatement();
           ps << "// operation " << PPExtString(op->getName().getStringRef());
@@ -793,6 +811,33 @@ void Emitter::emitStatement(InstanceOp op) {
 
   // Make sure we have a name like `<inst>.<port>` for each of the instance
   // result values.
+  SmallString<16> portName(legalName);
+  portName.push_back('.');
+  unsigned baseLen = portName.size();
+  for (unsigned i = 0, e = op.getNumResults(); i < e; ++i) {
+    portName.append(legalize(op.getPortName(i)));
+    addValueName(op.getResult(i), portName);
+    portName.resize(baseLen);
+  }
+}
+
+void Emitter::emitStatement(InstanceChoiceOp op) {
+  startStatement();
+  auto legalName = legalize(op.getNameAttr());
+  ps << "instchoice " << PPExtString(legalName) << " of "
+     << PPExtString(legalize(op.getDefaultTargetAttr().getAttr())) << ", "
+     << PPExtString(legalize(op.getOptionNameAttr())) << " :";
+  emitLocation(op);
+  ps.scopedBox(PP::bbox2, [&] {
+    for (const auto [optSym, targetSym] : op.getTargetChoices()) {
+      ps << PP::newline;
+      ps << PPExtString(legalize(optSym.getLeafReference()));
+      ps << " => ";
+      ps << PPExtString(legalize(targetSym.getAttr()));
+    }
+  });
+  setPendingNewline();
+
   SmallString<16> portName(legalName);
   portName.push_back('.');
   unsigned baseLen = portName.size();
