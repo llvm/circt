5312ae10087ce894aa1b35342efc62ab87e6cf59
Dobios <adobis@student.ethz.ch>
2024-01-19 17:51:53 -0800

    Added ltl to core opconversions

--- lib/Conversion/LTLToCore/LTLToCore.cpp
+++ lib/Conversion/LTLToCore/LTLToCore.cpp
@@ -17,6 +17,7 @@
 #include "circt/Dialect/LTL/LTLDialect.h"
 #include "circt/Dialect/LTL/LTLOps.h"
 #include "circt/Dialect/SV/SVDialect.h"
+#include "circt/Dialect/SV/SVOps.h"
 #include "circt/Dialect/Seq/SeqOps.h"
 #include "circt/Dialect/Verif/VerifOps.h"
 #include "circt/Support/Namespace.h"
@@ -38,15 +39,50 @@ namespace {
 struct DisableOpConversion : OpConversionPattern<ltl::DisableOp> {
   using OpConversionPattern<ltl::DisableOp>::OpConversionPattern;
 
+  // DisableOp translates to an implication in to the form of
+  // ~condition -> input
+  // As it is assumed that the inpu
   LogicalResult
   matchAndRewrite(ltl::DisableOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
+    // Generate the constant used to negate the
+    /*Value constOne = rewriter.create<hw::ConstantOp>(
+        op.getLoc(), mlir::IntegerAttr::get(
+                         getContext(), llvm::APSInt(llvm::StringRef("1"))));
 
-    /*rewriter.replaceOpWithNewOp<smt::ConstantOp>(
-        op, smt::BitVectorAttr::get(
-                getContext(), adaptor.getValue().getZExtValue(),
-                smt::BitVectorType::get(getContext(),
-                                        op.getType().getIntOrFloatBitWidth())));*/
+    // Generate the antecedant of the disable implication
+    Value antecedent = rewriter.create<comb::XorOp>(
+        op.getLoc(), mlir::UnitAttr(), adaptor.getCondition(), constOne);*/
+
+    // Replace the ltl::DisableOp with the OR part of the implication
+    rewriter.replaceOpWithNewOp<comb::OrOp>(op, adaptor.getCondition(),
+                                            adaptor.getInput());
+
+    return success();
+  }
+};
+
+struct HasBeenResetOpConversion : OpConversionPattern<verif::HasBeenResetOp> {
+  using OpConversionPattern<verif::HasBeenResetOp>::OpConversionPattern;
+
+  // HasBeenReset generates a 1 bit register that is set to one if the reset
+  // value is found to be active at any point
+  LogicalResult
+  matchAndRewrite(verif::HasBeenResetOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+
+    // Generate the constant used to set the register value
+    Value constOne = rewriter.create<hw::ConstantOp>(
+        op.getLoc(), mlir::IntegerAttr::get(
+                         getContext(), llvm::APSInt(llvm::StringRef("1"))));
+
+    // Generate a multiplexer to select the register's value
+    Value mux = rewriter.create<comb::MuxOp>(op.getLoc(), adaptor.getReset(),
+                                             constOne /*, ???*/);
+
+    // Finally generate the register to replace the hasbeenresetOp
+    rewriter.replaceOpWithNewOp<seq::CompRegOp>(op, mux, adaptor.getClock(),
+                                                llvm::StringRef("hbr"));
     return success();
   }
 };
@@ -54,20 +90,37 @@ struct DisableOpConversion : OpConversionPattern<ltl::DisableOp> {
 struct VerifAssertOpConversion : OpConversionPattern<verif::AssertOp> {
   using OpConversionPattern<verif::AssertOp>::OpConversionPattern;
 
+  // A verif assertion is translated together with it's parenting ltl clock
+  // This will yield an sv.always containing an sv.assert
   LogicalResult
   matchAndRewrite(verif::AssertOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
-    /*Value solver = op->getParentOfType<func::FuncOp>()
-                       .getFunctionBody()
-                       .front()
-                       .getArgument(0);
-    Value constOne = rewriter.create<smt::ConstantOp>(
-        op.getLoc(),
-        smt::BitVectorAttr::get(getContext(), 0,
-                                smt::BitVectorType::get(getContext(), 1)));
-    Value cond = rewriter.create<smt::EqOp>(op.getLoc(), adaptor.getProperty(),
-                                            constOne);
-    rewriter.replaceOpWithNewOp<smt::AssertOp>(op, solver, cond);*/
+
+    // Start by making sure that the input is associated to a clock
+    Value prop = adaptor.getProperty();
+    auto ltlclk = dyn_cast<ltl::ClockOp>(prop.getDefiningOp());
+
+    // If it's not clocked then we can't generate a proper sv assertion
+    if (!ltlclk)
+      op->emitError("Verif assertions must be clocked!!");
+
+    // Finish by genrating the parenting sv.always posedge clock from the ltl
+    // clock, containing the generated sv.assert
+    rewriter.create<sv::AlwaysOp>(
+        op.getLoc(), (sv::EventControl)ltlclk.getEdge(), ltlclk.getClock(),
+        [&] {
+          // Generate the sv assertion using the input to the parenting clock
+          rewriter.create<sv::AssertOp>(
+              op.getLoc(), ltlclk.getInput(),
+              sv::DeferAssertAttr::get(getContext(),
+                                       sv::DeferAssert::Immediate),
+              adaptor.getLabelAttr());
+        });
+
+    // Get rid of the two old operations
+    rewriter.eraseOp(ltlclk);
+    rewriter.eraseOp(op);
+
     return success();
   }
 };
