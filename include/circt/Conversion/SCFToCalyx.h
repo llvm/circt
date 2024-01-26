ba50574867277e843405c5efbc72987540638128
linuxlonelyeagle <lonelyeagle.02@bytedance.com>
2024-01-26 21:49:49 +0800

    delete literal identifier.

--- include/circt/Conversion/SCFToCalyx.h
+++ include/circt/Conversion/SCFToCalyx.h
@@ -25,6 +25,12 @@ namespace scfToCalyx {
 // used as the Calyx port name.
 static constexpr std::string_view sPortNameAttr = "calyx.port_name";
 
+// goPort ,donePort and resetPort identifie the attributes of the go, done and
+// reset port of the timing circuit.
+static constexpr std::string_view goPort = "go";
+static constexpr std::string_view donePort = "done";
+static constexpr std::string_view resetPort = "reset";
+
 } // namespace scfToCalyx
 
 /// Create an SCF to Calyx conversion pass.
