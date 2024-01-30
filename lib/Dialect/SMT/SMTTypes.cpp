fe6f6013f33f92f4297f9225fc15eeefd022ffee
Martin Erhart <maerhart@outlook.com>
2024-01-30 19:38:23 +0100

    [SMT] Add basic array operations

--- lib/Dialect/SMT/SMTTypes.cpp
+++ lib/Dialect/SMT/SMTTypes.cpp
@@ -26,6 +26,10 @@ void SMTDialect::registerTypes() {
       >();
 }
 
+bool smt::isAnySMTValueType(Type type) {
+  return isa<BoolType, BitVectorType, ArrayType>(type);
+}
+
 //===----------------------------------------------------------------------===//
 // BitVectorType
 //===----------------------------------------------------------------------===//
@@ -35,5 +39,19 @@ BitVectorType::verify(function_ref<InFlightDiagnostic()> emitError,
                       unsigned width) {
   if (width <= 0)
     return emitError() << "bit-vector must have at least a width of one";
+  return success();
+}
+
+//===----------------------------------------------------------------------===//
+// ArrayType
+//===----------------------------------------------------------------------===//
+
+LogicalResult ArrayType::verify(function_ref<InFlightDiagnostic()> emitError,
+                                Type domainType, Type rangeType) {
+  if (!isAnySMTValueType(domainType))
+    return emitError() << "domain must be any SMT value type";
+  if (!isAnySMTValueType(rangeType))
+    return emitError() << "range must be any SMT value type";
+
   return success();
 }
