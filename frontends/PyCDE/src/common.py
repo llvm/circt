d0e332bb8b7699a688de191ae64d1024c0b6b469
John Demme <john.demme@microsoft.com>
2024-02-15 08:12:18 -0800

    [PyCDE] Refactor Input/Output ports to extend property (#6700)
    Specify fget in the port class itself to that users still have access to the port's properties through the class variable instead of replacing the port itself. As a result, we only have to store a list of ports.

--- frontends/PyCDE/src/common.py
+++ frontends/PyCDE/src/common.py
@@ -10,25 +10,38 @@ from .circt import ir
 from .types import Type, Bundle, Channel, ChannelSignaling, ClockType, Bits
 
 from functools import singledispatchmethod
-from typing import Optional
+from typing import Callable, Optional
 
 
-class ModuleDecl:
+class ModuleDecl(property):
   """Represents an input or output port on a design module."""
 
-  __slots__ = ["name", "_type"]
+  __slots__ = ["idx", "name", "type"]
 
-  def __init__(self, type: Type, name: str = None):
-    self.name: str = name
-    self._type: Type = type
-
-  @property
-  def type(self):
-    return self._type
+  def __init__(self,
+               type: Type,
+               name: Optional[str] = None,
+               fget: Optional[Callable] = None,
+               fset: Optional[Callable] = None):
+    super().__init__(fget=fget, fset=fset)
+    self.idx: Optional[int] = None
+    self.name = name
+    self.type = type
 
 
 class Output(ModuleDecl):
   """Create an RTL-level output port"""
+
+  def __init__(self, type: Type, name: Optional[str] = None):
+    from .signals import _FromCirctValue
+
+    def fget(mod_inst, self=self):
+      return _FromCirctValue(mod_inst.inst.operation.results[self.idx])
+
+    super().__init__(type, name, fget=fget)
+
+  def __repr__(self) -> str:
+    return f"output '{self.name}': {self.type}"
 
 
 class OutputChannel(Output):
@@ -37,7 +50,7 @@ class OutputChannel(Output):
   def __init__(self,
                type: Type,
                signaling: int = ChannelSignaling.ValidReady,
-               name: str = None):
+               name: Optional[str] = None):
     type = Channel(type, signaling)
     super().__init__(type, name)
 
@@ -45,25 +58,39 @@ class OutputChannel(Output):
 class SendBundle(Output):
   """Create an ESI bundle output port (aka sending port)."""
 
-  def __init__(self, bundle: Bundle, name: str = None):
+  def __init__(self, bundle: Bundle, name: Optional[str] = None):
     super().__init__(bundle, name)
 
 
 class Input(ModuleDecl):
   """Create an RTL-level input port."""
 
+  def __init__(self, type: Type, name: Optional[str] = None):
+    from .signals import _FromCirctValue
+
+    def fget(mod_inst, self=self):
+      return _FromCirctValue(mod_inst.inst.operation.operands[self.idx])
+
+    super().__init__(type, name, fget=fget)
+
+  def __repr__(self) -> str:
+    return f"input '{self.name}': {self.type}"
+
 
 class Clock(Input):
   """Create a clock input"""
 
-  def __init__(self, name: str = None):
+  def __init__(self, name: Optional[str] = None):
     super().__init__(ClockType(), name)
+
+  def __repr__(self) -> str:
+    return f"clock {self.name}"
 
 
 class Reset(Input):
   """Create a reset input."""
 
-  def __init__(self, name: str = None):
+  def __init__(self, name: Optional[str] = None):
     super().__init__(Bits(1), name)
 
 
@@ -104,7 +131,7 @@ class AppID:
   def index(self) -> int:
     return self._appid.index
 
-  def __str__(self) -> str:
+  def __repr__(self) -> str:
     return f"{self.name}[{self.index}]"
 
 
