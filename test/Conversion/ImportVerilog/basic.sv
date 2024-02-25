9482ed38d9f35fa858b8fa38723d54ea6699d5de
Fabian Schuiki <fabian@schuiki.ch>
2024-02-25 14:29:20 -0800

    [ImportVerilog] Convert empty modules and instances (#6743)
    Extend the `ImportVerilog` conversion to support empty SystemVerilog
    module definitions and instances of such modules.
    
    To do this, add a `ImportVerilogInternals.h` header that defines a
    `Context` helper struct to aid in the conversion. Similar to other
    parsers and translations, the context has a builder, tracks operations,
    and offers entry points to convert different nodes of the Slang AST, and
    handles diagnostics and location conversion.
    
    An annoying quirk of Slang is the fact that the top-level declarations
    in an SV file are reordered and rearranged. All instantiable nodes, such
    as modules, programs, or interfaces, are moved into a separate list of
    top-level instances which is unordered. To allow for better testing with
    FileCheck and to produce more predictable output, the helper `Context`
    tracks an ordered `std::map` of the created top-level MLIR ops, indexed
    by their Slang location in the input. This map is used to determine the
    insertion point for new modules as the AST is converted, such that the
    resulting MLIR ops are essentially in source file order.
    
    This new conversion discards any module ports, and any module members
    except for instances and stray semicolons. This is already sufficient to
    create module hierarchies, and also to define modules nested in other
    modules, which Slang already helpfully resolves to an instance and
    outlined module definition.
    
    This commit also adds corresponding `moore.module` and `moore.instance`
    operations to capture the empty modules and instances. These will be
    extended in the future to deal with all the quirks of Verilog ingestion.
    
    Most of the module hierarchy conversion is in a new `Structure.cpp` file
    in the `ImportVerilog` conversion, in preparation for future additions
    of statement, expression, and type conversion in separate files.

--- test/Conversion/ImportVerilog/basic.sv
+++ test/Conversion/ImportVerilog/basic.sv
@@ -4,7 +4,35 @@
 // Internal issue in Slang v3 about jump depending on uninitialised value.
 // UNSUPPORTED: valgrind
 
-// CHECK: module {
-// CHECK: }
-module Foo;
+// CHECK-LABEL: moore.module @Empty {
+// CHECK:       }
+module Empty;
+  ; // empty member
+endmodule
+
+// CHECK-LABEL: moore.module @NestedA {
+// CHECK:         moore.instance "NestedB" @NestedB
+// CHECK:       }
+// CHECK-LABEL: moore.module @NestedB {
+// CHECK:         moore.instance "NestedC" @NestedC
+// CHECK:       }
+// CHECK-LABEL: moore.module @NestedC {
+// CHECK:       }
+module NestedA;
+  module NestedB;
+    module NestedC;
+    endmodule
+  endmodule
+endmodule
+
+// CHECK-LABEL: moore.module @Child {
+// CHECK:       }
+module Child;
+endmodule
+
+// CHECK-LABEL: moore.module @Parent
+// CHECK:         moore.instance "child" @Child
+// CHECK:       }
+module Parent;
+  Child child();
 endmodule
