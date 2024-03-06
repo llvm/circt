72b5b2fe3f9c9d9ecff7c523abc88f12924e42aa
Nandor Licker <n@ndor.email>
2024-03-06 05:44:08 -0800

    [NFC][Seq] Move a seq enum to its own file

---
+++ include/circt/Dialect/Seq/SeqEnums.h
@@ -0,0 +1,15 @@
+//===- SeqPasses.h - Seq pass enumeration -----------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+namespace circt {
+namespace seq {
+
+enum class ReadEnableMode { Zero, Ignore, Undefined };
+
+} // namespace seq
+} // namespace circt
