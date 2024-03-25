91ab0f80732ea596d88c82a382444e1915a2e771
Hideto Ueno <uenoku.tokotoko@gmail.com>
2024-03-25 03:10:47 -0700

    [FIRRTL] Decouple UNROnlyAssume generation from AssertOp lowering

---
+++ lib/Dialect/FIRRTL/Transforms/CreateCompanionAssume.cpp
@@ -0,0 +1,66 @@
+//===- CreateCompanionAssume.cpp - Create companion assume ----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the CreateCompanionAssume pass.
+//
+//===----------------------------------------------------------------------===//
+
+#include "PassDetails.h"
+#include "circt/Dialect/FIRRTL/FIRRTLOps.h"
+#include "circt/Dialect/FIRRTL/FIRRTLUtils.h"
+#include "circt/Dialect/FIRRTL/Passes.h"
+
+using namespace circt;
+using namespace firrtl;
+
+namespace {
+struct CreateCompanionAssumePass
+    : public CreateCompanionAssumeBase<CreateCompanionAssumePass> {
+  void runOnOperation() override {
+    getOperation().walk([](firrtl::AssertOp assertOp) {
+      OpBuilder builder(assertOp);
+      builder.setInsertionPointAfter(assertOp);
+      auto guards = assertOp->getAttrOfType<ArrayAttr>("guards");
+      Operation *assume;
+      bool isUnrOnlyAssert = false;
+      // Regard the assertion as UNR only if "USE_UNR_ONLY_CONSTRAINTS" is
+      // included in the guards.
+      if (guards) {
+        isUnrOnlyAssert = llvm::any_of(guards, [](Attribute attr) {
+          StringAttr strAttr = dyn_cast<StringAttr>(attr);
+          return strAttr && strAttr.getValue() == "USE_UNR_ONLY_CONSTRAINTS";
+        });
+      }
+
+      // If so, use AssumeEdgedPredicateIntrinsicOp.
+      if (isUnrOnlyAssert)
+        assume = builder.create<firrtl::AssumeEdgedPredicateIntrinsicOp>(
+            assertOp.getLoc(), assertOp.getPredicate(), assertOp.getEnable(),
+            assertOp.getMessage(), assertOp.getSubstitutions(),
+            assertOp.getName());
+      else
+        // Otherwise use concurrent assume.
+        assume = builder.create<firrtl::AssumeOp>(
+            assertOp.getLoc(), assertOp.getClock(), assertOp.getPredicate(),
+            assertOp.getEnable(), assertOp.getMessage(),
+            assertOp.getSubstitutions(), assertOp.getName(),
+            /*isConcurrent=*/true);
+      SmallVector<Attribute> newGuards{
+          builder.getStringAttr("USE_PROPERTY_AS_CONSTRAINT")};
+      if (guards)
+        newGuards.append(guards.begin(), guards.end());
+      assume->setAttr("guards", builder.getArrayAttr(newGuards));
+    });
+  }
+};
+
+} // end anonymous namespace
+
+std::unique_ptr<mlir::Pass> circt::firrtl::createCreateCompanionAssume() {
+  return std::make_unique<CreateCompanionAssumePass>();
+}
