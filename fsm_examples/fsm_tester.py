ecce10bb06ce1ead587b1bd918b47f17cfd09cc7
luisacicolini <lc985@cl.cam.ac.uk>
2024-04-06 10:14:20 +0100

    test bench for vars scaling

--- fsm_examples/fsm_tester.py
+++ fsm_examples/fsm_tester.py
@@ -6,17 +6,16 @@ reps = 10
 
 time = 100
 
-for file in os.listdir("structured/"):
-    states = int(file.split("_")[1].split("s")[0])-1
+for file in os.listdir("vars/"):
+    # states = int(file.split("_")[1].split("s")[0])-1
     # if to_check >= time:
     #     to_check = time - 1
     print(file)
-    command = "../build/bin/fsm-verification ../fsm_examples/structured/"+file+" "+str(time)+" "+str(states-1)+" 0"
+    command = "../build/bin/fsm-verification ../fsm_examples/vars/"+file+" "+str(time)+" 8 0"
     print(command)
-    if(states<30):
-        for i in range(reps):
-            print("rep "+ str(i)+" file "+file)
-            process = subprocess.run([command], shell=True)
+    for i in range(reps):
+        print("rep "+ str(i)+" file "+file)
+        process = subprocess.run([command], shell=True)
 
 
 # for fsm in range(5, 50, 5):
