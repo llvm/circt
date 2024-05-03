800209ca6ffb727f67b2156443fcb4ccb49c461c
Amelia Dobis <amelia.dobis@sifive.com>
2024-05-03 12:35:09 -0700

    [firtool] Integrate AssertProperty lowering into BTOR2 pipeline (#6975)
    * Add lowering for AssertProperty operations
    
    * removed nl at start of file
    
    * integrated ltl lowering into --btor2
    
    * sorted cmake and removed unreachable branches
    
    * reordered cmake
    
    * cleaned up test
    
    * added pass registration

--- lib/Firtool/Firtool.cpp
+++ lib/Firtool/Firtool.cpp
@@ -386,6 +386,7 @@ LogicalResult firtool::populateFinalizeIR(mlir::PassManager &pm,
 LogicalResult firtool::populateHWToBTOR2(mlir::PassManager &pm,
                                          const FirtoolOptions &opt,
                                          llvm::raw_ostream &os) {
+  pm.addNestedPass<hw::HWModuleOp>(circt::createLowerLTLToCorePass());
   pm.addNestedPass<hw::HWModuleOp>(circt::createConvertHWToBTOR2Pass(os));
   return success();
 }
