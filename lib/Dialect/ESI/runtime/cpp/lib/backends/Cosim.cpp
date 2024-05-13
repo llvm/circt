b0d354323d4546e2ee24366ff1444d2f5549e8a5
John Demme <john.demme@microsoft.com>
2024-05-13 22:19:15 +0000

    typo

--- lib/Dialect/ESI/runtime/cpp/lib/backends/Cosim.cpp
+++ lib/Dialect/ESI/runtime/cpp/lib/backends/Cosim.cpp
@@ -38,13 +38,11 @@ CosimAccelerator::connect(Context &ctxt, string connectionString) {
   string portStr;
   string host = "localhost";
 
-  size_t strLen = connectionString.size();
   size_t colon;
   if ((colon = connectionString.find(':')) != string::npos) {
     portStr = connectionString.substr(colon + 1);
     host = connectionString.substr(0, colon);
-  } else if (strLen >= 9 &&
-             connectionString.substr(strLen - 9) == "cosim.cfg") {
+  } else if (connectionString.ends_with("cosim.cfg")) {
     ifstream cfg(connectionString);
     string line, key, value;
 
