156cf0a0c0ae98d54726a7f65b36548e2c7cc58e
Amelia Dobis <amelia.dobis@sifive.com>
2024-05-17 11:32:51 -0700

    used multi-type-arg isa instead of disjunction

--- lib/Dialect/LTL/LTLTypes.cpp
+++ lib/Dialect/LTL/LTLTypes.cpp
@@ -5,20 +5,20 @@ using namespace circt;
 using namespace ltl;
 
 bool circt::ltl::isClocked(mlir::Type type) {
-  return isa<ClockedPropertyType>(type) || isa<ClockedSequenceType>(type) ||
-         isa<ClockedDisabledPropertyType>(type);
+  return isa<ClockedPropertyType, ClockedSequenceType,
+    ClockedDisabledPropertyType>(type);
 }
 
 bool circt::ltl::isDisabled(mlir::Type type) {
-  return isa<DisabledPropertyType>(type) ||
-         isa<ClockedDisabledPropertyType>(type);
+  return isa<DisabledPropertyType, ClockedDisabledPropertyType>(type);
 }
 
 bool circt::ltl::isProperty(mlir::Type type) {
-  return isa<ClockedPropertyType>(type) || isa<DisabledPropertyType>(type) ||
-         isa<ClockedDisabledPropertyType>(type) || isa<PropertyType>(type);
+  return isa<
+    ClockedPropertyType, DisabledPropertyType,
+    ClockedDisabledPropertyType, PropertyType>(type);
 }
 
 bool circt::ltl::isSequence(mlir::Type type) {
-  return isa<SequenceType>(type) || isa<ClockedSequenceType>(type);
+  return isa<SequenceType, ClockedSequenceType>(type);
 }
