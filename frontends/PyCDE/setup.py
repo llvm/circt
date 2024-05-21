2ba19927f3d288ffa473022c3d16e1e4ba5dd3bb
John Demme <john.demme@microsoft.com>
2024-05-20 17:37:07 -0700

    [PyCDE] Move build and publishing pipeline into repo (#7062)
    Previously was built and published internally. Resolves #1509.

--- frontends/PyCDE/setup.py
+++ frontends/PyCDE/setup.py
@@ -45,7 +45,6 @@ class CMakeBuild(build_py):
 
   def run(self):
     target_dir = self.build_lib
-    test = os.getenv("RUN_TESTS")
     cmake_build_dir = os.getenv("PYCDE_CMAKE_BUILD_DIR")
     if not cmake_build_dir:
       cmake_build_dir = os.path.join(target_dir, "..", "cmake_build")
@@ -54,6 +53,8 @@ class CMakeBuild(build_py):
         os.environ.get("CIRCT_DIRECTORY", os.path.join(_thisdir, "..", "..")))
     src_dir = os.path.abspath(os.path.join(circt_dir, "llvm", "llvm"))
     cfg = "Release"
+    if "BUILD_TYPE" in os.environ:
+      cfg = os.environ["BUILD_TYPE"]
     cmake_args = [
         "-DCMAKE_INSTALL_PREFIX={}".format(os.path.abspath(cmake_install_dir)),
         "-DPython3_EXECUTABLE={}".format(sys.executable.replace("\\", "/")),
@@ -66,9 +67,23 @@ class CMakeBuild(build_py):
         "-DCIRCT_ENABLE_FRONTENDS=PyCDE",
         "-DLLVM_EXTERNAL_PROJECTS=circt",
         "-DLLVM_EXTERNAL_CIRCT_SOURCE_DIR={}".format(circt_dir),
+        "-DESI_RUNTIME=ON",
     ]
+    if "COMPILER_LAUNCHER" in os.environ:
+      cmake_args += [
+          f"-DCMAKE_C_COMPILER_LAUNCHER={os.environ['COMPILER_LAUNCHER']}",
+          f"-DCMAKE_CXX_COMPILER_LAUNCHER={os.environ['COMPILER_LAUNCHER']}"
+      ]
+    if "CC" in os.environ:
+      cmake_args += [f"-DCMAKE_C_COMPILER={os.environ['CC']}"]
+    if "CXX" in os.environ:
+      cmake_args += [f"-DCMAKE_CXX_COMPILER={os.environ['CXX']}"]
     if "CIRCT_EXTRA_CMAKE_ARGS" in os.environ:
       cmake_args += os.environ["CIRCT_EXTRA_CMAKE_ARGS"].split(" ")
+    if "VCPKG_INSTALLATION_ROOT" in os.environ:
+      cmake_args += [
+          f"-DCMAKE_TOOLCHAIN_FILE={os.environ['VCPKG_INSTALLATION_ROOT']}/scripts/buildsystems/vcpkg.cmake"
+      ]
     build_args = []
     build_parallelism = os.getenv("CMAKE_PARALLELISM")
     if build_parallelism:
@@ -81,25 +96,22 @@ class CMakeBuild(build_py):
     cmake_cache_file = os.path.join(cmake_build_dir, "CMakeCache.txt")
     if os.path.exists(cmake_cache_file):
       os.remove(cmake_cache_file)
+    print(f"Running cmake with args: {cmake_args}", file=sys.stderr)
+    subprocess.check_call(["echo", "Running: cmake", src_dir] + cmake_args)
     subprocess.check_call(["cmake", src_dir] + cmake_args, cwd=cmake_build_dir)
+    targets = ["check-pycde"]
+    if "RUN_TESTS" in os.environ and os.environ["RUN_TESTS"] != "false":
+      # The pycde integration tests test both PyCDE and the ESIRuntime so
+      # failure shouldn't gate publishing PyCDE.
+      # targets.append("check-pycde-integration")
+      targets.append("check-circt")
     subprocess.check_call([
         "cmake",
         "--build",
         ".",
         "--target",
-        "check-pycde",
-    ] + build_args,
+    ] + targets + build_args,
                           cwd=cmake_build_dir)
-    if test.lower() == "true":
-      subprocess.check_call([
-          "cmake",
-          "--build",
-          ".",
-          "--target",
-          "check-pycde-integration",
-          "check-circt",
-      ] + build_args,
-                            cwd=cmake_build_dir)
     install_cmd = ["cmake", "--build", ".", "--target", "install-PyCDE"]
     subprocess.check_call(install_cmd + build_args, cwd=cmake_build_dir)
     shutil.copytree(os.path.join(cmake_install_dir, "python_packages"),
