37e5ad962424f1d16944674a62b72b689af8cc93
Fabian Schuiki <fabian@schuiki.ch>
2024-05-27 09:31:39 -0700

    [Moore] Support stripped type parsing and printing (#7090)
    Operations defined through TableGen will use a version of type parsing
    and printing that strips away the dialect prefix in case this does not
    lead to ambiguities. If an op specifies a type like `UnpackedType`, the
    generated parser and printer will call `UnpackedType::parse` and
    `UnpackedType::print` in order to parse and print that type. This method
    usually prints the type without any dialect prefixes, and often also
    without the type name itself (as it is obvious given the type).
    
    Add an implementation for `parse` and `print` to `UnpackedType` which
    performs this stripped printing. This makes the assembly more compact
    and in-line with what MLIR would generate by default if a
    TableGen-specified op would use a TableGen-specified type.
    
    This makes the integer flavor of `IntType` look exactly like the builtin
    integer type, which will facilitate a future change from Moore's integer
    type to the builtin integer. Parsing is unambiguous, since the stripped
    form is only used when the type is clear from the op context.

--- test/Conversion/ImportVerilog/types.sv
+++ test/Conversion/ImportVerilog/types.sv
@@ -8,9 +8,9 @@
 module Enums;
   typedef enum shortint { MAGIC } myEnum;
 
-  // CHECK-NEXT: %e0 = moore.variable : !moore.i32
-  // CHECK-NEXT: %e1 = moore.variable : !moore.i8
-  // CHECK-NEXT: %e2 = moore.variable : !moore.i16
+  // CHECK-NEXT: %e0 = moore.variable : i32
+  // CHECK-NEXT: %e1 = moore.variable : i8
+  // CHECK-NEXT: %e2 = moore.variable : i16
   enum { FOO, BAR } e0;
   enum byte { HELLO = 0, WORLD = 1 } e1;
   myEnum e2;
@@ -18,15 +18,15 @@ endmodule
 
 // CHECK-LABEL: moore.module @IntAtoms
 module IntAtoms;
-  // CHECK-NEXT: %d0 = moore.variable : !moore.l1
-  // CHECK-NEXT: %d1 = moore.variable : !moore.i1
-  // CHECK-NEXT: %d2 = moore.variable : !moore.l1
-  // CHECK-NEXT: %d3 = moore.variable : !moore.i32
-  // CHECK-NEXT: %d4 = moore.variable : !moore.i16
-  // CHECK-NEXT: %d5 = moore.variable : !moore.i64
-  // CHECK-NEXT: %d6 = moore.variable : !moore.l32
-  // CHECK-NEXT: %d7 = moore.variable : !moore.i8
-  // CHECK-NEXT: %d8 = moore.variable : !moore.l64
+  // CHECK-NEXT: %d0 = moore.variable : l1
+  // CHECK-NEXT: %d1 = moore.variable : i1
+  // CHECK-NEXT: %d2 = moore.variable : l1
+  // CHECK-NEXT: %d3 = moore.variable : i32
+  // CHECK-NEXT: %d4 = moore.variable : i16
+  // CHECK-NEXT: %d5 = moore.variable : i64
+  // CHECK-NEXT: %d6 = moore.variable : l32
+  // CHECK-NEXT: %d7 = moore.variable : i8
+  // CHECK-NEXT: %d8 = moore.variable : l64
   logic d0;
   bit d1;
   reg d2;
@@ -37,15 +37,15 @@ module IntAtoms;
   byte d7;
   time d8;
 
-  // CHECK-NEXT: %u0 = moore.variable : !moore.l1
-  // CHECK-NEXT: %u1 = moore.variable : !moore.i1
-  // CHECK-NEXT: %u2 = moore.variable : !moore.l1
-  // CHECK-NEXT: %u3 = moore.variable : !moore.i32
-  // CHECK-NEXT: %u4 = moore.variable : !moore.i16
-  // CHECK-NEXT: %u5 = moore.variable : !moore.i64
-  // CHECK-NEXT: %u6 = moore.variable : !moore.l32
-  // CHECK-NEXT: %u7 = moore.variable : !moore.i8
-  // CHECK-NEXT: %u8 = moore.variable : !moore.l64
+  // CHECK-NEXT: %u0 = moore.variable : l1
+  // CHECK-NEXT: %u1 = moore.variable : i1
+  // CHECK-NEXT: %u2 = moore.variable : l1
+  // CHECK-NEXT: %u3 = moore.variable : i32
+  // CHECK-NEXT: %u4 = moore.variable : i16
+  // CHECK-NEXT: %u5 = moore.variable : i64
+  // CHECK-NEXT: %u6 = moore.variable : l32
+  // CHECK-NEXT: %u7 = moore.variable : i8
+  // CHECK-NEXT: %u8 = moore.variable : l64
   logic unsigned u0;
   bit unsigned u1;
   reg unsigned u2;
@@ -56,15 +56,15 @@ module IntAtoms;
   byte unsigned u7;
   time unsigned u8;
 
-  // CHECK-NEXT: %s0 = moore.variable : !moore.l1
-  // CHECK-NEXT: %s1 = moore.variable : !moore.i1
-  // CHECK-NEXT: %s2 = moore.variable : !moore.l1
-  // CHECK-NEXT: %s3 = moore.variable : !moore.i32
-  // CHECK-NEXT: %s4 = moore.variable : !moore.i16
-  // CHECK-NEXT: %s5 = moore.variable : !moore.i64
-  // CHECK-NEXT: %s6 = moore.variable : !moore.l32
-  // CHECK-NEXT: %s7 = moore.variable : !moore.i8
-  // CHECK-NEXT: %s8 = moore.variable : !moore.l64
+  // CHECK-NEXT: %s0 = moore.variable : l1
+  // CHECK-NEXT: %s1 = moore.variable : i1
+  // CHECK-NEXT: %s2 = moore.variable : l1
+  // CHECK-NEXT: %s3 = moore.variable : i32
+  // CHECK-NEXT: %s4 = moore.variable : i16
+  // CHECK-NEXT: %s5 = moore.variable : i64
+  // CHECK-NEXT: %s6 = moore.variable : l32
+  // CHECK-NEXT: %s7 = moore.variable : i8
+  // CHECK-NEXT: %s8 = moore.variable : l64
   logic signed s0;
   bit signed s1;
   reg signed s2;
@@ -78,46 +78,46 @@ endmodule
 
 // CHECK-LABEL: moore.module @Dimensions
 module Dimensions;
-  // CHECK-NEXT: %p0 = moore.variable : !moore.l3
+  // CHECK-NEXT: %p0 = moore.variable : l3
   logic [2:0] p0;
-  // CHECK-NEXT: %p1 = moore.variable : !moore.l3
+  // CHECK-NEXT: %p1 = moore.variable : l3
   logic [0:2] p1;
-  // CHECK-NEXT: %p2 = moore.variable : !moore.array<6 x !moore.l3>
+  // CHECK-NEXT: %p2 = moore.variable : array<6 x l3>
   logic [5:0][2:0] p2;
-  // CHECK-NEXT: %p3 = moore.variable : !moore.array<6 x !moore.l3>
+  // CHECK-NEXT: %p3 = moore.variable : array<6 x l3>
   logic [0:5][2:0] p3;
 
-  // CHECK-NEXT: %u0 = moore.variable : !moore.uarray<3 x !moore.l1>
+  // CHECK-NEXT: %u0 = moore.variable : uarray<3 x l1>
   logic u0 [2:0];
-  // CHECK-NEXT: %u1 = moore.variable : !moore.uarray<3 x !moore.l1>
+  // CHECK-NEXT: %u1 = moore.variable : uarray<3 x l1>
   logic u1 [0:2];
-  // CHECK-NEXT: %u2 = moore.variable : !moore.uarray<6 x !moore.uarray<3 x !moore.l1>>
+  // CHECK-NEXT: %u2 = moore.variable : uarray<6 x uarray<3 x l1>>
   logic u2 [5:0][2:0];
-  // CHECK-NEXT: %u3 = moore.variable : !moore.uarray<6 x !moore.uarray<3 x !moore.l1>>
+  // CHECK-NEXT: %u3 = moore.variable : uarray<6 x uarray<3 x l1>>
   logic u3 [0:5][2:0];
-  // CHECK-NEXT: %u4 = moore.variable : !moore.open_uarray<!moore.l1>
+  // CHECK-NEXT: %u4 = moore.variable : open_uarray<l1>
   logic u4 [];
-  // CHECK-NEXT: %u5 = moore.variable : !moore.open_uarray<!moore.open_uarray<!moore.l1>>
+  // CHECK-NEXT: %u5 = moore.variable : open_uarray<open_uarray<l1>>
   logic u5 [][];
-  // CHECK-NEXT: %u6 = moore.variable : !moore.uarray<42 x !moore.l1>
+  // CHECK-NEXT: %u6 = moore.variable : uarray<42 x l1>
   logic u6 [42];
-  // CHECK-NEXT: %u7 = moore.variable : !moore.assoc_array<!moore.l1, !moore.i32>
+  // CHECK-NEXT: %u7 = moore.variable : assoc_array<l1, i32>
   logic u7 [int];
-  // CHECK-NEXT: %u8 = moore.variable : !moore.assoc_array<!moore.l1, !moore.l1>
+  // CHECK-NEXT: %u8 = moore.variable : assoc_array<l1, l1>
   logic u8 [logic];
-  //CHECK-NEXT: %u9 = moore.variable : !moore.queue<!moore.l1, 0>
+  //CHECK-NEXT: %u9 = moore.variable : queue<l1, 0>
   logic u9 [$];
-  //CHECK-NEXT: %u10 = moore.variable : !moore.queue<!moore.l1, 2>
+  //CHECK-NEXT: %u10 = moore.variable : queue<l1, 2>
   logic u10 [$:2];
 endmodule
 
 // CHECK-LABEL: moore.module @RealType
 module RealType;
-  // CHECK-NEXT: %d0 = moore.variable : !moore.real
-  // CHECK-NEXT: %d1 = moore.variable : !moore.real
-  // CHECK-NEXT: %d2 = moore.variable : !moore.real
+  // CHECK-NEXT: %d0 = moore.variable : real
   real d0;
+  // CHECK-NEXT: %d1 = moore.variable : real
   realtime d1;
+  // CHECK-NEXT: %d2 = moore.variable : real
   shortreal d2;
 endmodule
 
@@ -126,10 +126,10 @@ module Structs;
   typedef struct packed { byte a; int b; } myStructA;
   typedef struct { byte x; int y; } myStructB;
 
-  // CHECK-NEXT: %s0 = moore.variable : !moore.packed<struct<{foo: i1, bar: l1}>>
-  // CHECK-NEXT: %s1 = moore.variable : !moore.unpacked<struct<{many: assoc_array<!moore.i1, !moore.i32>}>>
-  // CHECK-NEXT: %s2 = moore.variable : !moore.packed<struct<{a: i8, b: i32}>>
-  // CHECK-NEXT: %s3 = moore.variable : !moore.unpacked<struct<{x: i8, y: i32}>>
+  // CHECK-NEXT: %s0 = moore.variable : packed<struct<{foo: i1, bar: l1}>>
+  // CHECK-NEXT: %s1 = moore.variable : unpacked<struct<{many: assoc_array<i1, i32>}>>
+  // CHECK-NEXT: %s2 = moore.variable : packed<struct<{a: i8, b: i32}>>
+  // CHECK-NEXT: %s3 = moore.variable : unpacked<struct<{x: i8, y: i32}>>
   struct packed { bit foo; logic bar; } s0;
   struct { bit many[int]; } s1;
   myStructA s2;
@@ -141,8 +141,8 @@ module Typedefs;
   typedef logic [2:0] myType1;
   typedef logic myType2 [2:0];
 
-  // CHECK-NEXT: %v0 = moore.variable : !moore.l3
-  // CHECK-NEXT: %v1 = moore.variable : !moore.uarray<3 x !moore.l1>
+  // CHECK-NEXT: %v0 = moore.variable : l3
+  // CHECK-NEXT: %v1 = moore.variable : uarray<3 x l1>
   myType1 v0;
   myType2 v1;
 endmodule
