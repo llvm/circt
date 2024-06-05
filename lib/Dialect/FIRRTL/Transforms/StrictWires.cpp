458e575d44b9a92e4c7bd65114faea1afeb80a73
Andrew Lenharth <andrew@lenharth.org>
2024-06-05 07:51:58 -0500

    Basic pass functionality.

--- lib/Dialect/FIRRTL/Transforms/StrictWires.cpp
+++ lib/Dialect/FIRRTL/Transforms/StrictWires.cpp
@@ -18,15 +18,53 @@
 #include "mlir/IR/ImplicitLocOpBuilder.h"
 #include "llvm/Support/Debug.h"
 
+#include <deque>
+
 #define DEBUG_TYPE "firrtl-strict-wires"
 
 using namespace circt;
 using namespace firrtl;
 
-template<typename RetTy>
-static RetTy cloneWireTo(WireOp wire);
+static StrictWireOp cloneWireTo(mlir::OpBuilder &builder, WireOp wire) {
+  builder.setInsertionPoint(wire);
+  return builder.create<StrictWireOp>(
+      wire.getLoc(), cast<FIRRTLBaseType>(wire.getResult().getType()),
+      wire.getNameAttr(), wire.getNameKindAttr(), wire.getAnnotations(),
+      wire.getInnerSymAttr(), wire.getForceableAttr());
+}
 
-static void updateWireUses(Value toReplace, Value readSide, Value writeSide);
+static void updateWireUses(mlir::OpBuilder &builder,
+                           std::deque<Operation *> &toDelete, Value toReplace,
+                           Value readSide, Value writeSide) {
+  for (auto user : toReplace.getUsers()) {
+    TypeSwitch<Operation *>(user)
+        .Case<SubfieldOp>([&](auto op) {
+          builder.setInsertionPoint(op);
+          auto newWrite = builder.create<LHSSubfieldOp>(op.getLoc(), writeSide,
+                                                        op.getFieldIndex());
+          return updateWireUses(builder, toDelete, op, op.getResult(),
+                                newWrite);
+        })
+        .Case<SubindexOp>([&](auto op) {
+          builder.setInsertionPoint(op);
+          auto newWrite = builder.create<LHSSubindexOp>(op.getLoc(), writeSide,
+                                                        op.getIndex());
+          return updateWireUses(builder, toDelete, op, op.getResult(),
+                                newWrite);
+        })
+        .Case<MatchingConnectOp>([&](auto op) {
+          if (op.getDest() == toReplace) {
+            builder.setInsertionPoint(op);
+            builder.create<StrictConnectOp>(op.getLoc(), writeSide,
+                                            op.getSrc());
+            toDelete.push_back(op);
+          }
+          return;
+        });
+  }
+  if (toReplace != readSide)
+    toReplace.replaceAllUsesWith(readSide);
+}
 
 //===----------------------------------------------------------------------===//
 // Pass Infrastructure
@@ -43,14 +81,24 @@ void StrictWiresPass::runOnOperation() {
   LLVM_DEBUG(debugPassHeader(this) << "\n";);
   auto module = getOperation();
   mlir::OpBuilder builder(module);
+  std::deque<Operation *> toDelete;
 
   module.walk([&](Operation *op) -> WalkResult {
     if (auto wire = dyn_cast<WireOp>(op)) {
-      auto newWire = cloneWireTo<StrictWireOp>(wire);
-      updateWireUses(wire.getResult(), newWire.getRead(), newWire.getWrite());
+      if (!cast<FIRRTLType>(wire.getResult().getType())
+               .getRecursiveTypeProperties()
+               .isPassive)
+        return WalkResult::advance();
+      auto newWire = cloneWireTo(builder, wire);
+      updateWireUses(builder, toDelete, wire.getResult(), newWire.getRead(),
+                     newWire.getWrite());
+      toDelete.push_back(wire);
     }
     return WalkResult::advance();
   });
+
+  for (auto w : toDelete)
+    w->erase();
 }
 
 /// This is the pass constructor.
