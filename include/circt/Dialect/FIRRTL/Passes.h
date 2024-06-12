52096a8c9a70c11a751f23fdc11d5071a633dcda
Andrew Young <youngar17@gmail.com>
2024-06-12 01:56:52 -0700

    [FIRRTL] Add pass to specialize layers

--- include/circt/Dialect/FIRRTL/Passes.h
+++ include/circt/Dialect/FIRRTL/Passes.h
@@ -196,6 +196,8 @@ std::unique_ptr<mlir::Pass> createMaterializeDebugInfoPass();
 
 std::unique_ptr<mlir::Pass> createLintingPass();
 
+std::unique_ptr<mlir::Pass> createSpecializeLayersPass();
+
 std::unique_ptr<mlir::Pass> createSpecializeOptionPass();
 
 std::unique_ptr<mlir::Pass> createCreateCompanionAssume();
