1e6295f08ddf3e1678966dc9a1216f939a069fe1
mingzheTerapines <mingzhe.zhang@terapines.com>
2024-06-18 15:43:41 +0800

    Fix some bugs

--- lib/Dialect/Moore/Transforms/Dedup.cpp
+++ lib/Dialect/Moore/Transforms/Dedup.cpp
@@ -33,14 +33,23 @@ using namespace moore;
 namespace {
 class DedupPass : public circt::moore::impl::DedupBase<DedupPass> {
   // This table contains information to determine module module uniqueness.
-  using StructuralHash = std::array<unsigned, 32>;
-  using ModuleInfoTable = DenseMap<mlir::StringAttr, StructuralHash>;
+  using ModuleInfo = struct ModuleStruct {
+    SmallVector<Type> portTypes;
+    SmallVector<Type> inputTypes;
+    SmallVector<Type> outputTypes;
+  };
+  friend bool operator==(const ModuleInfo &lhs, const ModuleInfo &rhs) {
+    return lhs.portTypes == rhs.portTypes && lhs.inputTypes == rhs.inputTypes &&
+           lhs.outputTypes == rhs.outputTypes;
+  }
+
+  using ModuleInfoTable = DenseMap<mlir::StringAttr, ModuleInfo>;
   ModuleInfoTable moduleInfoTable;
+
   // This tale records old module name and equiplance module name to update
   using Symbol2Symbol = DenseMap<mlir::StringAttr, mlir::StringAttr>;
   Symbol2Symbol replaceTable;
 
-  void setHashValue(StructuralHash &val, int index, mlir::StringAttr str);
   void runOnOperation() override;
 };
 } // namespace
@@ -49,32 +58,16 @@ std::unique_ptr<mlir::Pass> circt::moore::createDedupPass() {
   return std::make_unique<DedupPass>();
 }
 
-void DedupPass::setHashValue(StructuralHash &val, int index,
-                             mlir::StringAttr str) {
-  // We assume SHA256 is already a good hash and just truncate down to the
-  // number of bytes we need for DenseMap.
-  unsigned hash;
-  std::memcpy(&hash, str.str().c_str(), sizeof(unsigned));
-  val[index] = hash;
-}
-
 void DedupPass::runOnOperation() {
   // Do equiplance and record in replacTable
   // Dedup already exist module op
   getOperation()->walk([&](SVModuleOp moduleOp) {
-    mlir::OpBuilder builder(&getContext());
-    // If this Op has alreday in replace Table, erase it.
-    if (replaceTable.lookup(moduleOp.getSymNameAttr())) {
-      moduleOp->erase();
-      return WalkResult::advance();
-    }
-
-    // Do hash calculation
-    StructuralHash moduleInfo;
+    // Define uniqueness
     auto moduleName = moduleOp.getSymNameAttr();
-
-    auto moduleType = moduleOp.getModuleTypeAttrName();
-    setHashValue(moduleInfo, 0, moduleType);
+    auto moduleType = moduleOp.getModuleType();
+    ModuleInfo moduleInfo = {moduleType.getPortTypes(),
+                             moduleType.getInputTypes(),
+                             moduleType.getOutputTypes()};
 
     // Compare and record to replacetable and erase this op if there is a
     // equiplance
@@ -85,16 +78,15 @@ void DedupPass::runOnOperation() {
         return WalkResult::advance();
       }
     }
-    moduleInfoTable.insert({moduleName, moduleInfo});
+    moduleInfoTable[moduleName] = moduleInfo;
     return WalkResult::advance();
   });
 
   // updata instanceOp module name to the new name
   getOperation()->walk([&](InstanceOp InstanceOp) {
-    mlir::OpBuilder builder(&getContext());
-    if (auto oldName =
-            replaceTable.lookup(InstanceOp.getModuleNameAttrName())) {
-      InstanceOp.setModuleName(replaceTable[oldName]);
+    auto instanceName = InstanceOp.getModuleNameAttr().getAttr();
+    if (replaceTable.lookup(instanceName)) {
+      InstanceOp.setModuleName(replaceTable[instanceName]);
     }
     return WalkResult::advance();
   });
