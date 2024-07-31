41841ebb862215d310712803e53e1791146e141c
wenhu1024 <96782898+wenhu1024@users.noreply.github.com>
2024-07-31 13:52:11 +0800

    [ImportVerilog] Support for String Types, String Literals (#7403)
    Co-authored-by: itaras20 <ian.taras20@gmail.com>

--- test/Conversion/ImportVerilog/basic.sv
+++ test/Conversion/ImportVerilog/basic.sv
@@ -201,6 +201,19 @@ module Basic;
   // CHECK: %ev2 = moore.variable [[VARIANT_B]]
   MyEnum ev1 = VariantA;
   MyEnum ev2 = VariantB;
+
+  // CHECK: [[STR_WELCOME:%.+]] = moore.string_constant "Welcome to Moore" : i128
+  // CHECK: [[CONV_WELCOME:%.+]] = moore.conversion [[STR_WELCOME]] : !moore.i128 -> !moore.string
+  // CHECK: [[VAR_S:%.+]] = moore.variable [[CONV_WELCOME]] : <string>
+  string s = "Welcome to Moore";
+
+  // CHECK: [[VAR_S1:%.+]] = moore.variable : <string>
+  // CHECK: [[STR_HELLO:%.+]] = moore.string_constant "Hello World" : i88
+  // CHECK: [[CONV_HELLO:%.+]] = moore.conversion [[STR_HELLO]] : !moore.i88 -> !moore.string
+  // CHECK: moore.assign [[VAR_S1]], [[CONV_HELLO]] : string
+  string s1; 
+  assign s1 = "Hello World";
+
 endmodule
 
 // CHECK-LABEL: moore.module @Statements
