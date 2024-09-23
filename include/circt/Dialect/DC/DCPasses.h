e663ea87835f4e3da859cbd38d656713b57d1f9c
Luisa Cicolini <lc985@cl.cam.ac.uk>
2024-09-23 15:26:09 +0100

    attempting to refactor export to pass

--- include/circt/Dialect/DC/DCPasses.h
+++ include/circt/Dialect/DC/DCPasses.h
@@ -10,7 +10,9 @@
 #define CIRCT_DIALECT_DC_DCPASSES_H
 
 #include "mlir/Pass/Pass.h"
+#include "circt/Support/LLVM.h"
 #include <memory>
+
 
 namespace mlir {
 class Pass;
@@ -21,6 +23,7 @@ namespace dc {
 
 std::unique_ptr<mlir::Pass> createDCMaterializeForksSinksPass();
 std::unique_ptr<mlir::Pass> createDCDematerializeForksSinksPass();
+std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>> createDCDotPrintPass();
 
 #define GEN_PASS_REGISTRATION
 #include "circt/Dialect/DC/DCPasses.h.inc"
