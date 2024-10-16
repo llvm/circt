ea952baa3e0898cc4a243f508bf57879e6b00ab8
Lenny Truong <lenny.truong@sifive.com>
2024-10-15 17:04:44 -0700

    [Verif] Add LowerFormalToHW pass (#7707)
    * Rewrites `verif.formal` to `hw.module`
    * Lifts `verif.symbolic_value` to be an input to the module

---
+++ lib/Dialect/Verif/Transforms/LowerFormalToHW.cpp
@@ -0,0 +1,76 @@
+//===- LowerFormalToHW.cpp - Formal Preparations --*- C++ -*---------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//===----------------------------------------------------------------------===//
+//
+// Lower verif.formal to hw.module.
+//
+//===----------------------------------------------------------------------===//
+#include "circt/Dialect/Verif/VerifOps.h"
+#include "circt/Dialect/Verif/VerifPasses.h"
+
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+using namespace circt;
+
+namespace circt {
+namespace verif {
+#define GEN_PASS_DEF_LOWERFORMALTOHW
+#include "circt/Dialect/Verif/Passes.h.inc"
+} // namespace verif
+} // namespace circt
+
+using namespace mlir;
+using namespace verif;
+
+namespace {
+struct LowerFormalToHW
+    : circt::verif::impl::LowerFormalToHWBase<LowerFormalToHW> {
+  void runOnOperation() override;
+};
+
+struct FormalOpRewritePattern : public OpRewritePattern<verif::FormalOp> {
+  using OpRewritePattern<FormalOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(FormalOp op,
+                                PatternRewriter &rewriter) const override {
+    // Create the ports for all the symbolic values
+    SmallVector<hw::PortInfo> ports;
+    for (auto symOp : op.getBody().front().getOps<verif::SymbolicValueOp>()) {
+      ports.push_back(
+          hw::PortInfo({{rewriter.getStringAttr("symbolic_value_" +
+                                                std::to_string(ports.size())),
+                         symOp.getType(), hw::ModulePort::Input}}));
+    }
+
+    auto moduleOp =
+        rewriter.create<hw::HWModuleOp>(op.getLoc(), op.getNameAttr(), ports);
+
+    rewriter.inlineBlockBefore(&op.getBody().front(),
+                               &moduleOp.getBodyBlock()->front(),
+                               op.getBody().getArguments());
+
+    // Replace symbolic values with module arguments
+    size_t i = 0;
+    for (auto symOp : make_early_inc_range(
+             moduleOp.getBodyBlock()->getOps<SymbolicValueOp>())) {
+      rewriter.replaceAllUsesWith(symOp.getResult(),
+                                  moduleOp.getArgumentForInput(i));
+      i++;
+      rewriter.eraseOp(symOp);
+    }
+    rewriter.eraseOp(op);
+    return success();
+  }
+};
+} // namespace
+
+void LowerFormalToHW::runOnOperation() {
+  RewritePatternSet patterns(&getContext());
+  patterns.add<FormalOpRewritePattern>(patterns.getContext());
+
+  if (failed(applyPatternsAndFoldGreedily(getOperation(), std::move(patterns))))
+    signalPassFailure();
+}
