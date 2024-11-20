4e52431c12a0188423b296a03efda644aca6c0f9
Martin Erhart <martin.erhart@sifive.com>
2024-11-20 09:25:24 +0000

    [RTG] Add context resource interfaces (#7840)
    Co-authored-by: Andrew Lenharth <andrew@lenharth.org>

---
+++ include/circt/Dialect/RTG/IR/RTGTypeInterfaces.h
@@ -0,0 +1,21 @@
+//===- RTGTypeInterfaces.h - Declare RTG type interfaces --------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares type interfaces for the RTG Dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_RTG_IR_RTGTYPEINTERFACES_H
+#define CIRCT_DIALECT_RTG_IR_RTGTYPEINTERFACES_H
+
+#include "circt/Support/LLVM.h"
+#include "mlir/IR/Types.h"
+
+#include "circt/Dialect/RTG/IR/RTGTypeInterfaces.h.inc"
+
+#endif // CIRCT_DIALECT_RTG_IR_RTGTYPEINTERFACES_H
---
+++ lib/Dialect/RTG/IR/RTGOpInterfaces.cpp
@@ -0,0 +1,19 @@
+//===- RTGOpInterfaces.cpp - Implement the RTG op interfaces --------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the RTG op interfaces.
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/RTG/IR/RTGOpInterfaces.h"
+
+//===----------------------------------------------------------------------===//
+// TableGen generated logic.
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/RTG/IR/RTGOpInterfaces.cpp.inc"
