24beabfb06e4396806befc53cbb9acae579bb496
John Demme <john.demme@microsoft.com>
2024-11-21 19:14:06 +0000

    Minor enhancements for handshake.py
    Changes made in PyCDE bindings

--- lib/Bindings/Python/dialects/handshake.py
+++ lib/Bindings/Python/dialects/handshake.py
@@ -3,15 +3,14 @@
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from __future__ import annotations
+from typing import Dict, List, Tuple, Union
 
 from . import handshake
 from ._handshake_ops_gen import *
 from ._handshake_ops_gen import _Dialect
 
 from ..dialects._ods_common import _cext as _ods_cext
-from ..ir import ArrayAttr, FunctionType, StringAttr, Type, TypeAttr
-
-from typing import List, Tuple, Union
+from ..ir import ArrayAttr, Attribute, FunctionType, StringAttr, Type, TypeAttr
 
 from ._ods_common import (
     equally_sized_accessor as _ods_equally_sized_accessor,
@@ -32,17 +31,25 @@ class FuncOp(FuncOp):
   def create(sym_name: Union[StringAttr, str],
              args: List[Tuple[str, Type]],
              results: List[Tuple[str, Type]],
-             private: bool = False) -> FuncOp:
+             attributes: Dict[str, Attribute] = {},
+             loc=None,
+             ip=None) -> FuncOp:
     if isinstance(sym_name, str):
       sym_name = StringAttr.get(sym_name)
     input_types = [t for _, t in args]
     res_types = [t for _, t in results]
     func_type = FunctionType.get(input_types, res_types)
     func_type_attr = TypeAttr.get(func_type)
-    funcop = FuncOp(func_type_attr)
+    funcop = FuncOp(func_type_attr, loc=loc, ip=ip)
+    for k, v in attributes.items():
+      funcop.attributes[k] = v
     funcop.attributes["sym_name"] = sym_name
     funcop.attributes["argNames"] = ArrayAttr.get(
         [StringAttr.get(name) for name, _ in args])
     funcop.attributes["resNames"] = ArrayAttr.get(
         [StringAttr.get(name) for name, _ in results])
     return funcop
+
+  def add_entry_block(self):
+    self.body.blocks.append(*self.function_type.value.inputs)
+    return self.body.blocks[0]
