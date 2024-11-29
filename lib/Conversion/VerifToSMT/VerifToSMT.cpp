b9cc35a6d2cd647ffbde82bf4e6cb111cc4bea7d
Bea Healy <beahealy22@gmail.com>
2024-11-29 15:59:04 +0000

    Drop redundant signalPassFailure

--- lib/Conversion/VerifToSMT/VerifToSMT.cpp
+++ lib/Conversion/VerifToSMT/VerifToSMT.cpp
@@ -444,7 +444,6 @@ void ConvertVerifToSMTPass::runOnOperation() {
           if (numClockArgs > 1) {
             op->emitError(
                 "only modules with one or zero clocks are currently supported");
-            signalPassFailure();
             return WalkResult::interrupt();
           }
           SmallVector<mlir::Operation *> worklist;
