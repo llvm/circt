0bf7f7d8412948906ff8a8d109ea0aecc2d733fa
Martin Erhart <martin.erhart@sifive.com>
2025-01-24 13:47:34 +0000

    [RTGTest] Improved register representation (#8052)

--- lib/Bindings/Python/RTGTestModule.cpp
+++ lib/Bindings/Python/RTGTestModule.cpp
@@ -39,4 +39,260 @@ void circt::python::populateDialectRTGTestSubmodule(nb::module_ &m) {
           nb::arg("self"), nb::arg("id"), nb::arg("ctxt") = nullptr)
       .def_property_readonly(
           "id", [](MlirAttribute self) { return rtgtestCPUAttrGetId(self); });
+
+  mlir_attribute_subclass(m, "RegZeroAttr", rtgtestAttrIsARegZero)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegZeroAttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegRaAttr", rtgtestAttrIsARegRa)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegRaAttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegSpAttr", rtgtestAttrIsARegSp)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegSpAttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegGpAttr", rtgtestAttrIsARegGp)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegGpAttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegTpAttr", rtgtestAttrIsARegTp)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegTpAttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegT0Attr", rtgtestAttrIsARegT0)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegT0AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegT1Attr", rtgtestAttrIsARegT1)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegT1AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegT2Attr", rtgtestAttrIsARegT2)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegT2AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS0Attr", rtgtestAttrIsARegS0)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS0AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS1Attr", rtgtestAttrIsARegS1)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS1AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegA0Attr", rtgtestAttrIsARegA0)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegA0AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegA1Attr", rtgtestAttrIsARegA1)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegA1AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegA2Attr", rtgtestAttrIsARegA2)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegA2AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegA3Attr", rtgtestAttrIsARegA3)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegA3AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegA4Attr", rtgtestAttrIsARegA4)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegA4AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegA5Attr", rtgtestAttrIsARegA5)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegA5AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegA6Attr", rtgtestAttrIsARegA6)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegA6AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegA7Attr", rtgtestAttrIsARegA7)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegA7AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS2Attr", rtgtestAttrIsARegS2)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS2AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS3Attr", rtgtestAttrIsARegS3)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS3AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS4Attr", rtgtestAttrIsARegS4)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS4AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS5Attr", rtgtestAttrIsARegS5)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS5AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS6Attr", rtgtestAttrIsARegS6)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS6AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS7Attr", rtgtestAttrIsARegS7)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS7AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS8Attr", rtgtestAttrIsARegS8)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS8AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS9Attr", rtgtestAttrIsARegS9)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS9AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS10Attr", rtgtestAttrIsARegS10)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS10AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegS11Attr", rtgtestAttrIsARegS11)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegS11AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegT3Attr", rtgtestAttrIsARegT3)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegT3AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegT4Attr", rtgtestAttrIsARegT4)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegT4AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegT5Attr", rtgtestAttrIsARegT5)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegT5AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
+
+  mlir_attribute_subclass(m, "RegT6Attr", rtgtestAttrIsARegT6)
+      .def_classmethod(
+          "get",
+          [](py::object cls, MlirContext ctxt) {
+            return cls(rtgtestRegT6AttrGet(ctxt));
+          },
+          py::arg("self"), py::arg("ctxt") = nullptr);
 }
--- lib/CAPI/Dialect/RTGTest.cpp
+++ lib/CAPI/Dialect/RTGTest.cpp
@@ -47,3 +47,262 @@ MlirAttribute rtgtestCPUAttrGet(MlirContext ctxt, unsigned id) {
 unsigned rtgtestCPUAttrGetId(MlirAttribute attr) {
   return cast<CPUAttr>(unwrap(attr)).getId();
 }
+
+// Registers
+//===----------------------------------------------------------------------===//
+
+bool rtgtestAttrIsARegZero(MlirAttribute attr) {
+  return isa<RegZeroAttr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegZeroAttrGet(MlirContext ctxt) {
+  return wrap(RegZeroAttr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegRa(MlirAttribute attr) {
+  return isa<RegRaAttr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegRaAttrGet(MlirContext ctxt) {
+  return wrap(RegRaAttr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegSp(MlirAttribute attr) {
+  return isa<RegSpAttr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegSpAttrGet(MlirContext ctxt) {
+  return wrap(RegSpAttr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegGp(MlirAttribute attr) {
+  return isa<RegGpAttr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegGpAttrGet(MlirContext ctxt) {
+  return wrap(RegGpAttr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegTp(MlirAttribute attr) {
+  return isa<RegTpAttr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegTpAttrGet(MlirContext ctxt) {
+  return wrap(RegTpAttr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegT0(MlirAttribute attr) {
+  return isa<RegT0Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegT0AttrGet(MlirContext ctxt) {
+  return wrap(RegT0Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegT1(MlirAttribute attr) {
+  return isa<RegT1Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegT1AttrGet(MlirContext ctxt) {
+  return wrap(RegT1Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegT2(MlirAttribute attr) {
+  return isa<RegT2Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegT2AttrGet(MlirContext ctxt) {
+  return wrap(RegT2Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS0(MlirAttribute attr) {
+  return isa<RegS0Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS0AttrGet(MlirContext ctxt) {
+  return wrap(RegS0Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS1(MlirAttribute attr) {
+  return isa<RegS1Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS1AttrGet(MlirContext ctxt) {
+  return wrap(RegS1Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegA0(MlirAttribute attr) {
+  return isa<RegA0Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegA0AttrGet(MlirContext ctxt) {
+  return wrap(RegA0Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegA1(MlirAttribute attr) {
+  return isa<RegA1Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegA1AttrGet(MlirContext ctxt) {
+  return wrap(RegA1Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegA2(MlirAttribute attr) {
+  return isa<RegA2Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegA2AttrGet(MlirContext ctxt) {
+  return wrap(RegA2Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegA3(MlirAttribute attr) {
+  return isa<RegA3Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegA3AttrGet(MlirContext ctxt) {
+  return wrap(RegA3Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegA4(MlirAttribute attr) {
+  return isa<RegA4Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegA4AttrGet(MlirContext ctxt) {
+  return wrap(RegA4Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegA5(MlirAttribute attr) {
+  return isa<RegA5Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegA5AttrGet(MlirContext ctxt) {
+  return wrap(RegA5Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegA6(MlirAttribute attr) {
+  return isa<RegA6Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegA6AttrGet(MlirContext ctxt) {
+  return wrap(RegA6Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegA7(MlirAttribute attr) {
+  return isa<RegA7Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegA7AttrGet(MlirContext ctxt) {
+  return wrap(RegA7Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS2(MlirAttribute attr) {
+  return isa<RegS2Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS2AttrGet(MlirContext ctxt) {
+  return wrap(RegS2Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS3(MlirAttribute attr) {
+  return isa<RegS3Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS3AttrGet(MlirContext ctxt) {
+  return wrap(RegS3Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS4(MlirAttribute attr) {
+  return isa<RegS4Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS4AttrGet(MlirContext ctxt) {
+  return wrap(RegS4Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS5(MlirAttribute attr) {
+  return isa<RegS5Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS5AttrGet(MlirContext ctxt) {
+  return wrap(RegS5Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS6(MlirAttribute attr) {
+  return isa<RegS6Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS6AttrGet(MlirContext ctxt) {
+  return wrap(RegS6Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS7(MlirAttribute attr) {
+  return isa<RegS7Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS7AttrGet(MlirContext ctxt) {
+  return wrap(RegS7Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS8(MlirAttribute attr) {
+  return isa<RegS8Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS8AttrGet(MlirContext ctxt) {
+  return wrap(RegS8Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS9(MlirAttribute attr) {
+  return isa<RegS9Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS9AttrGet(MlirContext ctxt) {
+  return wrap(RegS9Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS10(MlirAttribute attr) {
+  return isa<RegS10Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS10AttrGet(MlirContext ctxt) {
+  return wrap(RegS10Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegS11(MlirAttribute attr) {
+  return isa<RegS11Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegS11AttrGet(MlirContext ctxt) {
+  return wrap(RegS11Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegT3(MlirAttribute attr) {
+  return isa<RegT3Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegT3AttrGet(MlirContext ctxt) {
+  return wrap(RegT3Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegT4(MlirAttribute attr) {
+  return isa<RegT4Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegT4AttrGet(MlirContext ctxt) {
+  return wrap(RegT4Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegT5(MlirAttribute attr) {
+  return isa<RegT5Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegT5AttrGet(MlirContext ctxt) {
+  return wrap(RegT5Attr::get(unwrap(ctxt)));
+}
+
+bool rtgtestAttrIsARegT6(MlirAttribute attr) {
+  return isa<RegT6Attr>(unwrap(attr));
+}
+
+MlirAttribute rtgtestRegT6AttrGet(MlirContext ctxt) {
+  return wrap(RegT6Attr::get(unwrap(ctxt)));
+}
