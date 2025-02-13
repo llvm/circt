99c7f3a2dc7b2e9eef535cd81e3e64badc3a1987
Hideto Ueno <uenoku.tokotoko@gmail.com>
2025-02-13 08:48:19 -0800

    save

--- tools/circt-synth/circt-synth.cpp
+++ tools/circt-synth/circt-synth.cpp
@@ -19,15 +19,21 @@
 #include "circt/Dialect/HW/HWDialect.h"
 #include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/HW/HWPasses.h"
+#include "circt/Dialect/OM/OMDialect.h"
+#include "circt/Dialect/Seq/SeqDialect.h"
+#include "circt/Support/InstanceGraph.h"
+#include "circt/Support/InstanceGraphInterface.h"
 #include "circt/Support/Passes.h"
 #include "circt/Support/Version.h"
 #include "mlir/IR/Diagnostics.h"
 #include "mlir/IR/OwningOpRef.h"
+#include "mlir/IR/Threading.h"
 #include "mlir/Parser/Parser.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Support/FileUtilities.h"
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Transforms/Passes.h"
+#include "llvm/ADT/PostOrderIterator.h"
 #include "llvm/ADT/ScopeExit.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/InitLLVM.h"
@@ -85,6 +91,15 @@ static cl::opt<bool>
                   cl::desc("Convert AIG to Comb at the end of the pipeline"),
                   cl::init(false), cl::cat(mainCategory));
 
+static cl::opt<std::string> topName("top", cl::desc("Top module name"),
+                                    cl::value_desc("name"), cl::init(""),
+                                    cl::cat(mainCategory));
+
+static cl::opt<bool>
+    allowUnregisteredDialects("allow-unregistered-dialects",
+                              cl::desc("Allow unregistered dialects"),
+                              cl::init(false), cl::cat(mainCategory));
+
 //===----------------------------------------------------------------------===//
 // Main Tool Logic
 //===----------------------------------------------------------------------===//
@@ -94,37 +109,151 @@ static bool untilReached(Until until) {
 }
 
 //===----------------------------------------------------------------------===//
+// Instance Graph Pass Manager
+//===----------------------------------------------------------------------===//
+
+struct ConditionallyRunPass
+    : public PassWrapper<ConditionallyRunPass, OperationPass<void>> {
+  ConditionallyRunPass(llvm::function_ref<void(OpPassManager &)> pipeline,
+                       llvm::function_ref<bool(Operation *)> shouldRunPass)
+      : PassWrapper<ConditionallyRunPass, OperationPass<void>>(),
+        pipeline(pipeline), shouldRunPass(shouldRunPass) {}
+
+  LogicalResult initialize(MLIRContext *context) override { return success(); }
+
+  void runOnOperation() override {
+    if (!shouldRunPass(getOperation()))
+      return markAllAnalysesPreserved();
+
+    auto pm = OpPassManager(getOperation()->getName());
+    pipeline(pm);
+    if (failed(runPipeline(pm, getOperation())))
+      return signalPassFailure();
+  }
+
+  llvm::function_ref<void(OpPassManager &)> pipeline;
+  llvm::function_ref<bool(Operation *)> shouldRunPass;
+};
+
+struct NestPassesUnderHierarchyPass
+    : public PassWrapper<NestPassesUnderHierarchyPass,
+                         OperationPass<mlir::ModuleOp>> {
+  NestPassesUnderHierarchyPass(
+      llvm::function_ref<void(OpPassManager &)> pipeline)
+      : PassWrapper<NestPassesUnderHierarchyPass,
+                    OperationPass<mlir::ModuleOp>>(),
+        pipeline(pipeline) {}
+
+  void runOnOperation() override {
+    auto &instanceGraph = getAnalysis<circt::igraph::InstanceGraph>();
+    std::function<bool(Operation *)> runPass;
+    llvm::SetVector<Operation *> visited;
+    if (topName.empty()) {
+      runPass = [&](Operation *op) { return true; };
+    } else {
+      auto name = mlir::StringAttr::get(getOperation()->getContext(), topName);
+      auto *top = instanceGraph.lookup(name);
+      if (!top)
+        return signalPassFailure();
+      SmallVector<igraph::InstanceGraphNode *> worklist;
+      worklist.push_back(top);
+      while (!worklist.empty()) {
+        auto *node = worklist.pop_back_val();
+        auto op = node->getModule();
+        if (!op || !visited.insert(op).second)
+          continue;
+        for (auto *child : *node) {
+          auto childOp = child->getInstance();
+          if (!childOp || childOp->hasAttr("doNotPrint"))
+            continue;
+          worklist.push_back(child->getTarget());
+        }
+      }
+
+      runPass = [&](Operation *op) -> bool { return visited.count(op); };
+    }
+
+    auto dynamicPM = OpPassManager(getOperation()->getName());
+    auto &pm = dynamicPM.nest<hw::HWModuleOp>();
+    pm.addPass(std::make_unique<ConditionallyRunPass>(pipeline, runPass));
+    if (failed(runPipeline(dynamicPM, getOperation())))
+      return signalPassFailure();
+
+    // We must maintain a fixed pool of pass managers which is at least as large
+    // as the maximum parallelism of the failableParallelForEach below.
+    // Note: The number of pass managers here needs to remain constant
+    // to prevent issues with pass instrumentations that rely on having the same
+    // pass manager for the main thread.
+    size_t numThreads = getContext().getNumThreads();
+    OpPassManager nestedPipelines;
+
+    llvm::SmallVector<OpPassManager> pipelines;
+    pipeline(nestedPipelines);
+    const auto &opPipelines = nestedPipelines;
+    if (pipelines.size() < numThreads) {
+      pipelines.reserve(numThreads);
+      pipelines.resize(numThreads, opPipelines);
+    }
+
+    // An atomic failure variable for the async executors.
+    std::vector<std::atomic<bool>> activePMs(pipelines.size());
+    std::fill(activePMs.begin(), activePMs.end(), false);
+    auto result = mlir::failableParallelForEach(
+        getContext(), visited.getArrayRef(), [&](Operation *node) -> LogicalResult {
+          // Find a pass manager for this operation.
+          auto it = llvm::find_if(activePMs, [](std::atomic<bool> &isActive) {
+            bool expectedInactive = false;
+            return isActive.compare_exchange_strong(expectedInactive, true);
+          });
+          assert(it != activePMs.end() &&
+                 "could not find inactive pass manager for thread");
+          unsigned pmIndex = it - activePMs.begin();
+          auto result = runPipeline(pipelines[pmIndex], node);
+          // Reset the active bit for this pass manager.
+          activePMs[pmIndex].store(false);
+          return result;
+        });
+  }
+
+  llvm::function_ref<void(OpPassManager &)> pipeline;
+};
+
+static std::unique_ptr<Pass> createNestPassesUnderHierarchyPass(
+    llvm::function_ref<void(OpPassManager &)> pipeline) {
+  return std::make_unique<NestPassesUnderHierarchyPass>(pipeline);
+}
+
+//===----------------------------------------------------------------------===//
 // Tool implementation
 //===----------------------------------------------------------------------===//
 
 static void populateSynthesisPipeline(PassManager &pm) {
-  // Add the AIG to Comb at the scope exit if requested.
-  auto addAIGToComb = llvm::make_scope_exit([&]() {
-    if (convertToComb) {
-      auto &mpm = pm.nest<hw::HWModuleOp>();
-      mpm.addPass(circt::createConvertAIGToComb());
-      mpm.addPass(createCSEPass());
-    }
-  });
-
-  auto &mpm = pm.nest<hw::HWModuleOp>();
-  mpm.addPass(circt::hw::createHWAggregateToCombPass());
-  mpm.addPass(circt::createConvertCombToAIG());
-  mpm.addPass(createCSEPass());
-  if (untilReached(UntilAIGLowering))
-    return;
-  mpm.addPass(createSimpleCanonicalizerPass());
-  mpm.addPass(createCSEPass());
-  mpm.addPass(aig::createLowerVariadic());
-  // TODO: LowerWordToBits is not scalable for large designs. Change to
-  // conditionally enable the pass once the rest of the pipeline was able to
-  // handle multibit operands properly.
-  mpm.addPass(aig::createLowerWordToBits());
-  mpm.addPass(createCSEPass());
-  mpm.addPass(createSimpleCanonicalizerPass());
-  // TODO: Add balancing, rewriting, FRAIG conversion, etc.
-  if (untilReached(UntilEnd))
-    return;
+  pm.addPass(createNestPassesUnderHierarchyPass([](OpPassManager &mpm) {
+    // Add the AIG to Comb at the scope exit if requested.
+    auto addAIGToComb = llvm::make_scope_exit([&]() {
+      if (convertToComb) {
+        mpm.addPass(circt::createConvertAIGToComb());
+        mpm.addPass(createCSEPass());
+      }
+    });
+    mpm.addPass(circt::hw::createHWAggregateToCombPass());
+    mpm.addPass(circt::createConvertCombToAIG());
+    mpm.addPass(createCSEPass());
+    if (untilReached(UntilAIGLowering))
+      return;
+    mpm.addPass(createSimpleCanonicalizerPass());
+    mpm.addPass(createCSEPass());
+    mpm.addPass(aig::createLowerVariadic());
+    // TODO: LowerWordToBits is not scalable for large designs. Change to
+    // conditionally enable the pass once the rest of the pipeline was able to
+    // handle multibit operands properly.
+    mpm.addPass(aig::createLowerWordToBits());
+    mpm.addPass(createCSEPass());
+    mpm.addPass(createSimpleCanonicalizerPass());
+    // TODO: Add balancing, rewriting, FRAIG conversion, etc.
+    if (untilReached(UntilEnd))
+      return;
+  }));
 
   // TODO: Add LUT mapping, etc.
 }
@@ -205,8 +334,12 @@ int main(int argc, char **argv) {
   // Register the supported CIRCT dialects and create a context to work with.
   DialectRegistry registry;
   registry.insert<circt::aig::AIGDialect, circt::comb::CombDialect,
-                  circt::hw::HWDialect>();
+                  circt::hw::HWDialect, circt::seq::SeqDialect,
+                  circt::om::OMDialect>();
   MLIRContext context(registry);
+  context.loadAllAvailableDialects();
+  if (allowUnregisteredDialects)
+    context.allowUnregisteredDialects();
 
   // Setup of diagnostic handling.
   llvm::SourceMgr sourceMgr;
