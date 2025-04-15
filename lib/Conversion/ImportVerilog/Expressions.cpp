8d4fef1b1984d99770dfaf3f2bb61110a55a960f
Annu Singh <annu4444.as@gmail.com>
2025-04-15 16:41:20 +0000

    corrections

--- lib/Conversion/ImportVerilog/Expressions.cpp
+++ lib/Conversion/ImportVerilog/Expressions.cpp
@@ -290,13 +290,13 @@ struct RvalueExprVisitor {
     }
 
     case BinaryOperator::Equality:
-      if (dyn_cast<moore::UnpackedArrayType>(lhs.getType()))
+      if (isa<moore::UnpackedArrayType>(lhs.getType()))
         return builder.create<moore::UArrayCmpOp>(
             loc, circt::moore::UArrayCmpPredicate::eq, lhs, rhs);
       else
         return createBinary<moore::EqOp>(lhs, rhs);
     case BinaryOperator::Inequality:
-      if (dyn_cast<moore::UnpackedArrayType>(lhs.getType()))
+      if (isa<moore::UnpackedArrayType>(lhs.getType()))
         return builder.create<moore::UArrayCmpOp>(
             loc, circt::moore::UArrayCmpPredicate::ne, lhs, rhs);
       else
