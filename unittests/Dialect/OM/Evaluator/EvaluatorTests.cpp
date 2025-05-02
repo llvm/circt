b1f94a51c31fb0cedcd2e2f5d805733d08c37bda
Mike Urbach <mikeurbach@gmail.com>
2025-05-01 21:51:43 -0600

    [OM] Ensure every sublist is finished in ListConcatOp evaluation. (#8460)
    We would previously check if the value corresponding to each operand
    of the ListConcatOp was finished evaluating, but in the case where the
    operand is a ReferenceValue, the reference may be done evaluating
    before while the underlying ListValue is not.
    
    To handle this, a check was added that the result of extractList is
    fully evaluated, and the evaluation of the ListConcatOp doesn't
    proceed until that is true.
    
    A small unit test shows this scenario, where there are object
    references in lists passed as field references to a ListConcatOp.

--- unittests/Dialect/OM/Evaluator/EvaluatorTests.cpp
+++ unittests/Dialect/OM/Evaluator/EvaluatorTests.cpp
@@ -1261,6 +1261,68 @@ TEST(EvaluatorTests, ListOfListConcat) {
                        .str());
 }
 
+TEST(EvaluatorTests, ListConcatPartialCycle) {
+  StringRef mod =
+      "om.class @Child(%field_in: !om.any) -> (field: !om.list<!om.any>) {"
+      "  %1 = om.list_create %field_in : !om.any"
+      "  om.class.fields %1 : !om.list<!om.any>"
+      "}"
+      "om.class @Leaf(%id_in: !om.integer) -> (id: !om.integer) {"
+      "  om.class.fields %id_in : !om.integer"
+      "}"
+      "om.class @ListConcatPartialCycle() -> (result: !om.list<!om.any>){"
+      "  %0 = om.object @Child(%7) : (!om.any) -> !om.class.type<@Child>"
+      "  %1 = om.object.field %0, [@field] : (!om.class.type<@Child>) -> "
+      "!om.list<!om.any>"
+      "  %2 = om.object @Child(%10) : (!om.any) -> !om.class.type<@Child>"
+      "  %3 = om.object.field %2, [@field] : (!om.class.type<@Child>) -> "
+      "!om.list<!om.any>"
+      "  %4 = om.list_concat %1, %3 : <!om.any>"
+      "  %5 = om.constant #om.integer<1 : i64>"
+      "  %6 = om.object @Leaf(%5) : (!om.integer) -> !om.class.type<@Leaf>"
+      "  %7 = om.any_cast %6 : (!om.class.type<@Leaf>) -> !om.any"
+      "  %8 = om.constant #om.integer<2 : i64>"
+      "  %9 = om.object @Leaf(%8) : (!om.integer) -> !om.class.type<@Leaf>"
+      "  %10 = om.any_cast %9 : (!om.class.type<@Leaf>) -> !om.any"
+      "  om.class.fields %4 : !om.list<!om.any>"
+      "}";
+
+  DialectRegistry registry;
+  registry.insert<OMDialect>();
+
+  MLIRContext context(registry);
+  context.getOrLoadDialect<OMDialect>();
+
+  OwningOpRef<ModuleOp> owning =
+      parseSourceString<ModuleOp>(mod, ParserConfig(&context));
+
+  Evaluator evaluator(owning.release());
+
+  auto result = evaluator.instantiate(
+      StringAttr::get(&context, "ListConcatPartialCycle"), {});
+
+  ASSERT_TRUE(succeeded(result));
+
+  auto fieldValue = llvm::cast<evaluator::ObjectValue>(result.value().get())
+                        ->getField("result")
+                        .value();
+
+  auto finalList =
+      llvm::cast<evaluator::ListValue>(fieldValue.get())->getElements();
+
+  ASSERT_EQ(2U, finalList.size());
+
+  auto obj1 = llvm::cast<evaluator::ObjectValue>(finalList[0].get());
+  auto id1 =
+      llvm::cast<evaluator::AttributeValue>(obj1->getField("id").value().get());
+  ASSERT_EQ(1U, id1->getAs<circt::om::IntegerAttr>().getValue().getValue());
+
+  auto obj2 = llvm::cast<evaluator::ObjectValue>(finalList[1].get());
+  auto id2 =
+      llvm::cast<evaluator::AttributeValue>(obj2->getField("id").value().get());
+  ASSERT_EQ(2U, id2->getAs<circt::om::IntegerAttr>().getValue().getValue());
+}
+
 TEST(EvaluatorTests, TupleGet) {
   StringRef mod = "om.class @Tuple() -> (val: !om.string) {"
                   "  %int = om.constant 1 : i1"
