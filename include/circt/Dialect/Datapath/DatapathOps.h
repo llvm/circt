157f8fc47e5cbab49f1c309ef107f5c83117312e
Sam Coward <s.coward111@gmail.com>
2025-05-05 11:42:57 +0100

    Initialise datapath dialect implementation

---
+++ include/circt/Dialect/Datapath/DatapathOps.h
@@ -0,0 +1,15 @@
+//===- DatapathOps.h - Datapath dialect operations --------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_DATAPATH_DATAPATHOPS_H
+#define CIRCT_DIALECT_DATAPATH_DATAPATHOPS_H
+
+#define GET_OP_CLASSES
+#include "circt/Dialect/Datapath/Datapath.h.inc"
+
+#endif // CIRCT_DIALECT_DATAPATH_DATAPATHOPS_H
