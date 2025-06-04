a3acd0cc1cfe45dc53d2e2ca308bb39392e9f3d7
Fabian Schuiki <fabian@schuiki.ch>
2025-06-04 16:22:01 -0700

    [LLHD] Add combinational process op
    Add a dedicated op for processes that should trigger when any of the
    values used as operands in its body region change. This reflects the
    behavior of `always_comb` in SystemVerilog and will allow passes such as
    desequentialization and process lowering to convert `llhd.process`es to
    this more streamlined and specialized representation.
    
    A later commit will replace uses of `scf.execute_region` in the LLHD
    pass pipeline with `llhd.combinational`.

--- lib/Dialect/LLHD/IR/LLHDOps.cpp
+++ lib/Dialect/LLHD/IR/LLHDOps.cpp
@@ -495,6 +495,25 @@ LogicalResult ProcessOp::canonicalize(ProcessOp op, PatternRewriter &rewriter) {
 }
 
 //===----------------------------------------------------------------------===//
+// CombinationalOp
+//===----------------------------------------------------------------------===//
+
+LogicalResult CombinationalOp::canonicalize(CombinationalOp op,
+                                            PatternRewriter &rewriter) {
+  // Inline the combinational region if it consists of a single block and
+  // contains no side-effecting operations.
+  if (op.getBody().hasOneBlock() && isMemoryEffectFree(op)) {
+    auto &block = op.getBody().front();
+    auto *terminator = block.getTerminator();
+    rewriter.inlineBlockBefore(&block, op, ValueRange{});
+    rewriter.replaceOp(op, terminator->getOperands());
+    rewriter.eraseOp(terminator);
+    return success();
+  }
+  return failure();
+}
+
+//===----------------------------------------------------------------------===//
 // WaitOp
 //===----------------------------------------------------------------------===//
 
@@ -502,10 +521,10 @@ static LogicalResult verifyYieldResults(Operation *op,
                                         ValueRange yieldOperands) {
   // Determine the result values of the parent.
   auto *parentOp = op->getParentOp();
-  TypeRange resultTypes =
-      TypeSwitch<Operation *, TypeRange>(parentOp)
-          .Case<ProcessOp>([](auto op) { return op.getResultTypes(); })
-          .Case<FinalOp>([](auto) { return TypeRange{}; });
+  TypeRange resultTypes = TypeSwitch<Operation *, TypeRange>(parentOp)
+                              .Case<ProcessOp, CombinationalOp>(
+                                  [](auto op) { return op.getResultTypes(); })
+                              .Case<FinalOp>([](auto) { return TypeRange{}; });
 
   // Check that the number of yield operands matches the process.
   if (yieldOperands.size() != resultTypes.size())
@@ -535,6 +554,14 @@ LogicalResult WaitOp::verify() {
 //===----------------------------------------------------------------------===//
 
 LogicalResult HaltOp::verify() {
+  return verifyYieldResults(*this, getYieldOperands());
+}
+
+//===----------------------------------------------------------------------===//
+// YieldOp
+//===----------------------------------------------------------------------===//
+
+LogicalResult YieldOp::verify() {
   return verifyYieldResults(*this, getYieldOperands());
 }
 
--- test/Dialect/LLHD/Canonicalization/processes.mlir
+++ test/Dialect/LLHD/Canonicalization/processes.mlir
@@ -5,6 +5,9 @@ hw.module @EmptyProcess() {
   llhd.process {
     llhd.halt
   }
+  llhd.combinational {
+    llhd.yield
+  }
   // CHECK-NEXT: hw.output
 }
 
@@ -18,4 +21,17 @@ hw.module @DontRemoveEmptyProcessWithResults(in %a: i42, out z: i42) {
     llhd.halt %a : i42
   }
   hw.output %0 : i42
+}
+
+// CHECK-LABEL: hw.module @InlineCombinational(
+hw.module @InlineCombinational(in %a: i42, in %b: i42, in %c: i8917, out u: i42, out v: i9001) {
+  // CHECK-NEXT: [[TMP1:%.+]] = comb.xor %a, %b
+  // CHECK-NEXT: [[TMP2:%.+]] = comb.concat %a, %b, %c
+  // CHECK-NEXT: hw.output [[TMP1]], [[TMP2]]
+  %0, %1 = llhd.combinational -> i42, i9001 {
+    %2 = comb.xor %a, %b : i42
+    %3 = comb.concat %a, %b, %c : i42, i42, i8917
+    llhd.yield %2, %3 : i42, i9001
+  }
+  hw.output %0, %1 : i42, i9001
 }
