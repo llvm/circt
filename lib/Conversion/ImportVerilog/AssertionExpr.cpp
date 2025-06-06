243eaa94e7927d68d1da3c18bcdeb381f3dfd792
Tobias Wölfel <tobias.woelfel@mailbox.org>
2025-06-06 16:29:26 +0200

    [ImportVerilog] Add SimpleAssertionExpr
    Add support for simple assertion expressions.
    Simple assertions consist of an expression, handled by 'Expression.h',
    and an optional repetition.
    Add conversion of the expression type to i1, which is the required input
    value of LTL operations.

--- lib/Conversion/ImportVerilog/AssertionExpr.cpp
+++ lib/Conversion/ImportVerilog/AssertionExpr.cpp
@@ -8,6 +8,11 @@
 
 #include "slang/ast/expressions/AssertionExpr.h"
 #include "ImportVerilogInternals.h"
+#include "circt/Dialect/LTL/LTLOps.h"
+#include "circt/Dialect/Moore/MooreOps.h"
+#include "circt/Support/LLVM.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/Support/LLVM.h"
 
 using namespace circt;
 using namespace ImportVerilog;
@@ -21,6 +26,91 @@ struct AssertionExprVisitor {
 
   AssertionExprVisitor(Context &context, Location loc)
       : context(context), loc(loc), builder(context.builder) {}
+
+  /// Add repetition operation to a sequence
+  Value createRepetition(Location loc,
+                         const slang::ast::SequenceRepetition &repetition,
+                         Value &inputSequence) {
+    // Extract cycle range
+    // Convert `min`/`max` of type `uint32_t` to the required 64 bit value for
+    // `base`/`more`
+    auto minRepetitions = mlir::IntegerAttr::get(builder.getIntegerType(64),
+                                                 repetition.range.min);
+    mlir::IntegerAttr repetitionRange;
+    // In repetitions, the max value is optional.
+    // The `more` value corresponds to the sequence range.
+    if (repetition.range.max.has_value()) {
+      repetitionRange = mlir::IntegerAttr::get(builder.getIntegerType(64),
+                                               repetition.range.max.value() -
+                                                   repetition.range.min);
+    }
+
+    using slang::ast::SequenceRepetition;
+
+    // Check if repetition range is required
+    if ((repetition.kind == SequenceRepetition::Nonconsecutive ||
+         repetition.kind == SequenceRepetition::GoTo) &&
+        !repetitionRange) {
+      mlir::emitError(loc,
+                      repetition.kind == SequenceRepetition::Nonconsecutive
+                          ? "Nonconsecutive repetition requires a maximum value"
+                          : "GoTo repetition requires a maximum value");
+      return {};
+    }
+
+    switch (repetition.kind) {
+    case SequenceRepetition::Consecutive:
+      return builder.create<ltl::RepeatOp>(loc, inputSequence, minRepetitions,
+                                           repetitionRange);
+    case SequenceRepetition::Nonconsecutive:
+      return builder.create<ltl::NonConsecutiveRepeatOp>(
+          loc, inputSequence, minRepetitions, repetitionRange);
+    case SequenceRepetition::GoTo:
+      return builder.create<ltl::GoToRepeatOp>(loc, inputSequence,
+                                               minRepetitions, repetitionRange);
+    }
+    llvm_unreachable("All enum values handled in switch");
+  }
+
+  Value visit(const slang::ast::SimpleAssertionExpr &expr) {
+    // A Simple Assertion expression has the following members:
+    // - Expression
+    // - Optional repetition
+
+    // The expression needs to have the type `i1` for the LTL operations.
+    // Convert first to a moore type and then add a conversion to `i1`.
+    auto value = context.convertRvalueExpression(expr.expr);
+    auto loc = context.convertLocation(expr.expr.sourceRange);
+    if (!value) {
+      mlir::emitError(loc,
+                      "Failed to convert expression in SimpleAssertionExpr");
+      return {};
+    }
+    value = context.convertToI1(value);
+    if (!value) {
+      mlir::emitError(
+          loc,
+          "Failed to convert expression to i1 type in SimpleAssertionExpr - "
+          "expression may be too wide for boolean conversion");
+      return {};
+    }
+
+    // The optional repetition is empty, return the converted expression
+    if (!expr.repetition.has_value()) {
+      return value;
+    }
+
+    // There is a repetition, embed the expression into the kind of given
+    // repetition
+    Value repeatedValue = createRepetition(loc, expr.repetition.value(), value);
+    if (!repeatedValue) {
+      mlir::emitError(loc,
+                      "Failed to create repetition for SimpleAssertionExpr");
+      return {};
+    }
+    return repeatedValue;
+  }
+  }
 
   /// Emit an error for all other expressions.
   template <typename T>
