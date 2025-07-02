76c34a48d9e4ace7da5aa2437b143b5f76dca2b6
cowardsa <s.coward111@gmail.com>
2025-07-02 10:14:13 +0100

    Define dialect include

---
+++ include/circt/Dialect/Datapath/DatapathDialect.h
@@ -0,0 +1,17 @@
+//===- DatapathDialect.h - Datapath dialect definition ----------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_DATAPATH_DATAPATHDIALECT_H
+#define CIRCT_DIALECT_DATAPATH_DATAPATHDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+// Pull in the dialect definition.
+#include "circt/Dialect/Datapath/DatapathDialect.h.inc"
+
+#endif // CIRCT_DIALECT_DATAPATH_DATAPATHDIALECT_H
---
+++ include/circt/Dialect/Datapath/DatapathOps.h
@@ -0,0 +1,20 @@
+//===- DatapathOps.h - Datapath dialect operations --------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CIRCT_DIALECT_DATAPATH_DATAPATHOPS_H
+#define CIRCT_DIALECT_DATAPATH_DATAPATHOPS_H
+
+#include "mlir/IR/OpImplementation.h"
+
+#include "circt/Dialect/Datapath/DatapathDialect.h"
+#include "circt/Dialect/HW/HWTypes.h"
+
+#define GET_OP_CLASSES
+#include "circt/Dialect/Datapath/Datapath.h.inc"
+
+#endif // CIRCT_DIALECT_DATAPATH_DATAPATHOPS_H
