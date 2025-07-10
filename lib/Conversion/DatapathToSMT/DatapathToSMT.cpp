c6780ed1cc6ae30d1b47e19eef4ba0cde6c558a9
cowardsa <s.coward111@gmail.com>
2025-07-10 15:08:40 +0100

    SMT dialect initialisation

---
+++ lib/Conversion/DatapathToSMT/DatapathToSMT.cpp
@@ -0,0 +1,157 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Conversion/DatapathToSMT.h"
+#include "circt/Conversion/HWToSMT.h"
+#include "circt/Dialect/Datapath/DatapathOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/SMT/IR/SMTOps.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
+
+namespace circt {
+#define GEN_PASS_DEF_CONVERTDATAPATHTOSMT
+#include "circt/Conversion/Passes.h.inc"
+} // namespace circt
+
+using namespace mlir;
+using namespace circt;
+using namespace datapath;
+
+//===----------------------------------------------------------------------===//
+// Conversion patterns
+//===----------------------------------------------------------------------===//
+
+namespace {
+
+// Lower to an SMT assertion that summing the results is equivalent to summing
+// the compress inputs
+// d:2 = compress(a, b, c) ->
+// assert(d#0 + d#1 == a + b + c)
+struct CompressOpConversion : OpConversionPattern<CompressOp> {
+  using OpConversionPattern<CompressOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(CompressOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+
+    ValueRange operands = adaptor.getOperands();
+    ValueRange results = op.getResults();
+
+    // Sum operands
+    Value operandRunner = operands[0];
+    for (Value operand : operands.drop_front())
+      operandRunner =
+          rewriter.create<smt::BVAddOp>(op.getLoc(), operandRunner, operand);
+
+    // Create free variables
+    SmallVector<Value, 2> newResults;
+    for (Value result : results) {
+      auto declareFunOp = rewriter.create<smt::DeclareFunOp>(
+          op.getLoc(), typeConverter->convertType(result.getType()));
+      newResults.push_back(declareFunOp.getResult());
+    }
+
+    // Sum the free variables
+    Value resultRunner = newResults.front();
+    for (auto freeVar : llvm::drop_begin(newResults, 1))
+      resultRunner =
+          rewriter.create<smt::BVAddOp>(op.getLoc(), resultRunner, freeVar);
+
+    // Assert sum operands == sum results (free variables)
+    auto premise =
+        rewriter.create<smt::EqOp>(op.getLoc(), operandRunner, resultRunner);
+    // Encode via an assertion (could be relaxed to an assumption).
+    rewriter.create<smt::AssertOp>(op.getLoc(), premise);
+
+    if (newResults.size() != results.size())
+      return rewriter.notifyMatchFailure(op, "expected same number of results");
+
+    rewriter.replaceOp(op, newResults);
+    return success();
+  }
+};
+
+// Lower to an SMT assertion that summing the results is equivalent to the
+// product of the partial_product inputs
+// c:<N> = partial_product(a, b) ->
+// assert(c#0 + ... + c#<N-1> == a * b)
+struct PartialProductOpConversion : OpConversionPattern<PartialProductOp> {
+  using OpConversionPattern<PartialProductOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(PartialProductOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+
+    ValueRange operands = adaptor.getOperands();
+    ValueRange results = op.getResults();
+
+    // Multiply the operands
+    auto mulResult =
+        rewriter.create<smt::BVMulOp>(op.getLoc(), operands[0], operands[1]);
+
+    // Create free variables
+    SmallVector<Value, 2> newResults;
+    for (Value result : results) {
+      auto declareFunOp = rewriter.create<smt::DeclareFunOp>(
+          op.getLoc(), typeConverter->convertType(result.getType()));
+      newResults.push_back(declareFunOp.getResult());
+    }
+
+    // Sum the free variables
+    Value resultRunner = newResults.front();
+    for (auto freeVar : llvm::drop_begin(newResults, 1))
+      resultRunner =
+          rewriter.create<smt::BVAddOp>(op.getLoc(), resultRunner, freeVar);
+
+    // Assert product of operands == sum results (free variables)
+    auto premise =
+        rewriter.create<smt::EqOp>(op.getLoc(), mulResult, resultRunner);
+    // Encode via an assertion (could be relaxed to an assumption).
+    rewriter.create<smt::AssertOp>(op.getLoc(), premise);
+
+    if (newResults.size() != results.size())
+      return rewriter.notifyMatchFailure(op, "expected same number of results");
+
+    rewriter.replaceOp(op, newResults);
+    return success();
+  }
+};
+} // namespace
+
+//===----------------------------------------------------------------------===//
+// Convert Datapath to SMT pass
+//===----------------------------------------------------------------------===//
+
+namespace {
+struct ConvertDatapathToSMTPass
+    : public circt::impl::ConvertDatapathToSMTBase<ConvertDatapathToSMTPass> {
+  void runOnOperation() override;
+};
+} // namespace
+
+void circt::populateDatapathToSMTConversionPatterns(
+    TypeConverter &converter, RewritePatternSet &patterns) {
+  patterns.add<CompressOpConversion, PartialProductOpConversion>(
+      converter, patterns.getContext());
+}
+
+void ConvertDatapathToSMTPass::runOnOperation() {
+  ConversionTarget target(getContext());
+  target.addIllegalDialect<datapath::DatapathDialect>();
+  target.addLegalDialect<smt::SMTDialect>();
+
+  RewritePatternSet patterns(&getContext());
+  TypeConverter converter;
+  populateHWToSMTTypeConverter(converter);
+  populateDatapathToSMTConversionPatterns(converter, patterns);
+
+  if (failed(mlir::applyPartialConversion(getOperation(), target,
+                                          std::move(patterns))))
+    return signalPassFailure();
+}
---
+++ test/Conversion/DatapathToSMT/datapath-to-smt.mlir
@@ -0,0 +1,40 @@
+// RUN: circt-opt %s --convert-datapath-to-smt | FileCheck %s
+
+// CHECK-LABEL: @compressor
+hw.module @compressor(in %a : i4, in %b : i4, in %c : i4, out carry : i4, out save : i4) {
+  // CHECK-NEXT: %[[C:.+]] = builtin.unrealized_conversion_cast %c : i4 to !smt.bv<4>
+  // CHECK-NEXT: %[[B:.+]] = builtin.unrealized_conversion_cast %b : i4 to !smt.bv<4>
+  // CHECK-NEXT: %[[A:.+]] = builtin.unrealized_conversion_cast %a : i4 to !smt.bv<4>
+  // CHECK-NEXT: %[[AB:.+]] = smt.bv.add %[[A]], %[[B]] : !smt.bv<4>
+  // CHECK-NEXT: %[[INS:.+]] = smt.bv.add %[[AB]], %[[C]] : !smt.bv<4>
+  // CHECK-NEXT: %[[COMP0:.+]] = smt.declare_fun : !smt.bv<4>
+  // CHECK-NEXT: %[[COMP0_BV:.+]] = builtin.unrealized_conversion_cast %[[COMP0]] : !smt.bv<4> to i4
+  // CHECK-NEXT: %[[COMP1:.+]] = smt.declare_fun : !smt.bv<4>
+  // CHECK-NEXT: %[[COMP1_BV:.+]] = builtin.unrealized_conversion_cast %7 : !smt.bv<4> to i4
+  // CHECK-NEXT: %[[OUT:.+]] = smt.bv.add %[[COMP0]], %[[COMP1]] : !smt.bv<4>
+  // CHECK-NEXT: %[[P:.+]] = smt.eq %[[INS]], %[[OUT]] : !smt.bv<4>  
+  // CHECK-NEXT: smt.assert %[[P]]
+  // CHECK-NEXT: hw.output %[[COMP0_BV]], %[[COMP1_BV]] : i4, i4
+  %0:2 = datapath.compress %a, %b, %c : i4 [3 -> 2]
+  hw.output %0#0, %0#1 : i4, i4
+}
+
+// CHECK-LABEL: @partial_product
+hw.module @partial_product(in %a : i3, in %b : i3, out pp0 : i3, out pp1 : i3, out pp2 : i3) {
+  //CHECK-NEXT: %[[B:.+]] = builtin.unrealized_conversion_cast %b : i3 to !smt.bv<3>
+  //CHECK-NEXT: %[[A:.+]] = builtin.unrealized_conversion_cast %a : i3 to !smt.bv<3>
+  //CHECK-NEXT: %[[MUL:.+]] = smt.bv.mul %[[A]], %[[B]] : !smt.bv<3>
+  //CHECK-NEXT: %[[PP0:.+]] = smt.declare_fun : !smt.bv<3>
+  //CHECK-NEXT: %[[PP0_BV:.+]] = builtin.unrealized_conversion_cast %3 : !smt.bv<3> to i3
+  //CHECK-NEXT: %[[PP1:.+]] = smt.declare_fun : !smt.bv<3>
+  //CHECK-NEXT: %[[PP1_BV:.+]] = builtin.unrealized_conversion_cast %5 : !smt.bv<3> to i3
+  //CHECK-NEXT: %[[PP2:.+]] = smt.declare_fun : !smt.bv<3>
+  //CHECK-NEXT: %[[PP2_BV:.+]] = builtin.unrealized_conversion_cast %7 : !smt.bv<3> to i3
+  //CHECK-NEXT: %[[ADD01:.+]] = smt.bv.add %[[PP0]], %[[PP1]] : !smt.bv<3>
+  //CHECK-NEXT: %[[ADD012:.+]] = smt.bv.add %[[ADD01]], %[[PP2]] : !smt.bv<3>
+  //CHECK-NEXT: %[[P:.+]] = smt.eq %[[MUL]], %[[ADD012]] : !smt.bv<3>
+  //CHECK-NEXT: smt.assert %[[P]]
+  //CHECK-NEXT: hw.output %[[PP0_BV:.+]], %[[PP1_BV:.+]], %[[PP2_BV:.+]] : i3, i3, i3
+  %0:3 = datapath.partial_product %a, %b : (i3, i3) -> (i3, i3, i3)
+  hw.output %0#0, %0#1, %0#2 : i3, i3, i3
+}
