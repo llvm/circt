b2b0f7cf4e42d64953e8397d95318e07d945222b
mafeguimaraes <mariafefe201@gmail.com>
2025-07-20 18:35:45 -0300

    update FlattenMemRefs

--- lib/Transforms/FlattenMemRefs.cpp
+++ lib/Transforms/FlattenMemRefs.cpp
@@ -302,9 +302,9 @@ struct CondBranchOpConversion
   matchAndRewrite(mlir::cf::CondBranchOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     rewriter.replaceOpWithNewOp<mlir::cf::CondBranchOp>(
-      op, adaptor.getCondition(),
-      op.getTrueDest(), adaptor.getTrueDestOperands(),
-      op.getFalseDest(), adaptor.getFalseDestOperands());
+        op, adaptor.getCondition(), adaptor.getTrueDestOperands(),
+        adaptor.getFalseDestOperands(), /*branch_weights=*/nullptr,
+        op.getTrueDest(), op.getFalseDest());
     return success();
   }
 };
