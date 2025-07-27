795aaeb725e3f02fa5edc1a3ba3e3d7f8d13161f
Hideto Ueno <uenoku.tokotoko@gmail.com>
2025-07-26 19:33:26 -0700

    [HW][HWAggregateToComb] Add support for hw.array_inject operation in HWAggregateToComb pass
    This patch implements lowering of hw.array_inject operations to combinational
    logic in the HWAggregateToComb transformation pass.
    
    The implementation creates a 2D array where each row represents the result
    of injecting the new value at a specific index. A multiplexer then selects
    the appropriate row based on the injection index.

--- lib/Dialect/HW/Transforms/HWAggregateToComb.cpp
+++ lib/Dialect/HW/Transforms/HWAggregateToComb.cpp
@@ -115,6 +115,66 @@ struct HWArrayGetOpConversion : OpConversionPattern<hw::ArrayGetOp> {
   }
 };
 
+struct HWArrayInjectOpConversion : OpConversionPattern<hw::ArrayInjectOp> {
+  using OpConversionPattern<hw::ArrayInjectOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(hw::ArrayInjectOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto arrayType = cast<hw::ArrayType>(op.getInput().getType());
+    auto elemType = arrayType.getElementType();
+    auto numElements = arrayType.getNumElements();
+    auto elemWidth = hw::getBitWidth(elemType);
+    if (elemWidth < 0)
+      return rewriter.notifyMatchFailure(op.getLoc(), "unknown element width");
+
+    Location loc = op.getLoc();
+
+    // Extract all elements from the input array
+    SmallVector<Value> originalElements;
+    auto inputArray = adaptor.getInput();
+    for (size_t i = 0; i < numElements; ++i) {
+      originalElements.push_back(rewriter.createOrFold<comb::ExtractOp>(
+          loc, inputArray, i * elemWidth, elemWidth));
+    }
+
+    // Create 2D array: each row represents what the array would look like
+    // if injection happened at that specific index
+    SmallVector<Value> arrayRows;
+    arrayRows.reserve(numElements);
+    for (int injectIdx = numElements - 1; injectIdx >= 0; --injectIdx) {
+      SmallVector<Value> rowElements;
+      rowElements.reserve(numElements);
+
+      // Build the row: array[n-1], array[n-2], ..., but replace element at
+      // injectIdx with newVal
+      for (int originalIdx = numElements - 1; originalIdx >= 0; --originalIdx) {
+        if (originalIdx == injectIdx) {
+          rowElements.push_back(adaptor.getElement());
+        } else {
+          rowElements.push_back(originalElements[originalIdx]);
+        }
+      }
+
+      // Concatenate elements to form this row
+      Value row = rewriter.create<hw::ArrayCreateOp>(loc, rowElements);
+      arrayRows.push_back(row);
+    }
+
+    // Create the 2D array by concatenating all rows
+    // arrayRows[0] corresponds to injection at index 0
+    // arrayRows[1] corresponds to injection at index 1, etc.
+    Value array2D = rewriter.create<hw::ArrayCreateOp>(loc, arrayRows);
+
+    // Create array_get operation to select the row
+    auto arrayGetOp =
+        rewriter.create<hw::ArrayGetOp>(loc, array2D, adaptor.getIndex());
+
+    rewriter.replaceOp(op, arrayGetOp);
+    return success();
+  }
+};
+
 /// A type converter is needed to perform the in-flight materialization of
 /// aggregate types to integer types.
 class AggregateTypeConverter : public TypeConverter {
@@ -152,8 +212,8 @@ static void populateHWAggregateToCombOpConversionPatterns(
   patterns.add<HWArrayGetOpConversion,
                HWArrayCreateLikeOpConversion<hw::ArrayCreateOp>,
                HWArrayCreateLikeOpConversion<hw::ArrayConcatOp>,
-               HWAggregateConstantOpConversion>(typeConverter,
-                                                patterns.getContext());
+               HWAggregateConstantOpConversion, HWArrayInjectOpConversion>(
+      typeConverter, patterns.getContext());
 }
 
 namespace {
@@ -169,7 +229,7 @@ void HWAggregateToCombPass::runOnOperation() {
 
   // TODO: Add ArraySliceOp and struct operatons as well.
   target.addIllegalOp<hw::ArrayGetOp, hw::ArrayCreateOp, hw::ArrayConcatOp,
-                      hw::AggregateConstantOp>();
+                      hw::AggregateConstantOp, hw::ArrayInjectOp>();
 
   target.addLegalDialect<hw::HWDialect, comb::CombDialect>();
 
