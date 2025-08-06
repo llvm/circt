llvm/circt

commit 3b94c0ea8027f26aa8c4e8efeefe3eb3ee7e1a8b

[FIRRTL][CreateSiFiveMetadata] Add memory read-under-write behavior to emitted metadata (#8604)
This PR adds information on the read under write behavior of memories, as specified by the FIRRTL/CHIRRTL IR, to the emitted memory metadata records. This includes the textual `repl-seq-mem-file`, the `seq_mem.json`, and the object model IR. It should allow external tools to generate memories with the appropriate behavior.

* In the JSON file, there is a new string attribute `ruw_behavior` that can take one of the following values : `Undefined`, `New`, `Old`.
* In the textual file, Undefined behavior is elided to keep the format unchanged for the common case. Otherwise `ruw New` or `ruw Old` is appended to the memory's line.
* In the object model it is added to the MemorySchema as a string-typed subfield called `ruwBehavior`.

lib/Dialect/FIRRTL/Transforms/InferReadWrite.cpp
@@ -154,9 +154,9 @@ struct InferReadWritePass
       ImplicitLocOpBuilder builder(memOp.getLoc(), memOp);
       portAnnotations.push_back(builder.getArrayAttr(portAtts));
       // Create the new rw memory.
-      auto rwMem = MemOp::create(
-          builder, resultTypes, memOp.getReadLatency(), memOp.getWriteLatency(),
-          memOp.getDepth(), RUWAttr::Undefined,
+      auto rwMem = builder.create<MemOp>(
+          resultTypes, memOp.getReadLatency(), memOp.getWriteLatency(),
+          memOp.getDepth(), RUWBehavior::Undefined,
           builder.getArrayAttr(resultNames), memOp.getNameAttr(),
           memOp.getNameKind(), memOp.getAnnotations(),
           builder.getArrayAttr(portAnnotations), memOp.getInnerSymAttr(),

lib/Dialect/FIRRTL/Transforms/LowerXMR.cpp
@@ -813,9 +813,9 @@ class LowerXMRPass : public circt::firrtl::impl::LowerXMRBase<LowerXMRPass> {
           portAnnotations.push_back(mem.getPortAnnotation(res.index()));
           oldResults.push_back(res.value());
         }
-        auto newMem = MemOp::create(
-            builder, resultTypes, mem.getReadLatency(), mem.getWriteLatency(),
-            mem.getDepth(), RUWAttr::Undefined,
+        auto newMem = builder.create<MemOp>(
+            resultTypes, mem.getReadLatency(), mem.getWriteLatency(),
+            mem.getDepth(), RUWBehavior::Undefined,
             builder.getArrayAttr(resultNames), mem.getNameAttr(),
             mem.getNameKind(), mem.getAnnotations(),
             builder.getArrayAttr(portAnnotations), mem.getInnerSymAttr(),

include/circt-c/Dialect/FIRRTL.h
@@ -292,7 +292,7 @@ MLIR_CAPI_EXPORTED MlirAttribute firrtlAttrGetParamDecl(MlirContext ctx,
 MLIR_CAPI_EXPORTED MlirAttribute firrtlAttrGetNameKind(MlirContext ctx,
                                                        FIRRTLNameKind nameKind);
 
-/// Creates a RUWAttr with the specified Read-Under-Write Behaviour.
+/// Creates a RUWBehaviorAttr with the specified Read-Under-Write Behavior.
 MLIR_CAPI_EXPORTED MlirAttribute firrtlAttrGetRUW(MlirContext ctx,
                                                   FIRRTLRUW ruw);
 

include/circt/Dialect/FIRRTL/CHIRRTLOps.td
@@ -60,7 +60,9 @@ def SeqMemOp : CHIRRTLOp<"seqmem", [HasCustomSSAName, DeclareOpInterfaceMethods<
     Define a new behavioral sequential memory.  Sequential memories have a
     write latency and a read latency of 1.
   }];
-  let arguments = (ins RUWAttr:$ruw, StrAttr:$name, NameKindAttr:$nameKind,
+  let arguments = (ins RUWBehaviorAttr:$ruw,
+                       StrAttr:$name,
+                       NameKindAttr:$nameKind,
                        AnnotationArrayAttr:$annotations,
                        OptionalAttr<InnerSymAttr>:$inner_sym,
                        OptionalAttr<MemoryInitAttr>:$init,
@@ -70,7 +72,7 @@ def SeqMemOp : CHIRRTLOp<"seqmem", [HasCustomSSAName, DeclareOpInterfaceMethods<
                          custom<SeqMemOp>(attr-dict) `:` qualified(type($result))}];
   let builders = [
     OpBuilder<(ins "firrtl::FIRRTLBaseType":$elementType, "uint64_t":$numElements,
-                   "firrtl::RUWAttr":$ruw, "mlir::StringRef":$name,
+                   "firrtl::RUWBehavior":$ruw, "mlir::StringRef":$name,
                    "firrtl::NameKindEnum":$nameKind, "ArrayAttr":$annotations,
                    CArg<"StringAttr", "StringAttr()">:$innerSym,
                    CArg<"firrtl::MemoryInitAttr", "firrtl::MemoryInitAttr{}">:$init)>

include/circt/Dialect/FIRRTL/FIRRTLDeclarations.td
@@ -246,7 +246,7 @@ def MemOp : HardwareDeclOp<"mem", [DeclareOpInterfaceMethods<CombDataflow>]> {
   let arguments =
     (ins ConfinedAttr<I32Attr, [IntMinValue<0>]>:$readLatency,
          ConfinedAttr<I32Attr, [IntMinValue<1>]>:$writeLatency,
-         ConfinedAttr<I64Attr, [IntMinValue<1>]>:$depth, RUWAttr:$ruw,
+         ConfinedAttr<I64Attr, [IntMinValue<1>]>:$depth, RUWBehaviorAttr:$ruw,
          StrArrayAttr:$portNames, StrAttr:$name, NameKindAttr:$nameKind,
          AnnotationArrayAttr:$annotations,
          PortAnnotationsAttr:$portAnnotations,

include/circt/Dialect/FIRRTL/FIRRTLEnums.td
@@ -76,15 +76,17 @@ def LayerSpecializationAttr :
     EnumAttr<FIRRTLDialect, LayerSpecialization, "layerspecialization">;
 
 //===----------------------------------------------------------------------===//
-// Read Under Write Behaviour
+// Read Under Write Behavior
 //===----------------------------------------------------------------------===//
 
-def RUW_Undefined: I32EnumAttrCase<"Undefined", 0>;
-def RUW_Old: I32EnumAttrCase<"Old", 1>;
-def RUW_New: I32EnumAttrCase<"New", 2>;
+def RUWBehavior
+    : I32EnumAttr<"RUWBehavior", "read under write behavior",
+                  [I32EnumAttrCase<"Undefined", 0>, I32EnumAttrCase<"Old", 1>,
+                   I32EnumAttrCase<"New", 2>]> {
+  let genSpecializedAttr = 0;
+}
 
-def RUWAttr: I32EnumAttr<"RUWAttr", "Read Under Write Enum",
-                         [RUW_Undefined, RUW_Old, RUW_New]>;
+def RUWBehaviorAttr : EnumAttr<FIRRTLDialect, RUWBehavior, "ruwbehavior">;
 
 //===----------------------------------------------------------------------===//
 // Memory Port Direction

include/circt/Dialect/FIRRTL/FIRRTLStructure.td
@@ -291,6 +291,8 @@ def FMemModuleOp : FIRRTLModuleLike<"memmodule"> {
            ArrayRefAttr:$portSymbols,
            ArrayRefAttr:$portNames,
            ArrayRefAttr:$portTypes,
+           DefaultValuedAttr<RUWBehaviorAttr,
+             "firrtl::RUWBehavior::Undefined">:$ruw,
            AnnotationArrayAttr:$annotations,
            DefaultValuedAttr<LayerArrayAttr, "{}">:$layers
       );
@@ -305,6 +307,8 @@ def FMemModuleOp : FIRRTLModuleLike<"memmodule"> {
                    "uint32_t":$numReadWritePorts, "uint32_t":$dataWidth,
                    "uint32_t":$maskBits, "uint32_t":$readLatency,
                    "uint32_t":$writeLatency, "uint64_t":$depth,
+                   CArg<"firrtl::RUWBehavior",
+                    "firrtl::RUWBehavior::Undefined">:$ruw,
                    CArg<"ArrayAttr", "ArrayAttr()">:$annotations,
                    CArg<"ArrayAttr", "ArrayAttr()">:$layers)>
   ];

lib/CAPI/Dialect/FIRRTL.cpp
@@ -342,21 +342,21 @@ MlirAttribute firrtlAttrGetNameKind(MlirContext ctx, FIRRTLNameKind nameKind) {
 }
 
 MlirAttribute firrtlAttrGetRUW(MlirContext ctx, FIRRTLRUW ruw) {
-  RUWAttr value;
+  RUWBehavior value;
 
   switch (ruw) {
   case FIRRTL_RUW_UNDEFINED:
-    value = RUWAttr::Undefined;
+    value = RUWBehavior::Undefined;
     break;
   case FIRRTL_RUW_OLD:
-    value = RUWAttr::Old;
+    value = RUWBehavior::Old;
     break;
   case FIRRTL_RUW_NEW:
-    value = RUWAttr::New;
+    value = RUWBehavior::New;
     break;
   }
 
-  return wrap(RUWAttrAttr::get(unwrap(ctx), value));
+  return wrap(RUWBehaviorAttr::get(unwrap(ctx), value));
 }
 
 MlirAttribute firrtlAttrGetMemInit(MlirContext ctx, MlirIdentifier filename,

commit 3722eb7ed56ecd436f1490116070e051e8600eb6

[NFC] Revert to new operation creation API
Commit 3b94c0ea8027f26aa8c4e8efeefe3eb3ee7e1a8b accidentally reintroduced the old style `builder.create` calls.

lib/Dialect/FIRRTL/Transforms/InferReadWrite.cpp
@@ -154,8 +154,8 @@ struct InferReadWritePass
       ImplicitLocOpBuilder builder(memOp.getLoc(), memOp);
       portAnnotations.push_back(builder.getArrayAttr(portAtts));
       // Create the new rw memory.
-      auto rwMem = builder.create<MemOp>(
-          resultTypes, memOp.getReadLatency(), memOp.getWriteLatency(),
+      auto rwMem = MemOp::create(
+          builder, resultTypes, memOp.getReadLatency(), memOp.getWriteLatency(),
           memOp.getDepth(), RUWBehavior::Undefined,
           builder.getArrayAttr(resultNames), memOp.getNameAttr(),
           memOp.getNameKind(), memOp.getAnnotations(),

lib/Dialect/FIRRTL/Transforms/LowerXMR.cpp
@@ -813,8 +813,8 @@ class LowerXMRPass : public circt::firrtl::impl::LowerXMRBase<LowerXMRPass> {
           portAnnotations.push_back(mem.getPortAnnotation(res.index()));
           oldResults.push_back(res.value());
         }
-        auto newMem = builder.create<MemOp>(
-            resultTypes, mem.getReadLatency(), mem.getWriteLatency(),
+        auto newMem = MemOp::create(
+            builder, resultTypes, mem.getReadLatency(), mem.getWriteLatency(),
             mem.getDepth(), RUWBehavior::Undefined,
             builder.getArrayAttr(resultNames), mem.getNameAttr(),
             mem.getNameKind(), mem.getAnnotations(),