4c4d91cac85bc8131a8ef3c300b9c8dc9fe4609a
Fabian Schuiki <fabian@schuiki.ch>
2025-08-08 15:37:40 -0700

    [Support] Use git commit hash as version in non-release builds
    Instead of just saying "unknown git version", use the current git commit
    hash as CIRCT version in non-release builds. This makes the output of
    `--version` of our tools more useful, because it points to an actual
    commit.
    
    With this change, the [sv-tests-results] website can provide an actual
    git commit tooltip for the circt-verilog tool being used.
    
    [sv-tests-results]: https://chipsalliance.github.io/sv-tests-results/

--- cmake/modules/GenVersionFile.cmake
+++ cmake/modules/GenVersionFile.cmake
@@ -1,36 +1,64 @@
-# This file borrows ideas from LLVM's GenerateVersionFromVCS.cmake and VersionFromVCS.cmake
+# This script determines the project version and configures a `Version.cpp` file
+# accordingly. (Based roughly on LLVM's `GenerateVersionFromVCS.cmake` and
+# `VersionFromVCS.cmake`.)
+#
+# Run this using `cmake -P GenVersionFile.cmake <input-vars>`.
+#
 # Input variables:
-#   IN_FILE             - An absolute path of Version.cpp.in
-#   OUT_FILE            - An absolute path of Version.cpp
-#   RELEASE_PATTERN     - A pattern to search release tags
-#   DRY_RUN             - If true, make the version unknown.
-#   SOURCE_ROOT         - Path to root directory of source
+#
+# - `IN_FILE`:              Absolute path of `Version.cpp.in`
+# - `OUT_FILE`:             Absolute path of `Version.cpp`
+# - `RELEASE_TAG_PATTERN`:  A pattern like `firtool*` to search release tags
+# - `RELEASE_TAG_ENABLED`:  Use release tag if true, or git hash if false
+# - `SOURCE_ROOT`:          Path to root git directory
 
-set(GIT_DESCRIBE_DEFAULT "unknown git version")
-if (DRY_RUN)
-  set(GIT_DESCRIBE_OUTPUT "${GIT_DESCRIBE_DEFAULT}")
-else ()
-  message(STATUS "Generating ${OUT_FILE} from ${IN_FILE} by `git describe --dirty --tags --match ${RELEASE_PATTERN}`")
-  find_package(Git QUIET)
-  if (Git_FOUND)
-    execute_process(COMMAND ${GIT_EXECUTABLE} describe --dirty --tags --match ${RELEASE_PATTERN}
-      WORKING_DIRECTORY "${SOURCE_ROOT}"
-      RESULT_VARIABLE GIT_OUTPUT_RESULT
-      OUTPUT_VARIABLE GIT_DESCRIBE_OUTPUT
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
-    if (NOT ${GIT_OUTPUT_RESULT} EQUAL 0)
-      message(WARNING "git describe failed, set version to ${GIT_DESCRIBE_DEFAULT}")
-      set(GIT_DESCRIBE_OUTPUT "${GIT_DESCRIBE_DEFAULT}")
-    endif ()
-  else ()
-    message(WARNING "Git not found: ${GIT_EXECUTABLE}, set version to ${GIT_DESCRIBE_DEFAULT}")
-    set(GIT_DESCRIBE_OUTPUT "${GIT_DESCRIBE_DEFAULT}")
+# Set the default version in case we fail to determine it.
+set(CIRCT_VERSION "unknown git version")
+
+# Determine the version using git.
+find_package(Git QUIET)
+if (Git_FOUND)
+  # Either use `git describe` to find a matching tag, or `rev-parse` to get a
+  # short hash of the current commit HEAD.
+  if (RELEASE_TAG_ENABLED)
+    set(git_cmd describe --dirty --tags --match "${RELEASE_TAG_PATTERN}")
+  else()
+    set(git_cmd rev-parse --short HEAD)
+  endif()
+
+  # Run the git command.
+  string(JOIN " " git_cmd_str ${git_cmd})
+  message(STATUS "Determining version with `git ${git_cmd_str}`")
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} ${git_cmd}
+    WORKING_DIRECTORY "${SOURCE_ROOT}"
+    RESULT_VARIABLE git_exitcode
+    OUTPUT_VARIABLE git_output
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  # If the git command was successful, use its output as CIRCT version.
+  # Otherwise display a warning and keep the version at its default.
+  if (NOT ${git_exitcode})
+    set(CIRCT_VERSION "${git_output}")
+  else()
+    message(WARNING "Cannot determine CIRCT version -- git command failed")
   endif ()
+else()
+  message(WARNING "Cannot determine CIRCT version -- git not found")
 endif()
 
-# This command will prepend CMAKE_CURRENT_{SOURCE,BINARY}_DIR if <input> or <output> is relative,
-# that's why I need IN_FILE and OUT_FILE to be absolute path.
-configure_file("${IN_FILE}" "${OUT_FILE}.tmp")
+# Display the version we have found.
+message(STATUS "CIRCT version: ${CIRCT_VERSION}")
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different "${OUT_FILE}.tmp" "${OUT_FILE}")
+# Replace the `@...@` placeholders in the input file. We use a temporary file to
+# hold the output, such that we can only overwrite the output file if the
+# version changed.
+#
+# (This command will prepend CMAKE_CURRENT_{SOURCE,BINARY}_DIR if <input> or
+# <output> is relative, which is why IN_FILE and OUT_FILE must be absolute.)
+set(tmpfile "${OUT_FILE}.tmp")
+configure_file("${IN_FILE}" "${tmpfile}")
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E copy_if_different "${tmpfile}" "${OUT_FILE}")
 file(REMOVE "${OUT_FILE}.tmp")
