bbaf207b10f4caf7558526c8a68fc8045bea7149
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-08-08 16:25:29 -0400

    [FIRRTL] Add InferDomains pass
    Add a pass that does domain inference and checking.  This is used to
    verify the legality of a FIRRTL circuit with respect to its domains.
    E.g., this pass is intended to be used for checking for illegal clock
    domain crossings.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- lib/Firtool/Firtool.cpp
+++ lib/Firtool/Firtool.cpp
@@ -49,6 +49,9 @@ LogicalResult firtool::populatePreprocessTransforms(mlir::PassManager &pm,
   pm.nest<firrtl::CircuitOp>().nest<firrtl::FModuleOp>().addPass(
       firrtl::createLowerIntrinsics());
 
+  if (opt.shouldInferDomains())
+    pm.nest<firrtl::CircuitOp>().addPass(firrtl::createInferDomains());
+
   return success();
 }
 
@@ -758,6 +761,11 @@ struct FirtoolCmdOptions {
       llvm::cl::desc("Emit bindfiles for private modules"),
       llvm::cl::init(false)};
 
+    llvm::cl::opt<bool> inferDomains{
+      "infer-domains",
+      llvm::cl::desc("Enable domain inference and checking"),
+      llvm::cl::init(false)};
+
   //===----------------------------------------------------------------------===
   // Lint options
   //===----------------------------------------------------------------------===
@@ -809,7 +817,7 @@ circt::firtool::FirtoolOptions::FirtoolOptions()
       disableCSEinClasses(false), selectDefaultInstanceChoice(false),
       symbolicValueLowering(verif::SymbolicValueLowering::ExtModule),
       disableWireElimination(false), lintStaticAsserts(true),
-      lintXmrsInDesign(true), emitAllBindFiles(false) {
+      lintXmrsInDesign(true), emitAllBindFiles(false), inferDomains(false) {
   if (!clOptions.isConstructed())
     return;
   outputFilename = clOptions->outputFilename;
@@ -862,4 +870,5 @@ circt::firtool::FirtoolOptions::FirtoolOptions()
   lintStaticAsserts = clOptions->lintStaticAsserts;
   lintXmrsInDesign = clOptions->lintXmrsInDesign;
   emitAllBindFiles = clOptions->emitAllBindFiles;
+  inferDomains = clOptions->inferDomains;
 }
