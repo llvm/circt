8aba4c9271e758f562434ccf80759a9d13f58867
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-08-16 10:41:21 -0400

    [test] Exclude circt-verilog test from valgrind
    Work around more "conditional jump or move depends on uninitialized
    value(s)" errors stemming from Slang code.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- test/circt-verilog/redundant-files.sv
+++ test/circt-verilog/redundant-files.sv
@@ -1,5 +1,7 @@
 // RUN: circt-verilog %s %s
 // REQUIRES: slang
+// Internal issue in Slang v9 about jump depending on uninitialised value.
+// UNSUPPORTED: valgrind
 
 // Listing a file twice caused the Slang's source manager to crash.
 
