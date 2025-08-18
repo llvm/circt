cb523eec13d132968a428144d95d8c2d1c90aa48
Fabian Schuiki <fabian@schuiki.ch>
2025-08-18 13:52:17 -0700

    [ArcToLLVM] Replace all i0 values with a constant 0
    I have noticed that the emission to LLVMIR doesn't like `i0` types in
    the IR, leading to assertions in `llvm::IntegerType` to fail. Add a
    preprocessing step to the Arc-to-LLVM conversion pass that replaces all
    `i0` values in the IR with an `hw.constant 0 : i0`. This causes the `i0`
    values in the LLVM lowering to disappear, with most of them getting
    constant-folded from `%1 = llvm.zext %0 : i0 to i42` to
    `%1 = llvm.mlir.constant(0 : i42) : i42`.

--- lib/Conversion/ArcToLLVM/LowerArcToLLVM.cpp
+++ lib/Conversion/ArcToLLVM/LowerArcToLLVM.cpp
@@ -610,6 +610,29 @@ struct LowerArcToLLVMPass
 } // namespace
 
 void LowerArcToLLVMPass::runOnOperation() {
+  // Replace any `i0` values with an `hw.constant 0 : i0` to avoid later issues
+  // in LLVM conversion.
+  {
+    DenseMap<Region *, hw::ConstantOp> zeros;
+    getOperation().walk([&](Operation *op) {
+      if (op->hasTrait<OpTrait::ConstantLike>())
+        return;
+      for (auto result : op->getResults()) {
+        auto type = dyn_cast<IntegerType>(result.getType());
+        if (!type || type.getWidth() != 0)
+          continue;
+        auto *region = op->getParentRegion();
+        auto &zero = zeros[region];
+        if (!zero) {
+          auto builder = OpBuilder::atBlockBegin(&region->front());
+          zero = hw::ConstantOp::create(builder, result.getLoc(),
+                                        APInt::getZero(0));
+        }
+        result.replaceAllUsesWith(zero);
+      }
+    });
+  }
+
   // Collect the symbols in the root op such that the HW-to-LLVM lowering can
   // create LLVM globals with non-colliding names.
   Namespace globals;
