ef5593e19856ddf17e10e63ba2820730a1a7f5a5
Atticus Kuhn <atticusmkuhn@gmail.com>
2025-08-27 16:17:10 +0100

    format

--- lib/Conversion/CoreToFSM/CoreToFSM.cpp
+++ lib/Conversion/CoreToFSM/CoreToFSM.cpp
@@ -1,5 +1,4 @@
-//===- CoreToFSM.cpp - Convert Core Dialects (HW + Seq + Comb) to FSM
-//Dialect===//
+//===CoreToFSM.cpp - Convert Core Dialects (HW + Seq + Comb) to FSM Dialect===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
