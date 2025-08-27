e422bb43a625c5ea5e11f44a915da2bdb31348ff
Atticus Kuhn <atticusmkuhn@gmail.com>
2025-08-27 16:16:20 +0100

    format

--- include/circt/Conversion/CoreToFSM.h
+++ include/circt/Conversion/CoreToFSM.h
@@ -1,4 +1,4 @@
-//===- CoreToFSM.h - Core to FSM conversions --------------------*- C++ -*-===//
+//===- CoreToFSM.h - Core to FSM conversions pass----------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
