93e43a45577d28e1ac6f81ebfd4bf416e20cc1dc
Morten Borup Petersen <morten_bp@live.dk>
2025-09-03 09:00:29 +0200

    [ESI] Don't assume __dict__ attribute for all objects (#8911)
    * [ESI] Don't assume __dict__ attribute for all objects
    
    ... when checking if a given object is valid for `StructType` serialization. E.g., `bytes` objects do not have a `__dict__` attribute.
    
    * Also use __dict__ in serialization if needed

--- lib/Dialect/ESI/runtime/python/esiaccel/types.py
+++ lib/Dialect/ESI/runtime/python/esiaccel/types.py
@@ -245,6 +245,8 @@ class StructType(ESIType):
   def is_valid(self, obj) -> Tuple[bool, Optional[str]]:
     fields_count = 0
     if not isinstance(obj, dict):
+      if not hasattr(obj, "__dict__"):
+        return (False, "must be a dict or have __dict__ attribute")
       obj = obj.__dict__
 
     for (fname, ftype) in self.fields:
@@ -260,6 +262,8 @@ class StructType(ESIType):
 
   def serialize(self, obj) -> bytearray:
     ret = bytearray()
+    if not isinstance(obj, dict):
+      obj = obj.__dict__
     for (fname, ftype) in reversed(self.fields):
       fval = obj[fname]
       ret.extend(ftype.serialize(fval))
