1a8381fdbb7f8e1de8d1c760320ad51a0f862978
Hideto Ueno <uenoku.tokotoko@gmail.com>
2025-09-08 12:09:52 -0700

    [Synth] Implement basic canonicalization/folder to mig.maj_inv
    Add canonicalize method with pattern matching for single operands,
    identical/complementary operands, and constants.
    Also implement fold method for constant evaluation along with constant
    materialization

--- lib/Dialect/Synth/SynthDialect.cpp
+++ lib/Dialect/Synth/SynthDialect.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "circt/Dialect/Synth/SynthDialect.h"
+#include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/Synth/SynthOps.h"
 
 using namespace circt;
@@ -17,6 +18,16 @@ void SynthDialect::initialize() {
 #define GET_OP_LIST
 #include "circt/Dialect/Synth/Synth.cpp.inc"
       >();
+}
+
+Operation *SynthDialect::materializeConstant(OpBuilder &builder,
+                                             Attribute value, Type type,
+                                             Location loc) {
+  // Integer constants.
+  if (auto intType = dyn_cast<IntegerType>(type))
+    if (auto attrValue = dyn_cast<IntegerAttr>(value))
+      return hw::ConstantOp::create(builder, loc, type, attrValue);
+  return nullptr;
 }
 
 #include "circt/Dialect/Synth/SynthDialect.cpp.inc"
