2e56369e8edb97ddd32e737626daf0ee9d1bbcaa
Moritz Scherer <scheremo@iis.ee.ethz.ch>
2025-09-13 21:48:26 +0100

    Condense test

--- test/Conversion/ImportVerilog/basic.sv
+++ test/Conversion/ImportVerilog/basic.sv
@@ -3164,21 +3164,13 @@ module UnarySingleBitIncrement (
 
 endmodule // UnarySingleBitIncrement
 
-// CHECK-LABEL: moore.module @UnpackedParameterArray(
-module UnpackedParameterArray #(
-    parameter int unsigned ParameterArray [2] = '{default: 0}
-) (
-    input  logic  clk_i,
-    input  logic  rst_ni
-);
-
-   function automatic int unsigned returnParameterArrayElement (int idx);
-      // CHECK: [[CONST0:%.+]] = moore.constant 0 : i32
-      // CHECK-NEXT: [[CONST1:%.+]] = moore.constant 0 : i32
-      // CHECK-NEXT: [[ARR:%.+]] = moore.array_create [[CONST0]], [[CONST1]] : !moore.i32, !moore.i32 -> uarray<2 x i32>
-      // CHECK: [[RETURN:%.+]] = moore.dyn_extract [[ARR]] from {{%.+}} : uarray<2 x i32>, i32 -> i32
-      return ParameterArray[idx];
-   endfunction
-
-endmodule // UnpackedParameterArray
+// CHECK-LABEL: func.func private @returnParameterArrayElement(
+function automatic int unsigned returnParameterArrayElement (int idx);
+  localparam int unsigned ParameterArray [2] = '{42, 9001};
+  // CHECK: [[CONST0:%.+]] = moore.constant 42 : i32
+  // CHECK-NEXT: [[CONST1:%.+]] = moore.constant 9001 : i32
+  // CHECK-NEXT: [[ARR:%.+]] = moore.array_create [[CONST0]], [[CONST1]] : !moore.i32, !moore.i32 -> uarray<2 x i32>
+  // CHECK: [[RETURN:%.+]] = moore.dyn_extract [[ARR]] from {{%.+}} : uarray<2 x i32>, i32 -> i32
+  return ParameterArray[idx];
+endfunction
 
