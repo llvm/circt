71b5602c7b36ff3b4cc669ac916c5053f5be0541
Fabian Schuiki <fabian.schuiki@sifive.com>
2025-09-18 16:16:36 -0700

    [Reduce] Add InnerSymbolUses helper; use in NodeSymbolRemover
    Add a helper struct that collects all inner references under a root op
    into a set. This is pretty pessimistic and specific to the reducer. Make
    use of this in the `NodeSymbolRemover` FIRRTL reduction to only remove
    symbols from ops if there are no references to them.

--- include/circt/Reduce/ReductionUtils.h
+++ include/circt/Reduce/ReductionUtils.h
@@ -1,4 +1,4 @@
-//===- ReductionUtils.h - Reduction pattern utilities -----------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -9,6 +9,7 @@
 #ifndef CIRCT_REDUCE_REDUCTIONUTILS_H
 #define CIRCT_REDUCE_REDUCTIONUTILS_H
 
+#include "circt/Dialect/HW/HWAttributes.h"
 #include "circt/Support/LLVM.h"
 
 namespace circt {
@@ -20,6 +21,25 @@ namespace reduce {
 /// Starting at the given `op`, traverse through it and its operands and erase
 /// operations that have no more uses.
 void pruneUnusedOps(Operation *initialOp, Reduction &reduction);
+
+/// A helper struct that scans a root operation and all its nested operations
+/// for `InnerRefAttr`s.
+struct InnerSymbolUses {
+  InnerSymbolUses(Operation *root);
+
+  InnerSymbolUses() = default;
+  InnerSymbolUses(const InnerSymbolUses &) = default;
+  InnerSymbolUses(InnerSymbolUses &&) = default;
+
+  InnerSymbolUses &operator=(const InnerSymbolUses &) = default;
+  InnerSymbolUses &operator=(InnerSymbolUses &&) = default;
+
+  bool hasUses(hw::InnerRefAttr inner) const;
+  bool hasUses(StringAttr mod, StringAttr sym) const;
+
+private:
+  DenseSet<std::pair<StringAttr, StringAttr>> uses;
+};
 
 } // namespace reduce
 } // namespace circt
