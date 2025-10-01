8868c6501356a9752a895446edfc7324c4478c6a
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-10-01 19:01:16 -0400

    fixup! [FIRRTL] Add LowerDomains pass

--- lib/Dialect/FIRRTL/Transforms/LowerDomains.cpp
+++ lib/Dialect/FIRRTL/Transforms/LowerDomains.cpp
@@ -1,4 +1,4 @@
-//===- LowerDomains.cpp - Erase all FIRRTL Domain Information -------------===//
+//===- LowerDomains.cpp - Lower domain information to properties ----------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
