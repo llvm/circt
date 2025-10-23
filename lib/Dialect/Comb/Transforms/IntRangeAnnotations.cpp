a928678680de43d4a9510bc36a51bc7219dc67fc
cowardsa <s.coward111@gmail.com>
2025-10-23 15:09:16 +0100

    Add overflow annotations pass

---
+++ lib/Dialect/Comb/Transforms/IntRangeAnnotations.cpp
@@ -0,0 +1,115 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/Comb/CombOps.h"
+#include "circt/Dialect/Comb/CombPasses.h"
+#include "circt/Dialect/HW/HWOps.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+#include "mlir/Analysis/DataFlowFramework.h"
+
+#include "mlir/Analysis/DataFlow/DeadCodeAnalysis.h"
+#include "mlir/Analysis/DataFlow/IntegerRangeAnalysis.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+using namespace circt;
+using namespace circt::comb;
+using namespace mlir;
+using namespace mlir::dataflow;
+
+namespace circt {
+namespace comb {
+#define GEN_PASS_DEF_COMBOVERFLOWANNOTATING
+#include "circt/Dialect/Comb/Passes.h.inc"
+} // namespace comb
+} // namespace circt
+
+/// Gather ranges for all the values in `values`. Appends to the existing
+/// vector.
+static LogicalResult collectRanges(DataFlowSolver &solver, ValueRange values,
+                                   SmallVectorImpl<ConstantIntRanges> &ranges) {
+  for (Value val : values) {
+    auto *maybeInferredRange =
+        solver.lookupState<IntegerValueRangeLattice>(val);
+    if (!maybeInferredRange || maybeInferredRange->getValue().isUninitialized())
+      return failure();
+
+    const ConstantIntRanges &inferredRange =
+        maybeInferredRange->getValue().getValue();
+    ranges.push_back(inferredRange);
+  }
+  return success();
+}
+
+namespace {
+template <typename CombOpTy>
+struct CombOpAnnotate : public OpRewritePattern<CombOpTy> {
+  CombOpAnnotate(MLIRContext *context, DataFlowSolver &s)
+      : OpRewritePattern<CombOpTy>(context), solver(s) {}
+
+  LogicalResult matchAndRewrite(CombOpTy op,
+                                PatternRewriter &rewriter) const override {
+    if (op->hasAttr("comb.upper_bit_trunc"))
+      return failure();
+
+    if (op->getNumOperands() != 2 || op->getNumResults() != 1)
+      return rewriter.notifyMatchFailure(
+          op, "Only support binary operations with one result");
+
+    if (!isa<comb::AddOp>(op) && !isa<comb::MulOp>(op) && !isa<comb::SubOp>(op))
+      return rewriter.notifyMatchFailure(op, "unsupported comb op");
+
+    SmallVector<ConstantIntRanges> ranges;
+    if (failed(collectRanges(solver, op->getOperands(), ranges)))
+      return rewriter.notifyMatchFailure(op, "input without specified range");
+
+    bool overflowed = false;
+    auto a = ranges[0].umax();
+    auto b = ranges[1].umax();
+
+    // Only use the overflow flag - need to explicitly discard the result
+    if (isa<comb::AddOp>(op))
+      (void)a.uadd_ov(b, overflowed);
+
+    if (isa<comb::MulOp>(op))
+      (void)a.umul_ov(b, overflowed);
+
+    op->setAttr("comb.upper_bit_trunc",
+                BoolAttr::get(op->getContext(), overflowed));
+    return success();
+  }
+
+private:
+  DataFlowSolver &solver;
+};
+
+struct CombOverflowAnnotatingPass
+    : comb::impl::CombOverflowAnnotatingBase<CombOverflowAnnotatingPass> {
+
+  using CombOverflowAnnotatingBase::CombOverflowAnnotatingBase;
+  void runOnOperation() override;
+};
+} // namespace
+
+void CombOverflowAnnotatingPass::runOnOperation() {
+  Operation *op = getOperation();
+  MLIRContext *ctx = op->getContext();
+  DataFlowSolver solver;
+  solver.load<DeadCodeAnalysis>();
+  solver.load<IntegerRangeAnalysis>();
+  if (failed(solver.initializeAndRun(op)))
+    return signalPassFailure();
+
+  RewritePatternSet patterns(ctx);
+  patterns.add<CombOpAnnotate<comb::AddOp>, CombOpAnnotate<comb::MulOp>>(
+      patterns.getContext(), solver);
+
+  if (failed(applyPatternsGreedily(op, std::move(patterns))))
+    signalPassFailure();
+}
---
+++ test/Dialect/Comb/comb-overflow-annotating.mlir
@@ -0,0 +1,62 @@
+// RUN: circt-opt %s --comb-overflow-annotating | FileCheck %s
+
+// CHECK-LABEL: @basic_add
+hw.module @basic_add(in %a : i4, in %b : i4, in %c : i4, out add1 : i4, out add2 : i5) {
+  // CHECK-NEXT: %false = hw.constant false
+  // CHECK-NEXT: comb.add %a, %b {comb.upper_bit_trunc = true} : i4
+  // CHECK-NEXT: %[[AEXT:.+]] = comb.concat %false, %a : i1, i4
+  // CHECK-NEXT: %[[BEXT:.+]] = comb.concat %false, %b : i1, i4
+  // CHECK-NEXT: comb.add %[[AEXT]], %[[BEXT]] {comb.upper_bit_trunc = false} : i5
+  %false = hw.constant false
+  %0 = comb.add %a, %b : i4
+  %1 = comb.concat %false, %a : i1, i4
+  %2 = comb.concat %false, %b : i1, i4
+  %3 = comb.add %1, %2 : i5
+  hw.output %0, %3 : i4, i5
+}
+
+// CHECK-LABEL: @basic_mul
+hw.module @basic_mul(in %a : i4, in %b : i4, out mul7 : i7, out mul8 : i8) {
+  // CHECK-NEXT: %c0_i3 = hw.constant 0 : i3
+  // CHECK-NEXT: %c0_i4 = hw.constant 0 : i4
+  // CHECK-NEXT: %[[AEXT7:.+]] = comb.concat %c0_i3, %a : i3, i4
+  // CHECK-NEXT: %[[BEXT7:.+]] = comb.concat %c0_i3, %b : i3, i4
+  // CHECK-NEXT: comb.mul %[[AEXT7]], %[[BEXT7]] {comb.upper_bit_trunc = true} : i7
+  // CHECK-NEXT: %[[AEXT8:.+]] = comb.concat %c0_i4, %a : i4, i4
+  // CHECK-NEXT: %[[BEXT8:.+]] = comb.concat %c0_i4, %b : i4, i4
+  // CHECK-NEXT: comb.mul %[[AEXT8]], %[[BEXT8]] {comb.upper_bit_trunc = false} : i8
+  %c0_i3 = hw.constant 0 : i3
+  %c0_i4 = hw.constant 0 : i4
+  %1 = comb.concat %c0_i3, %a : i3, i4
+  %2 = comb.concat %c0_i3, %b : i3, i4
+  %3 = comb.mul %1, %2 : i7
+  %4 = comb.concat %c0_i4, %a : i4, i4
+  %5 = comb.concat %c0_i4, %b : i4, i4
+  %6 = comb.mul %4, %5 : i8
+  hw.output %3, %6 : i7, i8
+}
+
+// CHECK-LABEL: @basic_fma
+hw.module @basic_fma(in %a : i4, in %b : i4, in %c : i4, in %d : i5, out fma1 : i8, out fma2 : i8) {
+  // CHECK-NEXT: %c0_i3 = hw.constant 0 : i3
+  // CHECK-NEXT: %c0_i4 = hw.constant 0 : i4
+  // CHECK-NEXT: %[[AEXT:.+]] = comb.concat %c0_i4, %a : i4, i4
+  // CHECK-NEXT: %[[BEXT:.+]] = comb.concat %c0_i4, %b : i4, i4
+  // CHECK-NEXT: %[[CEXT:.+]] = comb.concat %c0_i4, %c : i4, i4
+  // CHECK-NEXT: %[[DEXT:.+]] = comb.concat %c0_i3, %d : i3, i5
+  // CHECK-NEXT: %[[MUL:.+]] = comb.mul %0, %1 {comb.upper_bit_trunc = false} : i8
+  // Should not overflow when adding 4-bit c
+  // CHECK-NEXT: comb.add %4, %2 {comb.upper_bit_trunc = false} : i8
+  // Should overflow when adding 5-bit d
+  // CHECK-NEXT: comb.add %4, %3 {comb.upper_bit_trunc = true} : i8
+  %c0_i3 = hw.constant 0 : i3
+  %c0_i4 = hw.constant 0 : i4
+  %4 = comb.concat %c0_i4, %a : i4, i4
+  %5 = comb.concat %c0_i4, %b : i4, i4
+  %6 = comb.concat %c0_i4, %c : i4, i4
+  %7 = comb.concat %c0_i3, %d : i3, i5
+  %8 = comb.mul %4, %5 : i8
+  %9 = comb.add %8, %6 : i8
+  %10 = comb.add %8, %7 : i8
+  hw.output %9, %10 : i8, i8
+}
