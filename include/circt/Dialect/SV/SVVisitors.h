e47f7a8c1f883a7dcaed883a654990b08c6336ce
Trevor McKay <trevor.mckay@sifive.com>
2025-10-23 10:25:31 -0700

    add sv.verbatim.module op

--- include/circt/Dialect/SV/SVVisitors.h
+++ include/circt/Dialect/SV/SVVisitors.h
@@ -43,7 +43,7 @@ public:
             FWriteOp, FFlushOp, SystemFunctionOp, VerbatimOp, MacroRefOp,
             FuncCallOp, FuncCallProceduralOp, ReturnOp, IncludeOp, MacroErrorOp,
             // Type declarations.
-            InterfaceOp, InterfaceSignalOp, InterfaceModportOp,
+            InterfaceOp, SVVerbatimModuleOp, InterfaceSignalOp, InterfaceModportOp,
             InterfaceInstanceOp, GetModportOp, AssignInterfaceSignalOp,
             ReadInterfaceSignalOp, MacroDeclOp, MacroDefOp, FuncOp,
             FuncDPIImportOp,
@@ -147,6 +147,7 @@ public:
 
   // Type declarations.
   HANDLE(InterfaceOp, Unhandled);
+  HANDLE(SVVerbatimModuleOp, Unhandled);
   HANDLE(InterfaceInstanceOp, Unhandled);
   HANDLE(InterfaceSignalOp, Unhandled);
   HANDLE(InterfaceModportOp, Unhandled);
