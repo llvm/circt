28400723b5cc078c23d546e4c20ed1307a4f4ff2
yassinz <yassinz.you@gmail.com>
2025-11-22 19:24:34 +0000

    fitted to 80 cols

--- lib/Bindings/Python/ArcModule.cpp
+++ lib/Bindings/Python/ArcModule.cpp
@@ -1,4 +1,4 @@
-//===- ArcModule.cpp - Arc API nanobind module --------------------------===//
+//===- ArcModule.cpp - Arc API nanobind module ----------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
