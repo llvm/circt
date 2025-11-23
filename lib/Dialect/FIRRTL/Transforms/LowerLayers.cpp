cafe2c7c4ed3e776ab411ceba9b44b4eadbf9ae5
21verses <145851442+MrRoy09@users.noreply.github.com>
2025-11-23 11:47:20 +0530

    [FIRRTL] Added getExtModuelName to FExtModuleOp (#9233)

--- lib/Dialect/FIRRTL/Transforms/LowerLayers.cpp
+++ lib/Dialect/FIRRTL/Transforms/LowerLayers.cpp
@@ -257,10 +257,6 @@ class LowerLayersPass
   void preprocessModule(CircuitNamespace &ns, InstanceGraphNode *node,
                         FModuleOp module);
 
-  /// Get the verilog name of an extmodule, which could be recorded in its
-  /// defname property.
-  StringRef getExtModuleName(FExtModuleOp extModule);
-
   /// Record the supposed bindfiles for any known layers of the ext module.
   void preprocessExtModule(CircuitNamespace &ns, InstanceGraphNode *node,
                            FExtModuleOp extModule);
@@ -1004,12 +1000,6 @@ void LowerLayersPass::preprocessModule(CircuitNamespace &ns,
       buildBindFile(ns, node, b, sym, layer);
 }
 
-StringRef LowerLayersPass::getExtModuleName(FExtModuleOp extModule) {
-  if (auto name = extModule.getDefnameAttr())
-    return name.getValue();
-  return extModule.getName();
-}
-
 void LowerLayersPass::preprocessExtModule(CircuitNamespace &ns,
                                           InstanceGraphNode *node,
                                           FExtModuleOp extModule) {
@@ -1022,7 +1012,7 @@ void LowerLayersPass::preprocessExtModule(CircuitNamespace &ns,
   if (known.empty())
     return;
 
-  auto moduleName = getExtModuleName(extModule);
+  auto moduleName = extModule.getExtModuleName();
   auto &files = bindFiles[extModule];
   SmallPtrSet<Operation *, 8> seen;
 
