dc98b83e20456d80401975f62c43e19cd5ccdfc7
yassinz <yassinz.you@gmail.com>
2025-12-02 03:13:41 +0000

    Add pipeline tests

--- integration_test/Bindings/Python/import.py
+++ integration_test/Bindings/Python/import.py
@@ -2,7 +2,7 @@
 # RUN: %PYTHON% %s
 
 import circt
-from circt.dialects import comb, esi, hw, seq, sv, arc, pipeline
+from circt.dialects import comb, esi, hw, pipeline, seq, sv
 
 from circt.passmanager import PassManager
 from circt.ir import Context
