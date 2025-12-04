120133acd96996f3a831c4a8601e471706c56635
Bea Healy <57840981+TaoBi22@users.noreply.github.com>
2025-12-04 12:42:11 +0000

    [Comb] Add ConcatOp custom assembly format to enable nullaries (#9274)

--- lib/Dialect/Comb/CombOps.cpp
+++ lib/Dialect/Comb/CombOps.cpp
@@ -503,6 +503,56 @@ LogicalResult ConcatOp::inferReturnTypes(
   return success();
 }
 
+/// Parse a ConcatOp that can either follow the format:
+/// $inputs attr-dict `:` qualified(type($inputs))
+/// or have no operands, colon and typelist.
+ParseResult ConcatOp::parse(OpAsmParser &parser, OperationState &result) {
+  SmallVector<OpAsmParser::UnresolvedOperand, 4> operands;
+  SmallVector<Type, 4> types;
+
+  llvm::SMLoc allOperandLoc = parser.getCurrentLocation();
+
+  // Parse the operand list, attributes and colon
+  if (parser.parseOperandList(operands) ||
+      parser.parseOptionalAttrDict(result.attributes) || parser.parseColon())
+    return failure();
+
+  // Parse an optional list of types
+  Type parsedType;
+  auto parseResult = parser.parseOptionalType(parsedType);
+  if (parseResult.has_value()) {
+    if (failed(parseResult.value()))
+      return failure();
+    types.push_back(parsedType);
+    while (succeeded(parser.parseOptionalComma())) {
+      if (parser.parseType(parsedType))
+        return failure();
+      types.push_back(parsedType);
+    }
+  }
+
+  if (parser.resolveOperands(operands, types, allOperandLoc, result.operands))
+    return failure();
+
+  SmallVector<Type, 1> inferredTypes;
+  if (failed(ConcatOp::inferReturnTypes(
+          parser.getContext(), result.location, result.operands,
+          result.attributes.getDictionary(parser.getContext()),
+          result.getRawProperties(), {}, inferredTypes)))
+    return failure();
+
+  result.addTypes(inferredTypes);
+  return success();
+}
+
+void ConcatOp::print(OpAsmPrinter &p) {
+  p << " ";
+  p.printOperands(getOperands());
+  p.printOptionalAttrDict((*this)->getAttrs());
+  p << " : ";
+  llvm::interleaveComma(getOperandTypes(), p);
+}
+
 //===----------------------------------------------------------------------===//
 // ReverseOp
 //===----------------------------------------------------------------------===//
