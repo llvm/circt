32b9decab82504f5a0d0e57f319eda9ae23ad15f
Leon Hielscher <hielscher@fzi.de>
2025-12-17 16:24:47 +0100

    [arcilator] Integrate ArcRuntime

--- lib/Tools/arcilator/pipelines.cpp
+++ lib/Tools/arcilator/pipelines.cpp
@@ -135,7 +135,15 @@ void circt::populateArcStateAllocationPipeline(
   pm.addPass(arc::createArcCanonicalizerPass());
 }
 
-void circt::populateArcToLLVMPipeline(OpPassManager &pm) {
+void circt::populateArcToLLVMPipeline(OpPassManager &pm, bool insertRuntime,
+                                      StringRef extraRuntimeArgs) {
+  if (insertRuntime) {
+    InsertRuntimeOptions opts;
+    if (!extraRuntimeArgs.empty())
+      opts.extraArgs =
+          std::string(extraRuntimeArgs.begin(), extraRuntimeArgs.end());
+    pm.addPass(createInsertRuntime(opts));
+  }
   pm.addPass(createLowerArcToLLVMPass());
   pm.addPass(createCSEPass());
   pm.addPass(arc::createArcCanonicalizerPass());
--- tools/arcilator/arcilator-runtime.h
+++ tools/arcilator/arcilator-runtime.h
@@ -7,6 +7,11 @@
 #include <ostream>
 #include <vector>
 
+#ifdef ARC_USE_COMPILED_RUNTIME_LIB
+#include "ArcRuntime/ArcRuntime.h"
+#include "ArcRuntime/Common.h"
+#endif
+
 struct Signal {
   const char *name;
   unsigned offset;
