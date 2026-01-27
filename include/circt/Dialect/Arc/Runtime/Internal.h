910c2eb078762c1fe99bf72a935df60412db525e
Leon Hielscher <hielscher@fzi.de>
2026-01-27 23:17:22 +0100

    Change file headers

--- include/circt/Dialect/Arc/Runtime/Internal.h
+++ include/circt/Dialect/Arc/Runtime/Internal.h
@@ -1,4 +1,4 @@
-//===- Intenal.h - Shared internal runtime utilities-----------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- include/circt/Dialect/Arc/Runtime/TraceEncoder.h
+++ include/circt/Dialect/Arc/Runtime/TraceEncoder.h
@@ -1,4 +1,4 @@
-//===- TraceEncoder.h - Shared trace buffer handling logic ----------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
