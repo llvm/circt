8e4d74bc6cbd1aea936281bd77671ac86945e864
yassinz <yassinz.you@gmail.com>
2026-02-02 01:44:21 +0000

    [Python] Add HLMemType and FirMemType bindings to Seq dialect

--- include/circt-c/Dialect/Seq.h
+++ include/circt-c/Dialect/Seq.h
@@ -33,6 +33,48 @@ MLIR_CAPI_EXPORTED MlirType seqImmutableTypeGet(MlirType type);
 /// Creates a seq immutable type
 MLIR_CAPI_EXPORTED MlirType seqImmutableTypeGetInnerType(MlirType type);
 
+/// If the type is an HLMemType
+MLIR_CAPI_EXPORTED bool seqTypeIsAHLMem(MlirType type);
+
+/// Creates an seq HLMem type
+MLIR_CAPI_EXPORTED MlirType seqHLMemTypeGet(MlirContext ctx, intptr_t rank,
+                                            const int64_t *shape,
+                                            MlirType elementType);
+
+/// Gets the element type of an HLMem type
+MLIR_CAPI_EXPORTED MlirType seqHLMemTypeGetElementType(MlirType type);
+
+/// Gets the rank of an HLMem type
+MLIR_CAPI_EXPORTED intptr_t seqHLMemTypeGetRank(MlirType type);
+
+/// Gets a pointer to the shape array of an HLMem type
+/// The array length is equal to the rank.
+/// The pointer remains valid as long as the MlirType exists.
+MLIR_CAPI_EXPORTED const int64_t *seqHLMemTypeGetShape(MlirType type);
+
+/// If the type is a FirMem type
+MLIR_CAPI_EXPORTED bool seqTypeIsAFirMem(MlirType type);
+
+/// Creates a seq FirMem type
+/// \param maskWidth A pointer to the mask width. Pass NULL if no mask is
+/// present.
+MLIR_CAPI_EXPORTED MlirType seqFirMemTypeGet(MlirContext ctx, uint64_t depth,
+                                             uint32_t width,
+                                             const uint32_t *maskWidth);
+
+/// Gets the depth of a FirMem type
+MLIR_CAPI_EXPORTED uint64_t seqFirMemTypeGetDepth(MlirType type);
+
+/// Gets the width of a FirMem type
+MLIR_CAPI_EXPORTED uint32_t seqFirMemTypeGetWidth(MlirType type);
+
+/// Returns true if the FirMem type has a mask width defined
+MLIR_CAPI_EXPORTED bool seqFirMemTypeHasMask(MlirType type);
+
+/// Gets the mask width of a FirMem type
+/// Behavior is undefined if seqFirMemTypeHasMask returns false
+MLIR_CAPI_EXPORTED uint32_t seqFirMemTypeGetMaskWidth(MlirType type);
+
 #ifdef __cplusplus
 }
 #endif
