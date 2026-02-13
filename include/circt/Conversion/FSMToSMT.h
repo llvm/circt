d38dcce4c3656cdb9e430f6ede04a739cef0b86d
luisacicolini <luisacicolini@gmail.com>
2026-02-13 15:46:40 +0000

    chore: comm

--- include/circt/Conversion/FSMToSMT.h
+++ include/circt/Conversion/FSMToSMT.h
@@ -1,4 +1,4 @@
-//===- FSMToSMT.h - FSM to SMT-LIB conversions ------------------*- C++ -*-===//
+//===- FSMToSMT.h - FSM to SMT conversion ------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
