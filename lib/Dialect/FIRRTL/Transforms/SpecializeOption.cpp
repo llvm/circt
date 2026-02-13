5f68ca7b8b8bda91bd6afc248d9460fc8da90232
Hideto Ueno <uenoku.tokotoko@gmail.com>
2026-02-12 18:00:23 -0800

    [FIRRTL][SpecializeOption] Preserve unselected options (#9664)
    This patch changes the behavior of the SpecializeOption pass to only
    process and erase option declarations that are explicitly selected for
    specialization.

--- lib/Dialect/FIRRTL/Transforms/SpecializeOption.cpp
+++ lib/Dialect/FIRRTL/Transforms/SpecializeOption.cpp
@@ -68,13 +68,10 @@ struct SpecializeOptionPass
             auto it = selected.find(inst.getOptionNameAttr());
             FlatSymbolRefAttr target;
             if (it == selected.end()) {
-              if (!selectDefaultInstanceChoice) {
-                inst.emitError("missing specialization for option ")
-                    << inst.getOptionNameAttr();
-                failed = true;
+              if (selectDefaultInstanceChoice)
+                target = inst.getDefaultTargetAttr();
+              else
                 return;
-              }
-              target = inst.getDefaultTargetAttr();
             } else
               target = inst.getTargetOrDefaultAttr(it->second);
 
@@ -95,6 +92,8 @@ struct SpecializeOptionPass
 
     bool analysisPreserved = numInstances == 0;
     circuit->walk([&](OptionOp optionOp) {
+      if (!selected.contains(optionOp.getSymNameAttr()))
+        return;
       optionOp->erase();
       analysisPreserved = false;
     });
