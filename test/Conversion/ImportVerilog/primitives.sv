75c4d472e8391d1a40dad7e35b60a4fd5e152f2d
Bea Healy <beahealy22@gmail.com>
2026-04-08 08:55:34 +0100

    [ImportVerilog] Add support for remaining n-input primitives

--- test/Conversion/ImportVerilog/primitives.sv
+++ test/Conversion/ImportVerilog/primitives.sv
@@ -66,3 +66,69 @@ module multi_and_prim;
     logic A, B, C, D, Q;
     and a (Q, A, B, C, D);
 endmodule
+
+// CHECK-LABEL: moore.module @nand_prim()
+// CHECK: [[A:%.+]] = moore.variable : <l1>
+// CHECK: [[B:%.+]] = moore.variable : <l1>
+// CHECK: [[Q:%.+]] = moore.variable : <l1>
+// CHECK: [[RD_A:%.+]] = moore.read [[A]] : <l1>
+// CHECK: [[RD_B:%.+]] = moore.read [[B]] : <l1>
+// CHECK: [[AND:%.+]] = moore.and [[RD_A]], [[RD_B]] : l1
+// CHECK: [[NOT_AND:%.+]] = moore.not [[AND]] : l1
+// CHECK: moore.assign [[Q]], [[NOT_AND]] : l1
+
+module nand_prim;
+    logic A, B, Q;
+    nand a (Q, A, B);
+endmodule
+
+// CHECK-LABEL: moore.module @nor_prim()
+// CHECK: [[A:%.+]] = moore.variable : <l1>
+// CHECK: [[B:%.+]] = moore.variable : <l1>
+// CHECK: [[Q:%.+]] = moore.variable : <l1>
+// CHECK: [[RD_A:%.+]] = moore.read [[A]] : <l1>
+// CHECK: [[RD_B:%.+]] = moore.read [[B]] : <l1>
+// CHECK: [[OR:%.+]] = moore.or [[RD_A]], [[RD_B]] : l1
+// CHECK: [[NOT_OR:%.+]] = moore.not [[OR]] : l1
+// CHECK: moore.assign [[Q]], [[NOT_OR]] : l1
+
+module nor_prim;
+    logic A, B, Q;
+    nor a (Q, A, B);
+endmodule
+
+// CHECK-LABEL: moore.module @xnor_prim()
+// CHECK: [[A:%.+]] = moore.variable : <l1>
+// CHECK: [[B:%.+]] = moore.variable : <l1>
+// CHECK: [[Q:%.+]] = moore.variable : <l1>
+// CHECK: [[RD_A:%.+]] = moore.read [[A]] : <l1>
+// CHECK: [[RD_B:%.+]] = moore.read [[B]] : <l1>
+// CHECK: [[XOR:%.+]] = moore.xor [[RD_A]], [[RD_B]] : l1
+// CHECK: [[NOT_XOR:%.+]] = moore.not [[XOR]] : l1
+// CHECK: moore.assign [[Q]], [[NOT_XOR]] : l1
+
+module xnor_prim;
+    logic A, B, Q;
+    xnor c (Q, A, B);
+endmodule
+
+// CHECK-LABEL: moore.module @multi_nand_prim()
+// CHECK: [[A:%.+]] = moore.variable : <l1>
+// CHECK: [[B:%.+]] = moore.variable : <l1>
+// CHECK: [[C:%.+]] = moore.variable : <l1>
+// CHECK: [[D:%.+]] = moore.variable : <l1>
+// CHECK: [[Q:%.+]] = moore.variable : <l1>
+// CHECK: [[RD_A:%.+]] = moore.read [[A]] : <l1>
+// CHECK: [[RD_B:%.+]] = moore.read [[B]] : <l1>
+// CHECK: [[RD_C:%.+]] = moore.read [[C]] : <l1>
+// CHECK: [[RD_D:%.+]] = moore.read [[D]] : <l1>
+// CHECK: [[AND0:%.+]] = moore.and [[RD_A]], [[RD_B]] : l1
+// CHECK: [[AND1:%.+]] = moore.and [[AND0]], [[RD_C]] : l1
+// CHECK: [[AND2:%.+]] = moore.and [[AND1]], [[RD_D]] : l1
+// CHECK: [[NOT_AND:%.+]] = moore.not [[AND2]] : l1
+// CHECK: moore.assign [[Q]], [[NOT_AND]] : l1
+
+module multi_nand_prim;
+    logic A, B, C, D, Q;
+    nand a (Q, A, B, C, D);
+endmodule
