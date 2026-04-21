441c22d70a22ffea4a8d916e2fe79f9b6c6c2c53
Andrew Lenharth <andrew@lenharth.org>
2026-04-21 12:18:05 -0500

    [NFC] Missing requires line in circt-verilog test (#10290)

--- test/circt-verilog/xslang.sv
+++ test/circt-verilog/xslang.sv
@@ -1,4 +1,4 @@
 // RUN: not circt-verilog %s -Xslang=--std=foo 2>&1 | FileCheck %s
 // RUN: circt-verilog %s -Xslang=--std=latest
-
+// REQUIRES: slang
 // CHECK: invalid value for --std option: 'foo'
