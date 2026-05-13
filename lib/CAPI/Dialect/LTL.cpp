1ccd50c12fc47c78550ec61c4ad5a60c8b767e66
Clo91eaf <Clo91eaf@qq.com>
2026-05-13 16:05:20 +0800

    [ltl] Add LTL dialect CAPI implementation and unit tests

--- lib/CAPI/Dialect/LTL.cpp
+++ lib/CAPI/Dialect/LTL.cpp
@@ -8,7 +8,58 @@
 
 #include "circt-c/Dialect/LTL.h"
 #include "circt/Dialect/LTL/LTLDialect.h"
+#include "circt/Dialect/LTL/LTLTypes.h"
 
+#include "mlir/CAPI/IR.h"
 #include "mlir/CAPI/Registration.h"
 
+using namespace circt;
+using namespace circt::ltl;
+using namespace mlir;
+
+//===----------------------------------------------------------------------===//
+// Dialect API.
+//===----------------------------------------------------------------------===//
+
 MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(LTL, ltl, circt::ltl::LTLDialect)
+
+//===----------------------------------------------------------------------===//
+// Type API.
+//===----------------------------------------------------------------------===//
+
+bool ltlTypeIsASequence(MlirType type) {
+  return isa<SequenceType>(unwrap(type));
+}
+
+MlirType ltlSequenceTypeGet(MlirContext ctx) {
+  return wrap(SequenceType::get(unwrap(ctx)));
+}
+
+bool ltlTypeIsAProperty(MlirType type) {
+  return isa<PropertyType>(unwrap(type));
+}
+
+MlirType ltlPropertyTypeGet(MlirContext ctx) {
+  return wrap(PropertyType::get(unwrap(ctx)));
+}
+
+//===----------------------------------------------------------------------===//
+// Attribute API.
+//===----------------------------------------------------------------------===//
+
+static_assert(LTL_CLOCK_EDGE_POS == static_cast<unsigned>(ClockEdge::Pos));
+static_assert(LTL_CLOCK_EDGE_NEG == static_cast<unsigned>(ClockEdge::Neg));
+static_assert(LTL_CLOCK_EDGE_BOTH == static_cast<unsigned>(ClockEdge::Both));
+
+bool ltlAttrIsAClockEdgeAttr(MlirAttribute attr) {
+  return isa<ClockEdgeAttr>(unwrap(attr));
+}
+
+MlirAttribute ltlClockEdgeAttrGet(MlirContext ctx, LTLClockEdge edge) {
+  return wrap(ClockEdgeAttr::get(unwrap(ctx), static_cast<ClockEdge>(edge)));
+}
+
+LTLClockEdge ltlClockEdgeAttrGetValue(MlirAttribute attr) {
+  auto edge = cast<ClockEdgeAttr>(unwrap(attr)).getValue();
+  return static_cast<LTLClockEdge>(static_cast<unsigned>(edge));
+}
