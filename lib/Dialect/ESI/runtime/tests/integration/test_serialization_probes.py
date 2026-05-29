9e624b5b822a5fdd2344f4a92374d8c0b58da7dc
John Demme <jodemme@microsoft.com>
2026-05-29 21:18:08 +0000

    formatting

--- lib/Dialect/ESI/runtime/tests/integration/test_serialization_probes.py
+++ lib/Dialect/ESI/runtime/tests/integration/test_serialization_probes.py
@@ -1,7 +1,6 @@
 #  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 #  See https://llvm.org/LICENSE.txt for license information.
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
 """Pytest harness for the SerializationProbes integration test.
 
 This builds the C++ driver under ``sw/serialization_probes.cpp`` against
