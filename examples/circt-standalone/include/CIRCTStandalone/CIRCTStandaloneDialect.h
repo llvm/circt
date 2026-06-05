1a185179fe814fdf6298ee35f41094661f591af7
Hideto Ueno <uenoku.tokotoko@gmail.com>
2026-06-05 15:59:58 -0700

    Add example

--- examples/circt-standalone/include/CIRCTStandalone/CIRCTStandaloneDialect.h
+++ examples/circt-standalone/include/CIRCTStandalone/CIRCTStandaloneDialect.h
@@ -1,4 +1,4 @@
-//===- CIRCTStandaloneDialect.h - CIRCT standalone dialect ------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
