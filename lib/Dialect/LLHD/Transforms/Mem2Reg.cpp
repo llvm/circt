c8b0fd21d12d49853806ebdce0120edb7f3f2eaf
Fabian Schuiki <fabian@schuiki.ch>
2026-06-15 09:15:37 -0700

    Update lib/Dialect/LLHD/Transforms/Mem2Reg.cpp

--- lib/Dialect/LLHD/Transforms/Mem2Reg.cpp
+++ lib/Dialect/LLHD/Transforms/Mem2Reg.cpp
@@ -18,7 +18,6 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/GenericIteratedDominanceFrontier.h"
-#include <optional>
 
 #define DEBUG_TYPE "llhd-mem2reg"
 
