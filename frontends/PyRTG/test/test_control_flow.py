a23973d76e10129469cc9b96b51607361e42bafe
Martin Erhart <martin.erhart@sifive.com>
2026-07-23 08:18:27 +0200

    Mark control flow test unsupported for now

--- frontends/PyRTG/test/test_control_flow.py
+++ frontends/PyRTG/test/test_control_flow.py
@@ -1,4 +1,4 @@
-# XFAIL: *
+# UNSUPPORTED: *
 # RUN: %rtgtool% %s --seed=0 --output-format=mlir | FileCheck %s
 
 from pyrtg import test, sequence, Integer, IntegerType, Immediate, Array, If, Else, EndIf, For, Foreach, config, Config, Param
