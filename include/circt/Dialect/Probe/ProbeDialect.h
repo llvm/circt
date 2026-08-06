25f5b23131ec7ec6bd00d15da9cadd1e96b056db
woshiren <yejiacong@outlook.com>
2026-08-06 20:29:20 +0800

    remove filename and emacs tag in file header

--- include/circt/Dialect/Probe/ProbeDialect.h
+++ include/circt/Dialect/Probe/ProbeDialect.h
@@ -1,4 +1,4 @@
-//===- ProbeDialect.h - Probe dialect definition ----------------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
