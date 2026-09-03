5eaa0199b5175150f8eb605cc11fdf3bf448197f
5iri <78346797+5iri@users.noreply.github.com>
2026-09-03 13:53:24 +0530

    [BMC] Run PrepareForBMC in circt-bmc (#10983)

---
+++ integration_test/circt-bmc/system-verilog.sv
@@ -0,0 +1,20 @@
+// REQUIRES: slang
+// REQUIRES: libz3
+// REQUIRES: circt-bmc-jit
+// UNSUPPORTED: valgrind
+// RUN: circt-verilog %s --top=Top | circt-bmc - --module Top -b 1 --shared-libs=%libz3 --print-only-first-counterexample | FileCheck %s
+
+// CHECK: counterexample for Top:
+// CHECK: cycle 0:
+// CHECK: Assertion can be violated!
+
+module Top(
+    input logic clk,
+    input logic data,
+    output logic sampled
+);
+  always_ff @(posedge clk)
+    sampled <= data;
+
+  assert property (@(posedge clk) data);
+endmodule
