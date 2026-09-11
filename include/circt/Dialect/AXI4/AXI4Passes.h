6f5c56a5dca7c617cc890afcf990bfd6bcbf516b
Bea Healy <57840981+TaoBi22@users.noreply.github.com>
2026-09-11 10:19:24 +0200

    Update include/circt/Dialect/AXI4/AXI4Passes.h
    Co-authored-by: Fabian Schuiki <fabian@schuiki.ch>

--- include/circt/Dialect/AXI4/AXI4Passes.h
+++ include/circt/Dialect/AXI4/AXI4Passes.h
@@ -1,4 +1,4 @@
-//===- AXI4Passes.h - AXI4 pass entry points --------------------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
