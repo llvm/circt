39a012b1c368b50042497f557bc3c4f9e0fdc730
AlexanderPortland <alexanderportland1@gmail.com>
2026-09-23 15:29:52 -0700

    address hidetos minor feedback

--- lib/Dialect/FIRRTL/Transforms/LowerSizeOf.cpp
+++ lib/Dialect/FIRRTL/Transforms/LowerSizeOf.cpp
@@ -1,4 +1,4 @@
-//===- LowerSizeOf.cpp - Elaborate sizeof intrinsics -------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
