4ad698002b9b2a7975305efc5189f8e95fb191b3
Amelia Dobis <amelia.dobis@princeton.edu>
2026-09-25 17:43:07 +0200

    the final format

--- lib/Conversion/ExportVerilog/ExportVerilog.cpp
+++ lib/Conversion/ExportVerilog/ExportVerilog.cpp
@@ -2326,7 +2326,8 @@ private:
 
   /// Emit braced list of values surrounded by `{` and `}`.
   void emitBracedList(ValueRange ops) {
-    return emitBracedList(ops, [&]() { ps << "{"; }, [&]() { ps << "}"; });
+    return emitBracedList(
+        ops, [&]() { ps << "{"; }, [&]() { ps << "}"; });
   }
 
   /// Print an APInt constant.
